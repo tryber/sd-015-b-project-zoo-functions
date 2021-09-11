18f139545acbdc18654e679e236071670e2d9c3d
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-09-11 14:52:36 -0300

    Requisito 3 feito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,8 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const employee = employeeName ? data.employees.find(({firstName, lastName}) => [firstName, lastName].includes(employeeName)) : {};
+  return employee;
 }
 
 module.exports = getEmployeeByName;
