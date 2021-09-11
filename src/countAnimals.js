56913936cfc065484119f82798215c6378cad46b
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-09-11 18:32:23 -0300

    Requisito 5 completo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
+
+const allAnimalsCount = () => {
+  const species = data.species;
+  const count = species.reduce((acc, {name, residents}) => {
+    acc[name] = residents.length
+    return acc;
+  }, {});
+  return count
+}
+
+const selectedAnimalCount = (specie, gender) => {
+  const actualSpecie = data.species.find(({name}) => name === specie);
+  const count = gender ? actualSpecie.residents.filter(specie => specie.sex === gender).length : actualSpecie.residents.length;
+  return count;
+}
 function countAnimals(animal) {
   // seu código aqui
+  const count = animal ? selectedAnimalCount(animal.specie, animal.gender)  : allAnimalsCount();
+  return count;
 }
 
 module.exports = countAnimals;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 function getSpeciesByIds(...ids) {
   // seu código aqui
   const species = data.species.filter(({id}) => ids.includes(id));
-  return species
+  return species;
 }
 
 module.exports = getSpeciesByIds;
