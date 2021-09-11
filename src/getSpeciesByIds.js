96803ff5f35a0858a3c7217d96de6e2cd579af93
GabrielJorge <gabrieljorge.ca@gmail.com>
2021-09-11 15:50:51 -0400

    add function getSpeciesByIds.js

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return data.species.filter((animal) => ids.includes(animal.id));
 }
 
 module.exports = getSpeciesByIds;
