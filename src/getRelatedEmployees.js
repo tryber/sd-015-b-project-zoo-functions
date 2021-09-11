4c33f618a8d26b6a7d990c06680c64012682c98e
Denis Jonathan <denisjonathan7@gmail.com>
2021-09-11 01:37:37 -0300

    :heavy_check_mark: Requisito 4: concluído

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const managers = employees.filter((employee) => employee.managers.includes(managerId));
+
+    return managers.map((name) => `${name.firstName} ${name.lastName}`);
+  }
+
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
