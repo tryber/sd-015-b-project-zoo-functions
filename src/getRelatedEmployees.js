ab33d13f9eac5c66da4c9f57f0eb6391da7e29ba
David Souza <dabcs@poli.ufrj.br>
2021-09-11 02:28:12 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers
+    .some((serchId) => serchId === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return employees.filter((element) => element
+      .managers.some((id) => id === managerId))
+      .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
