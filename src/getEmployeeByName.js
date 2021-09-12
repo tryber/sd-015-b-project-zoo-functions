a23ed6e2a0ef3636cc53fb645551fefa85e9c011
GabrielJorge <gabrieljorge.ca@gmail.com>
2021-09-11 22:00:37 -0400

    add function getEmployeeByName.js

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,18 @@
-const data = require('../data/zoo_data');
+const { employees } = require("../data/zoo_data");
+const data = require("../data/zoo_data");
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return employees.find(
+    (employee) =>
+      employee.firstName === employeeName || employee.lastName === employeeName
+  );
+
+  
 }
+
+console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
