21a2ab44c92682977bd71e8b884527fad36e30a1
Denis Jonathan <denisjonathan7@gmail.com>
2021-09-12 03:57:53 -0300

    :heavy_check_mark: Requisito 9: concluído

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,27 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
+
+function findOldest(animals) {
+  const oldest = animals.reduce((accumulator, current) => {
+    let older = accumulator;
+    const { age } = current;
+
+    if (age > older) {
+      older = age;
+    }
+    return older;
+  }, 0);
+
+  return oldest;
+}
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = employees.find((name) => name.id === id);
+  const firstSpecie = employee.responsibleFor[0];
+  const groupOfSpecie = species.find((specie) => specie.id === firstSpecie);
+  const { residents } = groupOfSpecie;
+  const oldest = residents.find((resident) => resident.age === findOldest(residents));
+
+  return [oldest.name, oldest.sex, oldest.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
