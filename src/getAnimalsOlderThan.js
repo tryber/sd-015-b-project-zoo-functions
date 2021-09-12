ded9347f488623ba37e2958d76cb8b75fa594c8a
Amanda Fernandes <aferanda@gmail.com>
2021-09-12 18:45:15 -0300

    Requisito 1 e 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const findSpecie = species.find((specie) => specie.name === animal);
+  const verifyAge = findSpecie.residents.every((resident) => resident.age >= age);
+  return verifyAge;
 }
 
 module.exports = getAnimalsOlderThan;
