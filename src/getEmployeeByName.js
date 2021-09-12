436a9b1f97850219531675fe76d8e34637c0e7eb
GabrielJorge <gabrieljorge.ca@gmail.com>
2021-09-12 09:49:30 -0400

    add function getRelatedEmployess.js and fix lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,18 +1,13 @@
-const { employees } = require("../data/zoo_data");
-const data = require("../data/zoo_data");
+const { employees } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   if (!employeeName) {
     return {};
   }
   return employees.find(
-    (employee) =>
-      employee.firstName === employeeName || employee.lastName === employeeName
+    (employee) => employee.firstName === employeeName || employee.lastName === employeeName,
   );
-
-  
 }
-
-console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
