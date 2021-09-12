e66c6a96c7edf062658cab8f0aed1ad5d23c90f9
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-09-12 18:51:41 -0300

    Requisito 8 feito

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,40 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
+
+const getsDaysSchedule = (acc, day) => {
+  acc[day] = acc[day] || {};
+  const daySchedule = data.hours[day];
+  const {open, close} = daySchedule;
+  if (open && close){
+    acc[day].officeHour = `Open from ${open}am until ${close}pm`;
+    acc[day].exhibition = data.species
+      .filter(({ availability }) => availability.includes(day))
+      .map(({ name }) => name);
+  } else {
+    acc[day].officeHour = 'CLOSED';
+    acc[day].exhibition = 'The zoo will be closed!';
+  }
+  return acc;
+};
+
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  const days = Object.keys(data.hours);
+  const singleDay = days.includes(scheduleTarget);
+  const isAnimal = data.species.some(({ name }) => name === scheduleTarget);
+  const validParameter = scheduleTarget && !singleDay && !isAnimal;
+  let schedule;
+
+  if (singleDay || !scheduleTarget || validParameter) {
+    const dates = singleDay ? [scheduleTarget] : days;
+    schedule = dates.reduce(getsDaysSchedule, {}); 
+  } else if (isAnimal) {
+    schedule = data.species.find(({name}) => name === scheduleTarget).availability;
+  }
+  
+  return schedule;
 }
+
+console.log(getSchedule('lions'));
 
 module.exports = getSchedule;
