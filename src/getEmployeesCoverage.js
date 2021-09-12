298d7547f813ec33f69cff643e33f37c43ae556a
Denis Jonathan <denisjonathan7@gmail.com>
2021-09-12 05:42:43 -0300

    :heavy_check_mark: Requisito 10: concluído / Projeto finalizado

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,63 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function findLocationOfAnimal(animals) {
+  const locations = animals.map((animal) => species
+    .find((specie) => specie.name === animal).location);
+
+  return locations;
+}
+
+function convertAnimalIdInName(ids) {
+  const names = ids.map((id) => species
+    .find((specie) => specie.id === id).name);
+
+  return names;
+}
+
+function idOrName(identifier) {
+  let employee;
+
+  if (identifier.id) {
+    employee = employees.find((people) => people.id === identifier.id);
+  } else {
+    employee = employees.find((people) =>
+      people.firstName === identifier.name || people.lastName === identifier.name);
+  }
+
+  return employee;
+}
+
+function findByEmployeeIdOrName(identifierType) {
+  const employee = idOrName(identifierType);
+  if (employee === undefined) throw new Error('Informações inválidas');
+  const { firstName, lastName, responsibleFor, id } = employee;
+  const speciesName = convertAnimalIdInName(responsibleFor);
+  const fullName = `${firstName} ${lastName}`;
+  const locations = findLocationOfAnimal(speciesName);
+  const output = {
+    id,
+    fullName,
+    species: speciesName,
+    locations,
+  };
+  return output;
+}
+
+function coverageOfAllEmployees() {
+  const employeesIds = employees.map((employee) => employee.id);
+  const output = [];
+
+  employeesIds.forEach((employee) => {
+    output.push(findByEmployeeIdOrName({ id: employee }));
+  });
+
+  return output;
+}
+
+function getEmployeesCoverage(identifier) {
+  if (!identifier) return coverageOfAllEmployees();
+
+  return findByEmployeeIdOrName(identifier);
 }
 
 module.exports = getEmployeesCoverage;
