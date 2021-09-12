fdbbd85922ccf6af1628686fbb930887ad7a61f8
Denis Jonathan <denisjonathan7@gmail.com>
2021-09-12 03:01:10 -0300

    :package: Requisito 8: função noArgs criada
    Função responsável por retornar uma msg padrão caso não tenha parâmetros

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,30 @@
-const data = require('../data/zoo_data');
+const { hours, species } = require('../data/zoo_data');
+
+const scheduleEntries = Object.entries(hours);
+// console.log(scheduleEntries);
+
+function noArgs() {
+  const output = {};
+  scheduleEntries.forEach((day, index) => {
+    const { open, close } = day[1];
+    const tempObj = {};
+    if (open === 0) {
+      tempObj[day[0]] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+    } else {
+      const filteredByDay = species.filter((available) => available.availability.includes(day[0]));
+      const namesByDay = filteredByDay.map((specie) => specie.name);
+      tempObj[day[0]] = {
+        officeHour: `Open from ${open}am until ${close}pm`,
+        exhibition: namesByDay,
+      };
+    }
+    Object.assign(output, tempObj);
+  });
+  return output;
+}
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  return noArgs();
 }
 
 module.exports = getSchedule;
