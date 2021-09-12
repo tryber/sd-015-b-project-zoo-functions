2bcaa89e929a99da6291dc12cb068b62936d600e
Denis Jonathan <denisjonathan7@gmail.com>
2021-09-12 00:42:29 -0300

    :package: Requisito 7: função includeName criada
    Função responsavel por retornar espécies e seus nomes

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,21 +1,45 @@
 const { species } = require('../data/zoo_data');
 
-const objectOfRegions = { NE: [], NW: [], SE: [], SW: [] };
-const regions = Object.keys(objectOfRegions);
-
 function filterByRegions(region) {
   return species.filter((specie) => specie.location === region);
 }
 
 function outputDeafult() {
+  const objectOfRegions = { NE: [], NW: [], SE: [], SW: [] };
+  const regions = Object.keys(objectOfRegions);
+
   regions.forEach((region) => Object.assign(objectOfRegions[region],
     filterByRegions(region).map((resident) => resident.name)));
 
   return objectOfRegions;
 }
 
+function includeName() {
+  const objectOfRegions = { NE: [], NW: [], SE: [], SW: [] };
+  const regions = Object.keys(objectOfRegions);
+
+  regions.forEach((region) => {
+    filterByRegions(region).forEach((specie) => {
+      let tempObj = {};
+      const { name, residents } = specie;
+      tempObj = { [name]: [] };
+      residents.forEach((resident) => {
+        const residentName = resident.name;
+        tempObj[name].push(residentName);
+      });
+      objectOfRegions[region].push(tempObj);
+    });
+  });
+
+  return objectOfRegions;
+}
+
 function getAnimalMap(options) {
   if (!options) return outputDeafult();
+
+  const { includeNames = false } = options;
+
+  if (includeNames) return includeName();
 }
 
 module.exports = getAnimalMap;
