097042900752a3957b30e0cc3ee7eabea3de8333
Denis Jonathan <denisjonathan7@gmail.com>
2021-09-11 23:04:21 -0300

    :package: Requisito 7: função outputDeafult criada
    Função responsavel por retornar espécies, caso não haja parametros

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,21 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+const objectOfRegions = { NE: [], NW: [], SE: [], SW: [] };
+const regions = Object.keys(objectOfRegions);
+
+function filterByRegions(region) {
+  return species.filter((specie) => specie.location === region);
+}
+
+function outputDeafult() {
+  regions.forEach((region) => Object.assign(objectOfRegions[region],
+    filterByRegions(region).map((resident) => resident.name)));
+
+  return objectOfRegions;
+}
 
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options) return outputDeafult();
 }
 
 module.exports = getAnimalMap;
