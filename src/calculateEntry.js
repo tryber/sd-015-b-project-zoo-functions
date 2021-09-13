a00717965fff5247587c2b75f1761999db979e98
Caio R. Vieira <caio.robs98@gmail.com>
2021-09-13 19:16:43 -0300

    requisito 6 ok

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,30 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const entryes = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  entrants.forEach((entry) => {
+    if (entry.age < 18) entryes.child += 1;
+    if (entry.age >= 50) entryes.senior += 1;
+    if (entry.age >= 18 && entry.age < 50) entryes.adult += 1;
+  });
+  return entryes;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const entryes = countEntrants(entrants);
+  let total = 0;
+  Object.keys(entryes).forEach((entry) => {
+    total += entryes[entry] * data.prices[entry];
+  });
+  return total;
 }
 
-module.exports = { calculateEntry, countEntrants };
+module.exports = {
+  calculateEntry,
+  countEntrants,
+};
