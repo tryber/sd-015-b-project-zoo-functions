408a62d16fb75ff80ff961ef35178a8701f8c251
Mariah M. Rodrigues <mariah.manetti@gmail.com>
2021-09-13 14:14:53 -0300

    Inicio do Projeto

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -5,3 +5,8 @@ function getSpeciesByIds(ids) {
 }
 
 module.exports = getSpeciesByIds;
+
+// - Esta função é responsável pela busca das espécies de animais por id. Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids.
+// - Caso receba nenhum parâmetro, necessário retornar um array vazio;
+// - Ao receber como parâmetro um único id, retorna um array com a espécie referente à esse id;
+// - Ao receber mais de um id, retorna um array com as espécies referentes aos ids.
