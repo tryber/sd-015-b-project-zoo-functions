2e9b9671685aa81bb3cdc34ca8aa993a3d567abf
Guilherme Saboia <gsaboia@yahoo.com>
2021-09-13 15:15:42 -0300

    Refatora requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,8 +1,9 @@
-const { species, employees } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  return employees.filter((employee) => (employee.firstName === employeeName || employee.lastName === employeeName))[0] || {};
+  return employees.filter((employee) => (
+    employee.firstName === employeeName || employee.lastName === employeeName))[0] || {};
 }
 
 module.exports = getEmployeeByName;
