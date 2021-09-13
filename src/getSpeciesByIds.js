cd2ed8dc11179ab00369d0a7922c172a89265bb1
Gustavo <fariaguga@hotmail.com>
2021-09-13 15:08:27 -0300

    Adiciona Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const speciesId = species.filter(({ id }) => ids.includes(id));
+  return speciesId;
 }
+console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
