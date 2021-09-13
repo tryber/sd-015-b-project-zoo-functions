e6b3416b10bafaceda59b55f736b0175661e1661
Mariah M. Rodrigues <mariah.manetti@gmail.com>
2021-09-13 16:30:16 -0300

    Requisito 1 - Crie uma função que busca as espécies de animais por id

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,12 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ids.map((id) => species.find((specie) => specie.id === id));
 }
 
 module.exports = getSpeciesByIds;
 
-// - Esta função é responsável pela busca das espécies de animais por id. Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids.
+// Esta função é responsável pela busca das espécies de animais por id. Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids:
 // - Caso receba nenhum parâmetro, necessário retornar um array vazio;
 // - Ao receber como parâmetro um único id, retorna um array com a espécie referente à esse id;
 // - Ao receber mais de um id, retorna um array com as espécies referentes aos ids.
