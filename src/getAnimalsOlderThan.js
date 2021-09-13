0a9b33738a9b8f1045a99818f6a9671db591f23c
leandromliveira <leandrooliveira35@gmail.com>
2021-09-13 09:20:46 -0300

    requisito 2 completo

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const selected = data.species.find((specie) => specie.name === animal);
+  return selected.residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
