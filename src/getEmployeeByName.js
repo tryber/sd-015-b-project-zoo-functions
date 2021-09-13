742fd196928393689d1b832e69bf72091797700c
Vinicius Fernandes <viniciusfernandes126@gmail.com>
2021-09-13 17:11:52 -0300

    [Requisito 3] Implementa função getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) return {};
+
+  const employee = data.employees.find((name) => (name
+    .firstName === employeeName || name.lastName === employeeName));
+  return employee;
 }
 
 module.exports = getEmployeeByName;
