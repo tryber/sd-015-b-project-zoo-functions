7fb826d80a954e9d2e2151181624b4b2b92605b4
Valdênio <valdeniomartinho@gmail.com>
2021-09-13 16:29:58 -0300

    corrige erros de lint requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,8 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
-  return ids.map((id) => data.species.find((specie) => specie.id === id));   
-  }
+  return ids.map((id) => data.species.find((specie) => specie.id === id));
+}
 
 module.exports = getSpeciesByIds;
