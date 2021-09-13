0c2eb4ba1dedbc5cae622542439500cff27e54f1
Danilo <dcalado.fisio@yahoo.com.br>
2021-09-13 16:37:57 -0300

    feito requesito 5

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -6,11 +6,11 @@ function isManager(id) {
 
 function getRelatedEmployees(managerId) {
   if (isManager(managerId)) {
-    const obj = data.employees.filter((employee) => {
+    const objeto = data.employees.filter((employee) => {
       const employees = employee.managers.find((manager) => manager === managerId);
       return employees;
     });
-    const string = obj.map((obj) => `${obj.firstName} ${obj.lastName}`);
+    const string = objeto.map((obj) => `${obj.firstName} ${obj.lastName}`);
     return string;
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
