7c3c7367f6c404d4288809ea3a7bbcac9925754c
Andre Luiz <andreluizgyn1@gmail.com>
2021-09-13 17:42:42 -0300

    Adciona requisitos 2 e 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,21 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+
+  const employeeFirstName = employees.find((employee) => employee.firstName === employeeName);
+  const employeeLastName = employees.find((employee) => employee.lastName === employeeName);
+
+  if (employeeFirstName !== undefined) {
+    return employeeFirstName;
+  }
+
+  if (employeeLastName !== undefined) {
+    return employeeLastName;
+  }
 }
 
 module.exports = getEmployeeByName;
