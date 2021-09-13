dcf5fdb1c6e1a0edf66fe51c29e04f8214def04d
Mariah M. Rodrigues <mariah.manetti@gmail.com>
2021-09-13 18:20:33 -0300

    Requisito 3 - Implemente a função getRelatedEmployees

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,17 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return employees.find((name) =>
+    name.firstName === employeeName || name.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
+
+// Esta função é responsável pela busca das pessoas colaboradoras através do primeiro ou do último nome delas:
+// - Sem parâmetros, retorna um objeto vazio
+// - Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
+// - Quando provido o último nome do funcionário, retorna o objeto do funcionário
