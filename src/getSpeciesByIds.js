6d66e301d831f7dd7c507126de76a3010087b69c
Gabrielle-Murat <belle.murat@gmail.com>
2021-09-13 17:29:59 -0300

    Adiciona o requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function parametersUndefined() {
+  return [];
+}
+
+function secondParameterUndefined(id1) {
+  return data.species.filter((diversity) => diversity.id === id1);
+  // return kind.map((speciesName) => speciesName.name);
+}
+
+function parametersDefined(id1, id2) {
+  return data.species.filter((diversity) => diversity.id === id1 || diversity.id === id2);
+  // return kind.map((speciesName) => speciesName.name);
+}
+
+function getSpeciesByIds(id1, id2) {
+  if (id1 === undefined && id2 === undefined) {
+    parametersUndefined();
+  }
+  if (id1 !== undefined && id2 === undefined) {
+    secondParameterUndefined(id1);
+  }
+  return parametersDefined(id1, id2);
 }
 
 module.exports = getSpeciesByIds;
