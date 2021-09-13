95e3a59f24b9a11d7ca6f89e0cdc09a7f6c60f78
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-09-13 15:21:55 -0300

    Iniciando o projeto. Realiza requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const returnVariable = [];
+  data.species.forEach((element) => {
+    ids.forEach((id) => {
+      if (element.id === id) {
+        returnVariable.push(element);
+      }
+    });
+  });
+  return returnVariable;
 }
 
 module.exports = getSpeciesByIds;
