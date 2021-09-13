d10c547fd6fef0ef29e80ea5255a1b244749006a
Amanda Fernandes <aferanda@gmail.com>
2021-09-13 18:47:29 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species
+      .reduce((acc, specie) => Object.assign(acc, {
+        [specie.name]: specie.residents.length,
+      }), {});
+  }
+  if (animal.gender) {
+    return species
+      .find((specie) => specie.name === (animal.specie)).residents
+      .filter((resident) => resident.sex === (animal.gender)).length;
+  }
+  return species
+    .find((specie) => specie.name === (animal.specie)).residents.length;
 }
+
+console.log(countAnimals({ specie: 'elephants', gender: 'male' }));
 
 module.exports = countAnimals;
