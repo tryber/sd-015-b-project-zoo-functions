2d26d0c801a5d804fb5a77ff8db43521d0fd0f30
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-09-13 17:37:43 -0300

    terceiro requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,6 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(...employeeName) {
-
+// se o parametro for algum valor
+function getEmployeeByName(employeeName) {
+  if (!employeeName) {
+    return {};
+  }
+  return data.employees.find((employee) => employeeName.includes(employee.lastName)
+  || employeeName.includes(employee.firstName));
 }
 module.exports = getEmployeeByName;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
+// filtra em cada especie se o parametro inclui algum ID de alguma delas;
 function getSpeciesByIds(...ids) {
   return data.species.filter((specie) => ids.includes(specie.id));
 }
-getSpeciesByIds('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5', '78460a91-f4da-4dea-a469-86fd2b8ccc84');
 module.exports = getSpeciesByIds;
