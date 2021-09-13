9651873888b0b38aa8ed5e2d1eccada29d570df1
alvesgf16 <alvesgabriel1994@gmail.com>
2021-09-13 17:02:59 -0300

    Implementa a funçao countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,27 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAllAnimals(speciesArray) {
+  const result = {};
+
+  speciesArray.forEach((oneSpecies) => {
+    result[oneSpecies.name] = oneSpecies.residents.length;
+  });
+
+  return result;
 }
+
+function countSpecificAnimals(speciesArray, animal) {
+  return animal.gender === undefined
+    ? speciesArray.find((oneSpecies) => oneSpecies.name === animal.specie).residents.length
+    : speciesArray.find((oneSpecies) => oneSpecies.name === animal.specie).residents
+      .filter((resident) => resident.sex === animal.gender).length;
+}
+
+function countAnimals(animal) {
+  return animal === undefined ? countAllAnimals(species) : countSpecificAnimals(species, animal);
+}
+
+console.log(countAnimals({ specie: 'penguins' }));
 
 module.exports = countAnimals;
