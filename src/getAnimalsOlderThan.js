db427f8f608e18a860c93cb1cd507df796b7e5bd
Danilo <dcalado.fisio@yahoo.com.br>
2021-09-13 15:22:22 -0300

    feito requesito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const sameName = data.species.some((specie) => specie.name === animal);
+  const catchAnimal = data.species.find((specie) => specie.name === animal);
+  const older = catchAnimal.residents.every((value) => value.age > age);
+
+  return (sameName && older);
 }
 
 module.exports = getAnimalsOlderThan;
