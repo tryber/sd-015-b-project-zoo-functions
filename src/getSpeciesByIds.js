a86805e95012c225d78e1dbfc08f5cdfff8cab46
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-09-13 14:27:17 -0300

    Iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const species = [];
+
+  if (!ids) return species;
+
+  return species;
 }
 
 module.exports = getSpeciesByIds;
