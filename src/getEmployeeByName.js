d448661fb4ce70dc6931912159aef30e789c0a44
Bruno Bartolomeu <bruno.bartolomeu5@hotmail.com>
2021-09-13 16:59:13 -0300

    Envia requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,10 +1,10 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  if (!employeeName) return [];
+  if (!employeeName) return {};
+  return employees.find(({ firstName, lastName }) => firstName === employeeName
+  || lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
-
-
-console.log(getEmployeeByName())
