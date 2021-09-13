f253025c111748ec53c8690657ecd4e3e12d4ebc
aaguero96 <andre.luiz_1996@hotmail.com>
2021-09-13 13:21:14 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return data.species.map((element) => {
+    const conf = ids.some((id) => element.id === id);
+    return conf ? element : false; 
+  }).filter((element) => element !== false);
 }
 
 module.exports = getSpeciesByIds;
