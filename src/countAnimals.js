7b6f4b7976753c35985e822aff1ca5df6535f013
leandromliveira <leandrooliveira35@gmail.com>
2021-09-13 09:58:51 -0300

    requisito 4 completo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(species) {
+  const totalAnimals = {};
+  if (!species) {
+    data.species.forEach((specie) => {
+      const animal = specie.name;
+      const quantity = specie.residents.length;
+      totalAnimals[animal] = quantity;
+    });
+    return totalAnimals;
+  }
+  const selected = data.species.find((specie) => specie.name === species);
+  return selected.residents.length;
 }
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const allManagers = [];
+  data.employees.forEach((employee) => allManagers.push(employee.managers.toString()));
+  const result = allManagers.toString().includes(id);
+  return result;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const manager = isManager(managerId);
+  if (!manager) throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  const juniors = [];
+  data.employees.forEach((employee) => {
+    if (employee.managers.includes(managerId)) {
+      juniors.push(`${employee.firstName} ${employee.lastName}`);
+    }
+  });
+  return juniors;
 }
 
 module.exports = { isManager, getRelatedEmployees };
