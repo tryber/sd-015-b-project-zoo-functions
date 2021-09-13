fa7865279f6fb40e084a4418ff6595c5dc896a86
davi <davisoarescoura@gmail.com>
2021-09-13 20:53:19 +0200

    Requisito 3 do projeto concluido

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
-const data = require('../data/zoo_data');
+const { employees } = require("../data/zoo_data");
+const data = require("../data/zoo_data");
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const dataEmployeeFinded = data.employees
+    .find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
+  return dataEmployeeFinded == null ? {} : dataEmployeeFinded;
 }
 
 module.exports = getEmployeeByName;
