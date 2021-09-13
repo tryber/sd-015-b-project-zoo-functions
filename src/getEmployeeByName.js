0ec95e1027cbe5c0a5ea4e692ffe9c83f89600db
Valdênio <valdeniomartinho@gmail.com>
2021-09-13 18:37:28 -0300

    adiciona requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
+// const { employees } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
+function getEmployeeByName(employeeNa) {
   // seu código aqui
+  if (employeeNa === undefined) {
+    return {};
+  }
+  return data.employees
+    .find((employees) => employees.firstName === employeeNa || employees.lastName === employeeNa);
 }
 
 module.exports = getEmployeeByName;
