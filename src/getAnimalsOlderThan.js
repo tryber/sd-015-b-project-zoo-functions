1868fd8dc8423f52e8bb371c50ab68e9924b9e42
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2021-09-13 17:58:20 -0300

    comita o segundo  quesito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { name } = require('faker');
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const verify = data.species.find((Element) => Element.name === animal);
+  return verify.residents.every((Element) => Element.age >= age)
 }
 
 module.exports = getAnimalsOlderThan;
