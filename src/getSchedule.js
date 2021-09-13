b7fa8df4ba3984821d3990a2a654e61a6a0c300a
Camilla Del Guerso <camilladelguerso@gmail.com>
2021-09-13 18:45:42 -0300

    Adiciona requisito 4

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  // const objeto = {};
+  // const objeto2 = {};
+  // const dias = Object.keys(data.hours);
+  // dias.reduce((acc, day) => {
+  //   acc[day] = `Open from ${data.hours[day].open}am until ${data.hours[day].close - 12}pm`;
+  //   acc.Monday = 'CLOSED';
+  // return objeto1; }, {});
+  // if (!scheduleTarget){
+  //   for (let i = 0; i < dias.length; i += 1) {
+  //     if (dias[i] !== 'Monday') {
+  //       objeto[dias[i]] = () => {}
+  //       const especieAnimal = data.species.filter(({availability}) =>
+  //       availability.includes(dias[i])).map(({name}) => name);
+  //       console.log(especieAnimal);
+  //       console.log(objeto);
+  //     };
+  //   };
+  // };
 }
-
+getSchedule();
 module.exports = getSchedule;
