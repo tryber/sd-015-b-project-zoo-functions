35b5cff335fec659606736bafde57c3a80b77612
Ronald_D_Bezerra <rebulgame@gmail.com>
2021-09-13 20:10:03 -0300

    adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,23 @@
+/* eslint-disable array-callback-return */
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+// function getSpeciesByIds(...ids) {
+//   const resultado = ids.map((elemento) => species.filter((elemento2) => elemento2.id === elemento));
+//   return resultado;
+// }
+
+// function getSpeciesByIds(...ids) {
+//   let test = [];
+//   ids.forEach((idd) => {
+//     test.push(species.filter((elemento) => elemento.id === idd);
+//   });
+//   return test;
+// }
+
+function getSpeciesByIds(...ids) {
+  const resultado = species.filter((elemento) => ids.includes(elemento.id));
+  return resultado;
 }
 
 module.exports = getSpeciesByIds;
