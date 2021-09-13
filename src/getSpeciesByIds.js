58f90ab83e7ac2c0720b4883bccf805f3d5d7187
Rodolfo <rodolfoserralha@hotmail.com>
2021-09-13 14:25:55 -0300

    primeiro commit

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,6 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+
 
 module.exports = getSpeciesByIds;
