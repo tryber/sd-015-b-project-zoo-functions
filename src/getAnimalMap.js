5f94e076584eb96ff8eed5714fd8ce99b4243a50
aaguero96 <andre.luiz_1996@hotmail.com>
2021-09-13 18:13:37 -0300

    Finish part 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,70 @@
+const { Console } = require('console');
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function separeAnimalsByLocation() {
+  locationObj = {
+    NE: [],
+    NW: [],
+    SE: [],
+    SW: [],
+  };
+  data.species.forEach((element) => {
+    const { name, location } = element;
+    locationObj[location].push(name);
+  });
+  return locationObj;
 }
+
+function increaseAnimalNames(obj, species) {
+  const newObj = obj;
+  Object.keys(newObj).forEach((location) => {
+    newObj[location] = newObj[location].map((animal) => {
+      const newElement = {};
+      const filterByAnimal = species.find((element) => element.name === animal);
+      const arrayNames = filterByAnimal.residents.map((element) => element.name);
+      newElement[animal] = arrayNames;
+      return newElement;
+    });
+  });
+  return newObj;
+}
+
+function filterSpeciesBySex(arr, sex) {
+  return arr.map((element) => {
+    const newElement = element;
+    newElement.residents = element.residents.filter((resident) => resident.sex === sex);
+    return newElement;
+  });
+}
+
+function sortNames(obj) {
+  sortObject = obj;
+  Object.keys(obj).forEach((location) => {
+    sortObject[location].forEach((element, index) => {
+      const key = Object.keys(element)[0]
+      sortObject[location][index][key].sort();
+    }) 
+  });
+  return sortObject;
+}
+
+const getAnimalMap = (options) => {
+  const animalsByLocation = separeAnimalsByLocation();
+  if (options) {
+    const { includeNames, sex, sorted } = options;
+    const speciesArray = sex ? filterSpeciesBySex(data.species, sex) : data.species;
+    let returnObject;
+    if (includeNames) {
+      returnObject = increaseAnimalNames(animalsByLocation, speciesArray);
+      returnObject = sorted ? sortNames(returnObject) : returnObject;
+    } else {
+      returnObject = animalsByLocation;
+    }
+    return returnObject;
+  }
+  return animalsByLocation;
+}
+
+console.log(getAnimalMap({ includeNames: true }));
 
 module.exports = getAnimalMap;
