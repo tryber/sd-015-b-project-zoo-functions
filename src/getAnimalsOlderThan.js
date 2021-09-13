f351c6ebaadbb741c061f0977c5c83fb2c2d8a09
Marina Marques <marina.marqueso@outlook.com>
2021-09-13 17:28:39 -0300

    Trabalhando requisito 2, encontrando age

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,16 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  // coletar o animal pelo name
+  const findAnimal = species.find((elemento) => animal.includes(elemento.name))
+  // comparar idade do animal encontrado
+  const findAge = species.find((elemento) => elemento.residents.age < age)
+
+  return findAge
 }
 
+
 module.exports = getAnimalsOlderThan;
+
+console.log(getAnimalsOlderThan('otters', 4));
