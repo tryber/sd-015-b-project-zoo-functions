58e9ba6fbe7b190441102a733184cb2ec05912ee
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-09-13 17:23:53 -0300

    primeiro requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,6 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+function getEmployeeByName(...employeeName) {
 
+}
 module.exports = getEmployeeByName;
