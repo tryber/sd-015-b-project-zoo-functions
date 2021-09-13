aee563d3806322b8c7e36929ca52775ffe06a99f
Gabriel Peralta <prates_peralta@hotmail.com>
2021-09-13 14:38:38 -0300

    adiciona o primeiro exercicio

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,8 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  return species.
+function getSpeciesByIds(...ids) {
+  return species.filter((specie) => ids.includes(specie.id));
 }
 
 module.exports = getSpeciesByIds;
