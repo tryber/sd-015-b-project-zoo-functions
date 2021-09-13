790324db36fd21ad82a479493090622ec4ce2831
Rafael Toledo <rafael.toledo@engenharia.ufjf.br>
2021-09-13 15:32:29 -0300

    Requisitos 2 e 3 feitos

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { employees } = require('../data/zoo_data');
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName){
+    return {};
+  }
+  return employees.find((emp) => emp.firstName === employeeName || emp.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  const managers = employees.find((person) => person.id === 'stephanieId' || person.id === 'burlId' || person.id === 'olaId');
+  if(!managers) {
+    return false;
+  }
+ if(managers){
+    return true;
+  }
 }
 
 function getRelatedEmployees(managerId) {
