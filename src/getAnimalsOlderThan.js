72af9fc5aa8d194f7387224e2fb159aa105b809c
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2021-09-13 18:47:20 -0300

    Welckson Valentim - revisa requisito 1, apos aprovaçao local

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,7 +2,6 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  return species.find(((animals, index) => (animals.name === animal)
-  (test.residents.age[index] >= age)));
+  
 }
 module.exports = getAnimalsOlderThan;
