fcad02c51ffa90216f7bade76cf1b73d1fe180e8
samuel <samuelsoutosilva14@gmail.com>
2021-09-13 17:25:51 -0300

    reatora requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,13 +1,11 @@
-const { species } = require('../data/zoo_data');
-
 const data = require('../data/zoo_data');
+
+const { species } = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   if (ids === undefined) return [];
 
-  return species.filter(({ id }, position) => ids[position] === id);
+  return species.filter(({ id }) => ids.some((propriedade) => id === propriedade));
 }
 
 module.exports = getSpeciesByIds;
-
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
