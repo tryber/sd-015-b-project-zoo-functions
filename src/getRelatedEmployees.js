681930ee2f7549e4ab1501429bbccaccbb5a7061
Caio <caiomgp22@gmail.com>
2021-09-13 19:16:25 -0300

    Adicionando getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((person) => person.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const colab = employees.filter((person) => person.managers.includes(managerId))
+    .map((person) => `${person.firstName} ${person.lastName}`);
+  if (isManager(managerId) === true) return colab;
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
+getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992');
 module.exports = { isManager, getRelatedEmployees };
