e66ff0a3c22b036fabd74f0fd4d543030d237c07
davi <davisoarescoura@gmail.com>
2021-09-13 22:01:31 +0200

    Requisito 5 do projeto concluido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,10 +1,18 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
+  const child = entrants.filter((entrant) => entrant.age < 18);
+  const adult = entrants.filter((entrant) => (entrant.age >= 18 && entrant.age < 50));
+  const senior = entrants.filter((entrant) => entrant.age >= 50);
+  return { child: child.length, adult: adult.length, senior: senior.length };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  let total = 0;
+  if (!entrants || entrants.length === undefined) return total;
+  const inners = countEntrants(entrants);
+  total = inners.child * 20.99 + inners.adult * 49.99 + inners.senior * 24.99;
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
