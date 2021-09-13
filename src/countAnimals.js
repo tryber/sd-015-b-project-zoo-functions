53d9620ebbb32eb2002c10255e274548253318bd
Rafael Colombo <rcolombodev@gmail.com>
2021-09-13 19:09:04 -0300

    Resolve requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const emptyObject = species.reduce((result, current) => {
+  const currentSpecieName = current.name;
+  const currentSpecieLength = current.residents.length;
+  const tempObj = { [currentSpecieName]: currentSpecieLength };
+
+  return Object.assign(result, tempObj);
+}, {});
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return emptyObject;
+  }
+  return animal.gender ? species
+    .find((specie) => specie.name === (animal.specie))
+    .residents.filter((resident) => resident.sex === (animal.gender)).length : species
+    .find((specie) => specie.name === animal.specie).residents.length;
 }
 
 module.exports = countAnimals;
