bbeadf4a202b09283ddf4a69d837e99248da212d
Amanda Fernandes <aferanda@gmail.com>
2021-09-13 15:47:53 -0300

    Refatora requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,10 +2,8 @@ const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  const find = employees.find(({ firstName, lastName }) =>
-    employeeName === firstName || employeeName === lastName);
-
-  return !employeeName ? {} : find;
+  return !employeeName ? {} : employees
+    .find(({ firstName, lastName }) => employeeName === firstName || employeeName === lastName);
 }
 
 module.exports = getEmployeeByName;
