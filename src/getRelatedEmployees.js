bba47a5ce319ea5af0789c6787f74ecf81f2195b
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-09-13 17:02:32 -0300

    Realiza requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,14 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const managedObjects = data.employees.filter((element) => element.managers.includes(managerId));
+    return managedObjects.map((element) => `${element.firstName} ${element.lastName}`);
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
