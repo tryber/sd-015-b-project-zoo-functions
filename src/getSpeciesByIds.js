e1696b9a6c0d974cec907c1a81702e7e6c5eca03
Miguel_Reis <guelsouzar@gmail.com>
2021-09-13 19:33:25 -0300

    concerto de EsLint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,8 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(... ids) {
- return ids.map((id) => species.find((species) => species.id === id))
+function getSpeciesByIds(...ids) {
+  return ids.map((id) => species.find((species) => species.id === id));
 }
- console.log(getSpeciesByIds('89be95b3-47e4-4c5b-b687-1fabf2afa274','78460a91-f4da-4dea-a469-86fd2b8ccc84'))
- module.exports = getSpeciesByIds;
+// console.log(getSpeciesByIds('89be95b3-47e4-4c5b-b687-1fabf2afa274','78460a91-f4da-4dea-a469-86fd2b8ccc84'))
+module.exports = getSpeciesByIds;
