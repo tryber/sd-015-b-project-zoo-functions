0822a2f3529e74896791297cbe4ec155ed19bed8
Danielen Cestari Nunes <danielen.cestari@gmail.com>
2021-09-13 15:43:27 -0300

    requisito 01 do projeto

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return species.filter((idPesquisado) => ids.includes(idPesquisado.id));
 }
+// ajuda do Assis para corrigir o parametro passando qdo é mais de um id.
+// modelo de função de acordo com o visto no https://www.freecodecamp.org/news/javascript-array-filter-tutorial-how-to-iterate-through-elements-in-an-array/
 
 module.exports = getSpeciesByIds;
