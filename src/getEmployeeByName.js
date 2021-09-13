78585492799273ba0b616414e6114db7b5ee6f0b
Rodolfo <rodolfoserralha@hotmail.com>
2021-09-13 16:01:33 -0300

    melhorando linhas requisito 03

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,8 +2,8 @@ const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  return !employeeName ? {} : employees.find((employee) => employee.firstName === employeeName
-  || employee.lastName === employeeName);
+  return !employeeName ? {} : employees
+    .find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
