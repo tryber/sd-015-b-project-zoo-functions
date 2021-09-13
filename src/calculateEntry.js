1d4a89694b987ede2b9867da65cedc5c1d942852
Antonio Souza <antoniofelipe2001@hotmail.com>
2021-09-13 18:56:13 -0300

    Requisito 6 finalizado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,34 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const returnObj = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  entrants.forEach((person) => {
+    if (person.age < 18) {
+      returnObj.child += 1;
+    } else if (person.age >= 18 && person.age < 50) {
+      returnObj.adult += 1;
+    } else {
+      returnObj.senior += 1;
+    }
+  });
+  return returnObj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length < 1) {
+    return 0;
+  }
+  let entryPayment = 0;
+  const entrantsCountObj = countEntrants(entrants);
+  const keys = Object.keys(entrantsCountObj);
+  for (let index = 0; index < keys.length; index += 1) {
+    entryPayment += entrantsCountObj[keys[index]] * data.prices[keys[index]];
+  }
+  return entryPayment;
 }
 
 module.exports = { calculateEntry, countEntrants };
