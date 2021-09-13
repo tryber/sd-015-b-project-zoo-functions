4b26db4cda719229526468c3e6b7fc7867118fd4
Gustavo Gracio <gustavogracio@hotmail.com>
2021-09-13 16:05:31 -0300

    Requisito 2 completo (necessita refatoracao)

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  let result = '';
+  return species.some((specie) => {
+    if (specie.name === animal) {
+      result = specie.residents.every((resident) => resident.age >= age);
+    }
+    return result;
+  });
 }
 
 module.exports = getAnimalsOlderThan;
