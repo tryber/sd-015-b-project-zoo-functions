21076e92f98be63aad2a31fb129b5fc594ccb749
Allan Melo <allan-m-silva@hotmail.com>
2021-09-13 16:18:48 -0300

    adiciona requisito 2 e draft do requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,13 @@
+// const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
-  // seu código aqui
-  
+  // species.map((specie) => {
+  //   if (specie.id === ids) {
+  //     return true
+  //   } })
 }
+
+// console.log(getSpeciesByIds())
 
 module.exports = getSpeciesByIds;
