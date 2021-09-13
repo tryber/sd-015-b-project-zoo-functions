ab9cc979d85b53fa353e67bc0d75c6e18dd32155
gustavogss <gustavogss.dev@gmail.com>
2021-09-13 16:23:22 -0300

    Erros de lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,5 +1,5 @@
-const { employees } = require("../data/zoo_data");
-const data = require("../data/zoo_data");
+const { employees } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   if (!employeeName) {
