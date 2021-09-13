1069a0978130c9cee09b1bde17245f16913bd595
aaguero96 <andre.luiz_1996@hotmail.com>
2021-09-13 13:51:55 -0300

    Finish part 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  return data.employees.find((element) => {
+    return element.firstName === employeeName || element.lastName === employeeName
+  }) || {};
 }
+
+console.log(getEmployeeByName())
 
 module.exports = getEmployeeByName;
