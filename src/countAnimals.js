9a1b145724ba6c194d8e1b5882410c84e6d90481
David Souza <dabcs@poli.ufrj.br>
2021-09-13 12:12:56 -0300

    conclusão do requisito 6

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(...animal) {
+  if (animal.length === 0) {
+    const animalObj = {};
+    species.forEach((eachAnimal) => { animalObj[eachAnimal.name] = eachAnimal.residents.length; });
+    return animalObj;
+  }
+
+  if (animal[0].gender) {
+    return species.find((specieObj) => animal[0]
+      .specie === specieObj.name).residents.filter((sGender) => sGender
+      .sex === animal[0].gender).length;
+  }
+
+  return species.find((animalSearch) => animalSearch.name === animal[0].specie).residents.length;
 }
 
 module.exports = countAnimals;
