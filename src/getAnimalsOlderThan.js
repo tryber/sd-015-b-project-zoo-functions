af584fe7bed7f8a7c6c671c142ff9e0fe559da2f
Rodolpho Coller <rodolpho.coller12@gmail.com>
2021-09-13 14:53:00 -0300

    implementa funçao getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+function getAnimalsOlderThan(animalName, animalAge) {
+  const currentSpecie = data.species.find((animal) => animal.name === animalName);
+  return currentSpecie.residents.every((currentAnimal) => currentAnimal.age > animalAge);
 }
 
 module.exports = getAnimalsOlderThan;
