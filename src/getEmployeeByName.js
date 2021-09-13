de9130a0f0a131a37ccb2b71976955ab1275da96
Wander Luiz <wander.srv@gmail.com>
2021-09-13 19:48:46 -0300

    Resoluçao do requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName) {
+    const pesquisa = employees.find((param) =>
+      param.lastName === employeeName || param.firstName === employeeName);
+    return pesquisa;
+  }
+  return {};
 }
 
 module.exports = getEmployeeByName;
