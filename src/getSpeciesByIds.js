ab1d191d9d1057ddd199117b3fb882dc7a52f6af
Rafael <rafael.gomesbarretomoura@gmail.com>
2021-09-13 16:09:06 -0300

    adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ids.map((id) => data.species.find((specie) => specie.id === id));
 }
 
 module.exports = getSpeciesByIds;
