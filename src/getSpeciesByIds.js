07f0b759ed17720bc777d8e3769160f988073fc1
aaguero96 <andre.luiz_1996@hotmail.com>
2021-09-13 13:26:40 -0300

    Finish part 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,10 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return data.species.map((element) => {
-    const conf = ids.some((id) => element.id === id);
-    return conf ? element : false; 
-  }).filter((element) => element !== false);
+  // return data.species.map((element) => {
+  //   return ids.some((id) => element.id === id) ? element : false;
+  // }).filter((element) => element !== false);
+  const arrayId = [];
+  ids.forEach((id) => {
+    arrayId.push(data.species.find((element) => element.id === id));
+  });
+  return arrayId;
 }
 
 module.exports = getSpeciesByIds;
