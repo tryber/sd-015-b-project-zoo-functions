7523d4cf1909e7f164513045efec235e5ab32eb3
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-09-13 16:21:39 -0300

    implementa a funcao getAnimalsOlderThan.js

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalRequired = species.find((specie) => specie.name === animal);
+  return animalRequired.residents.every((animalAge) => animalAge.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
