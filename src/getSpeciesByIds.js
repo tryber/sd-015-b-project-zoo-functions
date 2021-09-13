c9bfb376c2cc94770f709ac28c67de4c59a301a1
Toineto <augusto.vilhena4@gmail.com>
2021-09-13 19:14:36 -0300

    adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
-  // seu código aqui
+  return species.filter((specie) => ids.includes(specie.id));
 }
 
 module.exports = getSpeciesByIds;
