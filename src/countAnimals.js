448bf0bf49edd04da3a989d884679488b56fb50b
Camilla Del Guerso <camilladelguerso@gmail.com>
2021-09-13 15:18:04 -0300

    Adicona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce((acc, { name, residents }) => {
+      acc[name] = residents.length;
+      return acc;
+    }, {});
+  }
+  const { specie, gender = undefined } = animal;
+  if (gender === undefined) {
+    const quantAnimal = data.species.find(({ name }) => name === specie);
+    return quantAnimal.residents.length;
+  }
+  const quantAnimal = data.species.find(({ name }) => name === specie)
+    .residents.filter(({ sex }) => sex === gender);
+  return quantAnimal.length;
 }
-
+console.log(countAnimals({ specie: 'bears', gender: 'female' }));
 module.exports = countAnimals;
