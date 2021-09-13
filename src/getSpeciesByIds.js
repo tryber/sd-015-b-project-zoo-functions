cecb0f0bf67c46636ca2f92314fd3d3f905a5d5f
Deivid <deividmborges@gmail.com>
2021-09-13 15:12:37 -0300

    Iniciando projeto

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+console.log(species);
 function getSpeciesByIds(ids) {
-  // seu código aqui
+  
 }
-
+getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce')
 module.exports = getSpeciesByIds;
