a2448d80fc7fcfca4d5b7020ec54922b1da994cd
Joao Marco <joaomarco.matta@gmail.com>
2021-09-13 20:17:33 -0300

    Implementa requisito 9.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,70 +1,72 @@
 const data = require('../data/zoo_data');
 
-const mapWithoutParameters = () => {
-  const animalsMap = data.species.reduce((acc, { location, name }) => {
-    acc[location] = acc[location] || [];
-    acc[location].push(name);
+// const mapWithoutParameters = () => {
+//   const animalsMap = data.species.reduce((acc, { location, name }) => {
+//     acc[location] = acc[location] || [];
+//     acc[location].push(name);
 
-    return acc;
-  }, {});
+//     return acc;
+//   }, {});
 
-  return animalsMap;
-};
+//   return animalsMap;
+// };
 
-const sortAnimalsName = (sorted, animalsName, specie) => {
-  const arrOfNames = animalsName;
+// const sortAnimalsName = (sorted, animalsName, specie) => {
+//   const arrOfNames = animalsName;
 
-  if (sorted) {
-    arrOfNames[specie].sort();
-  }
+//   if (sorted) {
+//     arrOfNames[specie].sort();
+//   }
 
-  return arrOfNames;
-};
+//   return arrOfNames;
+// };
 
-const getAnimalsName = (specie, animalsList, sorted, animalsSex) => {
-  const animalsName = animalsList.residents.reduce((acc, { name, sex }) => {
-    acc[specie] = acc[specie] || [];
+// const getAnimalsName = (specie, animalsList, sorted, animalsSex) => {
+//   const animalsName = animalsList.residents.reduce((acc, { name, sex }) => {
+//     acc[specie] = acc[specie] || [];
 
-    if (animalsSex === sex) acc[specie].push(name);
+//     if (animalsSex === sex) acc[specie].push(name);
 
-    if (!animalsSex) acc[specie].push(name);
+//     if (!animalsSex) acc[specie].push(name);
 
-    return acc;
-  }, {});
+//     return acc;
+//   }, {});
 
-  const sortedArrayOfAnimals = sortAnimalsName(sorted, animalsName, specie);
+//   const sortedArrayOfAnimals = sortAnimalsName(sorted, animalsName, specie);
 
-  return sortedArrayOfAnimals;
-};
+//   return sortedArrayOfAnimals;
+// };
 
-const mapWithAnimalNames = (sorted, animalsSex) => {
-  const animalsMap = data.species.reduce((acc, currentSpecie) => {
-    const { location, name } = currentSpecie;
-    acc[location] = acc[location] || [];
-    const arrOfAnimalsName = getAnimalsName(name, currentSpecie, sorted, animalsSex);
-    acc[location].push(arrOfAnimalsName);
+// const mapWithAnimalNames = (sorted, animalsSex) => {
+//   const animalsMap = data.species.reduce((acc, currentSpecie) => {
+//     const { location, name } = currentSpecie;
+//     acc[location] = acc[location] || [];
+//     const arrOfAnimalsName = getAnimalsName(name, currentSpecie, sorted, animalsSex);
+//     acc[location].push(arrOfAnimalsName);
 
-    return acc;
-  }, {});
+//     return acc;
+//   }, {});
 
-  return animalsMap;
-};
+//   return animalsMap;
+// };
+// if (!includeNames && sex === 'female') return mapWithoutParameters();
 
+// if (includeNames && sex) return mapWithAnimalNames(sorted, sex);
+
+// if (includeNames && sex && sorted) return mapWithAnimalNames(sorted, sex);
+
+// if (includeNames && sorted) return mapWithAnimalNames(sorted);
 function getAnimalMap(options) {
   // seu código aqui
-  if (!options) return mapWithoutParameters();
+  // if (!options) return mapWithoutParameters();
 
-  const { includeNames, sorted, sex } = options;
+  // const { includeNames, sorted, sex } = options;
 
-  if (!includeNames && sex === 'female') return mapWithoutParameters();
+  // if (includeNames) return mapWithAnimalNames();
 
-  if (includeNames && sex) return mapWithAnimalNames(sorted, sex);
+  // const test = verifyData(includeNames, sorted, sex);
 
-  if (includeNames && sex && sorted) return mapWithAnimalNames(sorted, sex);
-
-  if (includeNames && sorted) return mapWithAnimalNames(sorted);
-
-  return mapWithAnimalNames();
+  // return test;
 }
 
 module.exports = getAnimalMap;
