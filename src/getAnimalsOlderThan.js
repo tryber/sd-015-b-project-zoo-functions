9adbae41953e3f5a60d7505add5ebd91dd4b6b4d
Gabriel Borges <igblborges@gmail.com>
2021-09-13 15:24:52 -0300

    adiciono requisito-2-getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species.every(() => animal.age >= age);
 }
-
+// console.log(getAnimalsOlderThan(('penguins', 10)));
 module.exports = getAnimalsOlderThan;
