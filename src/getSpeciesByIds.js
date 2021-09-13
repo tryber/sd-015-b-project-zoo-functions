67f98e761b491672f2166da6917e91d192d8f5bd
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-09-13 15:46:43 -0300

    Adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const animals = data.species;
+  const busca = animals.filter((animal) => ids.includes(animal.id));
+  return busca;
 }
 
 module.exports = getSpeciesByIds;
