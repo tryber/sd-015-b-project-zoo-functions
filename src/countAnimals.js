2d029690c79fcdc2b94a3dd59d053d9b91050536
Rodolfo <rodolfoserralha@hotmail.com>
2021-09-13 19:10:33 -0300

    adicionando requisito 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,30 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const emptyObj = (animal) => {
+  if (!animal) {
+    return species.reduce((acc, current) => {
+      const currentSpecie = current.name;
+      const currentLength = current.residents.length;
+      const obj = { [currentSpecie]: currentLength };
+      return Object.assign(acc, obj);
+    }, {});
+  }
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return emptyObj();
+  }
+  if (animal.gender) {
+    const speciesGender = species.find((specie) => specie.name === animal.specie)
+      .residents.filter((resident) => resident.sex === animal.gender);
+    return speciesGender.length;
+  }
+  const speciee = species.find((specie) => specie.name === animal.specie);
+  return speciee.residents.length;
 }
+
+console.log(countAnimals({ specie: 'elephants' }));
 
 module.exports = countAnimals;
