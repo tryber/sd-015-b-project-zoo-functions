2666b66ef98164762a8aebc00970b33b889f49dc
Bruno Bartolomeu <bruno.bartolomeu5@hotmail.com>
2021-09-13 15:00:05 -0300

    Envia requisitos 1 e 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,10 +2,8 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  return species.some(({ name, residents }) => name === animal 
+  return species.some(({ name, residents }) => name === animal
   && residents.every((resident) => resident.age > age));
 }
-
-console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
