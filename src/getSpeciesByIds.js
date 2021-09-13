99e3aa6506445b5abeb6036d13c84854d8954cfc
Vinicius Fernandes <viniciusfernandes126@gmail.com>
2021-09-13 15:58:02 -0300

    [Requisito 1] Implementa função getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) { // Função recebe como parametro um array de parametros.
   // seu código aqui
+  if (ids.length === 0) return []; // Verifica se a função recebeu algum parametro.
+  
+  const filterId = ids.map((id) => {
+    const filter = data.species.filter((anotherId) => ( anotherId.id === id ? true : false ));
+    return filter[0];
+  });
+  return filterId;
 }
 
 module.exports = getSpeciesByIds;
