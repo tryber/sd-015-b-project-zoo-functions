ee3966562516b228abe382d4a3d8ad1cec420bb7
Bruno Bartolomeu <bruno.bartolomeu5@hotmail.com>
2021-09-13 18:50:52 -0300

    Envia requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const animalsInfo = {};
+  if (!animal) {
+    species.forEach(({ name, residents }) => {
+      animalsInfo[name] = residents.length;
+    });
+    return animalsInfo;
+  }
+
+  const animalSelected = species.find((specie) => specie.name === animal.specie);
+
+  if (animal.gender) {
+    const selectedGender = animalSelected.residents
+      .filter((resident) => resident.sex === animal.gender);
+    return selectedGender.length;
+  }
+
+  return animalSelected.residents.length;
 }
 
 module.exports = countAnimals;
