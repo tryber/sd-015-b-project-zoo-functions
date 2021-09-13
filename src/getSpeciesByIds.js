962fc087ffb3b064352d510417e7818cd61e35bd
Sayro <sayrodamaceno70@gmail.com>
2021-09-13 16:10:02 -0300

    cumpre requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aquii
+function getSpeciesByIds(...ids) {
+  const resultado = [];
+  ids.forEach((id) => {
+    resultado.push(species.find((animais) => animais.id === id));
+  });
+  return resultado;
 }
 
 module.exports = getSpeciesByIds;
