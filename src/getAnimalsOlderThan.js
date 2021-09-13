99689c1d9b9be70fa9eda20d9f6251f6cf680dfb
Richard Leal <rwsleal@gmail.com>
2021-09-13 18:51:50 -0300

    Requisito 2 finalizado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalMatch = data.species.find((elementAnimal) => elementAnimal.name === animal);
+  return animalMatch.residents.every((animalOfSpecies) => animalOfSpecies.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
