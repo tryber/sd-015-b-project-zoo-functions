3ad4b7ff520bdba3b4b08ef3909a0083ddef1ee2
Ge <gmarcoscardoso@gmail.com>
2021-09-13 18:29:59 -0300

    Requisito 4 incompleto

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  // verifica se é gerente ou não
+  const findManage = data.employees.filter((findId) => findId.id === id).shift();
+  return findManage.managers.length !== 0;
 }
 
+const colaborator = (idcolabe) => data.employees.filter((findId) => findId.id === idcolabe);
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const getManager = isManager(managerId);
+  const getIdPeople = colaborator(managerId).shift().responsibleFor;
+  const error = new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+
+  return (getManager) ? getIdPeople : error;
 }
 
+console.log(getRelatedEmployees('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
+// [ 'Burl Bethea', 'Ola Orloff', 'Emery Elser' ]
 module.exports = { isManager, getRelatedEmployees };
