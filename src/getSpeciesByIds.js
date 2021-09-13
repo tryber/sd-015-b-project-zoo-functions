5d759822822261c6e213e0bc5851fe597d74af95
Antonio Souza <antoniofelipe2001@hotmail.com>
2021-09-13 14:28:35 -0300

    Requisito 1 finalizado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const arrayRetorno = [];
+  ids.forEach((id) => arrayRetorno.push(data.species.find((object) => object.id === id)));
+  return arrayRetorno;
 }
 
 module.exports = getSpeciesByIds;
