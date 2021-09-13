95b35dfa15b05354c2e630d3c56d3d44cdca19a3
Samuel Pereira <gmr.quest.10@gmail.com>
2021-09-13 16:26:51 -0300

    Correcoes no requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return data.species.filter((especie, index) =>
-    especie.id === ids[index]).map((especie) => especie);
+  function especieById(especie, index) {
+    if (!ids) return [];
+    if (especie.id === ids[index]) return especie;
+  }
+
+  return data.species.filter(especieById);
 }
 
 console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
