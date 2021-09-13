e207d7b61f5579f8c234455373f88bdf5eb29b7f
Huijiro <huijirohankei@gmail.com>
2021-09-13 15:25:58 -0300

    Requisito 2 e 3 feito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,8 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const a = data.species.find((specie) => specie.name === animal);
+  return a.residents.every((e) => e.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName){
+    return data.employees.find((employee) => 
+    employee.firstName === employeeName || employee.lastName === employeeName
+    );
+  } 
+  return {};
 }
 
 module.exports = getEmployeeByName;
