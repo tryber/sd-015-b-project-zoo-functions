dce99504a768daf1ff4689bd1de188b4a4a66a4c
iuryroque <iury.roque@hotmail.com>
2021-09-13 17:10:13 -0300

    Requisito 1 finalizado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const { species } = data;
 
+function getSpeciesByIds(...ids) {
+  // seu código aqui
+  const buscaEspecies = species.filter((especie) => ids.includes(especie.id));
+  return buscaEspecies;
+}
 module.exports = getSpeciesByIds;
