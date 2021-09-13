0b09e6466d7422089ccdbaf6bcaa702f3d9cecf1
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-13 16:14:17 -0300

    Testa lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -6,21 +6,21 @@ const { species } = require('../data/zoo_data');
 function getSpeciesByIds(...arrayDeIDS) {
   // seu código aqui
   const arrayFiltrado = [];
-  species.filter( (specie) => {
-    arrayDeIDS.forEach(id => {
+  species.filter((specie) => {
+    arrayDeIDS.forEach( (id) => {
       if (specie.id === id) {
         arrayFiltrado.push(specie);
-        // return arrayFiltrado;
+        return arrayFiltrado;
       }
     });
-  })
+  });
   return arrayFiltrado;
 }
-const idsDeTeste = [
-  '0938aa23-f153-4937-9f88-4858b24d6bce',
-  'e8481c1d-42ea-4610-8e11-1752cfc05a46',
-  'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
-]
+// const idsDeTeste = [
+//   '0938aa23-f153-4937-9f88-4858b24d6bce',
+//   'e8481c1d-42ea-4610-8e11-1752cfc05a46',
+//   'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
+// ],
 // getSpeciesByIds(idsDeTeste);
 // console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46')); // [DEBUG]
 // console.log(getSpeciesByIds(idsDeTeste[0], idsDeTeste[1], idsDeTeste[2])); // [DEBUG]
