048f7ea9d30992a8c42bbb96e6d04685f8fbcf65
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-09-13 15:33:20 -0300

    Finaliza requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,10 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  return data.employees.filter((emp) => ((emp.firstName === employeeName)
+    || (emp.lastName === employeeName)))[0] === undefined ? {}
+    : data.employees.filter((emp) => ((emp.firstName === employeeName)
+    || (emp.lastName === employeeName)))[0];
 }
 
 module.exports = getEmployeeByName;
