7d7a98fdb332f320ecbe7cebb666b781ef0d0958
Murilo <murilocarrijoadm@gmail.com>
2021-09-13 17:30:04 -0300

    Adiciona o 1º requisito resolvido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ids.map((id) => species.find((animal) => animal.id === id));
 }
 
 module.exports = getSpeciesByIds;
