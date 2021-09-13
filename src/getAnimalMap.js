35ec3baccb433991fec2e086f5bcf6d45f7aca02
David Souza <dabcs@poli.ufrj.br>
2021-09-13 14:25:36 -0300

    inicio do exercicio 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// const includeNames = () => {
+//   const objLoc = { NE: [], NW: [], SE: [], SW: [] };
+//   objLoc.NE = species.filter((specie) => specie.location === 'NE').map(({ name, residents }) => {
+//     const obj = {};
+//     obj[name] = residents;
+//     return obj;
+//   });
+// };
+
 function getAnimalMap(options) {
-  // seu código aqui
+  const objLoc = { NE: [], NW: [], SE: [], SW: [] };
+  Object.keys(objLoc).forEach((element) => {
+    objLoc[element] = species.filter((specie) => specie.location === element);
+  });
+  return objLoc;
 }
 
 module.exports = getAnimalMap;
