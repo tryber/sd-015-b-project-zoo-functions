d99740212e5da0f06fc41e22271e6a6d1c12ba97
iuryroque <iury.roque@hotmail.com>
2021-09-13 18:05:26 -0300

    Requisito 2 finalizado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animalSelected = species.find((specie) => specie.name === animal);
+  return animalSelected.residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
