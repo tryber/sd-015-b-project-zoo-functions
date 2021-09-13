77dbaf98c12c5d5d84ed731bb4b139ba09b7bb70
Arthur Teixeira <tuzinf5@exemplo.br>
2021-09-13 14:33:16 -0300

    Requisito 1 - concluido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getSpeciesByIds(...ids) {
   // seu código aqui
+  const idsArray = ids; // coloquei em uma variavel para ficar visivel que é um array
+  const speciesArray = idsArray.map((speciesId) => species.find(({ id }) => id === speciesId));
+  return speciesArray;
 }
 
 module.exports = getSpeciesByIds;
