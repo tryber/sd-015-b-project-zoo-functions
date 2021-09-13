c95f228dcb488aff1d87eabe00fc9a660f84ed74
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-09-13 16:23:03 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return employees.find(({ firstName, lastName }) => employeeName === firstName 
+    || employeeName === lastName);
 }
 
 module.exports = getEmployeeByName;
