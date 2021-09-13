05fbbbd59d9adbc84582f38f3232a5b3819aa642
henrique <hickm018@gmail.com>
2021-09-13 15:38:44 -0300

    Corrige lint requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -7,5 +7,3 @@ function getSpeciesByIds(...ids) {
 }
 
 module.exports = getSpeciesByIds;
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
-'e8481c1d-42ea-4610-8e11-1752cfc05a46'))
