e4f80aff66731a55b0a206803756560fcdc8875c
Andre Luiz <andreluizgyn1@gmail.com>
2021-09-13 17:03:42 -0300

    Corrigi o lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,11 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  function getSpeciesByIds(...ids) {
-    const animals = species.filter((specie) => ids.find((id) => id === specie.id));
-    return animals;
-  }
-  
+function getSpeciesByIds(...ids) {
+  const animals = species.filter((specie) => ids.find((id) => id === specie.id));
+  return animals;
 }
 
 module.exports = getSpeciesByIds;
