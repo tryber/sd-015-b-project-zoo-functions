b722c1d7585c13c9bd5e443a52c5a4b2b90de426
aaguero96 <andre.luiz_1996@hotmail.com>
2021-09-13 20:40:36 -0300

    Fix lint part 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -4,28 +4,31 @@ function generalSchedule() {
   const daysObject = data.hours;
   const speciesArray = data.species;
   return Object.keys(daysObject).reduce((schedule, day) => {
+    const newObject = schedule;
     const { open, close } = daysObject[day];
     const officeHour = `Open from ${open}am until ${close}pm`;
     let exhibition = speciesArray.filter((element) => element.availability.includes(day));
     exhibition = exhibition.map((element) => element.name);
     if (day === 'Monday') {
-      schedule[day] = { 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' };
+      newObject[day] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
     } else {
-      schedule[day] = {
+      newObject[day] = {
         officeHour,
         exhibition,
       };
     }
-    return schedule;
+    return newObject;
   }, {});
 }
 
 function getScheduleByDay(scheduleTarget) {
   const scheduleArray = Object.entries(generalSchedule());
-  const scheduleArrayFiltered = scheduleArray.filter((element) => element[0] === scheduleTarget);
-  const scheduleArrayAdjusted = scheduleArrayFiltered.length !== 0 ? scheduleArrayFiltered : scheduleArray;
-  return scheduleObject = scheduleArrayAdjusted.reduce((acc, curr) => {
-    const [ key, value ] = curr;
+  let scheduleArrayFiltered = scheduleArray.filter((element) => element[0] === scheduleTarget);
+  if (scheduleArrayFiltered.length === 0) {
+    scheduleArrayFiltered = scheduleArray;
+  };
+  return scheduleArrayFiltered.reduce((acc, curr) => {
+    const [key, value] = curr;
     acc[key] = value;
     return acc;
   }, {});
@@ -40,7 +43,8 @@ function getSchedule(scheduleTarget) {
   const days = Object.keys(data.hours);
   if (animals.includes(scheduleTarget)) {
     return getScheduleByAnimal(scheduleTarget);
-  } else if (days.includes(scheduleTarget)) {
+  }
+  if (days.includes(scheduleTarget)) {
     return getScheduleByDay(scheduleTarget);
   }
   return generalSchedule();
