86be63d792c7e06108bad1e0758cbaa8c7d8ae4f
Guilherme Saboia <gsaboia@yahoo.com>
2021-09-13 15:14:22 -0300

    Adiciona requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,8 @@
+const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  return employees.filter((employee) => (employee.firstName === employeeName || employee.lastName === employeeName))[0] || {};
 }
 
 module.exports = getEmployeeByName;
