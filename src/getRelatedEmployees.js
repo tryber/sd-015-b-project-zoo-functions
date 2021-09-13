f07fc34f335503ee214922c16a2cc59812ee872d
Danilo <dcalado.fisio@yahoo.com.br>
2021-09-13 16:10:25 -0300

    feito requesito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const obj = data.employees.filter((employee) => {
+      const employees = employee.managers.find((manager) => manager === managerId);
+      return employees;
+    });
+    const string = obj.map((obj) => `${obj.firstName} ${obj.lastName}`);
+    return string;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
