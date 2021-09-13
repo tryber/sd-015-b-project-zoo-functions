938606635bc35dc5e54d3c369976ac4f50467e68
Camilla Del Guerso <camilladelguerso@gmail.com>
2021-09-13 16:24:15 -0300

    Adicona requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function getOldestFromFirstSpecies(ident) {
+  const especie = data.employees.find(({ id }) => id === ident).responsibleFor[0];
+  const animal = data.species.find(({ id }) => id === especie).residents
+    .sort((a, b) => b.age - a.age)[0];
+  return Object.values(animal);
 }
 
 module.exports = getOldestFromFirstSpecies;
