2c196ecfe7ddfccaa6abd8eabf540e7d9e72393f
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-13 16:08:07 -0300

    Inicia logica do requisito01

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  species.map( (specie) => {
+    console.log(specie.id); // [DEBUG] Imprime id de todas as Espécies
+    if (specie.id === ids) {
+
+
+    }
+  })
 }
 
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
 module.exports = getSpeciesByIds;
