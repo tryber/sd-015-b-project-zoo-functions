b22b5ec5892756e280cc9b6e24c14ee4971058f6
alvesgf16 <alvesgabriel1994@gmail.com>
2021-09-13 15:16:50 -0300

    Corrige erro de lint na funçao getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,6 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-
   return species.filter((oneSpecies) => ids.includes(oneSpecies.id));
 }
 
