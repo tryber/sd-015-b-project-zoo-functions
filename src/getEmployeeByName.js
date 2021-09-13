fcedce3ea5a6640fcf2758c9a2294ae7c75eb5fc
Ge <gmarcoscardoso@gmail.com>
2021-09-13 17:03:07 -0300

    Requisito 03 completo

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  return data.employees.filter((namePeople) => (namePeople.firstName === employeeName
+    || namePeople.lastName === employeeName)).shift() || {};
 }
 
 module.exports = getEmployeeByName;
