094837ba217ee70c2970abcbd829ca027435df41
Vinicius Fernandes <viniciusfernandes126@gmail.com>
2021-09-13 19:20:22 -0300

    [Requisito 4] Implementa função getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,37 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  const employeesList = employees.some((employee) => {
+    const employeeIsManager = employee.managers.find((employeeId) => employeeId === id);
+    return id === employeeIsManager;
+  });
+  return employeesList;
+}
+
+function containManager(manager, employeesList, managerId) {
+  const employeeOfManager = manager.managers.find((id) => {
+    if (id === managerId) {
+      return employeesList.push(`${manager.firstName} ${manager.lastName}`);
+    }
+    return false;
+  });
+  return employeeOfManager;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const responsiblesEmployees = employees.reduce((employeesList, manager) => {
+    if (containManager(manager, employeesList, managerId) === undefined) {
+      return employeesList;
+    }
+    return employeesList;
+  }, []);
+  return responsiblesEmployees;
 }
 
 module.exports = { isManager, getRelatedEmployees };
