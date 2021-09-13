dcf93f310963c37ec03f4a0bbe9fc5dc890b3dfb
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-13 19:14:28 -0300

    Captura ids dos managers diretamente do zoo_data/ inicia funçao if do getRelatedEmployees.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,9 +1,6 @@
 const data = require('../data/zoo_data');
-const {employees} = data;
+const {stephanieId, olaId, burlId} = data; // Importa variável que armazena as ids dos managers diretamente do zoo_data
 
-const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
-const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
-const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
 const managers = [stephanieId, olaId, burlId];
 
 function isManager(id) {
@@ -18,6 +15,11 @@ function getRelatedEmployees(managerId) {
   // console.log(resultOfId); // [REBUG] Consola o retorno da função isManager;
   // Cria uma condicional onde, se resultOfId === true, executa a função que mostra quem essa pessoa gerencia
   // Se não, retorna o erro: 'O id inserido não é de uma pessoa colaboradora gerente!';
+  if (resultOfId === true) {
+    // console.log(`resultOfId === ${resultOfId}`); // [DEBUG] Verifica o retorno da função
+    // return `resultOfId === ${resultOfId}`; // [DEBUG] Verifica o retorno da função
+  }
+
   return 'O id inserido não é de uma pessoa colaboradora gerente!';
 }
 
