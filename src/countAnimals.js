b1ef17c48a543e5460112fd180c108440b6e8a70
Rodrigo Lima <rodrigootavio19@gmail.com>
2021-09-13 13:57:44 -0400

    finaliza countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    const nameAndQuantity = species.reduce((acc, { name, residents }) => {
+      acc[name] = residents.length;
+      return acc;
+    }, {});
+    return nameAndQuantity;
+  }
+  const { specie, gender } = animal;
+  const animalData = species.find(({ name }) => name === specie);
+  if (!gender) {
+    return animalData.residents.length;
+  }
+  const filterGender = animalData.residents.filter(({ sex }) => sex === gender);
+  return filterGender.length;
 }
 
 module.exports = countAnimals;
