90240398d7c15586983fc07e329816d63e974445
Rodolfo <rodolfoserralha@hotmail.com>
2021-09-13 14:27:03 -0300

    adicionando exercicio 01

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,6 +1,6 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-
+const getSpeciesByIds = (...ids) => species.filter((specie) => ids.includes(specie.id));
 
 module.exports = getSpeciesByIds;
