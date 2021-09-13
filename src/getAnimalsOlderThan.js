48d113647522a2812b6937e513caa179613e149f
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-09-13 16:29:30 -0300

    Adiciona requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species: speciee } = data;
+  const animals = speciee.find((specie) => (specie.name === animal));
+  const verifyAge = animals.residents.every((resident) => resident.age > age);
+  return verifyAge;
 }
-
+console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
