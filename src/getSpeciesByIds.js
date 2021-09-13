d4c0131cb2381aa8f613841a4264b837fec8f137
claudiocassimiro <days_surfing@live.com>
2021-09-13 15:49:52 -0300

    Requisito 1 completo

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+const { species } = data;
+
+const findAnimals = (id) => species.find((element) => ((element.id === id) ? element : undefined));
+
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  if (!ids) {
+    return [];
+  }
+
+  const animals = ids.map((id) => findAnimals(id));
+  return animals;
 }
 
 module.exports = getSpeciesByIds;
