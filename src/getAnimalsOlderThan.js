6040f3b9238c0b4e3504b2e5a12f49d40406f1fb
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-13 16:02:26 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const obj = species.find((element) => element.name === animal);
+  const booleanReturnAge = obj.residents.every((elem) => elem.age >= age);
+  return booleanReturnAge;
 }
 
 module.exports = getAnimalsOlderThan;
