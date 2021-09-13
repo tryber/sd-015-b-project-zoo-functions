432636e9af85a7cf171fede470b170cd3ef90a04
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2021-09-13 17:24:00 -0300

    requesito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return species.reduce((acc, value) =>
+    // acessado : https://github.com/tryber/sd-014-b-project-zoo-functions/blob/Jo%C3%A3oHenrique-Zoo-Functions/src/zoo.js
+      ({ ...acc, [value.name]: value.residents.length }), {});
+    // ...
+  } if (animal.gender === undefined) {
+    return species.find((animals) => animals.name === animal.specie).residents.length;
+  //  Primeiro criamos o critério que só pode entrar quem tiver a chave animal.gender indefinodo
+  //  e depois procuramos animals.name iguais a animal.specie e contamos quantos residentes existem
+  //  residents.length
+  }
+  return species.find((animals) => animals.name === animal.specie).residents
+    .filter((number) => number.sex === animal.gender).length;
+  //  O início é exatamente igual ao anterior, só adicionamos o filter para
+  // nos dar apenas os animais do genero que se passa por parametro e assim contabilizar.
 }
-
 module.exports = countAnimals;
