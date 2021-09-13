18a2f16a575253c19438fe26313e25d0a239ecd9
LeonardoMonti <contatoleomonti@gmail.com>
2021-09-13 14:46:16 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aquii
+const { species } = data;
+function getSpeciesByIds(id, id2) {
+  const getId = species
+    .filter((iden) => (iden.id === id || iden.id === id2 ? iden.id : iden === []));
+
+  return getId;
 }
 
 module.exports = getSpeciesByIds;
