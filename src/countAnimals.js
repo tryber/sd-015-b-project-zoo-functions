b1a3af36382ab739efb08e41fe9de7e705b79cdb
Allan Melo <allan-m-silva@hotmail.com>
2021-09-13 20:31:48 -0300

    tentativas dos requisitos 4 e 5, ainda sem sucesso

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
-}
+  if (!animal) {
+    return list
+  } return list[animal.specie]
+  const number = list[animal.specie]  
+} 
+
+// function listWithGender () => {}
+
+const list = {
+  'lions': 4,
+  'tigers': 2,
+  'bears': 3,
+  'penguins': 4,
+  'otters': 4,
+  'frogs': 2,
+  'snakes': 2,
+  'elephants': 4,
+  'giraffes': 6,
+};
+
+const species = data.species;
+let maleSpecies = {}
+maleSpecies = species.forEach((element) => maleSpecies);
+// maleSpecies.element.name = element.residents.reduce((acc, element) => {
+//   if (element.sex === 'male') {
+//     acc + 1
+//   }
+// })
+// })
+console.log(maleSpecies);
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+// function isManager(id) {
+//   const employees = data.employees;
+//   employees.forEach((element) => {
+//     console.log(element.managers);
+//   })
+// }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
 }
+
+// const employees = data.employees;
+// console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'))
 
 module.exports = { isManager, getRelatedEmployees };
