f4519227c21bc03a6c3337a3f089cae52652ded9
Marina Marques <marina.marqueso@outlook.com>
2021-09-13 16:58:32 -0300

    Lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,14 +1,12 @@
 const { species } = require('../data/zoo_data');
+
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-// primeiro encontrar o id
-    const find = species.filter((elemento) => ids.includes(elemento.id));
-// imprimir o elemento
-    return find;
+  // primeiro encontrar o id
+  const find = species.filter((elemento) => ids.includes(elemento.id));
+  // imprimir elemento
+  return find;
 }
 
 module.exports = getSpeciesByIds;
-
-//primeiro commit
-
