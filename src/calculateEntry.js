d6031e0375c1190c62c9c4a2f405424ffd2913f3
matheusg18 <matheusgabriel.18@gmail.com>
2021-09-13 19:49:01 -0300

    Requisito 6 countEntrants feito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-const countEntrants = ({ age }) => {
+const countEntrants = (entrants = []) => {
   const totalByAge = { child: 0, adult: 0, senior: 0 };
+
+  entrants.forEach(({ age }) => {
+    if (age >= 50) totalByAge.senior += 1;
+    else if (age >= 18) totalByAge.adult += 1;
+    else totalByAge.child += 1;
+  });
+
+  return totalByAge;
 };
 
 function calculateEntry(entrants) {
