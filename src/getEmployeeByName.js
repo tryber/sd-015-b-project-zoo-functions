46cecce3847ad588614a6b8496f892061bc6d13c
henrique <hickm018@gmail.com>
2021-09-13 16:24:42 -0300

    Adiciona requisito 3, o anterior era requisito 2

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (typeof employeeName === 'undefined') {
+    return {};
+  }
+  return employees.find((name) => name.firstName === employeeName 
+  || name.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
