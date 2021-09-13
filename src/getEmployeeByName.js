08e438e09fc2cb3106d2fc69af451e9d7584d083
LeonardoMonti <contatoleomonti@gmail.com>
2021-09-13 16:00:21 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+
+  const getNames = employees
+    .find((name) => name.firstName === employeeName || name.lastName === employeeName);
+
+  return getNames;
 }
 
 module.exports = getEmployeeByName;
