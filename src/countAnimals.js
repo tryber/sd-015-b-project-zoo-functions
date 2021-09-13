1306e10a84a7ae8f00b21eeb85c47e0cd763eaab
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-09-13 18:15:49 -0300

    Realiza requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(param) {
+  if (param) {
+    const { specie, gender } = param;
+    const specieRequired = data.species
+      .find((element) => element.name.toLowerCase() === specie.toLowerCase());
+    let residentsRequired = specieRequired.residents;
+    if (gender) {
+      residentsRequired = residentsRequired
+        .filter((element) => element.sex.toLowerCase() === gender.toLowerCase());
+    } return residentsRequired.length;
+  }
+  const objectReturn = {};
+  data.species.forEach((element) => {
+    const animalName = element.name;
+    const animalResidents = element.residents.length;
+    objectReturn[`${animalName}`] = animalResidents;
+  });
+  return objectReturn;
 }
 
 module.exports = countAnimals;
