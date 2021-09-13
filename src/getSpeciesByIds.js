7c9a83a5f807b84f495763451fd66e3bbb86a4e7
José Erivam <erivamdev@gmail.com>
2021-09-13 16:44:09 -0300

    repair requisite 1 true

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,9 +2,8 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
-  return data.species.filter((specie, i) => specie.id === ids[i]);
+  return data.species.filter((especie) => ids.includes(especie.id));
   // Fim do código
 }
-console.log(getSpeciesByIds());
-//console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+
 module.exports = getSpeciesByIds;
