8ddf7b67466d52ce4168f77ea414613a6778b1f7
Huijiro <huijirohankei@gmail.com>
2021-09-13 18:19:13 -0300

    Requisito 6 feito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
+function countEntrants(entrants = []) {
   // seu código aqui
+  return {
+    child: entrants.filter((e) => e.age < 18).length,
+    adult: entrants.filter((e) => e.age >= 18 && e.age < 50).length,
+    senior: entrants.filter((e) => e.age >= 50).length,
+  };
 }
 
-function calculateEntry(entrants) {
+function calculateEntry(entrants = null) {
   // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const entries = countEntrants(entrants);
+  return (data.prices.child * entries.child)
+    + (data.prices.adult * entries.adult)
+    + (data.prices.senior * entries.senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -8,18 +8,14 @@ function countAnimals(animal) {
       obj[specie.name] = specie.residents.length;
     });
     return obj;
-  } 
-  if (animal.specie) {
-    return data.species.find((specie) => specie.name === animal.specie).residents.length;
   }
   if (animal.gender) {
     return data.species.find((specie) => specie.name === animal.specie)
-      .residents.filter((e) => e.sex === animal.gender);
+      .residents.filter((e) => e.sex === animal.gender).length;
+  }
+  if (animal.specie) {
+    return data.species.find((specie) => specie.name === animal.specie).residents.length;
   }
 }
-
-console.log(countAnimals());
-console.log(countAnimals({ specie: 'penguins' }));
-console.log(countAnimals({ specie: 'giraffes', gender: 'female' }));
 
 module.exports = countAnimals;
