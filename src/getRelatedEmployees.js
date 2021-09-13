a8decd38735567e6cea84402a8dae855e278b26c
aaguero96 <andre.luiz_1996@hotmail.com>
2021-09-13 14:26:14 -0300

    Finish part 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const arrayManager = data.employees.filter((element) => element.managers.includes(id));
+  return arrayManager.length !== 0;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return data.employees.map((element) => {
+      const {managers, firstName, lastName} = element;
+      return managers.includes(managerId) ? `${firstName} ${lastName}` : false;
+    }).filter((element) => element !== false);
+  } else {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
