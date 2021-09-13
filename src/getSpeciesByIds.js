8b2449f7bdfa9d3b6b08272bdbec96cf0c023ef5
José Erivam <erivamdev@gmail.com>
2021-09-13 16:24:11 -0300

    Starting and adding requisite 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  return data.species.filter((specie, i) => specie.id === ids[i]);
+  // Fim do código
 }
-
+console.log(getSpeciesByIds());
+//console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
