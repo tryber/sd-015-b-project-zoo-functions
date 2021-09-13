1424f7abcc893e2eab36cdf017a74616ab9438b2
leandromliveira <leandrooliveira35@gmail.com>
2021-09-13 15:48:21 -0300

    refatorando requisito 2

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeesCoverage(object) {
+  const employeeFirstName = data.employees.find((employee) => (object.name === employee.firstName));
+  const employeeLastName = data.employees.find((employee) => (object.name === employee.lastName));
+  const employeeId = data.employees.find((employee) => (object.name === employee.id));
+  if (employeeFirstName !== undefined) return employeeFirstName;
+  if (employeeLastName !== undefined) return employeeLastName;
+  if (employeeId !== undefined) return employeeId;
 }
+
+console.log(getEmployeesCoverage({ name: 'Nelson' }));
 
 module.exports = getEmployeesCoverage;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,13 +1,7 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids, id2) {
-  const result = [];
-  if (!ids) return result;
-  result.push(data.species.find((specie) => specie.id === ids));
-  if (id2) {
-    result.push(data.species.find((specie) => specie.id === id2));
-  }
-  return result;
+function getSpeciesByIds(...ids) {
+  return data.species.filter((specie) => ids.includes(specie.id));
 }
 
 module.exports = getSpeciesByIds;
