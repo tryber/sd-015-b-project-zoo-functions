e8ef25737ea5e32a0d3fc5c0dad0ac5ed16ee692
aaguero96 <andre.luiz_1996@hotmail.com>
2021-09-13 15:16:47 -0300

    Finish part 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
+function filterBySpecie(arr, specie) {
+  return arr.filter((element) => element.name === specie);
+}
+
+function filterByGender(arr, gender) {
+  return arr.map((element) => {
+    element.residents = element.residents.filter((resident) => resident.sex === gender);
+    return element;
+  });
+}
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal) {
+    const {specie, gender} = animal;
+    let animalFiltered = data.species;
+    animalFiltered = specie ? filterBySpecie(animalFiltered, specie) : animalFiltered;
+    animalFiltered = gender ? filterByGender(animalFiltered, gender) : animalFiltered;
+    return animalFiltered.reduce((acc, curr) => acc + curr.residents.length, 0);
+  } else {
+    const object = {};
+    data.species.forEach((element) => {
+      object[element.name] = element.residents.length;
+    });
+    return object;
+  }
 }
 
 module.exports = countAnimals;
