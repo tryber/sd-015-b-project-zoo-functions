9c79be534a852f31d8884400eb87ab1ba2613d80
1lucascq <1lucascq@gmail.com>
2021-09-13 16:56:31 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,9 +2,8 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return species.filter((id) => species.id === ids);
+  return species.filter((specie) => ids.find((id) => specie.id === id));
+  // return result;
 }
-
-// console.log(getSpeciesByIds(species));
 
 module.exports = getSpeciesByIds;
