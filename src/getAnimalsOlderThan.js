79ffa4c0388a605e5d6023095935ba224bb0280e
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2021-09-13 18:04:22 -0300

    ajeita o lint do segundo quesito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,10 +1,8 @@
-const { name } = require('faker');
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   const verify = data.species.find((Element) => Element.name === animal);
-  return verify.residents.every((Element) => Element.age >= age)
+  return verify.residents.every((Element) => Element.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
