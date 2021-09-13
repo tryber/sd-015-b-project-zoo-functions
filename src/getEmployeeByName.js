72c108f1f6b1cf61575ff87fd4798eea2d9fd55a
Lucas Rodrigues <theluks21@live.com>
2021-09-13 18:26:17 -0300

    Adiciona requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,14 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const employeeData = data.employees.filter((employee) => {
+    if (employee.firstName === employeeName || employee.lastName === employeeName) {
+      return true;
+    }
+    return false;
+  });
+
+  return employeeName === undefined ? {} : employeeData[0];
 }
 
 module.exports = getEmployeeByName;
