0281a7b041789a74eb8454008325944c30163100
Lucas Rodrigues <theluks21@live.com>
2021-09-13 16:43:44 -0300

    Adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  return data.species.filter((animal) => ids.some((enterId) => enterId === animal.id));
 }
 
 module.exports = getSpeciesByIds;
