fc421b4065b63d85a0e4a34ae555e197a1ae6d9e
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-09-13 16:54:46 -0300

    refatora  getEmployeeByName.js

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,10 +2,7 @@ const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  if (!employeeName) {
-    return {};
-  }
-  return employees.find((employee) => employee.firstName === employeeName
+  return (!employeeName) ? {} : employees.find((employee) => employee.firstName === employeeName
   || employee.lastName === employeeName);
 }
 
