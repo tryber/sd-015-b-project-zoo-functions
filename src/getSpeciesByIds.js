74978aa5f37434b609746f23718121aa66e74c68
gustavogss <gustavogss.dev@gmail.com>
2021-09-13 15:21:48 -0300

    Requisito 1 - Especies por ID

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const speciesById = ids.map((id) => data.species.find((specie) => specie.id === id));
+  return speciesById;
 }
 
 module.exports = getSpeciesByIds;
