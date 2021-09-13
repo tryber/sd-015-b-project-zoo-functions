7ca5f8251e66cce14b2699591a2726ba012ccfe8
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-09-13 18:57:43 -0300

    segundo requisito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
+// anim acha o animal com o nome do parametro e depois o .residents.every retorna true
+// ou false se as idades forem maiores q a passada
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const anim = data.species.find((specie) => specie.name === animal);
+  return anim.residents.every((specie) => specie.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,10 +1,10 @@
 const data = require('../data/zoo_data');
 
-// se o parametro for algum valor
+// se o parametro for algum valor falsy retorna um obj vazio ,se nao, retorna o obj que pertence ao nome dado no parametro procurando no zoo_data
+// se o parametro é igual
 function getEmployeeByName(employeeName) {
-  if (!employeeName) {
-    return {};
-  }
+  if (!employeeName) return {};
+
   return data.employees.find((employee) => employeeName.includes(employee.lastName)
   || employeeName.includes(employee.firstName));
 }
