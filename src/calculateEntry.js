ff91bb3f0e2c07a89cd5232007384f75d3fa0dac
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-13 18:49:57 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,26 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((elem) => elem.age < 18);
+  const adult = entrants.filter((elem) => elem.age >= 18 && elem.age < 50);
+  const senior = entrants.filter((elem) => elem.age >= 50);
+  const obj = {
+    child: child.length,
+    adult: adult.length,
+    senior: senior.length,
+  };
+  return obj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === 0 || entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const priceChild = (countEntrants(entrants).child * prices.child);
+  const priceAdult = (countEntrants(entrants).adult * prices.adult);
+  const priceSenior = (countEntrants(entrants).senior * prices.senior);
+  return (priceChild + priceAdult + priceSenior);
 }
 
 module.exports = { calculateEntry, countEntrants };
