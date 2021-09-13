54d51649a9b8f83433f6e69c5dcf88bb3d2fbc1c
Rodolpho Coller <rodolpho.coller12@gmail.com>
2021-09-13 14:37:31 -0300

    implementa funçao getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(id1, id2) {
+  return data.species.filter((animals) => animals.id === id1 || animals.id === id2);
 }
 
 module.exports = getSpeciesByIds;
