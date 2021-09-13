28743598ed0dff950886174d91f306f61ae90f37
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-09-13 17:39:51 -0300

    Adiciona requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+  const { employees: people } = data;
+  const takeEmployees = people.find((employee) =>
+    employee.firstName === employeeName || employee.lastName === employeeName);
+  return takeEmployees;
 }
-
+console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
