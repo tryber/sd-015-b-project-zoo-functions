71231e4bf52af38cfe49362fcf6e94f74d7000c5
Danielen Cestari Nunes <danielen.cestari@gmail.com>
2021-09-13 16:38:53 -0300

    requisito 02 do projeto

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+console.log(species);
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+// todas as species - dentro tem o name da especie tem os residentes e dentro tem o name/age de cada animal
+// a partir do nome de uma espécie e uma idade mínima, verifica se todos os animais daquela espécie possuem a idade mínima especificada.
+  const nomeSpecie = species.find((specie) => specie.name === animal);
+  return nomeSpecie.residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
