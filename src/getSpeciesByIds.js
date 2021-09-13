46cf1f978ce0112d701495e159c688dbf7b1cf03
Wander Luiz <wander.srv@gmail.com>
2021-09-13 18:57:21 -0300

    Resoluçao do requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  return species.filter((speciesId) => ids.includes(speciesId.id));
 }
 
 module.exports = getSpeciesByIds;
