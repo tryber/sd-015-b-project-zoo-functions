7f304fb4118fb07494ccd158683ef4d3ae1d4817
Rodrigo Lima <rodrigootavio19@gmail.com>
2021-09-13 00:05:11 -0400

    finaliza getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,15 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) return ({});
+
+  const result = data.employees.find(({ firstName, lastName }) => {
+    const verifyEmployee = firstName === employeeName || lastName === employeeName;
+
+    return verifyEmployee;
+  });
+
+  return result;
 }
 
 module.exports = getEmployeeByName;
