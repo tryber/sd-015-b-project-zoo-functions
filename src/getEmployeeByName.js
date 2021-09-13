3842c1f8a20b422c290a482785754370fad8e979
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-09-13 16:04:37 -0300

    Realiza requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  let returnObject = {};
+  data.employees.forEach((element) => {
+    const { firstName, lastName } = element;
+    if (firstName === employeeName || lastName === employeeName) {
+      returnObject = element;
+    }
+  });
+  return returnObject;
 }
 
 module.exports = getEmployeeByName;
