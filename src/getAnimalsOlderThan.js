be1f6e4e1b4010166b56f8176b7c71d4ab406774
Camilla Del Guerso <camilladelguerso@gmail.com>
2021-09-13 14:44:23 -0300

    Adiciona requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species.find(({ name }) => name === animal)
+    .residents.every((elemento) => elemento.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
