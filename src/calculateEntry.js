dc6d81e40fd4c3187403896b7d7bcb3c58dfdad9
GabrielJorge <gabrieljorge.ca@gmail.com>
2021-09-13 17:24:47 -0400

    add function calculateEntry.js

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,45 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
+const getSenior = (entrantSenior) => {
+  const seniors = entrantSenior.filter((senior) => senior.age >= 50);
+  return seniors.length;
+};
+
+const getAdult = (entrantAdult) => {
+  const adults = entrantAdult.filter(
+    (adult) => adult.age >= 18 && adult.age < 50,
+  );
+  return adults.length;
+};
+
+const getChildren = (entrantChildren) => {
+  const children = entrantChildren.filter((child) => child.age < 18);
+  return children.length;
+};
+
+function countEntrants(entrantPerson) {
+  return {
+    child: getChildren(entrantPerson),
+    adult: getAdult(entrantPerson),
+    senior: getSenior(entrantPerson),
+  };
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(personEntrant) {
+  if (!personEntrant || Object.values(personEntrant).length === 0) {
+    return 0;
+  }
+  const {
+    child: children,
+    adult: adults,
+    senior: seniors,
+  } = countEntrants(personEntrant);
+  const { child, adult, senior } = prices;
+  const result = children * child + adult * adults + seniors * senior;
+  return result;
 }
+
+console.log(calculateEntry());
 
 module.exports = { calculateEntry, countEntrants };
