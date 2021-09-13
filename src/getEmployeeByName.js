182d6174361e126ac2bb387ec67e95dccd01c463
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-13 17:37:08 -0300

    Altera complexidade da funçao.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,27 +1,24 @@
 const data = require('../data/zoo_data');
 const { employees } = require('../data/zoo_data');
 
-function getEmployeeByName(...employeeName) {
+function getEmployeeByName(employeeName) {
   // seu código aqui
   let colaboradores = {};
   if (employeeName === undefined || employeeName === '') return {};
 
   employees.filter( (employe) => {
-    employeeName.forEach( (name) => {
-      if (employe.firstName === name || employe.lastName === name) {
-        colaboradores = employe;
-      }
-      return colaboradores;
-    });
-    return colaboradores;  
-  })
+    if (employe.firstName === employeeName || employe.lastName === employeeName) {
+      colaboradores = employe;
+    }
+    return colaboradores;
+  });
   return colaboradores;
-  // return employeeName;
-}
+  }
 
 // getEmployeeByName('Teste');
 // console.log(getEmployeeByName('Emery', 'Nelson', 'Bethea', 'Wilburn'));
-console.log(getEmployeeByName('Emery'));
+// console.log(getEmployeeByName('Emery')); // [DEBUG] Realiza Pesquisa pelo Nome
+// console.log(getEmployeeByName('Elser')); // [DEBUG] Realiza Pesquisa pelo Sobrenome
 
 module.exports = getEmployeeByName;
 
@@ -30,4 +27,4 @@ module.exports = getEmployeeByName;
 // algum elemento com o devido nome e/ou sobrenome.
 // ** Depois de filtrados e armazenados no array, devemos retornar o array com os nomes dos
 // Colaboradores encontrados com estas características
-// ** 
+// **
