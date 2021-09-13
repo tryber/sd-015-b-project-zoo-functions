00af98cb3401ff6e4022b5ea832771fcd331d5f9
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-13 20:05:39 -0300

    Adiciona id dos managers na funçao isManager.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  const managers =
+  [
+    '9e7d4524-363c-416a-8759-8aa7e50c0992',
+    'fdb2543b-5662-46a7-badc-93d960fdc0a8',
+    '0e7b460e-acf4-4e17-bcb3-ee472265db83'
+  ];
   const idIsManager = managers.includes(id);
   return idIsManager;
 }
