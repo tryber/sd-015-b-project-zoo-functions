9b9ca1424a4e9048e451d926ef3f7cf894c765dd
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2021-09-13 19:17:45 -0300

    função getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animais = species.find((specie) => specie.name === animal);
+  return animais.residents.every((resid) => resid.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
