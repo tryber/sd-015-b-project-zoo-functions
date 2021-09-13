a6c5cbbf71b87b26f2a4ccc002d328541ec87bef
Samuel Pereira <gmr.quest.10@gmail.com>
2021-09-13 15:54:00 -0300

    Corrige requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,10 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return data.species.filter((especie, index) => {
-    if (!ids) return [];
-    if (especie.id === ids[index]) return especie;
-  });
+  return data.species.filter((especie, index) =>
+    especie.id === ids[index]).map((especie) => especie);
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
+  'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 module.exports = getSpeciesByIds;
