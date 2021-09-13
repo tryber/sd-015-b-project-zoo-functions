578e2a95a0ab6f86a32867f9c1cf0cd5e3f9d120
Guilherme Saboia <gsaboia@yahoo.com>
2021-09-13 19:12:45 -0300

    Adiciona requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  // gets id of first species that employee is responsible for
+  const employeeAnimal = data.employees.find((element) => element.id === id).responsibleFor[0];
+  // gets species that employee is responsible for
+  const managed = data.species.find((animal) => animal.id === employeeAnimal);
+  // gets oldest resident from species
+  const members = managed.residents;
+  const oldest = members.reduce((accum, currEl) => {
+    let result = 0;
+    if (accum.age > currEl.age) {
+      result = accum;
+    } else {
+      result = currEl;
+    }
+    return result;
+  });
+  const { name, sex, age } = oldest;
+  const myReturn = [name, sex, age];
+  return myReturn;
 }
 
 module.exports = getOldestFromFirstSpecies;
