c7be57a596358ecc755825e65cf6a52317dd1bc4
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-09-13 17:03:52 -0300

    Adiciona requisitos 2 e 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,8 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const specieObject = data.species.find(({ name }) => name === animal);
+  return specieObject.residents.every((elementOfResident) => elementOfResident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
