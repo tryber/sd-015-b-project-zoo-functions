28d97978a882a55d4c5e21af9347da0d60765fa5
Antonio Souza <antoniofelipe2001@hotmail.com>
2021-09-13 15:12:31 -0300

    Requisito 4 finalizado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  if (data.employees.find((employee) => employee.managers.find((managerId) => managerId === id))) {
+    return true;
+  }
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const responsibleObj = data.employees.filter((employee) => {
+      return employee.managers.find((manager) => manager === managerId)
+    });
+    const responsibleStr = responsibleObj.map((obj) => `${obj.firstName} ${obj.lastName}`)
+    return responsibleStr
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!')
 }
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992'
+console.log(getRelatedEmployees(stephanieId))
 
 module.exports = { isManager, getRelatedEmployees };
