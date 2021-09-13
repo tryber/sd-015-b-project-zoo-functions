19411027edc5794dc06a704aa4286eb0ca3d818d
DanielHott <daniel_hott@outlook.com>
2021-09-13 18:37:15 -0300

    Adicionando primeiro requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,18 @@
+/* eslint-disable array-callback-return */
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const especies = data.species;
+function getSpeciesByIds(...ids) {
+  const resultado = [];
+  ids.forEach((acc) =>
+    resultado.push(
+      especies.find((id) => {
+        if (id.id === acc) {
+          return id;
+        }
+      }),
+    ));
+  return resultado;
 }
 
 module.exports = getSpeciesByIds;
