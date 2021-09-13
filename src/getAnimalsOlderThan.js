e8d55a7fc85ab6eee4a81efb3ef6a76e05aa41b5
Jesse Benevides <jessebenevides@gmail.com>
2021-09-13 18:29:45 -0300

    Adiciona requisito 1 - getSpeciesByIds.js

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const objAnimal = species.find((specie) => specie.name === animal);
+  const { residents } = objAnimal;
+
+  const result = residents.every((resident) => resident.age >= age);
+
+  return result;
 }
+
+console.log(getAnimalsOlderThan('penguins', 10));
 
 module.exports = getAnimalsOlderThan;
