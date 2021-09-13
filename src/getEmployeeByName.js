288d50f578b8f53b5557f28a286283e41f09258a
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-09-13 19:35:14 -0300

    Refatora requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,10 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
   if (employeeName === undefined) return {};
-  const { employees: people } = data;
-  const takeEmployees = people.find((employee) =>
+
+  const takeEmployees = employees.find((employee) =>
     employee.firstName === employeeName || employee.lastName === employeeName);
+
   return takeEmployees;
 }
 
