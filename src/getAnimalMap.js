87ad849ebf52c27431a2968bedd88d67e2e3e797
David Souza <dabcs@poli.ufrj.br>
2021-09-13 15:08:52 -0300

    envio para avaliação

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,21 +1,31 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-// const includeNames = () => {
-//   const objLoc = { NE: [], NW: [], SE: [], SW: [] };
-//   objLoc.NE = species.filter((specie) => specie.location === 'NE').map(({ name, residents }) => {
-//     const obj = {};
-//     obj[name] = residents;
-//     return obj;
-//   });
-// };
-
-function getAnimalMap(options) {
+const includeNames = () => {
   const objLoc = { NE: [], NW: [], SE: [], SW: [] };
   Object.keys(objLoc).forEach((element) => {
-    objLoc[element] = species.filter((specie) => specie.location === element);
+    objLoc[element] = species.filter((specie) => specie.location === element)
+      .map(({ name, residents }) => {
+        const obj = {};
+        obj[name] = residents.name;
+        return obj;
+      });
   });
   return objLoc;
+};
+
+const byLocation = () => {
+  const objLoc = { NE: [], NW: [], SE: [], SW: [] };
+  Object.keys(objLoc).forEach((element) => {
+    objLoc[element] = species.filter((specie) => specie.location === element)
+      .map(({ name }) => name);
+  });
+  return objLoc;
+};
+
+function getAnimalMap(options) {
+  if (options.includeNames) { return includeNames(); }
+  return byLocation();
 }
 
 module.exports = getAnimalMap;
