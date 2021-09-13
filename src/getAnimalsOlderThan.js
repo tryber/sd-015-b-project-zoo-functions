8f7a490a3f76719e911c7c1f29484013ccb7af23
José Erivam <erivamdev@gmail.com>
2021-09-13 18:10:08 -0300

    adding requisite 3 and repair requisite 2(includes)

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,7 +2,7 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  const findAnimal = data.species.find((element) => element.name === animal);
+  const findAnimal = data.species.find((element) => element.name.includes(animal));
   const everyAnimal = findAnimal.residents.every((element) => element.age > age);
   return everyAnimal;
 }
