8465a177223984bfe0c6fae19600b156297d972a
assisMeneghetti <assismfabri@hotmail.com>
2021-09-13 15:05:37 -0300

    adiciona requisito 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,16 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// Iniciei porém não conclui
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const test = species.filter((specie) => specie.name === animal);
+  const test2 = test.every((specie, index) => {
+    const animals = specie.residents;
+    return animals[index].age >= age;
+  });
+  return test2;
 }
+
+console.log(getAnimalsOlderThan('penguins', 10));
 
 module.exports = getAnimalsOlderThan;
