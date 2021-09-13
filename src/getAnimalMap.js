1d1693bbb3d73923eab6499857d4f277d3695d43
alvesgf16 <alvesgabriel1994@gmail.com>
2021-09-13 19:47:11 -0300

    Implementa a funçao getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,50 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function generateResidentsListPerSpecies(options, oneSpecies) {
+  let residentsList = oneSpecies.residents;
+  if (Object.keys(options).includes('sex')) {
+    residentsList = residentsList.filter((resident) => resident.sex === options.sex);
+  }
+
+  return residentsList;
 }
+
+function generateNamesListPerSpecies(options, oneSpecies) {
+  const result = {};
+
+  const residentsList = generateResidentsListPerSpecies(options, oneSpecies);
+
+  const namesList = residentsList.map((resident) => resident.name);
+  if (options.sorted === true) namesList.sort();
+
+  result[oneSpecies.name] = namesList;
+
+  return result;
+}
+
+function getOptions(options, oneSpecies) {
+  if (!options || !options.includeNames) {
+    return oneSpecies.name;
+  }
+
+  return generateNamesListPerSpecies(options, oneSpecies);
+}
+
+function getAnimalMap(options) {
+  const { species } = data;
+  const locations = { NE: [], NW: [], SE: [], SW: [] };
+
+  Object.keys(locations).forEach((location) => {
+    species.forEach((oneSpecies) => {
+      if (oneSpecies.location === location) {
+        locations[location].push(getOptions(options, oneSpecies));
+      }
+    });
+  });
+
+  return locations;
+}
+
+console.log(getAnimalMap({ includeNames: true, sex: 'female' }));
 
 module.exports = getAnimalMap;
