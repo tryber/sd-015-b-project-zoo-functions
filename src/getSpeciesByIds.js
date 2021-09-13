b390378e98ccd9eade0259a4934aa8142b084c41
José Erivam <erivamdev@gmail.com>
2021-09-13 16:28:58 -0300

    repair requisite 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -5,6 +5,5 @@ function getSpeciesByIds(...ids) {
   return data.species.filter((specie, i) => specie.id === ids[i]);
   // Fim do código
 }
-console.log(getSpeciesByIds());
-//console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+
 module.exports = getSpeciesByIds;
