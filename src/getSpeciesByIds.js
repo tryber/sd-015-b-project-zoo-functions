483a6a24abce66ea3502016efbc023701fd05e45
Samuel Pereira <gmr.quest.10@gmail.com>
2021-09-13 17:15:19 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,15 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  function especieById(especie, index) {
-    if (!ids) return [];
-    if (especie.id === ids[index]) return especie;
-  }
-
-  return data.species.filter(especieById);
+  return ids.map((id) => data.species.find((specie) => specie.id === id));
 }
-
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
-  'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 module.exports = getSpeciesByIds;
