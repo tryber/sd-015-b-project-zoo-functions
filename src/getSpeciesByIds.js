af0c1ca2e168ecdc15a079c79e2dabd24ac1bb25
ArthurMoreira <arthurmcs96@gmail.com>
2021-09-13 17:57:47 -0300

    Adiciona o requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,10 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  ids.forEach((id) => {
-    const animals = data.species.filter((specie) => id === specie.id);
-    return animals;
-  });
+  const animals = ids.map((id) => data.species.find((specie) => id === specie.id));
+  return animals;
 }
 
 module.exports = getSpeciesByIds;
