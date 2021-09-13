403337cd9cab6570e797a7cba75838b84ddafcae
Lucas Maieski Marques <lucmaieski@gmail.com>
2021-09-13 18:26:27 -0300

    Feito o primeiro requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // começando projeto
+function getSpeciesByIds(...ids) {
+  if (ids === undefined) {
+    return [];
+  }
+  return ids.map((id) => species.find((animal) => animal.id === id));
 }
 
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
