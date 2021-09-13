b86a57e8039b76e6b93271e10e204d27b52c284d
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-09-13 15:33:06 -0300

    Adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
-  
+  if (!ids) return []; // retorna o array vazio caso receba nenhum parâmetro
+  // console.log(ids);
+  return data.species.filter(({ id }) => ids.some((element) => element === id));
 }
 
+// getSpeciesByIds(data.species[0].id);
 module.exports = getSpeciesByIds;
