7b349fbb44f0a0864607679a2ce51fd6173e9837
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-13 17:59:46 -0300

    Adiciona logica do requisito04.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -9,3 +9,12 @@ function getRelatedEmployees(managerId) {
 }
 
 module.exports = { isManager, getRelatedEmployees };
+
+// LÓGICA DO DESENVOLVIMENTO:
+// ** Recebe a chamada da função com a Id de um funcionário do Zoológico.
+// ** Verifica, através de uma nova função, se aquela Id corresponde a Id de uma pessoa Gerente do Zoológico.
+// Se sim, a função retorna true, se não, retorna false;
+// ** Com base no retorno dessa função secundária, a função primária verifica se o mesmo é true ou false
+// ** Se True, Executa o Array.map, que irá buscar os funcionários gerenciados pela pessoa informada no campo responsibleFor
+// Retornando um Array com os funcionários que essa pessoa gerencia
+// ** Se false, retorna uma mensagem no formado: "O id inserido não é de uma pessoa colaboradora gerente!"
