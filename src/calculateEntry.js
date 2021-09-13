bdfd4c8f9439eaa221ed6acb3ce0be239727a20c
iuryroque <iury.roque@hotmail.com>
2021-09-13 20:38:48 -0300

    Requisito 6 finalizado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age < 18);
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50);
+  const senior = entrants.filter((entrant) => entrant.age >= 50);
+  return { child: child.length, adult: adult.length, senior: senior.length };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  let total = 0;
+  if (!entrants || entrants.length === undefined) return total;
+  const inners = countEntrants(entrants);
+  total = inners.child * 20.99 + inners.adult * 49.99 + inners.senior * 24.99;
+  return total;
 }
-
 module.exports = { calculateEntry, countEntrants };
