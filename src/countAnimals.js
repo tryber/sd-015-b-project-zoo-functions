8de72dc2ee3e0a501831f9976f0a8599bfce8adc
Andre Luiz <andreluizgyn1@gmail.com>
2021-09-13 20:13:46 -0300

    Adciona requisitos 4 e 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,30 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function countAllAnimals() {
+  const animals = {};
+  species.forEach((animal) => {
+    animals[animal.name] = animal.residents.length;
+  });
+  return animals;
+}
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return countAllAnimals();
+  }
+  if (animal.gender === undefined) {
+    const specie = species.find((spec) => spec.name === animal.specie);
+    return specie.residents.length;
+  }
+  let count = 0;
+  const especie = species.find((specie1) => specie1.name === animal.specie);
+  especie.residents.forEach((resident) => {
+    if (resident.sex === animal.gender) {
+      count += 1;
+    }
+  });
+  return count;
 }
 
 module.exports = countAnimals;
