2b2720ed930c0db17a9f015e9ff7afe4619b3ffc
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2021-09-13 15:13:20 -0300

    requesito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,6 +3,9 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   return species.filter((animal) => ids.some((id) => animal.id === id));
+//  ids.some cada elemento recebido do ids sera passado como parametro (id)
+//  assim ele vai comparar se algum id é igual ao id recebido do animal que é
+//  nesse casa cada parametro recebido do especies.
 }
 
 module.exports = getSpeciesByIds;
