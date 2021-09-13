8117ae959e7f3ab7d99e3053c7dab16056554749
Marina Marques <marina.marqueso@outlook.com>
2021-09-13 16:53:25 -0300

    Adiciona primeiro requisitogit branch

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+// primeiro encontrar o id
+    const find = species.filter((elemento) => ids.includes(elemento.id));
+// imprimir o elemento
+    return find;
 }
 
 module.exports = getSpeciesByIds;
 
 //primeiro commit
+
