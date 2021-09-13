542c37efc10f27d2a561ef929a8f02f412e03007
Gabrielle-Murat <belle.murat@gmail.com>
2021-09-13 20:53:55 -0300

    Adiciona o requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const check = isManager(managerId);
+
+  if (check === true) {
+    const collaborators = data.employees.filter((employee) =>
+      employee.managers.includes(managerId));
+    return (collaborators.map((collabName) => `${collabName.firstName} ${collabName.lastName}`));
+  }
+
+  if (check === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 }
+getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992');
 
 module.exports = { isManager, getRelatedEmployees };
