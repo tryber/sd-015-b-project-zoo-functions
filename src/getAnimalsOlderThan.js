442b327e6e83224851b3328e4614c8d741277b3a
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-13 16:20:08 -0300

    Adiciona logica do requisito02.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -5,3 +5,10 @@ function getAnimalsOlderThan(animal, age) {
 }
 
 module.exports = getAnimalsOlderThan;
+
+// LÓGICA DO DESENVOLVIMENTO:
+// ** Receber na função, o nome de um Animal e uma idade mínima
+// ** Verificar se todos os animais daquela espécie {residents} possuem, no mínimo, aquela idade {redisend.age}
+// ** if (resident.age > age) // Se a idade do resident é maior ou igual que a passada por parâmetro
+// ** Retornar True
+// ** Se não, retornar false (Fora do laço if)
