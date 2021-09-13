41880c9deaca7a39d9f87f6762f9e8be65f6a27a
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-09-13 19:30:31 -0300

    Refatora requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,17 +1,16 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  const { employees: people } = data;
+const { employees } = data;
 
-  const confere = people.some((employee) => employee.managers.includes(id));
+function isManager(id) {
+  const confere = employees.some((employee) => employee.managers.includes(id));
 
   return confere;
 }
 
 function getRelatedEmployees(managerId) {
-  const { employees: people } = data;
   if (isManager(managerId) === true) {
-    const percorre = people.filter((employee) => employee.managers.includes(managerId))
+    const percorre = employees.filter((employee) => employee.managers.includes(managerId))
       .map((atualEmployee) => `${atualEmployee.firstName} ${atualEmployee.lastName}`);
     return percorre;
   } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
