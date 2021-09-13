10d7e81d33f95f73a6581513387a83e9e94bbf64
Rafael Colombo <rcolombodev@gmail.com>
2021-09-13 13:54:43 -0300

    Resolve Requisito 3

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -6,4 +6,5 @@ function getSpeciesByIds(...ids) {
   return species.filter((animal) => ids.some((id) => animal.id === id));
 }
 
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
 module.exports = getSpeciesByIds;
