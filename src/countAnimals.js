f44e5b7e18cb82966b5391742b2bcd84bb9a45b1
gustavogss <gustavogss.dev@gmail.com>
2021-09-13 17:01:49 -0300

    Requisito 5 - contagem

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals({ specie = undefined, gender = false } = {}) {
+  if (specie === undefined) {
+    return species.reduce((total, { name, residents }) => {
+      Object.assign(total, { [name]: residents.length });
+      return total;
+    }, {});
+  }
+  if (!gender) {
+    return species.find((element) => element.name === specie).residents.length;
+  }
+  return species.find((element) => element.name === specie).residents.filter((element) =>
+    element.sex === gender).length;
 }
 
 module.exports = countAnimals;
