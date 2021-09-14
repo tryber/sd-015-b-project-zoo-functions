06442feab3b2c09238184f46597e0fa2808c0a29
douglas <douglasmonteiro.m@gmail.com>
2021-09-14 17:02:49 -0300

    refatora requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,13 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   if (ids.length === 0) return [];
-  return ids.map((id) => data.species.filter((specie) => specie.id === id));
+  const resultado = [];
+  ids.forEach((id) => {
+    resultado.push(data.species.find((specie) => specie.id === id));
+  });
+  return resultado;
 }
+
+console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed'));
 
 module.exports = getSpeciesByIds;
