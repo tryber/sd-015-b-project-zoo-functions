1ab09f0efd53606e562aba7b5dde7351dc3afa1a
Amanda Fernandes <aferanda@gmail.com>
2021-09-13 21:28:07 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter(({ age }) => age < 18).length;
+  const adult = entrants.filter(({ age }) => age >= 18 && age < 50).length;
+  const senior = entrants.filter(({ age }) => age >= 50).length;
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  let { child, adult, senior } = prices;
+  child *= countEntrants(entrants).child;
+  adult *= countEntrants(entrants).adult;
+  senior *= countEntrants(entrants).senior;
+  return child + adult + senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
