9e869e922500c9787fd5cc11a7607554ee704393
Rodolfomno <Rodolfo_mno@hotmail.com>
2021-09-13 21:40:21 -0300

    adiciona resolucao requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(ids, ids2) {
   // seu código aqui
+  if (!ids) return [];
+  const retornoId = [];
+  retornoId.push(species.find((especie) => especie.id === ids));
+  if (!ids2) {
+    return retornoId;
+  }
+  retornoId.push(species.find((especie) => especie.id === ids2));
+  return retornoId;
 }
 
 module.exports = getSpeciesByIds;
