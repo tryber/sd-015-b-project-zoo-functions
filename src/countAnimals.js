5f0a5e5e82590cfa82963864a52ba7228bf1b421
Arthur Bernardo Messias <turmessias@gmail.com>
2021-09-14 15:57:11 -0300

    Resolve requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce(
+      (acc, current) => ({ ...acc, [current.name]: current.residents.length }),
+      {},
+    );
+  }
+  if (animal.specie && animal.gender) {
+    const genderAnimal = species
+      .find((element) => element.name === animal.specie)
+      .residents.filter((element) => element.sex === animal.gender);
+    return genderAnimal.length;
+  }
+  const animalObject = species.find(
+    (element) => element.name === animal.specie,
+  ).residents;
+  return animalObject.length;
 }
 
 module.exports = countAnimals;
