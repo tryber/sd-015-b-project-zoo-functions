94fcc63a39c762a315b6a45e4f82c7975c36fac3
Adriana <adrianam754@gmail.com>
2021-09-13 22:40:49 -0300

    Adicionada requisito 1, 2 e 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+    const consultarSpecie = species.find((specie) => specie.name === animal);
+  
+    const checkAge = consultarSpecie.residents.every((resident) => resident.age >= age);
+    return checkAge;
+  }
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// Requisito feito junto com Amanda turma 15 B
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const checkName = employees.find(({ firstName, lastName }) =>
+    employeeName === firstName || employeeName === lastName);
+
+  return !employeeName ? {} : checkName;
 }
 
 module.exports = getEmployeeByName;
