4b753056ba4149d3f536ed37ff6247cc216dc9e0
Wilian Pereira <wilian_pereira92@outlook.com>
2021-09-14 11:40:24 -0300

    adicionado requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,15 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+
+  const funcionario = data.employees.find((responsavel) => responsavel.id === id);
+  const idAnimal = funcionario.responsibleFor[0];
+  const fichaDaEspecie = data.species.find((especie) => especie.id === idAnimal);
+  const maiorIdade = fichaDaEspecie.residents.map((ano) => ano.age);
+  maiorIdade.sort((a, b) => b - a);
+  const infomacaoDosAnimais = fichaDaEspecie.residents.find((idade) => idade.age === maiorIdade[0]);
+  const resultado = [infomacaoDosAnimais.name, infomacaoDosAnimais.sex, infomacaoDosAnimais.age];
+  return resultado;
 }
 
 module.exports = getOldestFromFirstSpecies;
