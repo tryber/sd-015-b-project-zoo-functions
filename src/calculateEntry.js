2906378d2c5cc55a7a0247469fe511c0f3e62c1f
henrique <hickm018@gmail.com>
2021-09-14 12:29:19 -0300

    Adiciona requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,35 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const newObject = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      newObject.child += 1;
+    } else if (element.age >= 18 && element.age < 50) {
+      newObject.adult += 1;
+    } else {
+      newObject.senior += 1;
+    }
+  });
+  return newObject;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const entryes = countEntrants(entrants);
+  let valueEntry = 0;
+  valueEntry += entryes.child * prices.child;
+  valueEntry += entryes.adult * prices.adult;
+  valueEntry += entryes.senior * prices.senior;
+  return valueEntry;
 }
 
 module.exports = { calculateEntry, countEntrants };
+console.log(calculateEntry({}));
