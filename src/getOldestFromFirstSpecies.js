52419fe5c8e2f10e437bbc8e252747bac5a0e0c9
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-14 15:59:16 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animal = employees.find((elem) => elem.id === id).responsibleFor[0];
+  const obj = species.find((specie) => specie.id === animal).residents;
+  const calculateAge = obj.map((elem) => elem.age).reduce((acc, value) => Math.max(acc, value));
+  return obj.reduce((a, { name, age, sex }) => (age === calculateAge
+    ? [name, sex, age] : a), []);
 }
 
 module.exports = getOldestFromFirstSpecies;
