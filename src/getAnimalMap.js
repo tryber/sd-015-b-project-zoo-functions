29f345570b24589d0a410409994efdc08e387d8c
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-09-14 16:33:36 -0300

    Pull do master para corrigir erro

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,14 +1,40 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function animalsSortedByLocation() {
   return data.species.reduce((acc, spc) => {
-    const { location } = spc;
-    const { name } = spc;
-    acc[location] = name;
+    acc[spc.location] = data.species
+      .filter((spc2) => spc2.location === spc.location)
+      .map((spc3) => spc3.name);
     return acc;
   }, {});
 }
+function accRHSFunc(spc) {
+  const nameRHS = data.species
+    .filter((spc2) => spc2.location === spc.location)
+    .map((obj) => obj.residents
+      .map((residents) => residents.name));
+  return nameRHS;
+}
 
-console.log(getAnimalMap());
+function animalsSortedIncludeName() {
+  return data.species.reduce((acc, spc) => {
+    const accRHS = accRHSFunc(spc);
+    acc[spc.location] = accRHS;
+    return acc;
+  }, {});
+}
+function getAnimalMap(options) {
+  // seu código aqui
+  if (!options) {
+    const animalMap = animalsSortedByLocation();
+    return animalMap;
+  }
+
+  if (options.sorted === true) {
+    const animalMap = animalsSortedIncludeName();
+    return animalMap;
+  }
+}
+
+console.log(getAnimalMap({ sorted: true }));
 module.exports = getAnimalMap;
