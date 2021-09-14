aeb38fe1ed1a392dec31df3e05981471dbc5b970
Amanda Fernandes <aferanda@gmail.com>
2021-09-14 17:21:41 -0300

    Refatora requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -10,13 +10,13 @@ const findResidents = (animal) => species
   .find((specie) => specie.name === (animal.specie)).residents;
 
 const filterGender = (animal) => findResidents(animal)
-  .filter((resident) => resident.sex === (animal.gender));
+  .filter((resident) => resident.sex === (animal.sex));
 
 function countAnimals(animal) {
   if (!animal) {
     return verifyParameters();
   }
-  return animal.gender
+  return animal.sex
     ? filterGender(animal).length
     : findResidents(animal).length;
 }
