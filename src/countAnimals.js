0a3c09c3dec4531284531fa691de6bbb620a13be
Flávio Bianchetti <fjb.developer@gmail.com>
2021-09-14 15:55:24 -0300

    Atendido requisito 5 - validado via npm

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,50 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const ruleUndefined = (acc, specie) => {
+  acc[specie.name] = specie.residents.length;
+  return acc;
+};
+
+const ruleSpecie = (acc, specie, spec) =>
+  specie.name === spec ? specie.residents.length : acc;
+
+const ruleSpecieAndGender = (acc, specie, spec, gen) =>
+  specie.name === spec ?
+    specie.residents.reduce((acc, resident) =>
+      resident.sex === gen ?
+        acc += 1 :
+          acc, 0) :
+            acc;
+
+const myReduce = (spec, gen, func, defaultValue) =>
+  data.species.reduce((acc, specie) => {
+    acc = func(acc, specie, spec, gen);
+    return acc;
+  }, defaultValue);
+
+
+  const animalNotFound = () =>
+    myReduce(undefined, undefined, ruleUndefined, {});
+
+  const animalFound = (specie, gender) => {
+    let result;
+    if (gender === undefined) {
+      result = myReduce(specie, gender, ruleSpecie, 0);
+    } else {
+      result = myReduce(specie, gender, ruleSpecieAndGender, 0);
+    }
+    return result;
+  };
+
+const countAnimals = (...animal) => {
+  let result = {};
+  if (animal.length === 0) {
+    result = animalNotFound();
+  } else {
+    const {specie, gender} = animal[0];
+    result = animalFound(specie, gender);
+  }
+  return result;
+};
 
 module.exports = countAnimals;
