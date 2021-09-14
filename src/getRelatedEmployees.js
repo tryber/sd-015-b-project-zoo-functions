b7ba9536a24858fe9c403a5f1168d0804e2ae173
André E. Souto <andreesouto@hotmail.com>
2021-09-14 17:49:59 -0300

    adicionou requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((empregado) => empregado.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const gerente = isManager(managerId);
+  if (gerente === true) {
+    const colaboradores = data.employees.filter((empregado) =>
+      empregado.managers.includes(managerId));
+    const resposta = colaboradores.map((nomeCompletoColab) =>
+      `${nomeCompletoColab.firstName} ${nomeCompletoColab.lastName}`);
+    return resposta;
+  }
+  if (gerente === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
