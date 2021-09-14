4c40267f92c791acb77265c08b6d9e611ee8e881
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-14 11:31:24 -0300

    Reinicia implementaçao da funçao

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -4,7 +4,7 @@ function countEntrants(entrants) {
   // seu código aqui
   entrants.reduce((acc, entrant) => {
     if (entrant.age < 18) return { ...acc, child: acc.child += 1 };
-    if (entrant.age >= 18 && ageEntrant < 50) return { ...acc, adult: acc.adult += 1 };
+    if (entrant.age >= 18 && entrant.age < 50) return { ...acc, adult: acc.adult += 1 };
     if (entrant.age >= 50) {return { ...acc, senior: acc.senior += 1 } }
   }, {adult: 0, child: 0, senior: 0});
 }
@@ -12,22 +12,8 @@ function countEntrants(entrants) {
 function calculateEntry(entrants) {
   // seu código aqui
   let valorTotal = 0;
-  const arrayNovo = [];
   if (!entrants || Object.keys(entrants).length === 0) return 0;
-  // const functionEntrants = entrants.forEach( (entrant) => { // Para cada pessoa no Array Recebido
-  //   arrayNovo.push(entrant.age); // Capture sua idade
-  //   // console.log(arrayNovo); // [DEBUG] Verifica entrada do Array
-  // });
-  const contagem = countEntrants(entrants); // Chama a função que irá contar quantas pessoas de cada faixa etária entrarão,
-  // console.log(contagem); // [DEBUG] Verifica se o retorno é o esperado, com o formato correto.
-  // passando como parâmetro, o valor do array com a idade de Cada um.
-  const valorAdulto = 49.99;
-  const valorChild = 20.99;
-  const valorSenior = 24.99;
-  return (contagem.adult * valorAdulto) + (contagem.child * valorChild) + (contagem.senior * valorSenior);
-  // console.log(contagem.adult, contagem.child, contagem.senior);
-  // console.log(valorTotal);
-  // return valorTotal;
+  
 }
 
 const pessoasVisitantes = [
