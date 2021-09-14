4aa578897560c936584ae9e2550c237fa437b3a9
Rodrigo Lima <rodrigootavio19@gmail.com>
2021-09-13 23:13:30 -0400

    adiciona comentario dando credito ao colega (requisito 4)

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -3,6 +3,8 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  // Ideia para forma de verificação do isManager veio de dica do colega Erickson Siqueira,
+  // em uma thread no slack aberta no dia do projeto
   const verifyManager = employees.some(({ managers }) => managers.includes(id));
 
   return verifyManager;
