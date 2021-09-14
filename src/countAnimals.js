ed269c08ed2b159e2d7f92705d719bf87058beed
Richard Leal <rwsleal@gmail.com>
2021-09-14 20:40:21 -0300

    Removendo os console.log do requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -6,20 +6,16 @@ data.species.forEach((animal) => {
   speciesObject[animal.name] = animal.residents.length;
   return speciesObject;
 });
-console.log(speciesObject.lions);
 
 function countAnimals(animal = speciesObject) {
   if (Object.keys(speciesObject).includes(animal.specie)) {
     if (animal.sex) {
       const animalsOfSex = species.find((subject) => subject.name === animal.specie).residents;
-      console.log(animalsOfSex);
       return animalsOfSex.filter((element) => element.sex === animal.sex).length;
     }
     return speciesObject[animal.specie];
   }
   return animal;
 }
-
-console.log(countAnimals('lions'));
 
 module.exports = countAnimals;
