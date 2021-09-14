aae7513155beb8a32287de272100b3995b80d8f3
Rodolpho Coller <rodolpho.coller12@gmail.com>
2021-09-13 23:04:16 -0300

    implementa funçao countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  let obj = {};
+  if (animal) {
+    obj = 0;
+    const findAnimal = data.species.find((animalInfo) => animalInfo.name === animal.specie);
+    if (Object.values(animal)[1] === undefined) {
+      obj = findAnimal.residents.length;
+    } else {
+      const countAnimalsBySex = findAnimal.residents.filter((a) => a.sex === animal.gender);
+      obj = countAnimalsBySex.length;
+    }
+  } else {
+    data.species.forEach((animalInfo) => {
+      obj[animalInfo.name] = animalInfo.residents.length;
+    });
+  }
+  return obj;
 }
 
 module.exports = countAnimals;
