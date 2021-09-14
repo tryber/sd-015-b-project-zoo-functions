d812f1c3a3386a3aa593512b038d189b144cf558
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-09-14 12:41:45 -0300

    correções

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,8 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+
+  return !animal ? 'true' : 'false';
 }
 
 module.exports = countAnimals;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -3,7 +3,17 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
-  return Object.values(species.filter(propriedade => propriedade.id === employees.find(propriedade => propriedade.id === id).responsibleFor[0])[0].residents.reduce((comparador, comparado) => comparador.age < comparado.age ? comparado : comparador))
+  return Object.values(
+    species
+      .filter(
+        (propriedade) =>
+          propriedade.id
+          === employees.find((animal) => animal.id === id)
+            .responsibleFor[0],
+      )[0]
+      .residents.reduce((comparador, comparado) =>
+        (comparador.age < comparado.age ? comparado : comparador)),
+  );
 }
 
 module.exports = getOldestFromFirstSpecies;
