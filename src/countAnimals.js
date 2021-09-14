d8220f4c18a43478a186c5e37626950951ada99a
Ge <gmarcoscardoso@gmail.com>
2021-09-14 12:34:53 -0300

    Requisito 05 completo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+const getAnimals = (accumulator, currentAnimal) => {
+  const nameAnimal = currentAnimal.name;
+  const lengthAnimal = currentAnimal.residents.length;
+  return { ...accumulator, [nameAnimal]: lengthAnimal };
+};
+const allSpecies = (specie) => specie.reduce(getAnimals, {});
+
+const genderCheck = (animals, animal) => ((animal === undefined) ? animals.sex
+  : animals.sex === animal);
+
 function countAnimals(animal) {
-  // seu código aqui
+  return (!animal) ? allSpecies(data.species)
+    : data.species.find((animalZoo) => animalZoo.name === animal.specie).residents
+      .filter((sa) => genderCheck(sa, animal.gender)).length;
 }
 
 module.exports = countAnimals;
