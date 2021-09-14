61b5c368d582bfdae3b4826d48ec2340e62f0bfd
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-14 11:56:15 -0300

    Corrige Lint.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -6,8 +6,8 @@ function countEntrants(entrants) {
   return entrants.reduce((acc, entrant) => {
     if (entrant.age < 18) return { ...acc, child: acc.child += 1 };
     if (entrant.age >= 18 && entrant.age < 50) return { ...acc, adult: acc.adult += 1 };
-    if (entrant.age >= 50) {return { ...acc, senior: acc.senior += 1 } }
-  }, {adult: 0, child: 0, senior: 0});
+    if (entrant.age >= 50) return { ...acc, senior: acc.senior += 1 };
+  }, { adult: 0, child: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
@@ -15,28 +15,27 @@ function calculateEntry(entrants) {
   let valorTotal = 0;
   // Se entrants for NULL, retorna 0
   if (!entrants || Object.keys(entrants).length === 0) return 0;
-  
+
   // Captura valor de cada chave retornada pela função
-  const {child, adult, senior} = countEntrants(entrants);
+  const { child, adult, senior } = countEntrants(entrants);
   // const {pChild, pAdult, pSenior} = prices;
   // console.log("Preços: ", prices);
   // console.log(prices.adult);
 
-  valorTotal = ( child * prices.child ) + ( adult * prices.adult ) + ( senior * prices.senior );
+  valorTotal = (child * prices.child) + (adult * prices.adult) + (senior * prices.senior);
   // console.log(valorTotal);
 
   return valorTotal;
 }
 
-const pessoasVisitantes = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-
+// const pessoasVisitantes = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
 // calculateEntry({ name: 'Lara Carvalho', age: 5 }, { name: 'Frederico Moreira', age: 5 });
 // calculateEntry();
 // calculateEntry(pessoasVisitantes);
