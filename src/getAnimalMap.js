c732efaaa444662836ba4731f08dabab96254883
Rodrigo Lima <rodrigootavio19@gmail.com>
2021-09-14 14:45:38 -0400

    rascunho getAnimalMap para dar o pull da master

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
+const animalsByRegion = () => {
+  const regions = {
+    NE: [],
+    NW: [],
+    SE: [],
+    SW: [],
+  };
+  data.species.forEach(({ name, location }) => {
+    regions[location].push(name);
+  });
+  return regions;
+};
+
+const verifyAnimalsByRegion = (options) => {
+  let verify = false;
+  if (!options) verify = true;
+
+  return verify;
+};
 function getAnimalMap(options) {
   // seu código aqui
+  const teste = verifyAnimalsByRegion(options);
+
+  if (teste) return animalsByRegion();
 }
 
 module.exports = getAnimalMap;
