2da4ee255d79c2a8328e34ac623dee41782c78f7
Wilian Pereira <wilian_pereira92@outlook.com>
2021-09-13 21:29:12 -0300

    Adicionado requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  return ids.map((id) => data.species.find((specie) => specie.id === id));
 }
 
 module.exports = getSpeciesByIds;
