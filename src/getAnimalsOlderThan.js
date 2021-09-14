3f1884c105fbd787eaa753762ad350a0054f33b5
Adriana <adrianam754@gmail.com>
2021-09-13 22:42:49 -0300

    Adicionada requisito 1, 2 e 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,9 +2,9 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-    const consultarSpecie = species.find((specie) => specie.name === animal);
+    const checkSpecie = species.find((specie) => specie.name === animal);
   
-    const checkAge = consultarSpecie.residents.every((resident) => resident.age >= age);
+    const checkAge = checkSpecie.residents.every((resident) => resident.age >= age);
     return checkAge;
   }
 
