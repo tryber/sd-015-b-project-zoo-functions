a8fc63fd511e8deb048f181c829f8b279a788c56
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2021-09-13 22:04:20 -0300

    comita o quarto quesito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const manager = data.employees.filter((gerente) => gerente.managers.includes(id));
+  return manager.length !== 0;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return data.employees.map((Element) => {
+      const { managers, firstName, lastName } = Element;
+      return managers.includes(managerId) ? `${firstName} ${lastName}` : false;
+    }).filter((Element) => Element !== false);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
