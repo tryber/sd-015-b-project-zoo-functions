0f3778c66a354eb8f9a1501ad6818f73a2a2616c
Marina Marques <marina.marqueso@outlook.com>
2021-09-14 16:31:18 -0300

    Tentando adequar o objeto para array

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,32 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  // checar se esse id aparece em algum managers
+  const checkManager = employees.some((elemento) => {
+    const manag = elemento.managers;
+    if (manag.includes(id)) {
+      return true
+    }
+    return false
+  })
+  return checkManager;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  // recolhe os gerentes
+  if (isManager(managerId) === true) {
+    return employees.filter((elemento) => {
+      if (elemento.managers.includes(managerId)) {
+        const arrayEmp = [];
+        return arrayEmp.push(`${elemento.firstName} ${elemento.lastName}`);
+      };
+    });
+  }
+  // recolhido do exemplo no README.md
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
+
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
