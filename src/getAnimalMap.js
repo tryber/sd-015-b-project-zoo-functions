b0feec7d6f32a15a22b437a84f89c9a13aa7617c
Rodolfo <rodolfoserralha@hotmail.com>
2021-09-14 16:45:48 -0300

    adicionando requisito 1-2-3-4-5 do requisito 07

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,39 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const expected = {
+  NE: ['lions', 'giraffes'],
+  NW: ['tigers', 'bears', 'elephants'],
+  SE: ['penguins', 'otters'],
+  SW: ['frogs', 'snakes'],
+};
+
+function getArrayAnimals() {
+  const emptyObject = {};
+  species.forEach((specie) => {
+    if (!emptyObject[specie.location]) emptyObject[specie.location] = [];
+    emptyObject[specie.location].push(specie.name);
+  });
+  return emptyObject;
+}
+
+function includeName(sorted, sex) {
+  const emptyObject = {};
+  species.forEach((specie) => {
+    if (!emptyObject[specie.location]) emptyObject[specie.location] = [];
+    let residents = [...specie.residents];
+    if (sex) residents = residents.filter((specieSex) => specieSex.sex === sex);
+    residents = residents.map((resident) => resident.name);
+    if (sorted) residents.sort();
+    emptyObject[specie.location].push({ [specie.name]: residents });
+  });
+  return emptyObject;
+}
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options) return getArrayAnimals();
+  const { includeNames, sorted, sex } = options;
+  if (includeNames) return includeName(sorted, sex);
 }
 
 module.exports = getAnimalMap;
