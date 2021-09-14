1869d13bf32fec15dff86b4a650b7ab53637e2d3
davi <davisoarescoura@gmail.com>
2021-09-14 20:44:10 +0200

    Requisito 10 do projeto concluido

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,6 +1,45 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+function animalsIdToSpecies(ids) {
+  return ids.map(
+    (id) => data.species.find((element) => element.id === id).name,
+  );
+}
+
+function animalsIdToLocations(ids) {
+  return ids.map(
+    (id) => data.species.find((element) => element.id === id).location,
+  );
+}
+
+function generalEmployeesCoverage() {
+  return data.employees.map((element) => {
+    const { id, firstName, lastName, responsibleFor } = element;
+    return {
+      id,
+      fullName: `${firstName} ${lastName}`,
+      species: animalsIdToSpecies(responsibleFor),
+      locations: animalsIdToLocations(responsibleFor),
+    };
+  });
+}
+
+function getEmployeesCoverage(obj) {
+  if (!obj) {
+    return generalEmployeesCoverage();
+  }
+  const { name, id } = obj;
+  const employeesCoverageFilter = generalEmployeesCoverage().find((element) => {
+    const { fullName: fullNameElement, id: idElement } = element;
+    const [firstNameElement, lastNameElement] = fullNameElement.split(' ');
+    return (
+      firstNameElement === name || lastNameElement === name || idElement === id
+    );
+  });
+  if (employeesCoverageFilter !== undefined) {
+    return employeesCoverageFilter;
+  }
+  throw new Error('Informações inválidas');
 }
 
 module.exports = getEmployeesCoverage;
