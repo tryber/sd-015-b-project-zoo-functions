3183fe7d99c6f7a7b2c0bc494fcbb80b092f2153
fernandamichetti <f.michetti@hotmail.com>
2021-09-14 17:28:13 -0300

    arrumando lint

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 const getOldestFromFirstSpecies = (id) => {
   const employeeName = data.employees.find((employee) => employee.id === id).responsibleFor[0];
   const oldestAnimal = data.species.find((kind) => kind.id === employeeName).residents;
-  return Object.values(oldestAnimal.reduce((acumulator, curent) => ((acumulator.age < curent.age) ? curent : acumulator)));
-}
+  return Object.values(oldestAnimal.reduce((act, cnt) => ((act.age < cnt.age) ? cnt : act)));
+};
 
 module.exports = getOldestFromFirstSpecies;
