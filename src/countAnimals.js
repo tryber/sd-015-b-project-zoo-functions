171c32fc2998e5c6afdfedb2d721ce3147990ae1
Samuel Pereira <gmr.quest.10@gmail.com>
2021-09-14 15:51:46 -0300

    Requisito 5 concluido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const result = {};
+    species.forEach((specie) => {
+      result[`${specie.name}`] = specie.residents.length;
+    });
+
+    return result;
+  }
+
+  if (animal.specie && animal.gender) {
+    return species.find((specie) =>
+      specie.name === animal.specie).residents.filter((resident) =>
+      resident.sex === animal.gender).length;
+  }
+
+  if (animal.specie) {
+    return species.find((specie) => specie.name === animal.specie).residents.length;
+  }
 }
 
 module.exports = countAnimals;
