c46d15f0b815446cfeceb861ea1f110c8338c84c
Danielen Cestari Nunes <danielen.cestari@gmail.com>
2021-09-13 23:53:42 -0300

    comentários do requisito 04 - pra entender no futuro

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -8,15 +8,16 @@ function isManager(id) {
 
 function getRelatedEmployees(managerId) {
   const gerente = data.employees.filter((id) => id.managers.includes(managerId));
+  // filtra os dados dos empregrados para ver se neles tem o id de gerente
   const listagerente = gerente.map((nome) => `${nome.firstName} ${nome.lastName}`);
+  // usa o map pra fazer a iteração, seguindo o ex da página do free code camp. não precisei usar o index.
   if (isManager(managerId) === false) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
   return listagerente;
 }
 
-// https://www.freecodecamp.org/news/array-map-tutorial/
-/*
+/* https://www.freecodecamp.org/news/array-map-tutorial/
 How to Use the Index Argument
 In addition to the value being iterated over, map takes in its index position as well.
 This is very useful if you want to perform different kinds of operations depending on the index position of the item.
@@ -24,7 +25,6 @@ In the previous example, we appended a different suffix by checking for the inde
 To find out the index position of each of our items within the array, we can do this:
 
 let fullNames = ["wonderwoman", "spiderman", "antman", "ironman"]
-
 fullNames.map(function(firstName, index) {
     console.log(${firstName} is at ${index} position) */
 
