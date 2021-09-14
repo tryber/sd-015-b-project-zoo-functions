058ec01b1ab14ad467f7a8a6974387274fcba9e4
Ismael_Luviseti <ismaelluviseti@gmail.com>
2021-09-14 11:53:16 -0300

    Requisito 2 finalizado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species.some((specie) => specie.name === animal 
+  && specie.residents.every((resident) => resident.age >= age));
 }
 
 module.exports = getAnimalsOlderThan;
