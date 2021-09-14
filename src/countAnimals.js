52beac6584f6bb7ca8a8894fe776eb5ad3105a11
LeonardoMonti <contatoleomonti@gmail.com>
2021-09-13 22:49:51 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const amount = {};
+    species.forEach((specie) => {
+      amount[specie.name] = specie.residents.length;
+    });
+    return amount;
+  }
+  const animalName = species
+    .find((specieName) => specieName.name === animal.specie);
+
+  if (animal.gender) {
+    const animalGender = animalName.residents
+      .filter((specieGender) => specieGender.sex === animal.gender);
+
+    return animalGender.length;
+  }
+
+  return animalName.residents.length;
 }
+
+// console.log(countAnimals({ specie: 'elephants', gender: 'male' }));
 
 module.exports = countAnimals;
