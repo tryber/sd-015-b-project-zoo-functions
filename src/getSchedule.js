18e4057cc77703a20457a272c900cf9cd916acb7
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-09-14 19:26:57 -0300

    finaliza funcao getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,12 +1,25 @@
 const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const animalNames = () => {
+  const exp = [];
+  species.forEach((specie) => {
+    exp.push(specie.name);
+  });
+  return exp;
+};
+
 function officeHour(param) {
   const result = [...Object.values(hours[param])];
   if (param === 'Monday') {
     return 'CLOSED';
   }
   return `Open from ${result[0]}am until ${result[1]}pm`;
+}
+
+function animalAvaliabilities(param) {
+  const result = species.find((specie) => specie.name === param);
+  return result.availability;
 }
 
 function exhibition(param) {
@@ -33,9 +46,37 @@ function withoutParam() {
   return newObj;
 }
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+function withDate(param) {
+  const obj = {};
+  Object.keys(hours).forEach((key) => {
+    if (key === param) {
+      obj[param] = {
+        officeHour: officeHour(param),
+        exhibition: exhibition(param),
+      };
+    }
+  });
+  return obj;
 }
 
+function helloWorld(param) {
+  const days = Object.keys(hours);
+  const animals = animalNames();
+  if (days.includes(param)) {
+    return withDate(param);
+  }
+  if (animals.includes(param)) {
+    return animalAvaliabilities(param);
+  }
+}
+
+function getSchedule(scheduleTarget) {
+  const days = Object.keys(hours);
+  const animals = animalNames();
+  if (!scheduleTarget || (!days.includes(scheduleTarget) && !animals.includes(scheduleTarget))) {
+    return withoutParam();
+  }
+  return helloWorld(scheduleTarget);
+}
 module.exports = getSchedule;
-console.log(withoutParam());
+console.log(getSchedule('lions'));
