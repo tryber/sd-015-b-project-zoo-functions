7f822f077d0b14760681dad266b346cb81b796d4
Gabriel Peralta <prates_peralta@hotmail.com>
2021-09-13 21:54:33 -0300

    adicina o requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const result = { child: 0, adult: 0, senior: 0 };
+
+  entrants.forEach(({ age }) => {
+    if (age < 18) result.child += 1;
+    if (age >= 18 && age < 50) result.adult += 1;
+    if (age >= 50) result.senior += 1;
+  });
+  return result;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  let result = 0;
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const entries = countEntrants(entrants);
+  result += entries.child * 20.99 + entries.adult * 49.99 + entries.senior * 24.99;
+  return result;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species
+      .reduce((acc, current) => ({ ...acc, [current.name]: current.residents.length }), {});
+  }
+  return species.find((specie) => specie.name === animal);
 }
 
 module.exports = countAnimals;
