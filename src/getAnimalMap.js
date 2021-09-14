d7f82e4b2c6bc1af61319dfb93b4d7b5c10e5b90
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-09-14 16:16:38 -0300

    implementa a funçao getOldestFromFirstSpecies

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,88 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+function animalsNames(animal) {
+  const animalNames = { };
+  animalNames[animal] = [];
+  species.forEach((specie) => {
+    if (specie.name === animal) {
+      specie.residents.forEach((element) => animalNames[animal].push(element.name));
+    }
+  });
+  return animalNames;
+}
+
+function animalsSorted(animal) {
+  const animalName = { };
+  animalName[animal] = [];
+  species.forEach((specie) => {
+    if (specie.name === animal) {
+      specie.residents.forEach((element) => animalName[animal].push(element.name));
+    }
+  });
+  animalName[animal].sort();
+  return animalName;
+}
+
+function animalsWithoutParameters() {
+  const newObj = { NE: [], NW: [], SE: [], SW: [] };
+  species.forEach((specie) => {
+    if (specie.location === 'NE') {
+      newObj.NE.push(specie.name);
+    } else if (specie.location === 'NW') {
+      newObj.NW.push(specie.name);
+    } else if (specie.location === 'SE') {
+      newObj.SE.push(specie.name);
+    } else {
+      newObj.SW.push(specie.name);
+    }
+  });
+  return newObj;
+}
+
+function animalsWithTrue() {
+  const newObj = { NE: [], NW: [], SE: [], SW: [] };
+  species.forEach((specie) => {
+    if (specie.location === 'NE') {
+      newObj.NE.push(animalsNames(specie.name));
+    } else if (specie.location === 'NW') {
+      newObj.NW.push(animalsNames(specie.name));
+    } else if (specie.location === 'SE') {
+      newObj.SE.push(animalsNames(specie.name));
+    } else {
+      newObj.SW.push(animalsNames(specie.name));
+    }
+  });
+  return newObj;
+}
+
+function animalsWithSort() {
+  const newObj = { NE: [], NW: [], SE: [], SW: [] };
+  species.forEach((specie) => {
+    if (specie.location === 'NE') {
+      newObj.NE.push(animalsSorted(specie.name));
+    } else if (specie.location === 'NW') {
+      newObj.NW.push(animalsSorted(specie.name));
+    } else if (specie.location === 'SE') {
+      newObj.SE.push(animalsSorted(specie.name));
+    } else {
+      newObj.SW.push(animalsSorted(specie.name));
+    }
+  });
+  return newObj;
+}
 
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options) {
+    return animalsWithoutParameters();
+  } if (!options.sort) {
+    return animalsWithTrue();
+  }
+  return animalsWithSort();
 }
 
 module.exports = getAnimalMap;
+
+// console.log(animalsWithTrue());
+// console.log(animalsNames('lions'));
+console.log(animalsWithSort());
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,33 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function getFirstAnimal(param) {
+  let result;
+  employees.forEach((employee) => {
+    if (employee.id === param) {
+      result = employee.responsibleFor;
+      return result;
+    }
+  });
+  return result[0];
+}
+
+function getOlder(callback) {
+  let result;
+  species.forEach((specie) => {
+    if (specie.id === callback) {
+      result = specie.residents.reduce((acc, curr) => ((acc.age > curr.age) ? acc : curr));
+    }
+  });
+  const newResult = [...Object.values(result)];
+  return newResult;
+}
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  // getFirstAnimal(id);
+  return getOlder(getFirstAnimal(id));
 }
 
 module.exports = getOldestFromFirstSpecies;
+// console.log(getFirstAnimal('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+console.log(getOldestFromFirstSpecies('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
