57d775f7ef9b825e9f575d4b3c84d378ecfbb581
Paulo Flóra <tflora.2004@gmail.com>
2021-09-14 16:03:06 -0300

    Feito o requisito 3.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,19 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return employees.find((employee) => {
+    if (employee.firstName === employeeName || employee.lastName === employeeName) {
+      return true;
+    } return false;
+  });
 }
+
+console.log(getEmployeeByName());
+console.log(getEmployeeByName('Emery'));
+console.log(getEmployeeByName('Wishart'));
 
 module.exports = getEmployeeByName;
