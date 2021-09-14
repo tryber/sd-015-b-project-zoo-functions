63d9aacb946fcd0c79a27a582b75feb3de002d48
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-14 16:13:25 -0300

    [Requisito 10] - Cria uma constante que armazena o funcionario de acordo com o primeiro nome, segundo nome ou id passado por parametro

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+const { species, employees } = data;
+
+function getEmployeesCoverage(myEmployee) {
   // seu código aqui
+  const { name: employeeName, id: employeeId } = myEmployee;
+
+  const findEmployee = employees.find(
+    ({ id, firstName, lastName }) =>
+      id === employeeId || firstName === employeeName || lastName === employeeName,
+  );
+
+  const employeeCoverage = (id, fullName, species, locations) => ({
+    id,
+    fullName,
+    species,
+    locations,
+  });
+
+  return findEmployee;
 }
+
+console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 
 module.exports = getEmployeesCoverage;
