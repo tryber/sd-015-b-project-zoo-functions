52c3b6622bf766af5f30a145256c369b91601c9d
Arthur <viegas.arthur2013@hotmail.com>
2021-09-14 17:57:55 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(animal, gender) {
+  const allAnimals = {};
+  if (!animal) {
+    species.forEach(({ name, residents }) => {
+      allAnimals[name] = residents.length;
+    });
+    return allAnimals;
+  }
+  const findAnimal = species.find((specie) => specie.name === animal);
+  const residentsFound = findAnimal.residents.length;
+  if (animal && gender) {
+    const selectedResidents = findAnimal.residents;
+    const filterSex = selectedResidents.filter((resident) => resident.sex === gender);
+    return filterSex.length;
+  }
+  return residentsFound;
 }
 
+console.log(countAnimals('giraffes', 'female'));
 module.exports = countAnimals;
