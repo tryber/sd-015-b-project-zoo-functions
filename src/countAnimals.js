412f4f67f53ffea32ed0d61a06cf1df09a061b62
Bruno Bartolomeu <bruno.bartolomeu5@hotmail.com>
2021-09-14 15:50:11 -0300

    Resolve lint req 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,5 +1,5 @@
 const { species } = require('../data/zoo_data');
-const data = require('../data/zoo_data')
+const data = require('../data/zoo_data');
 
 function animalsCount() {
   const animalsInfo = {};
