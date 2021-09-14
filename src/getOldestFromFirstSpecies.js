fd4898d4eaeea497cbdb8fef6481c4d88ba22b66
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-09-14 15:51:48 -0300

    Remove comentarios de linha, console log e arruma indentacao

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -3,19 +3,21 @@ const data = require('../data/zoo_data');
 const { employees, species } = data;
 
 function getOldestFromFirstSpecies(id) {
-  // Usar o find para encontrar o primeiro animal cuidado do responsavel de acordo com o id
   const howAnimal = employees.find((employee) => employee.id.includes(id));
+
   const firtAnimal = howAnimal.responsibleFor[0];
+
   const getSpecie = species.find((specie) => specie.id.includes(firtAnimal)).residents;
+
   const whichOlder = getSpecie.reduce((bigAge, age) => (bigAge.age > age.age ? bigAge : age));
+
   const olderAnimal = [];
+
   olderAnimal.push(whichOlder.name);
   olderAnimal.push(whichOlder.sex);
   olderAnimal.push(whichOlder.age);
-  return olderAnimal;
-  // Usar o reduce pra pegar o animal mais velho
-}
 
-console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+  return olderAnimal;
+}
 
 module.exports = getOldestFromFirstSpecies;
