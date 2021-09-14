1b252bff91a71db701b2dfd60d0d8699d2eb6761
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2021-09-14 17:00:48 -0300

    arrumando lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -6,7 +6,10 @@ function countAnimals(animal) {
     return species.reduce((acc, value) =>
     // acessado : https://github.com/tryber/sd-014-b-project-zoo-functions/blob/Jo%C3%A3oHenrique-Zoo-Functions/src/zoo.js
       ({ ...acc, [value.name]: value.residents.length }), {});
-    // ...
+    // Aquí o primeiro valor de acc é um objeto vazio, e quando dizemos que o value
+    // que ele recebe sera uma chave com a propriedade nama (value.name), e esse value
+    //  tambem sera o valor como value e o numero (residents.length) de residents
+    //  e assim é feito sucessivamente; esse valor é retornado e utilizado como proximo acc.
   } if (animal.sex === undefined) {
     return species.find((animals) => animals.name === animal.specie).residents.length;
   //  Primeiro criamos o critério que só pode entrar quem tiver a chave animal.gender indefinodo
--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,10 +1,4 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  if (options === undefined) {
-    species.map((animals) => animals.location);
-  }
-}
-
+function getAnimalMap(options) {}
 module.exports = getAnimalMap;
