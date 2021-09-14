37b4128835b75c3bba85f8061c970f702d5a5888
Mariah M. Rodrigues <mariah.manetti@gmail.com>
2021-09-13 21:16:54 -0300

    Requisito 4 - Implemente a função getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,26 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    const checkRelated = employees.filter(
+      (employee) => employee.managers.includes(managerId) === true,
+    );
+    return checkRelated.map((employee) => `${employee.firstName} ${employee.lastName}`);
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
+
+// 1 -isManager - que será responsável por verificar se uma pessoa colaboradora é gerente ou não. O retorno dessa função deve ser um booleano: true ou false;
+// 2 - getRelatedEmployees - que utiliza a primeira função para apresentar as seguintes saídas:
+// Retorna true se o id passado for de um gerente;
+// Retorna false se o id passado não for de um gerente;
+// Se o id passado for de um gerente, retorna um array contendo nome e sobrenome das pessoas colaboradoras que ela é responsável;
+// Se o id passado não for de um gerente, dispara um erro com a mensagem: "O id inserido não é de uma pessoa colaboradora gerente!".
