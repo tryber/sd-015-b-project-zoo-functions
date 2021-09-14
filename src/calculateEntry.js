7116f033fa938a8c7032a67b2f402db540e0d68c
DanielHott <daniel_hott@outlook.com>
2021-09-14 20:14:06 -0300

    Resolvendo problemas do requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -4,7 +4,8 @@ function calculaEntrada(i) {
   let a = 0;
   let j = 0;
   let s = 0;
-  const ages = i.map((x) => x.age);
+  const il = i;
+  const ages = il.map((x) => x.age);
   ages.forEach((x) => {
     if (x < 18) { j += 1; }
     if (x >= 18 && x < 50) { a += 1; }
@@ -13,22 +14,20 @@ function calculaEntrada(i) {
   return { child: j, adult: a, senior: s };
 }
 function countEntrants(i) {
-  if (i === undefined) {
+  if (typeof (i) !== 'object') {
     return 0;
-  } if (i === { }) {
-    return 0;
-  } if (i.length < 1) {
-    return 0;
-  }
-  return calculaEntrada(i);
+  } return calculaEntrada(i);
 }
 
 function calculateEntry(entrants) {
-  if (entrants === undefined || entrants === {}) { return 0; }
-  const pagantes = countEntrants(entrants);
+  if (entrants === undefined || entrants === 0) { return 0; }
+  const pagantes = (entrants.length > 0) ? countEntrants(entrants) : 0;
   const kids = pagantes.child * 20.99;
   const adult = pagantes.adult * 49.99;
   const coroa = pagantes.senior * 24.99;
+  if (pagantes === 0) {
+    return 0;
+  }
   return (kids + adult + coroa);
 }
 
