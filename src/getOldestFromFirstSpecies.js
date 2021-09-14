a05712fad478e0552be52ab29d73167a0ae23a64
David Souza <dabcs@poli.ufrj.br>
2021-09-13 23:00:44 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const getSpecie = (id) => employees.find((employee) => employee.id === id);
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstSpecie = getSpecie(id).responsibleFor[0];
+  return species.find((specie) => specie
+    .id === firstSpecie).residents.reduce((count, { name, sex, age }) => {
+    if (age > count[2]) {
+      return [name, sex, age];
+    }
+    return count;
+  }, [0, 0, 0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
