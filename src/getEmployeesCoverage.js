c614781c101465e3c60e946d82fe10a20adab71e
Camilla Del Guerso <camilladelguerso@gmail.com>
2021-09-14 14:32:56 -0300

    Adiciona requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,44 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function lista() {
+  const array = [];
+  const obj = data.employees.reduce((acc, { id, firstName, lastName, responsibleFor }) => {
+    const fullName = `${firstName} ${lastName}`;
+    const species = responsibleFor.map((idAnimal) =>
+      data.species.find((elemento) => elemento.id === idAnimal).name);
+
+    const locations = responsibleFor.map((idAnimal) =>
+      data.species.find((elemento) => elemento.id === idAnimal).location);
+
+    array.push(acc = { id, fullName, species, locations });
+    return array;
+  }, ' ');
+  return obj;
 }
+
+function getEmploye(objeto) {
+  const { name = undefined, id: ident = undefined } = objeto;
+  const empregado = data.employees.find(({ id, firstName, lastName }) =>
+    firstName === name || lastName === name || id === ident);
+  if (!empregado) throw new Error('Informações inválidas');
+  const id = empregado.id;
+  const fullName = `${empregado.firstName} ${empregado.lastName}`;
+  const resp = empregado.responsibleFor;
+  const species = resp.map((idAnimal) =>
+    data.species.find((elemento) => elemento.id === idAnimal).name);
+  const locations = resp.map((idAnimal) =>
+    data.species.find((elemento) => elemento.id === idAnimal).location);
+  const obj = { id, fullName, species, locations };
+  return obj;
+}
+
+function getEmployeesCoverage(objeto) {
+  if (!objeto) {
+    return lista();
+  }
+  return getEmploye(objeto);
+}
+
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,24 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // const objeto = {};
-  // const objeto2 = {};
-  // const dias = Object.keys(data.hours);
-  // dias.reduce((acc, day) => {
-  //   acc[day] = `Open from ${data.hours[day].open}am until ${data.hours[day].close - 12}pm`;
-  //   acc.Monday = 'CLOSED';
-  // return objeto1; }, {});
-  // if (!scheduleTarget){
-  //   for (let i = 0; i < dias.length; i += 1) {
-  //     if (dias[i] !== 'Monday') {
-  //       objeto[dias[i]] = () => {}
-  //       const especieAnimal = data.species.filter(({availability}) =>
-  //       availability.includes(dias[i])).map(({name}) => name);
-  //       console.log(especieAnimal);
-  //       console.log(objeto);
-  //     };
-  //   };
-  // };
+// retorna objeto com 'dia' dendo dele tem um objeto com 'officeHour'/hotario e 'exhibition'/animais.
+// pegar dia;
+// pegar horário;
+// pegar animais que serão exibitos
 }
-getSchedule();
+console.log(getSchedule());
+
 module.exports = getSchedule;
