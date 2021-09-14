572184685e12a574417cbfea24f64dd41da387ec
Allan Melo <allan-m-silva@hotmail.com>
2021-09-14 18:56:57 -0300

    adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,10 +1,23 @@
 const data = require('../data/zoo_data');
-// const { species } = require('../data/zoo_data');
-// const list = (animals) => animals.reduce((acc, element) => {
-//   acc[element.name] = element.residents.length;
-//   return acc;
-// }, {} );
+const { species } = require('../data/zoo_data');
+
 function countAnimals(animal) {
+  if (animal) {
+    const { specie, sex } = animal;
+    const specieRequired = species.find((element) => element.name === specie);
+    let quantity = specieRequired.residents;
+    if (sex) {
+      quantity = quantity
+        .filter((element) => element.sex.toLowerCase() === sex.toLowerCase());
+    } return quantity.length;
+  }
+  const objectReturn = {};
+  species.forEach((element) => {
+    const animalName = element.name;
+    const animalResidents = element.residents.length;
+    objectReturn[`${animalName}`] = animalResidents;
+  });
+  return objectReturn;
 }
 
 module.exports = countAnimals;
