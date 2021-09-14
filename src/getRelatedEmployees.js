19cd5b4e49a6a153844fbb80177d0ab43123b1ad
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-14 11:54:51 -0300

    [Requisito 4] - Cria a funçao isManager que retorna true caso o id passado seja de um manager e false caso contrario. Para isso, utiliza a HOF map para criar um array de arrays com os managers de cada funcionario e a HOF reduce para tornar o array unidimensional.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
   // seu código aqui
+  const arrayOfManagers = employees
+    .map((element) => element.managers)
+    .reduce((acc, curVal) => acc.concat(curVal));
+
+  return !!arrayOfManagers.includes(id);
 }
 
 function getRelatedEmployees(managerId) {
