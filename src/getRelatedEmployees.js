5f7666e691dcdd3be0952c4badb18db74eafa303
Lucas Peres <lucasperesss@gmail.com>
2021-09-13 23:36:36 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const managers = [];
+  data.employees.forEach((emp) => {
+    emp.managers.forEach((manager) => {
+      if (!managers.includes(manager)) {
+        managers.push(manager);
+      }
+    });
+  });
+  return managers.includes(id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const managed = [];
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  data.employees.forEach((emp) => {
+    if (emp.managers.includes(managerId)) {
+      managed.push(`${emp.firstName} ${emp.lastName}`);
+    }
+  });
+  return managed;
 }
 
 module.exports = { isManager, getRelatedEmployees };
