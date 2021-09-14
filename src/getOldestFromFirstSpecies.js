90a5bbf7e9fee72ed77fdfa80eae80f5c2b01cef
Adriana <adrianam754@gmail.com>
2021-09-14 19:10:55 -0300

    Adicionada requisito 4 e 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function getOldestFromFirstSpecies(identification) {
+  const findEmployee = data.employees.find(({ id }) => id === identification).responsibleFor[0];
+  const findSpecie = data.species.find(({ id }) => id === findEmployee).residents
+    .sort((a, b) => b.age - a.age).find((value) => value);
+  return Object.values(findSpecie);
 }
 
 module.exports = getOldestFromFirstSpecies;
