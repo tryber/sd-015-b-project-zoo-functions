1f1de906c5d7645ff0c1618c274315cfef734db3
Wilian Pereira <wilian_pereira92@outlook.com>
2021-09-13 21:30:53 -0300

    Adicionado requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const nome = data.species.find((especie) => especie.name === animal);
+  console.log(nome);
+  return nome.residents.every((idade) => idade.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
