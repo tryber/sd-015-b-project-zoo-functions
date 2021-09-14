b4be0dfccc2adae66c9acf8c935cf9fa510df1f1
Ismael_Luviseti <ismaelluviseti@gmail.com>
2021-09-14 17:49:22 -0300

    Requisito 4 finalizado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  return data.employees.some((employee) => employee.managers == id)
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true){
+    const person = data.employees.filter((employee) => employee.managers.includes(managerId));
+    return person.map((employee) => `${employee.firstName} ${employee.lastName}`)
+  } else {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
