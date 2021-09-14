fb61530b4468afb3bfbf6d7def8ac6104429b180
Arthur <viegas.arthur2013@hotmail.com>
2021-09-14 15:39:44 -0300

    requisito 4

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,8 +1,8 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   if (!employeeName) { return {}; }
-  const employees = data.employees;
   const findEMployee = employees.find((individual) => individual
     .firstName === employeeName || individual
     .lastName === employeeName);
