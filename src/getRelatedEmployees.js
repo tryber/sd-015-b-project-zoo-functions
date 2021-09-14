042ca276002b552f5a6c7265655003f9661360e6
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-09-14 20:29:33 -0300

    Requisito 4, implementa funcao isManager

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const objEmployee = employees.some((employee) => employee.managers.includes(id));
+  return objEmployee;
+  // return employees[0].managers;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
 }
 
-module.exports = { isManager, getRelatedEmployees };
+// module.exports = { isManager, getRelatedEmployees };
+console.log(isManager('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
