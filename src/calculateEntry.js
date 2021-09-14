38a88883d6a8346da40bffce51f51a04a390678f
Wilian Pereira <wilian_pereira92@outlook.com>
2021-09-14 16:53:57 -0300

    Adicionado requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,23 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  const crianca = entrants.filter((idade) => idade.age < 18);
+  const adulto = entrants.filter((idade) => idade.age >= 18 && idade.age < 50);
+  const senior = entrants.filter((idade) => idade.age >= 50);
+  const resultado = { child: crianca.length, adult: adulto.length, senior: senior.length };
+  return resultado;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (entrants === undefined || entrants.length === undefined) return 0;
+  const cliente = countEntrants(entrants);
+  const valor = data.prices;
+  const valorCrianca = cliente.child * valor.child;
+  const valorAdulto = cliente.adult * valor.adult;
+  const valorSenio = cliente.senior * valor.senior;
+  const valorFinal = (valorCrianca + valorAdulto + valorSenio);
+  return valorFinal;
 }
 
 module.exports = { calculateEntry, countEntrants };
