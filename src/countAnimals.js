4583c52feeb3b1f31afcb89726f7f33b305064b5
Wander Luiz <wander.srv@gmail.com>
2021-09-14 15:01:57 -0300

    Resoluçao do requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,21 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    const todasSpecies = {};
+    data.species.forEach((qtdCadaAnimal) => {
+      todasSpecies[qtdCadaAnimal.name] = qtdCadaAnimal.residents.length;
+    });
+    return todasSpecies;
+  }
+  if (animal.specie && animal.gender === undefined) {
+    return data.species.find((nomeSpecie) => nomeSpecie.name === animal.specie).residents.length;
+  }
+  if (animal.gender) {
+    return data.species.find((nomeSpecie) => nomeSpecie.name === animal.specie)
+      .residents.filter((pesquisaGender) => pesquisaGender.sex === animal.gender).length;
+  }
 }
 
 module.exports = countAnimals;
+console.log(countAnimals({ specie: 'giraffes', gender: 'female' }));
