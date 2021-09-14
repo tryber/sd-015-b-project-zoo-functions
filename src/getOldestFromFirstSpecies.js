4adb9c65c3794c4e4b489411de7aef8d102d5ccc
Caio R. Vieira <caio.robs98@gmail.com>
2021-09-14 20:16:28 -0300

    transofrma getOldestFromFirstSpecies em arrow func

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,13 +1,13 @@
 const data = require('../data/zoo_data');
 const getSpeciesByIds = require('./getSpeciesByIds');
 
-function getOldestFromFirstSpecies(id) {
+const getOldestFromFirstSpecies = (id) => {
   const selectedEmployee = data.employees.find(({ id: employeeId }) => employeeId === id);
   const { responsibleFor: species } = selectedEmployee;
   const firstSpecie = getSpeciesByIds(species[0]);
   const { residents } = firstSpecie[0];
   const oldest = residents.reduce((acc, animal) => (acc.age >= animal.age ? acc : animal));
   return Object.values(oldest);
-}
+};
 
 module.exports = getOldestFromFirstSpecies;
