c41bdc88cc31e76645396a6cd450211b42e3c9ae
Danielen Cestari Nunes <danielen.cestari@gmail.com>
2021-09-13 22:00:20 -0300

    adiciona requisito 5

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -3,8 +3,11 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   if (employeeName === undefined) return {};
+  // de acordo com o teste, quando o parametro é indefinido/ vazio, ele retorna um objeto vazio
   return employees.find(({ firstName, lastName }) => employeeName.includes(firstName)
   || employeeName.includes(lastName));
+  // faz o find e desestrutura o objeto para então verificar se esse objeto descontriudo tem o nome pesquisado
+  // ele testa 1 o nome e depois o sobrenome.
 }
 
 module.exports = getEmployeeByName;
