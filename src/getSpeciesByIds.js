e654a3ff33505751c9dffe4630caa69ae2153051
Lucas Maieski Marques <lucmaieski@gmail.com>
2021-09-13 22:08:06 -0300

    Retira o console.log do requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -8,5 +8,4 @@ function getSpeciesByIds(...ids) {
   return ids.map((id) => species.find((animal) => animal.id === id));
 }
 
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
