1a25a8ea338ce0ef40ddd03bb06b842f203d53f9
FERNANDO <Fernando@FERNANDO.local>
2021-09-13 21:31:10 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const entradas = entrants;
+  const child = entradas.filter(({ age }) => age < 18).length;
+  const adult = entradas.filter(({ age }) => age >= 18 && age < 50).length;
+  const senior = entradas.filter(({ age }) => age >= 50).length;
+  const object = { child, adult, senior };
+  return object;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  let { child, adult, senior } = prices;
+  child *= countEntrants(entrants).child;
+  adult *= countEntrants(entrants).adult;
+  senior *= countEntrants(entrants).senior;
+  return child + adult + senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
