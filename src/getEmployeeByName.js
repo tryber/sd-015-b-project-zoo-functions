430612b1568fc11ff2747dccb331bc719dbd8b0d
Joao Pedro de Oli. Lage <JoaoPedro.Lage@outlook.com>
2021-09-14 16:19:14 -0300

    Implementa a função getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  return data.employees.find((name) => name.firstName === employeeName
+  || name.lastName === employeeName);
 }
 
+console.log(getEmployeeByName('Bethea'));
 module.exports = getEmployeeByName;
