bc6330a1a5301a6f99644f63b59707317895e178
matheusg18 <matheusgabriel.18@gmail.com>
2021-09-13 22:09:42 -0300

    Requisito 6 feito :heavy_check_mark:

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,19 +1,29 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants = []) => {
-  const totalByAge = { child: 0, adult: 0, senior: 0 };
+  const entraceByAge = { child: 0, adult: 0, senior: 0 };
 
   entrants.forEach(({ age }) => {
-    if (age >= 50) totalByAge.senior += 1;
-    else if (age >= 18) totalByAge.adult += 1;
-    else totalByAge.child += 1;
+    if (age >= 50) entraceByAge.senior += 1;
+    else if (age >= 18) entraceByAge.adult += 1;
+    else entraceByAge.child += 1;
   });
 
-  return totalByAge;
+  return entraceByAge;
 };
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+const calculateEntry = (entrants) => {
+  if (typeof entrants === 'undefined' || Object.keys(entrants).length === 0) return 0;
+
+  const { prices } = data;
+  const entranceByAge = countEntrants(entrants);
+  const childTotalEntry = entranceByAge.child * prices.child;
+  const adultTotalEntry = entranceByAge.adult * prices.adult;
+  const seniorTotalEntry = entranceByAge.senior * prices.senior;
+
+  return childTotalEntry + adultTotalEntry + seniorTotalEntry;
+};
 
 module.exports = { calculateEntry, countEntrants };
+
+console.log(calculateEntry({}));
