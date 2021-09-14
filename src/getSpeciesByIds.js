d89522d0b59af9d9304c6d8b409837726782d797
leandromliveira <leandrooliveira35@gmail.com>
2021-09-14 15:41:04 -0300

    requisito 8 completo

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,5 +3,6 @@ const data = require('../data/zoo_data');
 function getSpeciesByIds(...ids) {
   return data.species.filter((specie) => ids.includes(specie.id));
 }
+// metodo aprendido com os alunos Danielen Cestari Nunes e Assis Meneghetti Fabri
 
 module.exports = getSpeciesByIds;
