d83e5b25eb4bbec83f523618905dd800ed60b72f
Huijiro <huijirohankei@gmail.com>
2021-09-14 18:56:12 -0300

    Feito exercício 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,11 +2,12 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
-  const employee = data.employees.find((employee) => employee.id === id);
+  const employee = data.employees.find((e) => e.id === id);
   const animal = data.species.find((specie) => specie.id === employee.responsibleFor[0]);
   console.log(animal);
   animal.residents.sort((a, b) => b.age - a.age);
-  {name, sex, age} = animal.residents[0];
+  const {name, sex, age} = animal.residents[0];
+  return [name, sex, age];
 }
 
 console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
