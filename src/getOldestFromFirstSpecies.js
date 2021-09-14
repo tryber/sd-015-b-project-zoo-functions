cbf84c4d5f79c833e1ea4817b2a511d1653015ea
Rafael <rafael.gomesbarretomoura@gmail.com>
2021-09-14 20:50:12 -0300

    corrige erros

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,9 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  const { species, employees } = data;
-  const employeeFound = employees.find((employee) => employee.id === id);
-  const firstSpecie = species.find((specie) => specie.id === employeeFound.responsibleFor[0]);
+  const employeeFound = data.employees.find((employee) => employee.id === id);
+  const firstSpecie = data.species.find((specie) => specie.id === employeeFound.responsibleFor[0]);
   const animal = firstSpecie.residents.sort((animalA, animalB) => animalB.age - animalA.age)[0];
 
   return Object.values(animal);
