cf8a455ee3a0b91f37b138322cf6fc8a051840fd
Caio R. Vieira <caio.robs98@gmail.com>
2021-09-14 19:29:42 -0300

    requisito 10 a caminho

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,14 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeesCoverage(infoObj) {
+  const selectedEmployee = employees.find((employee) => { 
+    const firstNameIsEqual = infoObj.name === employee.name;
+    const lastNameIsEqual = infoObj.name === employee.lastName;
+    const idIsEqual = infoObj.id === employee.id;
+    return (firstNameIsEqual || lastNameIsEqual || idIsEqual);
+  });
+  return selectedEmployee;
 }
 
 module.exports = getEmployeesCoverage;
