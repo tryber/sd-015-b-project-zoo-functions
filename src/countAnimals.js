af64e62a23bff5dc2ddbe496a49db2387b8f2c62
Laurenz Pinder <laurenzdpinder90@gmail.com>
2021-09-14 17:18:43 -0300

    Adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const obj = {};
+    species.forEach((kind) => { obj[kind.name] = kind.residents.length; });
+    return obj;
+  }
+  if (Object.keys(animal).length === 1) {
+    return species.find((kind) => kind.name === animal.specie).residents.length;
+  }
+  return species.find((kind) => kind.name === animal.specie).residents
+    .filter(({ sex }) => sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
