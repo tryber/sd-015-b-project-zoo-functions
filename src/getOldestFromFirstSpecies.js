d78dd402833bc448c8c3a18357c591ea50379381
Danielen Cestari Nunes <danielen.cestari@gmail.com>
2021-09-14 18:42:33 -0300

    requisito 09 - com ajuda do Leandro Olivieira, Iury Roque e Virginia Alcântara

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,24 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  // employees  -  id funionario - responsibleFor[0] é o primeiro animal
+  // ai tem que ver qual o mais velho desses
+  const empregado = employees.find((employee) => employee.id === id);
+  const animal = empregado.responsibleFor[0];
+  const primeiraEspecie = species.find((name) => name.id === animal);
+  let maisvelho = 0;
+  let residenteIdoso = [];
+  primeiraEspecie.residents.forEach((residente) => {
+    if (residente.age > maisvelho) {
+      maisvelho = residente.age;
+      residenteIdoso = [];
+      residenteIdoso.push(residente.name);
+      residenteIdoso.push(residente.sex);
+      residenteIdoso.push(residente.age);
+    }
+  });
+  return residenteIdoso;
 }
 
 module.exports = getOldestFromFirstSpecies;
