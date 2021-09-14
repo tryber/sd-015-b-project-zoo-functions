54b222ac9180e913125eb0400788c5973a65465d
Rafael Toledo <rafael.toledo@engenharia.ufjf.br>
2021-09-13 21:27:43 -0300

    teste possivel erro de lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
   // seu código aqui
-}
+  const animals = {};
+  if (!animal) {
+    species.forEach((specie) => {
+      const { name } = specie;
+      const quantity = species.residents.length;
+      animals[name] = quantity;
+    });
+    return animals;
+  }
+  const animalSelected = species.find((specie) => specie.name === animal.specie);
+  if (animal.sex) {
+    const sexSelected = animalSelected.residents;
+    sexSelected.filter((resident) => resident.sex === animal.sex);
+    return sexSelected.length;
+  }
+  return animalSelected.residents.length;
 
-module.exports = countAnimals;
+  module.exports = countAnimals;
+}
