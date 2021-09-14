0eb17c6ecc1e1d3e16ffb080f9492324d0853cc7
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-09-14 17:35:33 -0300

    lint

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,36 @@
+const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function officeHour(param) {
+  const result = [...Object.values(hours[param])];
+  if (param === 'Monday') {
+    return 'CLOSED';
+  }
+  return `Open from ${result[0]}am until ${result[1]}pm`;
+}
+
+function exhibition(param) {
+  if (param === 'Monday') {
+    return 'The zoo will be closed!';
+  }
+  const exhibitionResult = [];
+  species.forEach((specie) => {
+    if (specie.availability.includes(param)) {
+      exhibitionResult.push(specie.name);
+    }
+  });
+  return exhibitionResult;
+}
+
 function withoutParam() {
-  
+  const newObj = {};
+  Object.keys(hours).forEach((key) => {
+    newObj[key] = {
+      officeHour: officeHour(key),
+      exhibition: exhibition(key),
+    };
+  });
+  return newObj;
 }
 
 function getSchedule(scheduleTarget) {
@@ -9,3 +38,4 @@ function getSchedule(scheduleTarget) {
 }
 
 module.exports = getSchedule;
+console.log(withoutParam());
