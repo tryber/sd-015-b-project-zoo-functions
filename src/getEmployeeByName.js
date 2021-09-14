f3570be33f783cbb07e4b1c87fb59d0049d25db0
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-09-14 09:57:01 -0300

    Adiciona requisito 3, nome funciobarios

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+  return data.employees.find((people) => people
+    .firstName === employeeName || people.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
