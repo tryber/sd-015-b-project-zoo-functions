600957644f1fd740ca1cd2edde6635d283d10c9f
Rodolfo <rodolfoserralha@hotmail.com>
2021-09-13 21:21:47 -0300

    arrumando problemas lint requisito 06

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,15 +1,6 @@
 const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const entrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-
 function countEntrants(entrants) {
   const child = entrants.filter(({ age }) => age < 18).length;
   const adult = entrants.filter(({ age }) => (age >= 18 && age < 50)).length;
