6a3f703dba8b7c1bb987cf61452f53371055b321
Arthur Bernardo Messias <turmessias@gmail.com>
2021-09-14 20:51:45 -0300

    Inicia requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,44 @@
+const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeesCoverage(param) {
+  let filterEmployees
+  if (!param) {
+    // TODO: return coverage from all workers;
+    filterEmployees = employees;
+    return employees.map((element) => getCoverage(element));
+  }
+  if (param.name) {
+    // TODO: return only the employee with this name
+    return employees
+      .filter((element) => fullName(element).includes(param.name))
+      .map((element) => getCoverage(element));
+  }
+  if (param.id) {
+    // TODO: return only the employee with this id
+    return employees
+      .filter((element) => element.id === param.id)
+      .map((element) => getCoverage(element));
+  }
+}
+
+function getSpecies(employee) {
+  return species.filter((element) =>
+    employee.responsibleFor.includes(element.id),
+  );
+}
+
+function getCoverage(employee) {
+  return {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: getSpecies(employee).map((element) => element.name),
+    locations: getSpecies(employee).map((element) => element.location),
+  };
+}
+
+function fullName(employee) {
+  return `${employee.firstName} ${employee.lastName}`;
 }
 
 module.exports = getEmployeesCoverage;
