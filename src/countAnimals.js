1aee4bfadf508397fbc52402d916c7348dbf1a80
Richard Leal <rwsleal@gmail.com>
2021-09-14 20:39:13 -0300

    Requisito 5 finalizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const speciesObject = {};
+data.species.forEach((animal) => {
+  speciesObject[animal.name] = animal.residents.length;
+  return speciesObject;
+});
+console.log(speciesObject.lions);
+
+function countAnimals(animal = speciesObject) {
+  if (Object.keys(speciesObject).includes(animal.specie)) {
+    if (animal.sex) {
+      const animalsOfSex = species.find((subject) => subject.name === animal.specie).residents;
+      console.log(animalsOfSex);
+      return animalsOfSex.filter((element) => element.sex === animal.sex).length;
+    }
+    return speciesObject[animal.specie];
+  }
+  return animal;
 }
+
+console.log(countAnimals('lions'));
 
 module.exports = countAnimals;
