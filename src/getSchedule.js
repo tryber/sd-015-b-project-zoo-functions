b3890401283f1b564f9782373240483f66286fbe
matheusg18 <matheusgabriel.18@gmail.com>
2021-09-14 16:27:37 -0300

    Requisito 8 feito :heavy_check_mark:

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,35 @@
-const data = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+const getDaySchedule = (day) =>
+  (day === 'Monday'
+    ? {
+      officeHour: 'CLOSED',
+      exhibition: 'The zoo will be closed!',
+    }
+    : {
+      officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+      exhibition: species.reduce((acc, { availability, name }) =>
+        (availability.includes(day) ? [...acc, name] : acc), []),
+    });
+
+const getAnimalsList = () => species.map(({ name }) => name);
+
+const getAnimalSchedule = (animalName) =>
+  species.find(({ name }) => animalName === name).availability;
+
+const getSchedule = (scheduleTarget) => {
+  const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  const animals = getAnimalsList();
+
+  if (daysOfWeek.includes(scheduleTarget)) {
+    return { [scheduleTarget]: getDaySchedule(scheduleTarget) };
+  }
+
+  if (animals.includes(scheduleTarget)) {
+    return getAnimalSchedule(scheduleTarget);
+  }
+
+  return daysOfWeek.reduce((acc, day) => ({ ...acc, [day]: getDaySchedule(day) }), {});
+};
 
 module.exports = getSchedule;
