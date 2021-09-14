3e8191765bfc40fc73b9bae2cc34d44e2f577a86
Ge <gmarcoscardoso@gmail.com>
2021-09-14 15:39:15 -0300

    Requisito 6 completo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return {
+    child: entrants.filter((peoples) => peoples.age < 18).length,
+    adult: entrants.filter((peoples) => peoples.age >= 18 && peoples.age < 50).length,
+    senior: entrants.filter((peoples) => peoples.age >= 50).length,
+  };
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
-}
+  if (entrants === undefined || Object.keys(entrants).length === 0) { return 0; }
 
+  const people = countEntrants(entrants);
+  const childPrice = data.prices.child * people.child;
+  const adultPrice = data.prices.adult * people.adult;
+  const seniorPrice = data.prices.senior * people.senior;
+  return childPrice + adultPrice + seniorPrice;
+}
+console.log(calculateEntry({}));
 module.exports = { calculateEntry, countEntrants };
