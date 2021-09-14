9f37092337385415692f8816feebb19334c7164f
Gabriel Peralta <prates_peralta@hotmail.com>
2021-09-14 10:51:42 -0300

    adiciona o requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
+const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = employees.find((person) => person.id === id)
+    .responsibleFor[0];
+  const animal = species.find((specie) => specie.id === employee);
+  const result = animal.residents
+    .sort((firstAge, seconAge) => seconAge.age - firstAge.age)
+    .find((specie) => specie);
+
+  return Object.values(result);
 }
 
 module.exports = getOldestFromFirstSpecies;
