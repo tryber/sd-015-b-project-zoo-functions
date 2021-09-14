74ffcfbf5672ec5213a5bc2a7535000592e8e881
douglas <douglasmonteiro.m@gmail.com>
2021-09-14 16:27:40 -0300

    Adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui!
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) return [];
+  return ids.map((id) => data.species.filter((specie) => specie.id === id));
 }
 
 module.exports = getSpeciesByIds;
