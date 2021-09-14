fc5d988a73fa299019498ff8dc856873e05207d1
Claudio Junior Haas <claudiohaas.cjh@gmail.com>
2021-09-13 22:37:49 -0300

    Requisito 2: Concluido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { name } = require('faker');
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+    const finderAnimal = species.find((element) => element.name === animal).residents;
+    const checkAge = finderAnimal.every((specie) => specie.age >= age);
+    return checkAge;
 }
 
 module.exports = getAnimalsOlderThan;
