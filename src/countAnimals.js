fc48d0a7ce698b472c22ef1ad955792ee9c8dfb6
Marina Marques <marina.marqueso@outlook.com>
2021-09-14 20:27:19 -0300

    Resolvendo parte de 'gender' do requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,24 +2,30 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // finding specie
   if (animal === undefined) {
-    // encontrando todos animais
-    const gettingAnimals = species.map((elemento) => elemento.name);
-    // recolhendo a popularity
-    const gettinPopularity = species.map((elemento) => elemento.popularity);
-    // juntando os dois resultados
-    const obj = {};
-    for (let i = 0; i < gettingAnimals.length; i++) {
-      const animal = gettingAnimals[i];
-      for (j in animal) {
-        obj[animal] = gettinPopularity[j];
-      };
-  }
-  return obj
-}
-}
 
+  }
+
+  if (animal.gender === undefined) {
+  // finding specie
+  const specie = animal.specie;
+  // finding specie
+  const findingAnimal = species.find((elemento) => specie.includes(elemento.name));
+  // filtering popularity
+  return findingAnimal.residents.length;
+  }
+
+  if (animal.gender !== undefined) {
+  // finding specie
+  const specie = animal.specie;
+  // finding specie
+  const findingAnimal = species.find((elemento) => specie.includes(elemento.name));
+  // filtering gender
+  const sex = animal.gender;
+  const filterGender = species.find((elemento) => sex.includes(elemento.residents.sex));
+  return filterGender;
+  }
+}
 module.exports = countAnimals;
 
-console.log(countAnimals());
+console.log(countAnimals({specie: 'otters'}));
