1240850a5b90869f8f66f36921bac448fe059f9a
Marina Marques <marina.marqueso@outlook.com>
2021-09-14 16:43:13 -0300

    Preparando para dar pull

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  // finding specie
+  const findingAnimal = species.find((elemento) => animal === elemento.specie);
+  return findingAnimal;
 }
 
 module.exports = countAnimals;
+
+console.log(countAnimals({ specie: 'penguins' }))
