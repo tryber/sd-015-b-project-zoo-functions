b1c22f95b8ba689662a7db2c9ca1ba0627a2835a
Caio <caiomgp22@gmail.com>
2021-09-14 17:29:51 -0300

    Adicionando getOldest

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const idAnimal = employees.find((person) => person.id === id).responsibleFor[0];
+  const listOfAnimals = species.find((specie) => specie.id === idAnimal).residents;
+  const oldest = listOfAnimals.reduce((older, animal) => (older.age > animal.age ? older : animal));
+  const { name, sex, age } = oldest;
+  const array = [];
+  array.push(name);
+  array.push(sex);
+  array.push(age);
+  return array;
 }
 
 module.exports = getOldestFromFirstSpecies;
