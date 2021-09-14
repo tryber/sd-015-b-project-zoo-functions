301e493fb7e40817a07f61e4001860301e29c3cd
Wander Luiz <wander.srv@gmail.com>
2021-09-13 21:03:02 -0300

    Resoluçao do requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const buscaNomaEspecie = species.find((especies) => especies.name === animal);
+
+  return buscaNomaEspecie.residents.every((animais) => animais.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
