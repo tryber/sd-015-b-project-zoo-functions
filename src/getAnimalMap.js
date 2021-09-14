a821d942efdb5c942afe782fdc50a86dafce3af1
Guilherme Saboia <gsaboia@yahoo.com>
2021-09-14 18:41:36 -0300

    Refatora requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,22 +1,28 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// adds residents names for each animal
 function getResidents(animalName, locationAnimals, options) {
   let animalInfo = '';
   let residentsNames = '';
+  // filters species using animalName
   animalInfo = species.filter((element) => element.name === animalName);
   let { residents } = animalInfo[0];
+  // filters if sex is passed
   if (options.sex) {
     const { sex } = options;
     residents = residents.filter((resident) => resident.sex === sex);
   }
+  // pushes spread list of residents names to value of item with key = animalName
   residentsNames = residents.map((resident) => resident.name);
   locationAnimals[animalName].push(...residentsNames);
+  // sorts if sorted is passed
   if (options.sorted) {
     locationAnimals[animalName].sort();
   }
 }
 
+// loops through each animal in location
 function loopThroughLocationAnimals(locationAnimals, options) {
   for (let i = 0; i < locationAnimals.length; i += 1) {
     const animalName = Object.keys(locationAnimals[i])[0];
@@ -24,6 +30,7 @@ function loopThroughLocationAnimals(locationAnimals, options) {
   }
 }
 
+// loops through each location (NE, NW, SE, SW)
 function loopThroughLocations(locations, options) {
   const locationsKeys = Object.keys(locations);
   for (let i = 0; i < locationsKeys.length; i += 1) {
@@ -32,6 +39,7 @@ function loopThroughLocations(locations, options) {
   }
 }
 
+// changes locations values to format [{ 'lions': [] }, { 'bears': [] }]
 function mapToObj(locations, options) {
   data.species.forEach((specie) => locations[specie.location].push({ [specie.name]: [] }));
   loopThroughLocations(locations, options);
@@ -40,7 +48,6 @@ function mapToObj(locations, options) {
 function dealWithOptions(options, locations) {
   const { includeNames } = options;
   if (includeNames) {
-    // console.log('includeNames');
     mapToObj(locations, options);
   }
 }
