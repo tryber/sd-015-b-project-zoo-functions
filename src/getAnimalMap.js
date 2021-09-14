328189696933dc55ceed2db1cc0654c0f3032c92
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-14 18:20:32 -0300

    Requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function listSex(options, specie) {
+  return specie.residents.reduce((arrayList, animal) => {
+    if (options.sex && animal.sex === options.sex) arrayList.push(animal.name);
+    if (!options.sex) arrayList.push(animal.name);
+    return arrayList;
+  }, []);
+}
+
+function getAnimalMap(options = { includeNames: false, sorted: false, sex: false }) {
+  const animalMap = data.species.reduce((obj, specie) => {
+    if (options.includeNames) {
+      const animalSex = listSex(options, specie);
+      if (options.sorted) {
+        animalSex.sort();
+      }
+      obj[specie.location].push({ [specie.name]: animalSex });
+    } else {
+      obj[specie.location].push(specie.name);
+    }
+    return obj;
+  }, { NE: [], NW: [], SE: [], SW: [] });
+  return animalMap;
 }
 
 module.exports = getAnimalMap;
