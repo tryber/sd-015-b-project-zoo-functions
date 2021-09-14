ca11f9a5a98a96a40bedd46732570208e1839391
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-09-14 18:54:26 -0300

    Refatoraçao no requisito 4 e citaçao no requisito 1

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,16 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  return data.employees.some((employee) => employee.managers.includes(id));
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   if (!isManager(managerId)) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
-  const relateds = data.employees.filter((employee) => (employee.managers.includes(managerId)));
-  return relateds.map((employee) => `${employee.firstName} ${employee.lastName}`);
+  const relatedEmployees = employees.filter((employee) => (employee.managers.includes(managerId)));
+  return relatedEmployees.map((employee) => `${employee.firstName} ${employee.lastName}`);
 }
-console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
