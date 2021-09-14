b9d88ba5d52e0b5674a0a57802ce2e68b7760574
Arthur Teixeira <tuzinf5@exemplo.br>
2021-09-14 17:00:10 -0300

    Requisito 7 - concluido

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
+const { species } = data;
+
+const withoutParameter = () => {
+  const object = {};
+  species.forEach((specie) => {
+    if (!object[specie.location]) object[specie.location] = [];
+    object[specie.location].push(specie.name);
+  });
+  return object;
+};
+
+const includeNameSortedSex = (sorted, sex) => {
+  const object = {};
+  species.forEach((specie) => {
+    if (!object[specie.location]) object[specie.location] = [];
+    let residents = [...specie.residents];
+    if (sex) residents = residents.filter((specieName) => specieName.sex === sex);
+    residents = residents.map((element) => element.name);
+    if (sorted) residents.sort();
+    object[specie.location].push({ [specie.name]: residents });
+  });
+  return object;
+};
+
+function getAnimalMap(options = {}) {
   // seu código aqui
+  if (options.includeNames) return includeNameSortedSex(options.sorted, options.sex);
+  return withoutParameter();
 }
 
 module.exports = getAnimalMap;
