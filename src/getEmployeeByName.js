d460c52fe56bbcff95e9cb7d812f254d94e5966f
Wilian Pereira <wilian_pereira92@outlook.com>
2021-09-14 08:48:49 -0300

    Adicionado requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) return {};
+  const nome = employeeName;
+  return data.employees.find((nomes) => nomes.firstName === nome || nomes.lastName === nome);
 }
 
 module.exports = getEmployeeByName;
