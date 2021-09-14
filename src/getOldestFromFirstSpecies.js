fcdc462328197a31814f940b3525cab186c6349f
Rafael <rafael.gomesbarretomoura@gmail.com>
2021-09-14 20:18:38 -0300

    adiciona requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const { species, employees } = data;
+  const employeeFound = employees.find((employee) => employee.id === id);
+  const firstSpecie = species.find((specie) => specie.id === employeeFound.responsibleFor[0]);
+  const animal = firstSpecie.residents.sort((animalA, animalB) => animalB.age - animalA.age)[0];
+
+  return Object.values(animal);
 }
 
 module.exports = getOldestFromFirstSpecies;
