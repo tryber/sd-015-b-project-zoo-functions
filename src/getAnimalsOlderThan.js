e21a6716b0f5f652aea0b9dff2947c04688d1781
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-14 01:08:18 -0300

    [Requisito 2] - Cria uma funçao que recebe dois parametros: animal e idade minima. A funçao verifica se todos os residentes possuem a idade minima. Se sim, retorna true, caso contrario, false

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,10 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const { species } = data;
+  const chosenAnimalResidents = species.find((obj) => obj.name === animal).residents;
+
+  return chosenAnimalResidents.every((element) => element.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
