fd54f2bb3733c68e5ba633af96e278046cc6aa4d
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-09-14 16:25:44 -0300

    Implementa a funcao getSpeciesByIds para o desafio 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const condition = (specie) => ids.includes(specie.id);
+  const filteredSpecies = species.filter(condition);
+  return filteredSpecies;
 }
 
 module.exports = getSpeciesByIds;
