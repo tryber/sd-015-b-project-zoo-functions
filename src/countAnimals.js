8680a23196fffebb0b962796a434c3ace4c222cd
Mariah M. Rodrigues <mariah.manetti@gmail.com>
2021-09-14 17:38:30 -0300

    Requisito 5 (incompleto) - Implemente a função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+const printAnimals = {};
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    species.forEach((specie) => {
+      printAnimals[specie.name] = specie.residents.length;
+    });
+    return printAnimals;
+  } if (!animal.gender) {
+    return species.find((animals) => animals.name === animal.specie).residents.length;
+  }
 }
 
 module.exports = countAnimals;
+
+// Esta função é responsável por contabilizar a quantidade de animais de cada espécie.
+// Sem parâmetros, retorna as espécies e sua quantidade;
+// Recebendo como parâmetro um objeto com a chave specie, retorna um número, a quantidade de animais daquela espécie;
+// Recebendo como parâmetro um objeto com a chave specie e sex, retorna um número, a quantidade de animais daquela espécie, no gênero selecionado.
