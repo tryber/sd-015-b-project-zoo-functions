656b398c2a60c148626a5b4d843afd37650b05c6
Marco <marquinco@outlook.com>
2021-09-14 15:02:45 -0300

    cumpre requisito um

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  } const idsFound = [];
+  ids.forEach((idUnico) => species.forEach((atual) => {
+    if (atual.id === idUnico) {
+      idsFound.push(atual);
+    }
+  }, []));
+  return idsFound;
 }
-
 module.exports = getSpeciesByIds;
