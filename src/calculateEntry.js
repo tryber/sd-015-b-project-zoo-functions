97f9685ddad9d055ca21f348d74221deb3515589
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-13 22:12:11 -0300

    Inicia implementaçao da logica do calcualteEntry.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,11 +2,38 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  let adult = 0; // Define o contador inicialmente com 0
+  let child = 0; // Define o contador inicialmente com 0
+  let senior = 0; // Define o contador inicialmente com 0
 }
 
-function calculateEntry(entrants) {
+function calculateEntry(...entrants) {
   // seu código aqui
+  const arrayNovo = [];
+  const functionEntrants = entrants.forEach( (entrant) => { // Para cada pessoa no Array Recebido
+    arrayNovo.push(entrant.age); // Capture sua idade
+    // console.log(arrayNovo); // [DEBUG] Verifica entrada do Array
+    countEntrants(arrayNovo); // Chama a função que irá contar quantas pessoas de cada faixa etária entrarão,
+    // passando como parâmetro, o valor do array com a idade de Cada um.
+  });
 }
+
+const pessoasVisitantes = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
+// calculateEntry({ name: 'Lara Carvalho', age: 5 }, { name: 'Frederico Moreira', age: 5 });
+calculateEntry(pessoasVisitantes[0],
+  pessoasVisitantes[1],
+  pessoasVisitantes[2],
+  pessoasVisitantes[3],
+  pessoasVisitantes[4],
+  pessoasVisitantes[5]);
 
 module.exports = { calculateEntry, countEntrants };
 
