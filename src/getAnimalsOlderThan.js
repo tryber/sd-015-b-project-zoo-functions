2134a4cc6d029c74ab4839ed4b21d5179a8ea597
Claudio Junior Haas <claudiohaas.cjh@gmail.com>
2021-09-14 09:17:58 -0300

    Requisito 2: Correçao Linch

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,11 +1,11 @@
-const { name } = require('faker');
+// const { name } = require('faker');
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-    const finderAnimal = species.find((element) => element.name === animal).residents;
-    const checkAge = finderAnimal.every((specie) => specie.age >= age);
-    return checkAge;
+  const finderAnimal = species.find((element) => element.name === animal).residents;
+  const checkAge = finderAnimal.every((specie) => specie.age >= age);
+  return checkAge;
 }
 
 module.exports = getAnimalsOlderThan;
