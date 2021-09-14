89879f37b48d20fce057730213c3c62cea7bb168
fernandamichetti <f.michetti@hotmail.com>
2021-09-14 15:38:14 -0300

    Refatorando o Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,6 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 // um ou mais ids (parametros, então coloquei os ..., chamado de rest argument) //
-const getSpeciesByIds = (...ids) => species.filter((kind) => ids.includes(kind.id));
+const getSpeciesByIds = (...ids) => data.filter((kind) => ids.includes(kind.id));
 
 module.exports = getSpeciesByIds;
