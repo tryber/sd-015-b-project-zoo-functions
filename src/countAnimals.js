4f61445cc399472d7d102c4d9400cf501e5e5f23
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-14 00:15:04 -0300

    Adiciona logica para quando o animal nao e passado.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
+const { species } = data;
+
 
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    // console.log("Resultado sem nenhum animal passado!!!"); // [DEBUG] Verifica entrada na função
+    const arrayAnimals = [];
+    const mapAnimais = species.map( (specie) => {
+      arrayAnimals.push(`${specie.name}: ${specie.residents.length}`);
+    });
+    console.log(arrayAnimals);
+    return arrayAnimals;
+  }
+  
 }
 
+countAnimals();
+
 module.exports = countAnimals;
+
+// LÓGICA DA FUNÇÃO:
+// ** Contabiliza quantidade de animais
+// ** Se nenhum parâmetro é passado, retorna um objeto { nomeDoAnimal: value };
+// ** Se é passada somente a espécie, é retornado todos os animais daquela espécie
+// ** Se é passado um animal e seu gênero, é retornado todos os animais daquela espécie e gênero
