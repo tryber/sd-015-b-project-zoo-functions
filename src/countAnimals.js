47db36307f597c655cf34a815974ce94e9e25207
Lucas Peres <lucasperesss@gmail.com>
2021-09-14 00:36:14 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(obj) {
+  if (!obj) {
+    const all = {};
+    data.species.forEach((specie) => {
+      all[specie.name] = specie.residents.length;
+    });
+    return all;
+  }
+  const { specie: specieName, gender = 'both' } = obj;
+  const actualSpecie = data.species.find((specie) => specie.name === specieName);
+  if (gender === 'both') {
+    return actualSpecie.residents.length;
+  }
+  return actualSpecie.residents.filter((individual) => individual.sex === gender).length;
 }
 
 module.exports = countAnimals;
