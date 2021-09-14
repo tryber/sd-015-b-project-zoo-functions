9c2d8455d5d1a4f02a6710a937b4288b41b701d0
Caio R. Vieira <caio.robs98@gmail.com>
2021-09-14 13:28:17 -0300

    requisito 7 ok

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,45 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function getNames(animal, options) {
+  const { sex } = options;
+  const { sorted } = options;
+  const nomes = data.species.find(({ name }) => (name === animal));
+  const residentes = nomes.residents;
+  const objeto = {
+    [animal]: residentes.map((resident) => resident.name),
+  };
+
+  if (sex) {
+    const residentesFromSex = residentes.filter((resident) => resident.sex === sex);
+    objeto[animal] = residentesFromSex.map((resident) => resident.name);
+  }
+  if (sorted) {
+    objeto[animal] = objeto[animal].sort();
+  }
+  return objeto;
 }
+
+function getAnimalMap(options) {
+  const { species } = data;
+  const directions = {
+    NE: species.filter(({ location }) => location === 'NE').map(({ name }) => name),
+    NW: species.filter(({ location }) => location === 'NW').map(({ name }) => name),
+    SE: species.filter(({ location }) => location === 'SE').map(({ name }) => name),
+    SW: species.filter(({ location }) => location === 'SW').map(({ name }) => name),
+  };
+  function insertNames() {
+    directions.NE = directions.NE.map((animal) => (getNames(animal, options)));
+    directions.NW = directions.NW.map((animal) => (getNames(animal, options)));
+    directions.SE = directions.SE.map((animal) => (getNames(animal, options)));
+    directions.SW = directions.SW.map((animal) => (getNames(animal, options)));
+  }
+  if (!options || !options.includeNames) return directions;
+  if (options.includeNames) {
+    insertNames();
+    return directions;
+  }
+}
+
+// console.table(getAnimalMap({ includeNames: true }));
 
 module.exports = getAnimalMap;
