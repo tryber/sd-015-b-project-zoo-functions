637bf0963df71f47004ed5194495a1e26863e010
Gustavo Gracio <gustavogracio@hotmail.com>
2021-09-14 18:00:41 -0300

    Requisito 9 refatorado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,18 +1,14 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(...ids) {
+function getOldestFromFirstSpecies(id) {
   // seu código aqui
-  const result = ids.map((id) => {
-    const idSelected = data.employees.filter((employee) => employee.id === id);
-    const idAnimal = idSelected[0].responsibleFor[0];
-    const animail = species.filter((an) => an.id === idAnimal);
-    const oldAnimail = animail[0].residents.sort((a, b) => b.age - a.age)[0];
-    const { name, sex, age } = oldAnimail;
-    return [name, sex, age];
-  });
-  if (ids.length === 1) return result[0];
-  return result;
+  const idSelected = data.employees.filter((employee) => employee.id === id);
+  const idAnimal = idSelected[0].responsibleFor[0];
+  const animail = species.filter((an) => an.id === idAnimal);
+  const oldAnimail = animail[0].residents.sort((a, b) => b.age - a.age)[0];
+  const { name, sex, age } = oldAnimail;
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
