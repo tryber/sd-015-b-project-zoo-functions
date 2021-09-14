89e3fcf3ceff180d61ffc880d3ddf11b050c5a5d
Erik Lopes <erik.lopesdev@gmail.com>
2021-09-14 13:38:31 -0300

    Desafio 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,21 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) return {};
+
+  let employee = data.employees.find(
+    (verifyEmployeeName) => employeeName === verifyEmployeeName.firstName,  
+    );
+
+  if (!employee) {
+    employee = data.employees.find(
+      (verifyEmployeeName) => employeeName === verifyEmployeeName.lastName,  
+      );
+  }
+
+  return employee;
 }
+
+console.log(getEmployeeByName('Nelson'));
 
 module.exports = getEmployeeByName;
