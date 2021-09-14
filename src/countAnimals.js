8fc4f2f1c9c6837bb4fc06d68a30a462abfe9dc4
Paulo Flóra <tflora.2004@gmail.com>
2021-09-14 18:33:16 -0300

    Feito o requisito 5.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const result = {};
+    const animals = data.species;
+    animals.forEach((specie) => {
+      const count = specie.residents.length;
+      result[`${specie.name}`] = count;
+    });
+    return result;
+  }
+  let count = 0;
+  const countedAnimal = data.species.find((specie) => specie.name === animal.specie).residents;
+  if (!animal.gender) {
+    count = countedAnimal.length;
+    return count;
+  }
+  count = countedAnimal.filter((specie) => specie.sex === animal.gender).length;
+  console.log(count);
+  return count;
 }
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
