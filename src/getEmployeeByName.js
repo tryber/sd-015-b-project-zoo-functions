823a4dba3e7ebb0bba5dd0177e5634318b9e4798
Antônio Campos <aclcampos@live.com>
2021-09-14 15:59:24 -0300

    Realizado requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const employeeByName = employees.filter((employee) => (employeeName.includes(employee.firstName)
+  || employeeName.includes(employee.lastName)));
+  return employeeByName.reduce(() => employeeByName);
 }
+
+console.log(getEmployeeByName('Wishart'));
 
 module.exports = getEmployeeByName;
