153fcf23fab565c11801bf0b863cc3e390dc6a77
Caio R. Vieira <caio.robs98@gmail.com>
2021-09-14 13:54:38 -0300

    mais refatoraçoes requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,39 +1,37 @@
 const data = require('../data/zoo_data');
 
 function getNames(animal, options) {
-  const { sex } = options;
-  const { sorted } = options;
-  const nomes = data.species.find(({ name }) => (name === animal));
-  const residentes = nomes.residents;
-  const objeto = {
-    [animal]: residentes.map((resident) => resident.name),
+  const { sex, sorted } = options;
+  const selectedAnimal = data.species.find(({ name }) => (name === animal));
+  const { residents } = selectedAnimal;
+  const animalObject = {
+    [animal]: residents.map(({ name }) => name),
   };
 
   if (sex) {
-    const residentesFromSex = residentes.filter((resident) => resident.sex === sex);
-    objeto[animal] = residentesFromSex.map((resident) => resident.name);
+    const residentesFromSex = residents.filter(({ sex: residentSex }) => residentSex === sex);
+    animalObject[animal] = residentesFromSex.map((resident) => resident.name);
   }
   if (sorted) {
-    objeto[animal] = objeto[animal].sort();
+    animalObject[animal] = animalObject[animal].sort();
   }
-  return objeto;
+  return animalObject;
 }
 
 function getAnimalMap(options) {
-  const { species } = data;
   const directions = {
-    NE: species.filter(({ location }) => location === 'NE').map(({ name }) => name),
-    NW: species.filter(({ location }) => location === 'NW').map(({ name }) => name),
-    SE: species.filter(({ location }) => location === 'SE').map(({ name }) => name),
-    SW: species.filter(({ location }) => location === 'SW').map(({ name }) => name),
+    NE: data.species.filter(({ location }) => location === 'NE').map(({ name }) => name),
+    NW: data.species.filter(({ location }) => location === 'NW').map(({ name }) => name),
+    SE: data.species.filter(({ location }) => location === 'SE').map(({ name }) => name),
+    SW: data.species.filter(({ location }) => location === 'SW').map(({ name }) => name),
   };
+  if (!options || !options.includeNames) return directions;
   function insertNames() {
     directions.NE = directions.NE.map((animal) => (getNames(animal, options)));
     directions.NW = directions.NW.map((animal) => (getNames(animal, options)));
     directions.SE = directions.SE.map((animal) => (getNames(animal, options)));
     directions.SW = directions.SW.map((animal) => (getNames(animal, options)));
   }
-  if (!options || !options.includeNames) return directions;
   if (options.includeNames) {
     insertNames();
     return directions;
