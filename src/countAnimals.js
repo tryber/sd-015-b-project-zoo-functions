c12f957f64212e5dcbc13fc44c05765e32e4b18d
iuryroque <iury.roque@hotmail.com>
2021-09-14 17:12:35 -0300

    Requisito 5 finalizado e tests 5 corrigido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const result = {};
+    species.forEach((specie) => {
+      result[specie.name] = specie.residents.length;
+    });
+    return result;
+  }
+
+  const bichos = species.find((specie) => specie.name === animal.specie);
+  if (animal.sex) {
+    return bichos.residents.filter((bicho) => bicho.sex === animal.sex).length;
+  }
+  return bichos.residents.length;
 }
 
 module.exports = countAnimals;
