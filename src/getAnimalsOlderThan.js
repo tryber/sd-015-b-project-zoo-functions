a4d8a1dc59076649beae67e8b0d76f4da7c6e84b
Rodolfomno <Rodolfo_mno@hotmail.com>
2021-09-13 22:20:07 -0300

    adiciona resolucao requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const bixo = species.find((especie) => especie.name === animal);
+  const every = bixo.residents.every((especie) => especie.age >= age);
+  return every;
 }
 
 module.exports = getAnimalsOlderThan;
