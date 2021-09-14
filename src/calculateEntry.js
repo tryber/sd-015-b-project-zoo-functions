1f45a14487c4cb47cbf9fffd005cceff0fdf7bc9
LeonardoMonti <contatoleomonti@gmail.com>
2021-09-14 14:56:44 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,34 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const objAmount = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  entrants.forEach(({ age }) => {
+    if (age < 18) objAmount.child += 1;
+
+    else if (age >= 18 && age < 50) objAmount.adult += 1;
+
+    else objAmount.senior += 1;
+  });
+
+  return objAmount;
 }
 
+const { prices } = data;
+
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.entries(entrants).length === 0) return 0;
+  const counts = countEntrants(entrants);
+
+  const { child: childPrice, adult: adultPrice, senior: seniorPrice } = counts;
+  const { child, adult, senior } = prices;
+
+  const result = ((child * childPrice) + (adult * adultPrice) + (senior * seniorPrice));
+
+  return result;
 }
 
 module.exports = { calculateEntry, countEntrants };
