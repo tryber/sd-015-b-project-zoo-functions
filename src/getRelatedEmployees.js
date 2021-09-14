d4752e0159dabaa345f931e923feba1e4c37a76e
Valdênio <valdeniomartinho@gmail.com>
2021-09-14 11:47:11 -0300

    adiciona requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
   // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
+function employeesByManager(managerId) {
+  return employees.reduce((acc, curr) => ((curr.managers.includes(managerId))
+    ? acc.concat(`${curr.firstName} ${curr.lastName}`) : acc), []);
+}
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId) === true) {
+    return employeesByManager(managerId);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
