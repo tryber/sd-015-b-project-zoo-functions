aad5dca9335cecb4bdb26dad2fe5fb98ec4fb40d
jpcp0614 <jpcp0614@gmail.com>
2021-09-14 14:32:48 -0300

    Requisito 6 OK

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,21 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age < 18).length;
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+
+  const ages = countEntrants(entrants);
+  const countChild = ages.child * prices.child;
+  const countAdult = ages.adult * prices.adult;
+  const countSenior = ages.senior * prices.senior;
+  return countChild + countAdult + countSenior;
 }
 
 module.exports = { calculateEntry, countEntrants };
