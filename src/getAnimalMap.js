be29da0c6b216967a07f7f159d72d558ed1326a0
Denis Jonathan <denisjonathan7@gmail.com>
2021-09-13 21:41:08 -0300

    :art: Requisito 7: refatorado

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,123 +1,41 @@
 const { species } = require('../data/zoo_data');
 
-const regions = ['NE', 'NW', 'SE', 'SW'];
+function searchDefault(sorted, sex) {
+  const regions = {};
 
-function filterByRegions(region) {
-  return species.filter((specie) => specie.location === region);
-}
-
-function outputDeafult() {
-  const objectOfRegions = { NE: [], NW: [], SE: [], SW: [] };
-
-  regions.forEach((region) => Object.assign(objectOfRegions[region],
-    filterByRegions(region).map((resident) => resident.name)));
-
-  return objectOfRegions;
-}
-
-function includeName() {
-  const objectOfRegions = { NE: [], NW: [], SE: [], SW: [] };
-
-  regions.forEach((region) => {
-    filterByRegions(region).forEach((specie) => {
-      let tempObj = {};
-      const { name, residents } = specie;
-      tempObj = { [name]: [] };
-      residents.forEach((resident) => {
-        const residentName = resident.name;
-        tempObj[name].push(residentName);
-      });
-      objectOfRegions[region].push(tempObj);
-    });
+  species.forEach((specie) => {
+    if (!regions[specie.location]) regions[specie.location] = [];
+    if (sex) {
+      const verifyGender = specie.residents.some((resident) => resident.sex === sex);
+      if (!verifyGender) return;
+    }
+    regions[specie.location].push(specie.name);
+    if (sorted) regions[specie.location].sort();
   });
 
-  return objectOfRegions;
+  return regions;
 }
 
-function includeNameSorted() {
-  const objectOfRegions = { NE: [], NW: [], SE: [], SW: [] };
+function animalNames(sorted, sex) {
+  const regions = {};
 
-  regions.forEach((region) => {
-    filterByRegions(region).forEach((specie) => {
-      let tempObj = {};
-      const { name, residents } = specie;
-      tempObj = { [name]: [] };
-      residents.forEach((resident) => {
-        const residentName = resident.name;
-        tempObj[name].push(residentName);
-      });
-      tempObj[name].sort();
-      objectOfRegions[region].push(tempObj);
-    });
+  species.forEach((specie) => {
+    if (!regions[specie.location]) regions[specie.location] = [];
+    let residents = [...specie.residents];
+    if (sex) residents = residents.filter((resident) => resident.sex === sex);
+    residents = residents.map((resident) => resident.name);
+    if (sorted) residents.sort();
+    regions[specie.location].push({ [specie.name]: residents });
   });
 
-  return objectOfRegions;
+  return regions;
 }
 
-function filterOfGender(gender) {
-  const objectOfRegions = { NE: [], NW: [], SE: [], SW: [] };
+function getAnimalMap(options = {}) {
+  const { includeNames, sorted, sex } = options;
 
-  regions.forEach((region) => {
-    filterByRegions(region).forEach((specie) => {
-      let tempObj = {};
-      const { name, residents } = specie;
-      tempObj = { [name]: [] };
-      residents.forEach((resident) => {
-        if (resident.sex === gender) {
-          const residentName = resident.name;
-          tempObj[name].push(residentName);
-        }
-      });
-      objectOfRegions[region].push(tempObj);
-    });
-  });
-
-  return objectOfRegions;
-}
-
-function filterOfGenderSorted(gender) {
-  const objectOfRegions = { NE: [], NW: [], SE: [], SW: [] };
-
-  regions.forEach((region) => {
-    filterByRegions(region).forEach((specie) => {
-      let tempObj = {};
-      const { name, residents } = specie;
-      tempObj = { [name]: [] };
-      residents.forEach((animal) => {
-        if (animal.sex === gender) {
-          const residentName = animal.name;
-          tempObj[name].push(residentName);
-        }
-      });
-      tempObj[name].sort();
-      objectOfRegions[region].push(tempObj);
-    });
-  });
-  return objectOfRegions;
-}
-
-function verifyInput(input, gender) {
-  const objOfOptions = {
-    truefalsefalse: includeName(),
-    truetruefalse: includeNameSorted(),
-    truefalsefemale: filterOfGender(gender),
-    truefalsemale: filterOfGender(gender),
-    truetruefemale: filterOfGenderSorted(gender),
-    truetruemale: filterOfGenderSorted(gender),
-    falsefalsefemale: outputDeafult(),
-    falsetruefemale: outputDeafult(),
-  };
-  return objOfOptions[input];
-}
-
-function getAnimalMap(options) {
-  if (!options) return outputDeafult();
-
-  const { includeNames = false, sorted = false, sex = false } = options;
-
-  const optionsOfOptions = `${includeNames}${sorted}${sex}`;
-
-  return verifyInput(optionsOfOptions, sex);
+  if (includeNames) return animalNames(sorted, sex);
+  return searchDefault(sorted, sex);
 }
 
 module.exports = getAnimalMap;
