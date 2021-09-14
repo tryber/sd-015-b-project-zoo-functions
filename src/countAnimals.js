fac5cbf85151924b60933743b239079fa84a40e1
Flávio Bianchetti <fjb.developer@gmail.com>
2021-09-14 16:36:38 -0300

    Requisito 5 - alterada estrutura para correçao dos novos erros de indentaçao

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -9,14 +9,11 @@ const ruleSpecie = (acc, specie, spec) =>
   (specie.name === spec ? specie.residents.length : acc);
 
 const ruleSpecieAndGender = (acc, specie, spec, gen) =>
-  (specie.name === spec ? specie.residents.reduce((accumulator, resident) =>
-    resident.sex === gen ? accumulator += 1 : accumulator, acc) : acc);
+  (specie.name === spec ? (specie.residents.reduce((accumulator, resident) =>
+    resident.sex === gen ? accumulator += 1 : accumulator, acc)) : acc);
 
 const myReduce = (spec, gen, func, defaultValue) =>
-  data.species.reduce((acc, specie) => {
-    acc = func(acc, specie, spec, gen);
-    return acc;
-  }, defaultValue);
+  data.species.reduce((acc, specie) => func(acc, specie, spec, gen), defaultValue);
 
 const animalNotFound = () =>
   myReduce(undefined, undefined, ruleUndefined, {});
