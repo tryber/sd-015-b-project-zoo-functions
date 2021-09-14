a1faf2c0e9cd48b35880f777ca9d8cbcde3114dc
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-09-14 19:11:16 -0300

    implementa a funcao getAnimalsOlderThan para o desafio 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const findSpecie = (specie) => specie.name === animal.toLowerCase();
+  const foundSpecie = data.species.find(findSpecie);
+  const checkAge = (individual) => individual.age >= age;
+  const checkedAges = foundSpecie.residents.every(checkAge);
+  return checkedAges;
 }
 
 module.exports = getAnimalsOlderThan;
