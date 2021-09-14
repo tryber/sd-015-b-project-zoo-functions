c1320378b95ada98a381ac37a9d55930891c9d89
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2021-09-14 16:27:09 -0300

    arrumando requesito 5 depois do pull

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -7,14 +7,14 @@ function countAnimals(animal) {
     // acessado : https://github.com/tryber/sd-014-b-project-zoo-functions/blob/Jo%C3%A3oHenrique-Zoo-Functions/src/zoo.js
       ({ ...acc, [value.name]: value.residents.length }), {});
     // ...
-  } if (animal.gender === undefined) {
+  } if (animal.sex === undefined) {
     return species.find((animals) => animals.name === animal.specie).residents.length;
   //  Primeiro criamos o critério que só pode entrar quem tiver a chave animal.gender indefinodo
   //  e depois procuramos animals.name iguais a animal.specie e contamos quantos residentes existem
   //  residents.length
   }
   return species.find((animals) => animals.name === animal.specie).residents
-    .filter((number) => number.sex === animal.gender).length;
+    .filter((number) => number.sex === animal.sex).length;
   //  O início é exatamente igual ao anterior, só adicionamos o filter para
   // nos dar apenas os animais do genero que se passa por parametro e assim contabilizar.
 }
