5f0c1b4fca6136b296b106eda30ab1bbc7e2fdd0
Bruno Bartolomeu <bruno.bartolomeu5@hotmail.com>
2021-09-14 14:58:52 -0300

    Envia requisito 8

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,39 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const animalsLocation = {
+  NE: [],
+  NW: [],
+  SE: [],
+  SW: [],
+};
+function getMap() {
+  const animals1 = { ...animalsLocation };
+  species.forEach(({ name, location }) => {
+    animals1[location].push(name);
+  });
+  console.log(animals1);
+  return animals1;
 }
+
+function getNamesMap() {
+  const animals2 = { ...animalsLocation };
+  species.forEach(({ name, location, residents }) => {
+    const obj = {};
+    const animalNames = residents.map((resident) => resident.name);
+    obj[name] = animalNames;
+    animals2[location].push(obj);
+  });
+  console.log(animals2);
+  return animals2;
+}
+
+function getAnimalMap(options) {
+  if (!options) return getMap();
+  if (options.includeNames) return getNamesMap();
+}
+
+getAnimalMap({ includeNames: true });
+getAnimalMap();
 
 module.exports = getAnimalMap;
