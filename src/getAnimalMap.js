71563af0406f49b172f0449f2bcf705faafd93ad
Amanda Fernandes <aferanda@gmail.com>
2021-09-14 15:50:14 -0300

    Requisito 7 - parte 2

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,18 +1,41 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getAnimalLocation = () => species
-  .reduce((object, current) => Object
-    .assign(object, { [current.location]: (species
-      .filter(({ location }) => location === current.location))
-      .map(({ name }) => name) }), {});
+const getAnimalLocation = () => {
+  const object = {};
+  species.forEach(({ location, name }) => {
+    if (!object[location]) {
+      object[location] = [];
+    }
+    object[location].push(name);
+  });
+  return object;
+};
+
+const getAnimalIncludeName = () => {
+  const object = {};
+  species.forEach(({ location, name, residents }) => {
+    if (!object[location]) {
+      object[location] = [];
+    }
+    let resid = [...residents];
+    resid = residents.map((resident) => resident.name);
+    object[location].push({ [name]: resid });
+  });
+  return object;
+};
 
 function getAnimalMap(options) {
   if (!options) {
     return getAnimalLocation();
   }
+
+  if (options.includeNames) {
+    return getAnimalIncludeName();
+  }
 }
 
 console.log(getAnimalMap());
+console.log(getAnimalMap({ includeNames: true }));
 
 module.exports = getAnimalMap;
