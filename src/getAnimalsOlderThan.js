504eb6ee1a22b3b78fc4f4bc94839abcfeb1afab
fernandamichetti <f.michetti@hotmail.com>
2021-09-14 15:57:09 -0300

    Corrigindo lint requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 // Const Animal acha o animal que foi passado no parametro //
 const getAnimalsOlderThan = (animal, age) => {
   const animalSpecie = data.species.find((kind) => kind.name === animal);
-return animalSpecie.residents.every((ageAnimal) => ageAnimal.age > age);
-}
+  return animalSpecie.residents.every((ageAnimal) => ageAnimal.age > age);
+};
 
 module.exports = getAnimalsOlderThan;
