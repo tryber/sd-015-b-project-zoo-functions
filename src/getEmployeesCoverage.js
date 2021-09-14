eaed4bc7d5f6f40339cf6a8a50acfc3eecd07f21
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-14 17:22:14 -0300

    [Requisito 10] - Cria uma funçao que retorna um objeto com o id, fullname, species e locations dos funcionarios. Caso o parametro nao exista, retorna um array com o objeto para todos os funcionarios. Caso o id ou nome seja invalido, retorna um error. Se nada disso ocorrer, retorna o objeto para o funcionario passado por parametro

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,25 +2,53 @@ const data = require('../data/zoo_data');
 
 const { species, employees } = data;
 
+function getParameters(employeeFound) {
+  const { id, firstName, lastName, responsibleFor } = employeeFound;
+
+  const fullName = `${firstName} ${lastName}`;
+  const getSpecies = responsibleFor.map(
+    (animal) => species.find((obj) => obj.id.includes(animal)).name,
+  );
+  const getLocations = responsibleFor.map(
+    (animal) => species.find((obj) => obj.id.includes(animal)).location,
+  );
+
+  return [id, fullName, getSpecies, getLocations];
+}
+
+const createObjectCoverage = (id, fullName, species, locations) => ({
+  id,
+  fullName,
+  species,
+  locations,
+});
+
 function getEmployeesCoverage(myEmployee) {
   // seu código aqui
+  if (myEmployee === undefined) {
+    return employees.map((element) => createObjectCoverage(...getParameters(element)));
+  }
+
   const { name: employeeName, id: employeeId } = myEmployee;
+
+  const arrayOfIds = employees.map((obj) => obj.id);
+  const arrayOfNames = employees
+    .map((obj) => [obj.firstName, obj.lastName])
+    .reduce((acc, curVal) => acc.concat(curVal));
+  if (
+    (employeeName !== undefined && arrayOfNames.includes(employeeName) === false)
+    || (employeeId !== undefined && arrayOfIds.includes(employeeId) === false)
+  ) {
+    throw new Error('Informações inválidas');
+  }
 
   const findEmployee = employees.find(
     ({ id, firstName, lastName }) =>
       id === employeeId || firstName === employeeName || lastName === employeeName,
   );
-
-  const employeeCoverage = (id, fullName, species, locations) => ({
-    id,
-    fullName,
-    species,
-    locations,
-  });
-
-  return findEmployee;
+  return createObjectCoverage(...getParameters(findEmployee));
 }
 
-console.log(getEmployeesCoverage({ name: 'Sharonda' }));
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
