bfb89c5f30de6be2a923b31b783386387b9d40b5
Ismael_Luviseti <ismaelluviseti@gmail.com>
2021-09-14 12:36:07 -0300

    requisito 3 finalizado

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  return data.employees.find((employee) => (employee.firstName === employeeName)
+  || (employee.lastName === employeeName)) || {};
 }
 
 module.exports = getEmployeeByName;
