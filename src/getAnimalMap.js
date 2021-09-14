99b59a1dd1dcde2a0f04b449996bf414837610de
Rafael Colombo <rcolombodev@gmail.com>
2021-09-14 11:31:49 -0300

    Refatora requisito 3

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,18 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+function makeObjectWithoutParameters() {
+  const cardinalDirections = {};
 
+  species.forEach((specie) => {
+    if (!cardinalDirections[specie.location]) cardinalDirections[specie.location] = [];
+
+    cardinalDirections[specie.location].push(specie.name);
+  });
+  return cardinalDirections;
+}
+function getAnimalMap(options) {
+  if (!options) return makeObjectWithoutParameters();
+}
+console.log(getAnimalMap());
 module.exports = getAnimalMap;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,8 +2,8 @@ const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  return !employeeName ? {} : employees
-    .find((person) => person.firstName === employeeName || person.lastName === employeeName);
+  return employees
+    .find((person) => person.firstName === employeeName || person.lastName === employeeName) || {};
 }
 
 module.exports = getEmployeeByName;
