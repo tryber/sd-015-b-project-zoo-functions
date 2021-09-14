de66da9a2487d68da3dac8c721e59e6ebd3d8365
1lucascq <1lucascq@gmail.com>
2021-09-14 10:21:48 -0300

    Requisito 3 completo, requisitos 4 e 5 parciais

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(animal, gender) {
+  const allSpecies = species.map((specie) => specie.name);
+  const howManyAnimals = species.map((specie) => specie.residents.length);
+  const reduceSpecies = species.reduce((acc, cur, i, ar) => {
+    // console.log('-------ACC é:', acc);
+    // console.log('-------CUR é:', cur);
+    // console.log('-------IND é:', i);
+    // console.log('-------ACC é:', acc);
+    acc[allSpecies[i]] = howManyAnimals[i];
+    return acc;
+  }, {});
+  // console.log('-->  allSpecies é:', allSpecies);
+  // console.log('-->  howManyAnimals é:', howManyAnimals);
+  // const allAnimals = {
+  //   allSpecies: 'teste',
+  // };
+  return reduceSpecies;
 }
+console.log(countAnimals())
+// console.log(data.species.length)
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const check = employees.some(() => (employees.find((k) => k.managers.find((m) => m === id))));
+  // const employee = employees.filter((person) => person.id === id);
+  // console.log('----------------------check É:', check);
+  return check;
 }
+console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
-}
+// function getRelatedEmployees(managerId) {
+//   // seu código aqui
+// }
 
-module.exports = { isManager, getRelatedEmployees };
+// module.exports = { isManager, getRelatedEmployees };
