184c58ad03867bb52e54bb8913fca289a7bf965a
DanielHott <daniel_hott@outlook.com>
2021-09-14 18:04:52 -0300

    Adicionando elemento 4

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const animais = data.species;
+
+function countAnimals(object) {
+  if (object === undefined) {
+    return null;
+  }
+  const animal = object.specie;
+  const genero = object.gender;
+  const quantidade = animais.find((element) => element.name === animal);
+  const numero = quantidade.residents.length;
+  let contador = 0;
+  quantidade.residents.forEach((element) => {
+    if (element.sex === genero) {
+      contador += 1;
+    }
+  });
+  if (genero === undefined) {
+    return numero;
+  } return contador;
 }
 
+console.log(countAnimals());
 module.exports = countAnimals;
