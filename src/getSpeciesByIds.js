d64423c9622bd289adacd42787869e97af379356
Hugoacr <hugoacr@yahoo.com.br>
2021-09-13 23:48:18 -0300

    responde requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+// utilizado o código do perfil de  lucas-peres-zoo-functions-project; identifiquei onde eu estava errando
+function getSpeciesByIds(...ids) {
+  // verifica se o id repassado é um array vazio;
+  if (ids.length === 0) {
+    return [];
+  }
+  const answer = [];
+  // cria um loop com os id's inseridos
+  ids.forEach((id) => {
+    // preenche o array com os Id's repassados;
+    const selectedId = species.find((specie) => specie.id === id);
+    answer.push(selectedId);
+  });
+  return answer;
 }
 
 module.exports = getSpeciesByIds;
