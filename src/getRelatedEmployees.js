71a97feb35ba4dcdf67666c92569f730e6e7e191
Richard Leal <rwsleal@gmail.com>
2021-09-14 16:25:34 -0300

    Requisito 4 finalizado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((person) => person.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const collaborators = data.employees.filter((person) => person.managers.includes(managerId));
+    return collaborators.map((fullName) => `${fullName.firstName} ${fullName.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
+console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 module.exports = { isManager, getRelatedEmployees };
