bf32e09982f47fc722751fb8cd50227f3f59b9f7
Allan Melo <allan-m-silva@hotmail.com>
2021-09-14 15:56:33 -0300

    ajusta requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,25 +1,27 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  const childNum = entrants.filter((element) => element.age < 18);
-  const seniorNum = entrants.filter((element) => element.age >= 50);
-  const adultNum = entrants.filter((element) => element.age >= 18 && element.age < 50);
+  if (Object.keys(entrants).length !== 0) {
+    const childNum = entrants.filter((element) => element.age < 18);
+    const seniorNum = entrants.filter((element) => element.age >= 50);
+    const adultNum = entrants.filter((element) => element.age >= 18 && element.age < 50);
 
-  const result = { child: childNum.length, adult: adultNum.length, senior: seniorNum.length };
-  return result;
+    return { child: childNum.length, adult: adultNum.length, senior: seniorNum.length };
+  }
+  return false;
 }
 
 function calculateEntry(entrants) {
-  if (!entrants) {
-    return 0;
-  }
-  const visitors = countEntrants(entrants);
-  const { prices } = data;
-  const adult = visitors.adult * prices.adult;
-  const child = visitors.child * prices.child;
-  const senior = visitors.senior * prices.senior;
-  const newSum = adult + child + senior;
-  return newSum;
+  if (entrants && countEntrants(entrants) !== false) {
+    // resultado final do problema de objeto visto e entendido a partir do projeto de Daniel Acorroni
+    const visitors = countEntrants(entrants);
+    const { prices } = data;
+    const adult = visitors.adult * prices.adult;
+    const child = visitors.child * prices.child;
+    const senior = visitors.senior * prices.senior;
+    const sum = adult + child + senior;
+    return sum;
+  } return 0;
 }
 
 module.exports = { calculateEntry, countEntrants };
