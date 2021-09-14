fc1f5f94e26327235a79806479934ec7daf40aaa
PedroAugmed <pedroaugmed7@gmail.com>
2021-09-14 16:53:06 -0300

    Corrige 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -8,16 +8,16 @@ function countAnimals(animal) {
       object[element.name] = element.residents.length;
     });
     return object;
-  } if (animal.specie && animal.gender === undefined) {
+  } if (animal.specie && animal.sex === undefined) {
     const dataSpecies = data.species.find((specie) => specie.name === animal.specie);
     return dataSpecies.residents.length;
-  } if (Object.keys(animal)[1] === 'gender') {
+  } if (Object.keys(animal)[1] === 'sex') {
     const specieAndSex = data.species.find((bicho) => bicho.name === animal.specie);
-    return specieAndSex.residents.filter((wildAnimal) => wildAnimal.sex === animal.gender).length;
+    return specieAndSex.residents.filter((wildAnimal) => wildAnimal.sex === animal.sex).length;
   }
 }
 
 module.exports = countAnimals;
 console.log(countAnimals());// OK
 console.log(countAnimals({ specie: 'penguins' }));// OK
-console.log(countAnimals({ specie: 'giraffes', gender: 'female' }));// OK
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));// OK
