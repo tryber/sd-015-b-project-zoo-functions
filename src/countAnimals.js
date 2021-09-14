6b9b31160a343f6ffbce2c8562d21f8a89d13793
matheusg18 <matheusgabriel.18@gmail.com>
2021-09-14 16:37:31 -0300

    Requisito 5 -> Troquei o nome do parametro gender para sex :grey_exclamation:

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,19 +2,19 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 
-const countAnimals = ({ specie: specieName, gender } = {}) => {
+const countAnimals = ({ specie: specieName, sex } = {}) => {
   let count;
 
   if (typeof specieName === 'undefined') {
     count = species.reduce(
       (total, specie) => ({ ...total, [specie.name]: specie.residents.length }), {},
     );
-  } else if (typeof gender === 'undefined') {
+  } else if (typeof sex === 'undefined') {
     count = species.find((specie) => specie.name === specieName).residents.length;
   } else {
     count = species
       .find((specie) => specie.name === specieName)
-      .residents.filter((resident) => resident.sex === gender).length;
+      .residents.filter((resident) => resident.sex === sex).length;
   }
 
   return count;
