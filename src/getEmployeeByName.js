d937e0cb7ba5aad225e00d0e1dda41f365eb84f5
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-09-14 16:47:53 -0300

    Requisito 3 em processo...

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,18 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
+function getEmployeeByName(...employeeName) {
+  // A função receberá um nome completo ou um nome parcial de uma pessoa
+  const splitedName = [];
+  employeeName.forEach((currentName) => {
+    splitedName.push(currentName.split(' '));
+  });
+  // O Objeto que contém a lista de pessoas funcionárias contém as chaves {firstName, lastName}
+  // Será verificado, dado um nome, se esse nome corresponde ao conteúdo de uma das chaves do nome da pessoa funcionária.
+  // Passo 1: Obter os nomes da pessoa
+  // Passo 2: Verificar se algum desses nomes corresponde ao parâmetro recebido
+  return splitedName.forEach((name) => data.employees.filter((employee) => name.includes(employee.firstName) || name.includes(employee.lastName)));
 }
+console.log(getEmployeeByName('Wilburn Wishart'));
 
 module.exports = getEmployeeByName;
