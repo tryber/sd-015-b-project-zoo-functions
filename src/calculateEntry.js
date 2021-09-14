0c4ce47f78e10fb361ee58dd938a759b5deeb034
Caio <caiomgp22@gmail.com>
2021-09-14 15:33:17 -0300

    Adicionando calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.reduce((criança, person) => (person.age < 18 ? criança + 1 : criança), 0);
+  const senior = entrants.reduce((idoso, person) => (person.age >= 50 ? idoso + 1 : idoso), 0);
+  const adult = entrants.length - child - senior;
+  return {
+    child,
+    adult,
+    senior,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  let valors = 0;
+  if (!entrants || Object.keys(entrants).length === 0) return valors;
+  const visitant = countEntrants(entrants);
+  valors = visitant.child * prices.child + visitant.adult * prices.adult
+   + visitant.senior * prices.senior;
+  return valors;
 }
 
 module.exports = { calculateEntry, countEntrants };
