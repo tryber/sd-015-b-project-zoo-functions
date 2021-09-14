049169c2e47b1f25a589f270c7f838a6e3e47eb1
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-13 21:53:52 -0300

    Escreve logica da calculateEntry.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -9,3 +9,11 @@ function calculateEntry(entrants) {
 }
 
 module.exports = { calculateEntry, countEntrants };
+
+// LÓGICA DA FUNÇÃO:
+// ** Recebe um Array de Visitantes
+// ** Implementa uma HoF que verifica a idade de cada pessoa nesse array 'obj.age'
+// ** Com base no [Range - Intervalo] de idades, a faixa etária é uma {child < 18, adult >= 18 && < 50, senior >= 50}
+// **** Realizar a verificação da idade de cada elemento;
+// **** Adicionar 1 unidade ao contador da faixa etária
+// **** Retornar, ao final do array, o valor da soma de cada faixa etária
