bcc5fb6d3b0ec9e212e4153a68831f6038301d89
Flávio Bianchetti <fjb.developer@gmail.com>
2021-09-14 08:19:01 -0300

    Atendido requisito 2 - validado via npm

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
+const verifyAge = (specie, age) =>
+  Object.values(specie.residents).every((resident) =>
+    resident.age >= age);
+
+const findAnimal = (species, animal, age) =>
+  species.some((specie) =>
+    specie.name === animal && verifyAge(specie, age));
+
+const getAnimalsOlderThan = (animal, age, func = findAnimal) => {
   // seu código aqui
-}
+  return func(data.species, animal, age);
+};
 
 module.exports = getAnimalsOlderThan;
