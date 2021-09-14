0fb199b91e3d1376f23ea17d4e2766c8eb394deb
Wander Luiz <wander.srv@gmail.com>
2021-09-14 16:26:12 -0300

    Resoluçao do requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,29 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+
+  const visitantes = {
+    child: entrants.filter((crianca) => crianca.age < 18).length,
+    adult: entrants.filter((adulto) => adulto.age >= 18 && adulto.age < 50).length,
+    senior: entrants.filter((idoso) => idoso.age >= 50).length,
+  };
+  return visitantes;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  const verificaEntrada = countEntrants(entrants);
+  if (verificaEntrada === 0) {
+    return 0;
+  }
+
+  const valorTotal = (verificaEntrada.child * data.prices.child)
+    + (verificaEntrada.adult * data.prices.adult) + (verificaEntrada.senior * data.prices.senior);
+
+  return valorTotal;
 }
 
 module.exports = { calculateEntry, countEntrants };
