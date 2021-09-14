77776b7e7b2be1a51a79796f6739c8373a265c43
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-14 14:50:26 -0300

    [Requisito 5] - Cria uma funçao que retorna o numero de ocorrencias de animais. Necessario corrigir lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
   // seu código aqui
+
+  if (animal === undefined) {
+    const animalOccurrences = {};
+    species.forEach((obj) => (animalOccurrences[obj.name] = obj.residents.length));
+
+    return animalOccurrences;
+  }
+  const { specie: myAnimal } = animal;
+  const { name, residents } = species.find((obj) => obj.name.includes(myAnimal.slice(0, -2)));
+  const myAnimalOccurrences = {
+    [name]: residents.length,
+  };
+
+  return myAnimalOccurrences;
 }
 
 module.exports = countAnimals;
