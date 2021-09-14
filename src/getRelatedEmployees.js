5ba3136753faecbcf5be2f1ba0af2f114f794b17
LeonardoMonti <contatoleomonti@gmail.com>
2021-09-13 23:39:29 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,24 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const verify = isManager(managerId);
+
+  if (verify === true) {
+    const managers = employees
+      .filter((employee) => employee.managers.includes(managerId));
+
+    const getEmployees = managers
+      .map((employee) => `${employee.firstName} ${employee.lastName}`);
+
+    return getEmployees;
+  }
+
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
