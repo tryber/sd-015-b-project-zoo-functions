4c984b0d62e773fad6ea2c52345e723414933b65
Flávia Labanca <flavialabanca@gmail.com>
2021-09-14 10:02:41 -0300

    requisito 3

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,7 @@
 const data = require('../data/zoo_data');
-const { species } = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return species.filter((specie) => ids.includes(specie.id));
+  return data.species.filter((specie) => ids.includes(specie.id));
 }
 
 module.exports = getSpeciesByIds;
