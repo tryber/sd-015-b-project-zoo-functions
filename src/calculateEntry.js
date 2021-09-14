5e7060a1353dfec6232b1b2fe39ebcd3feae9d67
Deivid <deividmborges@gmail.com>
2021-09-14 19:13:35 -0300

    Adiciona resolução parte 1 requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,38 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
+const entrants = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
+const contador = {
+  child: 0,
+  adult: 0,
+  senior: 0,
+}
 function countEntrants(entrants) {
-  // seu código aqui
+  entrants.filter((entrant) => {
+    if(entrant.age < 18) {
+      contador.child += 1;
+    } else if (entrant.age < 50) {
+      contador.adult += 1;
+    }
+    else contador.senior += 1;
+  });
+  return contador;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  
 }
+
+console.log(countEntrants(entrants));
+console.log(calculateEntry(entrants));
 
 module.exports = { calculateEntry, countEntrants };
