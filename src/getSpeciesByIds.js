8d0b1828e04c0790f8c0bf6bef7585a3dd3ec47a
fernandamichetti <f.michetti@hotmail.com>
2021-09-14 15:20:26 -0300

    Corrigindo erros de lint do requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,10 +1,7 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-//um ou mais ids (parametros, então coloquei os ..., chamado de rest argument)
-const getSpeciesByIds = (...ids) => species.filter((kind) => ids.includes(kind.id))
-
+// um ou mais ids (parametros, então coloquei os ..., chamado de rest argument) //
+const getSpeciesByIds = (...ids) => species.filter((kind) => ids.includes(kind.id));
 
 module.exports = getSpeciesByIds;
-
-
