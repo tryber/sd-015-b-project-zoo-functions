3e4f5a326919bf5dc85ac53ea85641f0bccade17
Caio <caiomgp22@gmail.com>
2021-09-14 17:58:49 -0300

    Melhorando getOldest

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -5,11 +5,7 @@ function getOldestFromFirstSpecies(id) {
   const idAnimal = employees.find((person) => person.id === id).responsibleFor[0];
   const listOfAnimals = species.find((specie) => specie.id === idAnimal).residents;
   const oldest = listOfAnimals.reduce((older, animal) => (older.age > animal.age ? older : animal));
-  const { name, sex, age } = oldest;
-  const array = [];
-  array.push(name);
-  array.push(sex);
-  array.push(age);
+  const array = [...Object.values(oldest)];
   return array;
 }
 
