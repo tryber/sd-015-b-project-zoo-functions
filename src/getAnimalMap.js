b4f01697873f5cfdd7b91bea91fe9541854a51da
Huijiro <huijirohankei@gmail.com>
2021-09-14 18:10:50 -0300

    Requisito 7 feito

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,43 @@
 const data = require('../data/zoo_data');
 
+function returnAll() {
+  return data.species.reduce((acc, specie) => {
+    if (!acc[specie.location]) {
+      acc[specie.location] = [];
+    }
+    acc[specie.location].push(specie.name);
+    return acc;
+  }, {});
+}
+
+function returnNames(options) {
+  const { sorted, sex } = options;
+  return data.species.reduce((acc, specie) => {
+    if (!acc[specie.location]) {
+      acc[specie.location] = [];
+    }
+    let list = [];
+    if (sex) {
+      list = specie.residents.filter((resident) => resident.sex === sex).map((resident) => resident.name);
+    }else {
+      list = specie.residents.map((resident) => resident.name);
+    }
+    if (sorted) {
+      list.sort();
+    }
+    acc[specie.location].push({ [specie.name]: list });
+    return acc;
+  }, {});
+}
+
 function getAnimalMap(options) {
   // seu código aqui
+  if (options) {
+    if (options.includeNames) {
+      return returnNames(options);
+    }
+  }
+  return returnAll();
 }
 
 module.exports = getAnimalMap;
