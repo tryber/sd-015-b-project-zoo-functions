362350d36ca3e990200e0c8450d32a9c744196c7
JoaoVMont <joaovmt1905@gmail.com>
2021-09-14 10:57:19 -0300

    Requisitos 1, 2 e 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  function minAge(number) {
+    if (number.age >= age) {
+      return number;
+    }
+  }
+  return data.species.some((specie) => specie.name === animal && specie.residents.every(minAge));
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  if (ids.length > 1) {
+    return data.species.filter((specie, index) => specie.id === ids[index]);
+  }
+  return data.species.filter((specie) => specie.id === ids[0]);
+  // return ids.map((id) =>
+  // data.species.filter((specie, index) =>
+  // specie.id === id));
 }
 
 module.exports = getSpeciesByIds;
