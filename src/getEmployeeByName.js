8adde30f33eed10a7fa711ab16b5940a81052d97
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-09-14 17:34:40 -0300

    Cumprido o requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,18 +1,14 @@
-const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(...employeeName) {
-  // A função receberá um nome completo ou um nome parcial de uma pessoa
-  const splitedName = [];
-  employeeName.forEach((currentName) => {
-    splitedName.push(currentName.split(' '));
-  });
-  // O Objeto que contém a lista de pessoas funcionárias contém as chaves {firstName, lastName}
-  // Será verificado, dado um nome, se esse nome corresponde ao conteúdo de uma das chaves do nome da pessoa funcionária.
-  // Passo 1: Obter os nomes da pessoa
-  // Passo 2: Verificar se algum desses nomes corresponde ao parâmetro recebido
-  return splitedName.forEach((name) => data.employees.filter((employee) => name.includes(employee.firstName) || name.includes(employee.lastName)));
+const splitName = (receivedName) => receivedName.split(' ');
+
+function getEmployeeByName(employeeName) {
+  if (!employeeName) return {};
+  const splitedName = splitName(employeeName);
+  return data.employees.find((employee) => splitedName.includes(employee.firstName)
+  || splitedName.includes(employee.lastName));
 }
-console.log(getEmployeeByName('Wilburn Wishart'));
+console.log(getEmployeeByName());
+console.log(getEmployeeByName('Strauss'));
 
 module.exports = getEmployeeByName;
