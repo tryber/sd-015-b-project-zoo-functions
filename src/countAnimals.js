26e16db7dfd27ecb2fb3013ad1366fabb1e4b3da
Bruno Bartolomeu <bruno.bartolomeu5@hotmail.com>
2021-09-14 15:47:02 -0300

    Rolve linkt do req 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,5 @@
 const { species } = require('../data/zoo_data');
-const data = require('../data/zoo_data');
-
-
+const data = require('../data/zoo_data')
 
 function animalsCount() {
   const animalsInfo = {};
