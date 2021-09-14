6dab0f8e3584aeca8be367e04d402826e2026e6c
Caio <caiomgp22@gmail.com>
2021-09-14 11:02:06 -0300

    Adicionando countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const list = {};
+  if (!animal) {
+    species.forEach((specie) => {
+      list[specie.name] = specie.residents.length;
+    });
+    return list;
+  }
+  if (Object.keys(animal).length === 1) {
+    const bicho = species.find((elem) => animal.specie === elem.name);
+    return bicho.residents.length;
+  }
+  const bicho = species.find((elem) => animal.specie === elem.name);
+  const bichoPorSexo = bicho.residents.filter((elem) => animal.gender === elem.sex);
+  return bichoPorSexo.length;
 }
 
 module.exports = countAnimals;
