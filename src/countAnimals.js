2d114b983037b1d21115f3ef3007f3e125bf494a
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-09-14 10:15:13 -0300

    Adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const allCountAnimals = species.reduce((acc, { name, residents }) => {
+      acc[name] = residents.length;
+      return acc;
+    }, {});
+
+    return allCountAnimals;
+  }
+
+  if (animal.gender === undefined) {
+    const amountAnimals = species.find((specie) => specie.name === animal.specie);
+
+    return amountAnimals.residents.length;
+  }
+  const amountAnimals = species.find((specie) => specie.name === animal.specie);
+  const amountSexAnimals = amountAnimals.residents;
+  const whichAnimalSex = amountSexAnimals.filter((animalSex) => animalSex.sex === animal.gender);
+  return whichAnimalSex.length;
 }
 
 module.exports = countAnimals;
