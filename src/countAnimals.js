62201c254e307abeaeac53e495dd643b3266df62
Walace <walacewab@gmail.com>
2021-09-14 17:11:45 -0300

    Adicionando requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const obj = {};
+
+function nullAnimals() {
+  species.forEach((element) => {
+    obj[element.name] = element.residents.length;
+  });
+  return obj;
+}
+
+function selectedAnimals({ specie, gender }) {
+  let { residents } = species.find((x) => x.name === specie);
+  residents = !gender ? residents : residents.filter((resident) => resident.sex === gender);
+  return residents.length;
+}
+
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    return nullAnimals();
+  }
+  return selectedAnimals(animal);
 }
 
 module.exports = countAnimals;
