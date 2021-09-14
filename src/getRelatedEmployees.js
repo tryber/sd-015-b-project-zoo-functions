9bb278edcf01329e90a77d6ce011f46ba5eec508
Flávio Bianchetti <fjb.developer@gmail.com>
2021-09-14 11:04:36 -0300

    Requisito 4 - alterada estrutura para correçao de erros de indentaçao

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -6,14 +6,16 @@ const isManager = (id) => data.employees.some((employee) =>
 
 const getEmpoyees = (employees, managerId) =>
   employees.reduce((acc, employee) => {
-    if (employee.managers.some((manager) => manager === managerId)) 
+    if (employee.managers.some((manager) => manager === managerId)) {
       acc.push(`${employee.firstName} ${employee.lastName}`);
+    }
     return acc;
   }, []);
 
 const getRelatedEmployees = (managerId, func = getEmpoyees) => {
-  if (!isManager(managerId))
+  if (!isManager(managerId)) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
   return func(data.employees, managerId);
 };
 
