7c2af58ae72c8cf83cc925022829b420f60c1162
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-14 15:32:34 -0300

    [Requisito 6] - Cria a funçao countEntrants que retorna um objeto com o numero de ocorrencias de child, adult e senior. Tambem cria a funçao calculateEntry que retorna o valor total dos ingressos

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,29 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  const numberOfChilds = entrants.filter(({ age }) => age < 18).length;
+  const numberOfAdults = entrants.filter(({ age }) => age >= 18 && age < 50).length;
+  const numberOfSeniors = entrants.filter(({ age }) => age >= 50).length;
+
+  const entrantsOccurrences = (child, adult, senior) => ({
+    child,
+    adult,
+    senior,
+  });
+
+  return entrantsOccurrences(numberOfChilds, numberOfAdults, numberOfSeniors);
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (entrants === undefined || Object.entries(entrants).length === 0) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  const [childValue, adultValue, seniorValue] = [20.99, 49.99, 24.99];
+  const totalPrice = child * childValue + adult * adultValue + senior * seniorValue;
+
+  return totalPrice;
 }
 
 module.exports = { calculateEntry, countEntrants };
