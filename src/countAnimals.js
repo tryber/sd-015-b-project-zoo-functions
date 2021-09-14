e674714cacd8939ef7da52cf1e7a5ce74fd00fe3
Deivid <deividmborges@gmail.com>
2021-09-14 15:52:10 -0300

    Adiciona resolução do requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+function emptyParameters() {
+  const animalQtd = {};
+  species.map((specie) => {
+    animalQtd[specie.name] = specie.residents.length;
+    return animalQtd;
+  });
+  return animalQtd;
+}
+
+function speciesAndGenderParameter({ specie, gender }) {
+  let { residents } = species.find((specieData) => specieData.name === specie);
+  residents = !gender ? residents : residents.filter((resident) => resident.sex === gender);
+  return residents.length;
+}
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return emptyParameters();
+  }
+  return speciesAndGenderParameter(animal);
 }
 
 module.exports = countAnimals;
