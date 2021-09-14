ddf743896965cee64b831c6e1e18cf8c18fd9502
LaizaBertelli <laizabertelli@hotmail.com>
2021-09-14 13:46:30 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,36 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function countAllSpecies() {
+  const allSpecies = { };
+  species.forEach(({ name, residents } = species) => {
+    allSpecies[name] = residents.length;
+  });
+  return allSpecies;
+}
+function countSpeciesByGender(animals, gender) {
+  let count = 0;
+  animals.forEach(({ sex } = animals) => {
+    if (sex === gender) {
+      count += 1;
+    }
+  });
+  return count;
+}
+function countSpeciesByName(animal) {
+  const filteredSpecie = species.find(({ name } = species) => name === animal.specie);
+
+  if (Object.keys(animal).length === 1) {
+    return filteredSpecie.residents.length;
+  }
+  return countSpeciesByGender(filteredSpecie.residents, animal.gender);
+}
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    return countAllSpecies();
+  }
+  return countSpeciesByName(animal);
 }
 
 module.exports = countAnimals;
