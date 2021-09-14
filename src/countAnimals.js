d4cea32ccee23ce1b540e8f137fa6067b5f07f72
PedroAugmed <pedroaugmed7@gmail.com>
2021-09-14 14:23:11 -0300

    Add requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const object = {};
+
+    data.species.forEach((element, index) => {
+      object[element.name] = element.residents.length;
+    });
+    return object;
+  } if (animal.specie && animal.gender === undefined) {
+    const dataSpecies = data.species.find((specie) => specie.name === animal.specie);
+    return dataSpecies.residents.length;
+  } if (Object.keys(animal)[1] === 'gender') {
+    const specieAndSex = data.species.find((bicho) => bicho.name === animal.specie);
+    return specieAndSex.residents.filter((wildAnimal) => wildAnimal.sex === animal.gender).length;
+  }
 }
 
 module.exports = countAnimals;
+console.log(countAnimals());// OK
+console.log(countAnimals({ specie: 'penguins' }));// OK
+console.log(countAnimals({ specie: 'giraffes', gender: 'female' }));// OK
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,13 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const trueOrFalse = data.employees.some((element) => element.managers.includes(id));
+  return trueOrFalse;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+
 }
 
 module.exports = { isManager, getRelatedEmployees };
+console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
