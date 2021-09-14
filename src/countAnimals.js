81efca8240366d6995a2d9a164c26a3b0e92d91e
Lucas Rodrigues <theluks21@live.com>
2021-09-13 22:30:15 -0300

    Adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const array = {};
+data.species.forEach((one) => {
+  array[one.name] = one.residents.length;
+});
+
+function countAnimals(animal = '') {
+  // seu código aqui;
+  const filteredSpecie = data.species.filter((specie) => specie.name === animal.specie);
+
+  if (filteredSpecie.length > 0 && animal.gender === undefined) {
+    return filteredSpecie[0].residents.length;
+  }
+
+  if (animal.gender) {
+    const gender = filteredSpecie[0].residents.filter((resident) => resident.sex === animal.gender);
+    return gender.length;
+  }
+
+  return array;
 }
 
 module.exports = countAnimals;
