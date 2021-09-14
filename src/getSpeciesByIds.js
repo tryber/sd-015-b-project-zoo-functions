6226c7fe0ba831fc3639db2febf54ff7c0b640a4
Arthur <viegas.arthur2013@hotmail.com>
2021-09-14 01:44:12 -0300

    corrige lint requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,13 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids1, ids2) {
-  if (!ids1) {return []}
+  if (!ids1) { return [] };
   const species = Object.values(data.species);
-  const speciesById = []
-  speciesById.push(species.find((animal) => animal.id === ids1))
-  if (!ids2) {return speciesById}
-  speciesById.push(species.find((animal) => animal.id === ids2))
-  return speciesById
+  const speciesById = [];
+  speciesById.push(species.find((animal) => animal.id === ids1));
+  if (!ids2) { return speciesById };
+  speciesById.push(species.find((animal) => animal.id === ids2));
+  return speciesById;
 }
 
 module.exports = getSpeciesByIds;
