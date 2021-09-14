e44abc2e7ebed066c1d767389535078699c2d307
Sayro <sayrodamaceno70@gmail.com>
2021-09-14 18:09:23 -0300

    cumpre requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,39 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
+// retorna todos os animais
+const allAnimals = () => {
+  const animals = species.reduce((acc, animalData) => {
+    acc[animalData.name] = animalData.residents.length;
+    return acc;
+  }, {});
+  return animals;
+};
+
+// retorna todos os animais de uma especie
+const allAnimalsBySpecie = (specie) => {
+  const animal = species.find((animalFind) => animalFind.name === specie.specie);
+
+  if (!specie.gender) {
+    return animal.residents.length;
+  }
+
+  return animal.residents.reduce((acc, resident) => {
+    if (resident.sex === specie.gender) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+};
+
+function countAnimals(specie) {
   // seu código aqui
+  if (!specie) {
+    return allAnimals();
+  }
+  return allAnimalsBySpecie(specie);
 }
+
+console.log(countAnimals({ specie: 'bears', gender: 'female' }));
 
 module.exports = countAnimals;
