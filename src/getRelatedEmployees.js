3d50e2656f7926f3f0dc2353e53718ac851ca101
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-09-14 18:44:38 -0300

    Cumprido o requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const relateds = data.employees.filter((employee) => (employee.managers.includes(managerId)));
+  return relateds.map((employee) => `${employee.firstName} ${employee.lastName}`);
 }
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
