e032d65531358dd2e1ecc9c19ce697d6e8203321
Flávio Bianchetti <fjb.developer@gmail.com>
2021-09-14 08:52:24 -0300

    Atendido requisito 3 - validado via npm

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
+const findEmployee = (employees, employeeName) => {
+  return employees.find((employee) =>
+    employee.firstName === employeeName || employee.lastName === employeeName);
+};
+
+const getEmployeeByName = (employeeName, func = findEmployee) => {
+  let result = {};
+  if (employeeName !== undefined) {
+    result = func(data.employees, employeeName);
+  }
+  return result;
 }
 
 module.exports = getEmployeeByName;
