6cea7d738c192f8deca84dbc99d5504f16d1e7a0
Flávio Bianchetti <fjb.developer@gmail.com>
2021-09-13 22:04:30 -0300

    Atendido requisito 1 - validado via npm

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+const speciesByIds = (species, ids) =>
+  ids.map((id) =>
+  species.find((specie) =>
+  specie.id === id));
+
+const getSpeciesByIds = (...ids) => {
   // seu código aqui
-}
+  let result = [];
+  if (ids[0] !== undefined) {
+    result = speciesByIds(data.species, ids);
+  }
+  return result;
+};
 
 module.exports = getSpeciesByIds;
