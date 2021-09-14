40fb3dbe88adf38554f684727756cbe7a191e275
Ismael_Luviseti <ismaelluviseti@gmail.com>
2021-09-14 16:33:10 -0300

    Metade do requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employee) => employee.managers == id)
 }
 
 function getRelatedEmployees(managerId) {
