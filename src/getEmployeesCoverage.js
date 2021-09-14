1611e19f07832e91d69dee62e38531d1092213c2
Bruno Bartolomeu <bruno.bartolomeu5@hotmail.com>
2021-09-14 19:22:03 -0300

    Resolve requisito 5

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,19 +1,11 @@
-const { employees } = require('../data/zoo_data');
+// const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeeSpecies() {
-
-}
+// function getEmployee() {
+//   const employee = employees.find(({ firstName }) => firstName === name);
+// }
 
 function getEmployeesCoverage({ name }) {
-  const employee = employees.find(({ firstName }) => firstName === name);
-  const employeeInfo = {
-    id: employee.id,
-    fullName: `${employee.firstName} ${employee.lastName}`,
-    species: getEmployeeSpecies(employee),
-    location: getEmployeeLocation(),
-  };
-  console.log(employeeInfo);
 }
 
 getEmployeesCoverage({ name: 'Sharonda' });
