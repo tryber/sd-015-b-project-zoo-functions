1ac732f27e036de9c6c61d6be55bce9502eed01b
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-14 12:03:07 -0300

    Corrige return.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -4,9 +4,10 @@ const { prices } = data;
 function countEntrants(entrants) {
   // seu código aqui
   const contador = entrants.reduce((acc, entrant) => {
-    if (entrant.age < 18) return { ...acc, child: acc.child += 1 };
-    if (entrant.age >= 18 && entrant.age < 50) return { ...acc, adult: acc.adult += 1 };
-    if (entrant.age >= 50) return { ...acc, senior: acc.senior += 1 };
+    if (entrant.age < 18) return ({ ...acc, child: acc.child += 1 });
+    if (entrant.age >= 18 && entrant.age < 50) return ({ ...acc, adult: acc.adult += 1 });
+    if (entrant.age >= 50) return ({ ...acc, senior: acc.senior += 1 });
+    return acc;
   }, { adult: 0, child: 0, senior: 0 });
   return contador;
 }
