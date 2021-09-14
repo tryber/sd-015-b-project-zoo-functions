c7519652135930b2a054ece8cc6dbb0b3633b8b0
Jesse Benevides <jessebenevides@gmail.com>
2021-09-14 20:29:30 -0300

    Requisito 9 - adiciona função getOldestAnimalById

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const { employees } = data;
+
+const getOldestAnimal = (specieId) => {
+  const foundSpecie = species.find((specie) => specie.id === specieId);
+  const { residents } = foundSpecie;
+
+  const animal = residents.reduce((oldest, resident) => {
+    if (oldest.age > resident.age) {
+      return oldest;
+    }
+    return resident;
+  });
+
+  return animal;
+};
+
+const getEmployeeById = (employeeId) => {
+  const employeeFound = employees.find((employee) => employee.id === employeeId);
+  return employeeFound;
+};
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = getEmployeeById(id);
+  const [firstSpecieId] = employee.responsibleFor;
+  const oldestAnimal = getOldestAnimal(firstSpecieId);
+
+  return oldestAnimal;
 }
+
+console.log(getOldestFromFirstSpecies('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = getOldestFromFirstSpecies;
