5c8ab6e63352afd96c8f2e1aa89a68d139674153
vinicius rampazzo <vinicius_rampazzo10@hotmail.com>
2021-09-14 18:13:24 -0300

    realizando requisito 4

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,8 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  
 }
 
 console.log(countAnimals());
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,18 +2,17 @@ const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // return employees.some((elemento) => elemento.managers === id);
+  // return employees.some((elemento) => elemento.managers == id);
   return employees.some((elemento) => elemento.managers.includes(id));
 }
 console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 function getRelatedEmployees(managerId) {
   if (isManager(managerId)) {
-    return employees.map((elemento) => {
-      if (isManager(managerId) === true) {
-        return elemento.responsibleFor;
-      }
-    });
+    return employees.filter((elemento) => elemento
+      .managers.includes(managerId)).map((nomes) => `${nomes.firstName} ${nomes.lastName}`);
+  } else {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
 }
 console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
