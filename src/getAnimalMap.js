4d02d160b5168d32284dc2e8c9131f495fa7a845
gustavogss <gustavogss.dev@gmail.com>
2021-09-14 15:31:45 -0300

    Requisito 8 - Hour Zoo

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,7 +2,23 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
-  return species.map((specie) => specie.name);
+  if (!options) {
+    const specieMap = () => ({
+      NE: species
+        .filter((specie) => specie.location === 'NE')
+        .map((actualSpecie) => actualSpecie.name),
+      NW: species
+        .filter((specie) => specie.location === 'NW')
+        .map((actualSpecie) => actualSpecie.name),
+      SE: species
+        .filter((specie) => specie.location === 'SE')
+        .map((actualSpecie) => actualSpecie.name),
+      SW: species
+        .filter((specie) => specie.location === 'SW')
+        .map((actualSpecie) => actualSpecie.name),
+    });
+    return specieMap;
+  }
 }
-console.log(getAnimalMap);
+
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,25 +1,40 @@
 const data = require('../data/zoo_data');
 
-const { hours } = data;
+const { species, hours } = data;
 
-function getSchedule(dayName) {
-  if (!dayName) {
-    return Object.entries(hours).reduce((acc, [day, hourOpening]) => {
-      if (day === 'Monday') {
-        acc[day] = 'CLOSED';
-        return acc;
-      }
-      acc[day] = `Open from ${hourOpening.open}am until ${hourOpening.close - 12}pm`;
-      return acc;
-    }, {});
+const totalSchedule = () => {
+  const total = {};
+  Object.keys(hours).forEach((element) => {
+    const timeTotal = hours[element];
+    if (element === 'Monday') {
+      total[element] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    } else {
+      total[element] = {
+        officeHour: `Open from ${timeTotal.open}am until ${timeTotal.close}pm`,
+        exhibition: species
+          .filter((animal) => animal.availability.includes(element))
+          .map((elem) => elem.name),
+      };
+    }
+  });
+  return total;
+};
+
+const animalOrDay = (scheduleTarget, day, animals) => {
+  if (animals) return species.find((animal) => animal.name === scheduleTarget).availability;
+  if (day) return { [scheduleTarget]: totalSchedule()[scheduleTarget] };
+};
+
+const getSchedule = (scheduleTarget) => {
+  const day = Object.keys(hours).includes(scheduleTarget);
+  const animal = species.some((element) => element.name === scheduleTarget);
+  if (animal || day) {
+    return animalOrDay(scheduleTarget, day, animal);
   }
-  const hourSelected = {};
-  const [daySelected, hourOpening] = Object.entries(hours).find((day) => day[0] === dayName);
-  if (dayName !== 'Monday') {
-    hourSelected[daySelected] = `Open from ${hourOpening.open}am until ${hourOpening.close - 12}pm`;
-    return hourSelected;
-  }
-  return { Monday: 'CLOSED' };
-}
+  return totalSchedule();
+};
 
 module.exports = getSchedule;
