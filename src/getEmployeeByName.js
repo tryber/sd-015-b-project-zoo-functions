f2f27cf48242b1abbc6ed5b7c399bfab72cedd96
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-09-14 08:33:48 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) return {};
+  return data.employees.find(
+    (propriedade) =>
+      propriedade.firstName === employeeName
+      || propriedade.lastName === employeeName,
+  );
 }
 
 module.exports = getEmployeeByName;
