b2061f5f9142b26944d54c391dff0eb10469fb4e
Claudio Cassimiro <days_surfing@live.com>
2021-09-14 18:48:58 -0300

    resolvendo erros de lint

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,14 +1,5 @@
 const data = require('../data/zoo_data');
 
-const entrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-
 function countEntrants(entries) {
   // seu código aqui
   if (!entries) {
@@ -48,5 +39,5 @@ function calculateEntry(entries) {
   const total = totalPrice(adult, child, senior);
   return total;
 }
-// console.log(calculateEntry(entrants));
+
 module.exports = { calculateEntry, countEntrants };
