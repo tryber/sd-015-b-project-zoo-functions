907ffafb3feeeba5da0a54c3b012a9e1fb9abf6d
Laurenz Pinder <laurenzdpinder90@gmail.com>
2021-09-14 00:40:21 -0300

    Refatora requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -3,14 +3,14 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
-  return employees.some((employee) => employee.managers.includes(id));
+  return employees.some(({ managers }) => managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
   if (isManager(managerId) === true) {
     return employees.filter((employee) => employee.managers.includes(managerId))
-      .map((name) => `${name.firstName} ${name.lastName}`);
+      .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
