5c748b1094744522a5dde492ee7bef68425a7578
Richard Leal <rwsleal@gmail.com>
2021-09-15 15:53:16 -0300

    Requisito 9 finalizado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const responsableFor = data.employees.find((personId) => personId.id === id).responsibleFor[0];
+  const oldestAnimal = data.species.find((animal) =>
+    animal.id === responsableFor).residents.sort((a, b) => b.age - a.age)[0];
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
