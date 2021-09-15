273c97986b11a7d69f226e03b083d631bbed8198
Lucas Rodrigues <theluks21@live.com>
2021-09-15 14:16:22 -0300

    Adiciona requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
+const reduceFunc = (acc, animal) => ((animal.age > acc.age) ? animal : acc);
+
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const filteredEmployee = employees.filter((employee) => employee.id === id);
+  const animalId = filteredEmployee[0].responsibleFor[0];
+  const filteredSpecies = species.filter((specie) => specie.id === animalId);
+  const oldAnimal = filteredSpecies[0].residents.reduce(reduceFunc);
+
+  return [oldAnimal.name, oldAnimal.sex, oldAnimal.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
+
+// console.log(getOldestFromFirstSpecies('9e7d4524-363c-416a-8759-8aa7e50c0992'));
