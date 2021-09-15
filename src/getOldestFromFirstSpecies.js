755540a3ae2abb5eea313ef44c8ed9e3d8d6625b
uroque <ulissesroque@protonmail.com>
2021-09-15 11:20:07 -0300

    Adiciona requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = employees.find((_employee) => _employee.id === id);
+  const firstSpecies = species.find((_species) => _species.id === employee.responsibleFor[0]);
+  let higherAge = 0;
+  firstSpecies.residents.forEach((resident) => {
+    if (resident.age > higherAge) {
+      higherAge = resident.age;
+    }
+  });
+  const oldest = firstSpecies.residents.find((resident) => higherAge === resident.age)
+  return [oldest.name, oldest.sex, oldest.age];
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
