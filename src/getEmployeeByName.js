7a7666553cd0f80d4a022d0b086df8d08c93c2e9
Claudio Junior Haas <claudiohaas.cjh@gmail.com>
2021-09-15 13:23:53 -0300

    Requisito 3: Concluido

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+const getEmployeeByName = (nameEmployee) => {
+  if (!nameEmployee) return {};
+    const employee = employees.find(finderEmployee => finderEmployee.firstName === nameEmployee || finderEmployee.lastName === nameEmployee);
+    return employee;
+  };
 
 module.exports = getEmployeeByName;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,6 +1,6 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (...ids) => ids.map((i) => species.find((animal) => animal.id === i));
+const getSpeciesByIds = (...ids) => ids.map(element => species.find(animal=> animal.id === element));
 
 module.exports = getSpeciesByIds;
