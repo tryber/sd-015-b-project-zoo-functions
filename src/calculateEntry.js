0bc38934fd644e4ffda696ef56a25ac14672d796
fernandamichetti <f.michetti@hotmail.com>
2021-09-15 11:32:35 -0300

    Requisito 6 completo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,15 +1,23 @@
 const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  const childrens = entrants.filter((person) => (person.age < 18)).length;
-  const adults = entrants.filter((person) => (person.age >= 18 && person.age < 50)).length;
-  const seniors = entrants.filter((person) => (person.age >= 50)).length;
-  const result = { child: childrens, adult: adults, senior: seniors };
-  return result
+  const child = entrants.filter((person) => (person.age < 18)).length;
+  const adult = entrants.filter((person) => (person.age >= 18 && person.age < 50)).length;
+  const senior = entrants.filter((person) => (person.age >= 50)).length;
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+ const ages = countEntrants(entrants);
+ const priceChildrens = ages.child * prices.child;
+ const priceAdults = ages.adult * prices.adult;
+ const priceSeniors = ages.senior * prices.senior;
+ return priceChildrens + priceAdults + priceSeniors;
+
 }
+
+
 
 module.exports = { calculateEntry, countEntrants };
