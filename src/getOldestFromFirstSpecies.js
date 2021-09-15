7908f75105eab00e9970baea48dc9e083bae321d
Ronald_D_Bezerra <rebulgame@gmail.com>
2021-09-14 21:57:46 -0300

    adiciona requisito 09

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,23 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable no-param-reassign */
+const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const test = employees.filter((elemento) => elemento.id === id);
+
+  const idAnimal = test[0].responsibleFor.shift();
+
+  const buscarIdAnimal = species.filter((elemento) => elemento.id === idAnimal);
+
+  const buscarAnimalMaisVelho = buscarIdAnimal[0].residents.reduce((acumulador, elemento) => {
+    if (elemento.age > acumulador.age) {
+      return elemento;
+    }
+    return acumulador;
+  });
+  const resultado = Object.values(buscarAnimalMaisVelho);
+  return resultado;
 }
 
 module.exports = getOldestFromFirstSpecies;
