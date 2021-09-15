fbde036594525e9ad824d13f773703de0773b6af
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-15 09:49:46 -0300

    [Requisito 5] - Refatora o codigo. Cria 3 funçoes auxiliares

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,26 +1,41 @@
 const data = require('../data/zoo_data');
 
 const { species } = data;
+function getAllAnimals() {
+  const animalOccurrences = {};
+  species.forEach((obj) => {
+    animalOccurrences[obj.name] = obj.residents.length;
+  });
+  return animalOccurrences;
+}
+
+function getNoSexAnimal(animal) {
+  const { specie: myAnimal } = animal;
+  const { residents } = species.find((obj) => obj.name.includes(myAnimal.slice(0, -2)));
+
+  return residents.length;
+}
+
+function getAnimal(animal) {
+  const { specie: myAnimal, sex: animalSex } = animal;
+  const { residents } = species.find((obj) => obj.name.includes(myAnimal.slice(0, -2)));
+  const genderResidents = residents.filter(({ sex }) => sex === animalSex);
+
+  return genderResidents.length;
+}
 
 function countAnimals(animal) {
   // seu código aqui
   if (animal === undefined) {
-    const animalOccurrences = {};
-    species.forEach((obj) => {
-      animalOccurrences[obj.name] = obj.residents.length;
-    });
-    return animalOccurrences;
-  }
-  const { specie: myAnimal, sex: animalGender } = animal;
-  if (animalGender === undefined) {
-    const { residents } = species.find((obj) => obj.name.includes(myAnimal.slice(0, -2)));
-    return residents.length;
+    return getAllAnimals();
   }
 
-  const { residents } = species.find((obj) => obj.name.includes(myAnimal.slice(0, -2)));
-  const genderResidents = residents.filter(({ sex }) => sex === animalGender);
+  const { sex: animalSex } = animal;
+  if (animalSex === undefined) {
+    return getNoSexAnimal(animal);
+  }
 
-  return genderResidents.length;
+  return getAnimal(animal);
 }
 
 module.exports = countAnimals;
