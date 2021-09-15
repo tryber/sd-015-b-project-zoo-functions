f7ba72970d234b3cf060f01c289e7caab71b9822
Lucas Peres <lucasperesss@gmail.com>
2021-09-14 21:33:46 -0300

    Requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,44 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const speciesPerRegion = () => {
+  const animalMap = {};
+  data.species.forEach((specie) => {
+    if (!animalMap[specie.location]) {
+      animalMap[specie.location] = [];
+      animalMap[specie.location].push(specie.name);
+    } else if (animalMap[specie.location]) {
+      animalMap[specie.location].push(specie.name);
+    }
+  });
+  return animalMap;
+};
+
+const mapIncludesNames = (sort, sex) => {
+  const animalMap = {};
+  data.species.forEach((specie) => {
+    if (!animalMap[specie.location]) { animalMap[specie.location] = []; }
+    let residents = [...specie.residents];
+    if (sex) {
+      residents = residents.filter((resident) => resident.sex === sex);
+    }
+    residents = residents.map((resident) => resident.name);
+    if (sort === true) {
+      residents.sort();
+    }
+    animalMap[specie.location].push({ [specie.name]: residents });
+  });
+  return animalMap;
+};
+
+function getAnimalMap(options = {}) {
+  const { includeNames, sorted, sex } = options;
+
+  if (includeNames === true) {
+    return mapIncludesNames(sorted, sex);
+  }
+  return speciesPerRegion();
 }
 
 module.exports = getAnimalMap;
+
+console.log(getAnimalMap({ includeNames: true, sex: 'female' }));
