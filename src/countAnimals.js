942644cdbc7ac6de45c74a69f1cd32e8893ae1e9
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-09-14 21:10:38 -0300

    requisito 4

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,9 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  const { gender, specie } = animal;
 
-  return !animal ? 'true' : 'false';
+  console.log(specie);
+  return !animal ? gender : species.find((especie) => especie.name === animal.specie).residents;
 }
 
 module.exports = countAnimals;
+
+console.log(countAnimals({ specie: 'bears' }));
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,13 +1,32 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
-
+  return employees.some((employe) =>
+    employe.managers.some((manager) => manager === id));
 }
+const searchByManager = (id) => employees
+  .filter((funcionario) => funcionario.managers
+    .includes(id));
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
-  return isManager;
+  const ifIsManager = isManager(managerId);
+
+  if (!ifIsManager) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    const funcionariosGerenciadas = searchByManager(managerId);
+    const arrayDeNomes = [];
+
+    funcionariosGerenciadas.forEach((nomeSobrenome) =>
+      arrayDeNomes.push(`${nomeSobrenome.firstName} ${nomeSobrenome.lastName}`));
+
+    return arrayDeNomes;
+  }
 }
+
+console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 module.exports = { isManager, getRelatedEmployees };
