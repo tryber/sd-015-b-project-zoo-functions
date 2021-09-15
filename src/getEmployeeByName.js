a81fd3dbd6af30c3b32eecd60df1b3444ce76e7a
alxedv <alexmaia.etel@gmail.com>
2021-09-15 03:29:14 -0300

    Adiciona o requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (typeof employeeName === 'undefined'){
+    return {};
+  }
+  const searchEmployee = employees.find(element => element.firstName === employeeName || element.lastName === employeeName);
+  return searchEmployee;
 }
+
+getEmployeeByName()
 
 module.exports = getEmployeeByName;
