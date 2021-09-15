baa8ec2cfc7273d6ff9cea4127f3141a488700e1
Erik Lopes <erik.lopesdev@gmail.com>
2021-09-15 14:17:40 -0300

    Desafio 5 e 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,25 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  const childs = entrants.filter((person) => (person.age < 18)).length;
+  const adults = entrants.filter((person) => (person.age >= 18 && person.age < 50)).length;
+  const seniors = entrants.filter((person) => (person.age >= 50)).length;
+  
+  const resultList = { child: childs, adult: adults, senior: seniors };
+
+  return resultList;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants || entrants.length === undefined) return 0;
+  const countResult = countEntrants(entrants);
+  const prices = [20.99, 49.99, 24.99];
+  const childResult = countResult.child * prices[0];
+  const adultResult = countResult.adult * prices[1];
+  const seniorResult = countResult.senior * prices[2];
+  const result = childResult + adultResult + seniorResult;
+  return result;
 }
 
 module.exports = { calculateEntry, countEntrants };
