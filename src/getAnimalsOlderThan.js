cd3c7d8fe9f1183dea983b6067146a59ce9e3cd0
allpha23 <allpha23@gmail.com>
2021-09-15 11:00:33 -0300

    finalizado requisito 2 [correção]

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,8 +2,8 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const getSpeciesName = data.species.filter((species) => species.name === animal);
-  const getAnimalsByAge = getSpeciesName[0].residents.every((species) => species.age > age);
+  const getSpeciesName = data.species.find((species) => species.name === animal);
+  const getAnimalsByAge = getSpeciesName.residents.every((species) => species.age > age);
   return getAnimalsByAge;
 }
 
