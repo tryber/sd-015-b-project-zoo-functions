4757977f7c035fb8755962e6e390e1f4fd344c58
Amanda Fernandes <aferanda@gmail.com>
2021-09-15 14:59:14 -0300

    Requisito 10 - parte 5

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,6 +1,14 @@
 const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const checkEmployee = (obj) => {
+  const { values } = Object;
+  return employees.some(({ id, firstName, lastName }) =>
+    values(obj).includes(id)
+    || values(obj).includes(firstName)
+    || values(obj).includes(lastName));
+};
+
 const getEmployee = (obj) => {
   const { values } = Object;
   return employees.find(({ id, firstName, lastName }) =>
@@ -19,6 +27,8 @@ const getSpeciesLocation = (obj) => getEmployee(obj).responsibleFor
 
 const objectConstructing = (obj) => {
   const object = {};
+  const employee = checkEmployee(obj);
+  if (!employee) throw new Error('Informações inválidas');
   const { id, firstName, lastName } = getEmployee(obj);
   Object.assign(object, {
     id,
@@ -31,7 +41,6 @@ const objectConstructing = (obj) => {
 
 const objectConstructingAll = () => {
   const employeesAll = [];
-
   employees.forEach((employee) => {
     employeesAll.push(objectConstructing({ id: employee.id }));
   });
@@ -42,5 +51,7 @@ function getEmployeesCoverage(obj) {
   if (!obj) return objectConstructingAll();
   return objectConstructing(obj);
 }
+
+console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 
 module.exports = getEmployeesCoverage;
