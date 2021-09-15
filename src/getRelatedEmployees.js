f8258ae32804ecf92f23c285183212b7b70a0914
Joao Pedro de Oli. Lage <JoaoPedro.Lage@outlook.com>
2021-09-15 11:32:42 -0300

    Corrigi erro na função getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,22 +1,21 @@
 const data = require('../data/zoo_data');
 
+function isManager(id) {
+  return data.employees.some((managersId) =>
+    managersId.managers.find((espeficManager) => espeficManager === id));
+}
+
 function getRelatedEmployees(managerId) {
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
   const findManager = data.employees.filter((employees) =>
     employees.managers.find((espeficManager) => espeficManager === managerId));
   return findManager.map((employeesNames) =>
     `${employeesNames.firstName} ${employeesNames.lastName}`);
 }
 
-function isManager(id) {
-  getRelatedEmployees(id);
-  return data.employees.some((managersId) =>
-    managersId.managers.find((espeficManager) => espeficManager === id));
-}
-
-if (isManager() === false) {
-  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
-}
-
-console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
-
-module.exports = { isManager, getRelatedEmployees };
+module.exports = {
+  isManager,
+  getRelatedEmployees,
+};
