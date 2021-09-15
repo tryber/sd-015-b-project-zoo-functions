0a4ac68bdc9ab89e8c6cf2c4ff7ce27b76a157fa
Caio R. Vieira <caio.robs98@gmail.com>
2021-09-15 00:49:58 -0300

    terminado o projeto

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+const getManagersIds = () => {
+  const managerList = [];
+  data.employees.forEach(({ managers }) => {
+    managers.forEach((manager) => {
+      if (!managerList.includes(manager)) managerList.push(manager);
+    });
+  });
+  return managerList;
+};
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
-}
+const isManager = (id) => {
+  const managerIds = getManagersIds();
+  if (managerIds.includes(id)) return true;
+  return false;
+};
+
+const getRelatedEmployees = (managerId) => {
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const relateds = data.employees.filter((employee) => employee.managers.includes(managerId));
+  return relateds.map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+};
+
+getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992');
 
 module.exports = { isManager, getRelatedEmployees };
