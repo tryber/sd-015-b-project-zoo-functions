040eb09d13657b52318de555fb08d79d0fd59a68
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-09-15 13:42:16 -0300

    Retira comentarios de linha e arruma alguns espacamentos

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -5,21 +5,19 @@ const days = Object.keys(hours);
 const allAnimals = species.map((specie) => specie.name);
 const daysAndAnimals = days.concat(allAnimals);
 
-// Cria function que cria os OBJ;
-
 function scheduleGenerator(day) {
   let officeHour;
   let exhibition;
-  // Cria mensagem caso o dia seja Monday
+
   if (day === 'Monday') {
     officeHour = 'CLOSED';
     exhibition = 'The zoo will be closed!';
   } else {
-    // Cria nessa linha a mensagem de acordo para cada dia
     const animals = species.filter((specie) => specie.availability.includes(day));
     officeHour = `Open from ${hours[day].open}am until ${hours[day].close}pm`;
     exhibition = animals.map((animal) => animal.name);
   }
+
   return {
     officeHour,
     exhibition,
@@ -28,18 +26,23 @@ function scheduleGenerator(day) {
 
 function crateFullSchedule() {
   const schedule = {};
+
   days.forEach((day) => { schedule[day] = scheduleGenerator(day); });
+
   return schedule;
 }
 
 function createScheduleOfDay(scheduleTarget) {
   const schedule = {};
+
   schedule[scheduleTarget] = scheduleGenerator(scheduleTarget);
+
   return schedule;
 }
 
 function createScheduleOfAnimal(scheduleTarget) {
   const getDaysOfAnimal = species.find((specie) => specie.name === scheduleTarget);
+
   return getDaysOfAnimal.availability;
 }
 
