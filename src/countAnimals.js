a865b124113b0f4a4deb9d00e3a450c084c95da7
samuel <samuelsoutosilva14@gmail.com>
2021-09-14 21:19:34 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-
+  if (animal === undefined) return species.map((curr) => `${curr.name}: ${curr.residents.length}`);
+  const { residents } = species.find((specie) => specie.name === animal.specie);
+  const sexResidentsLength = residents.filter((resident) => resident.sex === animal.sex).length;
+  return (animal.sex) ? sexResidentsLength : residents.length;
 }
 
 module.exports = countAnimals;
