3cd3a3af51867667ebbe64620d70b390e8dcce60
Valdênio <valdeniomartinho@gmail.com>
2021-09-15 10:27:04 -0300

    corrige lint do requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,29 +1,29 @@
 const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-//function countEntrants(entrants) {
-  // seu código aqui
-//}
+// function countEntrants(entrants) {
+// seu código aqui
+// }
 const countEntrants = (entrants) => {
-if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
   const entries = {
     child: entrants.filter((child) => child.age < 18).length,
     adult: entrants.filter((adult) => adult.age >= 18 && adult.age < 50).length,
     senior: entrants.filter((senior) => senior.age >= 50).length,
   };
-  return entries;  
-}
+  return entries;
+};
 
 function calculateEntry(entrants) {
   // seu código aqui
- const checkedEntries = countEntrants(entrants);
- if (checkedEntries === 0) return 0;
- 
- const totalChilds = checkedEntries.child * prices.child;
- const totalAdults = checkedEntries.adult * prices.adult;
- const totalSeniors = checkedEntries.senior * prices.senior;
+  const checkedEntries = countEntrants(entrants);
+  if (checkedEntries === 0) return 0;
 
- return totalChilds + totalAdults + totalSeniors;
+  const totalChilds = checkedEntries.child * prices.child;
+  const totalAdults = checkedEntries.adult * prices.adult;
+  const totalSeniors = checkedEntries.senior * prices.senior;
+
+  return totalChilds + totalAdults + totalSeniors;
 }
 
 module.exports = { calculateEntry, countEntrants };
