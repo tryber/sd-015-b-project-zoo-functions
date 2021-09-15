7dff4d79f1289aca6ae4fd1abc9922405a734cb8
Joao Pedro de Oli. Lage <JoaoPedro.Lage@outlook.com>
2021-09-14 22:21:54 -0300

    Corrigi erro na função getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -13,7 +13,9 @@ function isManager(id) {
     managersId.managers.find((espeficManager) => espeficManager === id));
 }
 
-if (isManager() === false) throw new Error('parameter y must not be 0');
+if (isManager() === false) {
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+}
 
 console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
