33d8611b74545a81500f35ce00a6a2646f0d5780
Deivid <deividmborges@gmail.com>
2021-09-15 14:02:38 -0300

    Corrige lint requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,10 +1,11 @@
 const data = require('../data/zoo_data');
+
 const { species } = data;
 const { employees } = data;
 
 function getOldestFromFirstSpecies(employeedId) {
   const getFirstSpecie = employees.find((animal) => employeedId === animal.id).responsibleFor
-  .filter((id) => species.find((specie) => specie.id === id).name)[0];
+    .filter((id) => species.find((specie) => specie.id === id).name)[0];
   const getResidentsById = species.find((specie) => specie.id === getFirstSpecie).residents;
 
   const older = getResidentsById.sort((a, b) => b.age - a.age)[0];
