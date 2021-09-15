7dd431fb3c889b681c0e3b347d5aa2c9e8224b15
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2021-09-14 23:48:02 -0300

    função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,38 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+function contadorPorGenero(animal, gender) {
+  let count = 0;
+  animal.forEach(({ sex } = animal) => {
+    if (sex === gender) {
+      count += 1;
+    }
+  });
+  return count;
+}
+
+function contadorPorEspecie(animal) {
+  const filtroDeEspecie = species.find(({ name } = species) => name === animal.specie);
+
+  if (Object.keys(animal).length === 1) {
+    return filtroDeEspecie.residents.length;
+  }
+  return contadorPorGenero(filtroDeEspecie.residents, animal.gender);
+}
+
+function contadorTodosAnnimais() {
+  const todasEspecies = {};
+  species.forEach(({ name, residents } = species) => {
+    todasEspecies[name] = residents.length;
+  });
+  return todasEspecies;
+}
 
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    return contadorTodosAnnimais();
+  }
+  return contadorPorEspecie(animal);
 }
-
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,13 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+
+  // employees.some((pessoa) => pessoa.managers !== []);
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+
 }
 
 module.exports = { isManager, getRelatedEmployees };
