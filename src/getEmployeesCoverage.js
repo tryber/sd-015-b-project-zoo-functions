e5458f07a1e47396070f4b579d603b1d29b59164
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-14 23:57:01 -0300

    Inicia implementaçao da funçao.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,13 +1,22 @@
 const data = require('../data/zoo_data');
+const {employees} = data;
+const noData = () => {
+  // Se nenhum dado é passado
+  // Retorna todos os dados formatados
+  // Para cada Funcionário, pegar os animais que ele cuida
+  // E também a localização desse animais
+  const allData = [];
+  const funcMap = allData.push(employees.map( (employee) => {
+    // console.log(employee.id);
+    return employee.id;
+  }))
+  // console.log(employees);
+  return allData
+}
+console.log(noData());
 
 function getEmployeesCoverage() {
   // seu código aqui
 }
 
 module.exports = getEmployeesCoverage;
-
-// LÓGICA DA FUNÇÃO:
-// **
-// **
-// **
-// **
