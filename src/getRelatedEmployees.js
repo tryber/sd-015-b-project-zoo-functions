b85c85ec07d24aed1d0fb2359570bd94545f6ef8
Joao Pedro de Oli. Lage <JoaoPedro.Lage@outlook.com>
2021-09-14 22:19:32 -0300

    Implementa a função getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
+function getRelatedEmployees(managerId) {
+  const findManager = data.employees.filter((employees) =>
+    employees.managers.find((espeficManager) => espeficManager === managerId));
+  return findManager.map((employeesNames) =>
+    `${employeesNames.firstName} ${employeesNames.lastName}`);
 }
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
+function isManager(id) {
+  getRelatedEmployees(id);
+  return data.employees.some((managersId) =>
+    managersId.managers.find((espeficManager) => espeficManager === id));
 }
+
+if (isManager() === false) throw new Error('parameter y must not be 0');
+
+console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
