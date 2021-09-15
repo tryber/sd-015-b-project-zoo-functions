8cced306fa50062c2a60358b9c4ed1b9e4f0c34e
Toineto <augusto.vilhena4@gmail.com>
2021-09-15 09:40:12 -0300

    refatora requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const { species } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  return species.filter((specie) => ids.includes(specie.id));
+function getSpeciesByIds(...ids) {
+  return species.filter((idPesquisado) => ids.includes(idPesquisado.id));
 }
 
 module.exports = getSpeciesByIds;
