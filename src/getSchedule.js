c8c4c797f05caeb9c3f496568f7779f0ad4aa091
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-15 15:14:13 -0300

    Elimina redundancia.

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -2,33 +2,33 @@ const data = require('../data/zoo_data');
 
 const { species, hours } = data;
 
-function argNull() {
-  const diasSemana = Object.keys(hours);
-  const objetoRetornado = {};
-  diasSemana.forEach((day) => {
+const days = Object.keys(hours);
+
+function argNull() { // Retornado quando nenhum argumento é passado, ou argumento é inválido
+  const objetoComDias = {};
+  days.forEach((day) => {
     const messageOffice = `Open from ${hours[day].open}am until ${hours[day].close}pm`;
     const animaisExibicao = species
     .filter( (animal) => animal.availability
     .includes(day))
     .map((day) => day.name);
     if (day === 'Monday') {
-      objetoRetornado[day] = `{ 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' }`;
+      objetoComDias[day] = `{ 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' }`;
     } else {
-      objetoRetornado[day] = `{ 'officeHour': '${messageOffice}','exhibition': '[ ${animaisExibicao.join(`, `)} ]'}`;
+      objetoComDias[day] = `{ 'officeHour': '${messageOffice}','exhibition': '[ ${animaisExibicao.join(`, `)} ]'}`;
     }
   });
-  return objetoRetornado;
+  return objetoComDias;
 }
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
   const objetoRetorno = {};
-  const diasDaSemana = Object.keys(hours);
-  const found = diasDaSemana.includes(scheduleTarget);
+  const found = days.includes(scheduleTarget);
 
   if(found) { // Se encontrar o nome do dia passado no: diasDaSemana
     // console.log("ENCONTRADO!!!");
-    diasDaSemana.forEach((day) => {
+    days.forEach((day) => {
       const messageOffice = `Open from ${hours[day].open}am until ${hours[day].close}pm`;
       const animaisExibicao = species //Verifica as espécies
       .filter( (animal) => animal.availability // Verifica se o animal está disponível naquele dia
@@ -48,15 +48,7 @@ function getSchedule(scheduleTarget) {
 }
 
 // console.log(getSchedule()); // Elemento Vazio
-console.log(getSchedule('abc')); // Elemento Inválido
-// console.log(getSchedule('Thursday')); // Elemento Válido
+// console.log(getSchedule('abc')); // Elemento Inválido
+console.log(getSchedule('Thursday')); // Elemento Válido
 
 module.exports = getSchedule;
-
-// LÓGICA DA FUNÇÃO:
-// ** A função é chamada, com ou sem parâmetro
-// **** Se o parâetro não é passado, ou não é encontrado, retorna-se um objeto
-// com o dia, horário de funcionamento daquele dia, e animais em exibição.
-// ** Se o parâmetro é passado, a arrowFunction retorna um obj construído com reduce,
-// com todos os dias e horários que aquele animal está disponível
-// **
