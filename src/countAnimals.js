b31195a8c360d0fb2e96a43ad027281538466a2b
Lucas Maieski Marques <lucmaieski@gmail.com>
2021-09-14 22:27:19 -0300

    Feito requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return species.reduce((acc, atual) => {
+      acc[atual.name] = atual.residents.length;
+      return acc;
+    }, {});
+  }
+  const { residents } = species.find((animals) => animals.name === animal.specie);
+  const genderQuantity = residents.filter((resident) => resident.sex === animal.gender).length;
+
+  return (animal.gender) ? genderQuantity : residents.length;
 }
 
 module.exports = countAnimals;
