302fc6f89a09676fedd796f7b459f256d2529f13
Richard Leal <rwsleal@gmail.com>
2021-09-14 21:22:28 -0300

    Requisito 6 finalizado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,26 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const childCount = entrants.filter((children) => children.age < 18).length;
+  const adultCount = entrants.filter((adults) => adults.age >= 18 && adults.age < 50).length;
+  const seniorCount = entrants.filter((seniors) => seniors.age >= 50).length;
+
+  return {
+    child: childCount,
+    adult: adultCount,
+    senior: seniorCount,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+
+  const { child, adult, senior } = countEntrants(entrants);
+
+  return (child * prices.child) + (adult * prices.adult) + (senior * prices.senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
