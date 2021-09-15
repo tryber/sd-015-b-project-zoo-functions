80df0cfc68f67e024f89af4b67d37db08902375f
Rafael Colombo <rcolombodev@gmail.com>
2021-09-15 13:33:26 -0300

    Resolve requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,61 @@
+const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+function checkParameters(object) {
+  const { id, name } = object;
+  let teste;
+  if (id) teste = employees.find((person) => person.id === id);
+  if (name) {
+    teste = employees
+      .find(({ firstName, lastName }) => firstName === name || lastName === name);
+  }
+  return teste;
+}
 
+function getEmployee(object) {
+  return employees.find(({ id, firstName, lastName }) => Object.values(object).includes(id)
+    || Object.values(object).includes(firstName)
+    || Object.values(object).includes(lastName));
+}
+
+function getSpecies(responsibleFor) {
+  const getNameById = responsibleFor.map((specie) => species.find(({ id }) => id === specie).name);
+  return getNameById;
+}
+
+function getLocation(responsibleFor) {
+  const locations = responsibleFor.map((specie) => species
+    .find(({ id }) => id === specie).location);
+  return locations;
+}
+
+function objectConstructing(object) {
+  const employee = checkParameters(object);
+  if (!employee) throw new Error('Informações inválidas');
+  const obj = {};
+  const { id, firstName, lastName, responsibleFor } = getEmployee(object);
+  const speciesName = getSpecies(responsibleFor);
+  const animalLocation = getLocation(responsibleFor);
+  Object.assign(obj, {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: speciesName,
+    locations: animalLocation,
+  });
+
+  return obj;
+}
+
+function getAllEmployees() {
+  const allEmployees = [];
+  employees.forEach((employee) => {
+    allEmployees.push(objectConstructing({ id: employee.id }));
+  });
+  return allEmployees;
+}
+
+function getEmployeesCoverage(object) {
+  return object ? objectConstructing(object) : getAllEmployees();
 }
 
 module.exports = getEmployeesCoverage;
