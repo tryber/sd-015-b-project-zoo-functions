cad741f4edb9da1f2b49d801662de71f11aaccf4
alxedv <alexmaia.etel@gmail.com>
2021-09-15 00:13:51 -0300

    Adiciona o requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,19 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const searchResult = [];
+  if (ids.length === 0) {
+    return [];
+  }
+  for (let index = 0; index < ids.length; index += 1) {
+    const verifyId = element => element.id === ids[index];
+     searchResult.push(species.find(verifyId));
+  }
+  return searchResult;
 }
+
+getSpeciesByIds('89be95b3-47e4-4c5b-b687-1fabf2afa274');
 
 module.exports = getSpeciesByIds;
