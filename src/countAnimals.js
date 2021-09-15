1dcf6517becf92d2a2fa7a569b3db9937aaf8c0b
Hugoacr <hugoacr@yahoo.com.br>
2021-09-15 09:25:22 -0300

    responde requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const animals = data.species.reduce((accumulator, specieAnimal) => {
+      accumulator[specieAnimal.name] = specieAnimal.residents.length;
+      return accumulator;
+    }, {});
+    return animals;
+  }
+  const { specie, gender } = animal;
+  const animalSpecie = data.species.find((element) => element.name === specie);
+  if (Object.keys(animal).includes('specie') && Object.keys(animal).includes('gender')) {
+    const genderAnimals = animalSpecie.residents.filter((element) => element.sex === gender);
+    return genderAnimals.length;
+  }
+  return animalSpecie.residents.length;
 }
 
 module.exports = countAnimals;
