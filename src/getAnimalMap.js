7924de837cfe60f4f0290b43fcda851573c20dc1
Sayro <sayrodamaceno70@gmail.com>
2021-09-15 06:15:04 -0300

    cumpre requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,57 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+// cria um array com o nome de todos os animais escolhido
+const animalsNames = (animalName) => {
+  const animalSpecie = species.find((animal) => animal.name === animalName);
+  return animalSpecie.residents.map((animal) => animal.name);
+};
+
+// cria um objeto com a posição de todos os animais.
+const locations = species.reduce((acc, animals) => {
+  if (animals.location === 'NE') {
+    acc.NE.push(animals.name);
+    return acc;
+  }
+  if (animals.location === 'NW') {
+    acc.NW.push(animals.name);
+    return acc;
+  }
+  if (animals.location === 'SE') {
+    acc.SE.push(animals.name);
+    return acc;
+  }
+  acc.SW.push(animals.name);
+  return acc;
+}, { NE: [], NW: [], SE: [], SW: [] });
+
+// cria um objeto com a posição de todos os animais e os nomes.
+const locationsAndNames = species.reduce((acc, animals) => {
+  if (animals.location === 'NE') {
+    const animalSpecie = animals.name;
+    const arrayNames = animalsNames(animalSpecie);
+    acc.NE.push(objectNames);
+    return acc;
+  }
+  if (animals.location === 'NW') {
+    acc.NW.push(animalsNames(animals.name));
+    return acc;
+  }
+  if (animals.location === 'SE') {
+    acc.SE.push(animalsNames(animals.name));
+    return acc;
+  }
+  acc.SW.push(animalsNames(animals.name));
+  return acc;
+}, { NE: [], NW: [], SE: [], SW: [] });
 
 function getAnimalMap(options) {
   // seu código aqui
+  if (!options) {
+    return locations;
+  }
 }
+
+console.log(locationsAndNames);
 
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,60 @@
+const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+const daysOfWeek = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+const animalsName = species.map((animal) => animal.name);
+
+// cria o array com os animais do dia
+function animalsAvaliables(day) {
+  return species.filter((animal) => animal.availability.includes(day)).map((animal) => animal.name);
+}
+
+// cria o texto com o horario de exibição
+function hoursAvailable(day) {
+  return `Open from ${hours[day].open}am until ${hours[day].close}pm`;
+}
+
+// cria um objeto com officeHour e exhibition
+function createObj(day) {
+  if (day === 'Monday') {
+    return { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+  }
+  return {
+    officeHour: hoursAvailable(day),
+    exhibition: animalsAvaliables(day),
+  };
+}
+
+// cria um objeto com o horario de funcionamento e animais disponiveis
+function createDayExhibition(day) {
+  const dayExhibition = {};
+  dayExhibition[day] = createObj(day);
+  return dayExhibition;
+}
+
+// cria um objeto com todos os dias da semana
+function allDaysExhibition() {
+  return daysOfWeek.reduce((acc, day) => {
+    acc[day] = createObj(day);
+    return acc;
+  }, {});
+}
+
+// função que retorna um array com os dias da semana que um animal esta em exibição
+function daysInExhibition(animalName) {
+  return species.find((animal) => animal.name === animalName).availability;
+}
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  if (daysOfWeek.includes(scheduleTarget)) {
+    return createDayExhibition(scheduleTarget);
+  }
+
+  if (animalsName.includes(scheduleTarget)) {
+    return daysInExhibition(scheduleTarget);
+  }
+  return allDaysExhibition();
 }
 
 module.exports = getSchedule;
