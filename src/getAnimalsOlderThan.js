6b3962e4d9554e37cf29082f43c0acdc03816a63
Denis Jonathan <denisjonathan7@gmail.com>
2021-09-15 00:23:11 -0300

    🎨 Requisito 2: refatorado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,9 +1,10 @@
 const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  return species
-    .find((specie) => (specie.name === animal)).residents
-    .every((resident) => resident.age >= age);
+  const { residents } = species.find((specie) => (specie.name === animal));
+  const filterByAge = residents.every((resident) => resident.age >= age);
+  console.log(residents);
+  return filterByAge;
 }
 
 module.exports = getAnimalsOlderThan;
