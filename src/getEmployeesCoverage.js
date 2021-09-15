72fa505c3e938b6e60a78a0e6cfc43c4648b91fe
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-15 00:25:44 -0300

    Testa funçao getAnimal.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -4,33 +4,38 @@ const {employees} = data;
 
 const getAnimal = (...ids) => {
   const nomesAnimais = [];
-  nomesAnimais.push(ids.forEach( (id) => {
-    return id;
-  }));
-  return nomesAnimais;
+  // console.log(ids);
+  const capturaIds = ids.forEach( (id) => {
+    nomesAnimais.push(id);
+  })
+  // console.log(nomesAnimais);
+  return nomesAnimais[0];
 }
+// const testeIdAnimais = ['0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'];
+// console.log(getAnimal(testeIdAnimais));
 
 const noData = () => {
-  // Se nenhum dado é passado
-  // Retorna todos os dados formatados
+  // Retorna todos os dados formatados.
   // Para cada Funcionário, pegar os animais que ele cuida
   // E também a localização desse animais
   
   const funcMap = employees.map( (employee) => {
     // console.log(employee.id);
     const arrayAnimais = 0;
+    const idsAnimais = employee.responsibleFor;
     return { 
       id: employee.id,
       fullName: `${employee.firstName} ${employee.lastName}`,
-      species: `Tratar Id dos animais capturar o objeto retornado, e salvar seu nome`,
-      locations: `Tratar o objeto retornado e mostrar sua localização`,
+      // species: `Tratar Id dos animais capturar o objeto retornado, e salvar seu nome`, // getAnimal(idsAnimais);
+      species: getAnimal(idsAnimais),
+      locations: `Tratar o objeto retornado e mostrar sua localização`, // getLocation();
      };
   })
   // console.log(employees);
   const allData = (funcMap);
   return allData
 }
-// console.log(noData());
+console.log(noData());
 
 function getEmployeesCoverage(peopleId) {
   // seu código aqui
