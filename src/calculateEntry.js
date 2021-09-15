b22247bc501fe66a72571d1d68ea327ad708b8c4
Flavio Junior <flaviopds.jr@gmail.com>
2021-09-15 14:43:26 -0300

    Adiciona exercicio

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,44 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const obj = { child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      obj.child += 1;
+    } else if (element.age >= 18 && element.age < 50) {
+      obj.adult += 1;
+    } else {
+      obj.senior += 1;
+    }
+  });
+  return obj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  console.log(entrants);
+  if (Object.keys(entrants).length === 0 || !entrants) {
+    return 0;
+  }
+  const childTotal = entrants.child * prices.child;
+  const adultTotal = entrants.adult * prices.adult;
+  const seniorTotal = entrants.senior * prices.senior;
+  const total = childTotal + adultTotal + seniorTotal;
+  console.log(total);
+  return total;
 }
 
+const entrants = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
+calculateEntry(countEntrants(entrants));
 module.exports = { calculateEntry, countEntrants };
--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,26 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function arrayNW() {
+  const arrayNW = [];
+  const NW = species.filter((a) => {
+    if
+  })
+}
+
+function getAnimalsLocation() {
+  const obj = {
+    NE: arrayNW(),
+    NW: [],
+    SE: [],
+    SW: [],
+  };
+  console.log(obj);
+}
+
+getAnimalsLocation();
 function getAnimalMap(options) {
-  // seu código aqui
+
 }
 
 module.exports = getAnimalMap;
