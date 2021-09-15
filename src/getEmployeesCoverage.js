01d6ae2dc5d83e93b0b45d7ef01f82680ec7a14f
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-15 00:07:54 -0300

    Salva codigo.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,16 +1,32 @@
 const data = require('../data/zoo_data');
+
 const {employees} = data;
+
+const getAnimal = (...ids) => {
+  const nomesAnimais = [];
+  nomesAnimais.push(ids.forEach( (id) => {
+    return id;
+  }));
+  return nomesAnimais;
+}
+
 const noData = () => {
   // Se nenhum dado é passado
   // Retorna todos os dados formatados
   // Para cada Funcionário, pegar os animais que ele cuida
   // E também a localização desse animais
-  const allData = [];
-  const funcMap = allData.push(employees.map( (employee) => {
+  
+  const funcMap = employees.map( (employee) => {
     // console.log(employee.id);
-    return employee.id;
-  }))
+    const arrayAnimais = 0;
+    return { 
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: `${getAnimal(employee.responsibleFor)}`,
+     };
+  })
   // console.log(employees);
+  const allData = (funcMap);
   return allData
 }
 console.log(noData());
