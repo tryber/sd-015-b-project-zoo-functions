93d709c3033bfd53f67e7290c1cf03f75ce1d325
Marina Marques <marina.marqueso@outlook.com>
2021-09-14 21:54:00 -0300

    Adiciona requisito 5 feito, sem lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -3,46 +3,48 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   if (animal === undefined) {
-  // finding specie
-  const findingAnimals = species.map((elemento) => elemento.name);
-  // filtering residents
-  const findingResidest = species.map((elemento) => elemento.residents.length);
-  // construindo objeto
-  const obj = {};
-  for (let i = 0; i < findingAnimals.length; i++) {
-    const animal = findingAnimals[i];
-    obj[animal] = findingResidest;
-  }
-  return obj;
-  }
-
-  if (animal.gender === undefined) {
-  // finding specie
-  const specie = animal.specie;
-  // finding specie
-  const findingAnimal = species.find((elemento) => specie.includes(elemento.name));
-  // filtering residents
-  return findingAnimal.residents.length;
-  }
-
-  if (animal.gender !== undefined) {
-  // finding specie
-  const specie = animal.specie;
-  // finding specie
-  const findingAnimal = species.find((elemento) => specie.includes(elemento.name));
-  // filtering gender
-  const sex = animal.gender;
-  // finding specie gender
-  const animales = findingAnimal.residents;
-  // filtrar o genero dos animales para conta-los
-  const filteringGender = animales.filter((elemento) => {
-    if (elemento.sex === sex) {
-      return elemento;
+    // finding specie
+    const findingAnimals = species.map((elemento) => elemento.name);
+    // filtering residents
+    const findingResidents = species.map((elemento) => elemento.residents.length);
+    // construindo objeto
+    const obj = {};
+    for (let i = 0; i < findingAnimals.length; i += 1) {
+      for (let j = 0; j < findingResidents.length; j += 1) {
+        obj[findingAnimals[i]] = findingResidents[i];
+      }
     }
-  });
-  return filteringGender.length;
+    return obj;
+  }
+
+  if (animal.sex === undefined) {
+  // finding specie
+    const { specie } = animal;
+    // finding specie
+    const findingAnimal = species.find((elemento) => specie.includes(elemento.name));
+    // filtering residents
+    return findingAnimal.residents.length;
+  }
+
+  if (animal.sex !== undefined) {
+  // finding specie
+    const { specie } = animal;
+    // finding specie
+    const findingAnimal = species.find((elemento) => specie.includes(elemento.name));
+    // filtering sex
+    const { sex } = animal;
+    // finding specie gender
+    const animales = findingAnimal.residents;
+    // filtrar o genero dos animales para conta-los
+    const filteringGender = animales.filter((elemento) => {
+      if (elemento.sex === sex) {
+        return elemento;
+      }
+    });
+    return filteringGender.length;
   }
 }
+
 module.exports = countAnimals;
 
 console.log(countAnimals());
