6e98d0e6fa2f186f2834415f25bf1064bd9bf22c
Amanda Fernandes <aferanda@gmail.com>
2021-09-15 12:18:45 -0300

    Requisito 10 - partes 1 a 3

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,40 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const getEmployee = (obj) => {
+  const { values } = Object;
+  return employees.find(({ id, firstName, lastName }) =>
+    values(obj).includes(id)
+    || values(obj).includes(firstName)
+    || values(obj).includes(lastName));
+};
+
+const getSpeciesName = (obj) => getEmployee(obj).responsibleFor
+  .map((specie) => species
+    .find(({ id }) => id === specie).name);
+
+const getSpeciesLocation = (obj) => getEmployee(obj).responsibleFor
+  .map((specie) => species
+    .find(({ id }) => id === specie).location);
+
+const objectConstructing = (obj) => {
+  const object = {};
+  const { id, firstName, lastName } = getEmployee(obj);
+  Object.assign(object, {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: getSpeciesName(obj),
+    locations: getSpeciesLocation(obj),
+  });
+  return object;
+};
+
+function getEmployeesCoverage(obj) {
+  return objectConstructing(obj);
 }
+
+console.log(getEmployeesCoverage({ name: 'Sharonda' }));
+// console.log(getEmployeesCoverage({ name: 'Spry' }));
+// console.log(getEmployeesCoverage({ id: '4b40a139-d4dc-4f09-822d-ec25e819a5ad' }));
 
 module.exports = getEmployeesCoverage;
