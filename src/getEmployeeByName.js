1737fca1f6c061f5a39f9d506cf54d736a43fb84
Carlos Lima <carlosedu151@gmail.com>
2021-09-15 09:53:35 -0300

    Adicionando o terceiro requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const searchEmployee = employees
+    .find(({ firstName, lastName }) => employeeName === firstName || employeeName === lastName);
+  return employeeName ? searchEmployee : {};
 }
 
 module.exports = getEmployeeByName;
