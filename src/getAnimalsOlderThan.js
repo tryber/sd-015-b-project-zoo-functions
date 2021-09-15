b90f637a0d4609518cd489f046d595cc08daa52e
Denis Jonathan <denisjonathan7@gmail.com>
2021-09-15 01:47:11 -0300

    🔧 Removido console.log

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,7 +3,6 @@ const { species } = require('../data/zoo_data');
 function getAnimalsOlderThan(animal, age) {
   const { residents } = species.find((specie) => (specie.name === animal));
   const filterByAge = residents.every((resident) => resident.age >= age);
-  console.log(residents);
   return filterByAge;
 }
 
