def56de4882ca9fc84f7000fe9b46abcc58bcc91
Flávio Bianchetti <fjb.developer@gmail.com>
2021-09-15 00:31:07 -0300

    Requisito 6 - corrigidos erros de indentaçao

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -14,17 +14,17 @@ const calculateEntry = (entrants) => {
   let result = 0;
   const validateEntrants = entrants !== undefined && entrants[0] !== undefined;
   if (validateEntrants) {
-    const {child, adult, senior} = countEntrants(entrants);
+    const { child, adult, senior } = countEntrants(entrants);
 
     const sumEntrants = {
       amountChild: child * data.prices.child,
       amountAdult: adult * data.prices.adult,
       amountSenior: senior * data.prices.senior,
-    }
+    };
 
     result = sumEntrants.amountChild + sumEntrants.amountAdult + sumEntrants.amountSenior;
   }
   return result;
-}
+};
 
 module.exports = { calculateEntry, countEntrants };
