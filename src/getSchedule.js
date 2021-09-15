ba91f6b7d5b2dcb866a237a13e1d03fa97f9f840
Rafael Colombo <rcolombodev@gmail.com>
2021-09-14 23:33:20 -0300

    Resolve requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -4,11 +4,7 @@ const { species, hours } = data;
 
 const infoHours = Object.entries(hours);
 
-// for (let day in hours) {
-//   console.log(`${day} = ${hours[day]}`)
-// }
-
-function withoutParameters() {
+function zooSchedule() {
   const object = {};
   infoHours.forEach((day) => {
     const { open, close } = day[1];
@@ -21,10 +17,18 @@ function withoutParameters() {
   object.Monday = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
   return object;
 }
+const getDays = (scheduleTarget) => ({ [scheduleTarget]: zooSchedule()[scheduleTarget] });
 
-function getSchedule(scheduleTarget, animal, day) {
-  if (!scheduleTarget) return withoutParameters();
-  if (!animal && !day) return withoutParameters();
+function getAnimals(scheduleTarget) {
+  return species.find((specie) => specie.name === scheduleTarget).availability;
+}
+
+function getSchedule(scheduleTarget) {
+  const checkDays = Object.keys(zooSchedule()).includes(scheduleTarget);
+  const checkAnimals = species.map((specie) => specie.name).includes(scheduleTarget);
+  if (checkDays) return getDays(scheduleTarget);
+  if (checkAnimals) return getAnimals(scheduleTarget);
+  return zooSchedule();
 }
 
 module.exports = getSchedule;
