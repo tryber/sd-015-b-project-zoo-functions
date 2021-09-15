43d50437e59dcdd8f2d4a6327637dabed22e116d
Alex Menezes <alexmenezes.business@outlook.com>
2021-09-15 00:37:20 -0300

    adiciona requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,16 +2,25 @@ const data = require('../data/zoo_data');
 
 const { species, employees } = data;
 
-const findOldestResident = (animals) => ('cheguei');
+function findOldestResident(animals) {
+  const oldest = animals.reduce((acc, cur) => {
+    let older = acc;
+    const { age } = cur;
+    if (age > older) {
+      older = age;
+    }
+    return older;
+  }, 0);
+  return oldest;
+}
 
 function getOldestFromFirstSpecies(id) {
   const employeeById = employees.find((employee) => employee.id === id);
-  const firstSpecie = employeeById.resposibleFor[0];
+  const firstSpecie = employeeById.responsibleFor[0];
   const speciesGroup = species.find((animal) => animal.id === firstSpecie);
   const { residents } = speciesGroup;
   const oldestAnimal = residents.find((animal) => animal.age === findOldestResident(residents));
   return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
-console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
