80dadc1a83033dae2ae6edd66a2c7f206b7ad39c
Laurenz Pinder <laurenzdpinder90@gmail.com>
2021-09-15 00:00:57 -0300

    Adiciona primeiro item do requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,25 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+function getSpeciesLocation() {
+  const cardinalPoints = [];
+  species.forEach(({ location }) => cardinalPoints.push(location));
+  const speciesLocation = {};
+  cardinalPoints.forEach((point) => {
+    speciesLocation[point] = species
+      .filter(({ location }) => location === point)
+      .map(({ name }) => name);
+  });
+  return speciesLocation;
+}
 
 function getAnimalMap(options) {
   // seu código aqui
+  if (!options) {
+    return getSpeciesLocation();
+  }
 }
+
+// console.log(getAnimalMap());
 
 module.exports = getAnimalMap;
