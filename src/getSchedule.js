4d5b44fb6c44a53c0b28a0dd69de089a0e721b6b
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-09-15 09:38:18 -0300

    Requisito 8.1 ok

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,42 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+function exhibitionAnimals(weekDays) {
+  // return species.map((obj) => {
+  //   if (obj.availability.includes(weekDays)) {
+  //     return obj.name;
+  //   }
+  // });
+  return species.map((obj) => (obj.availability.includes(weekDays) ? obj.name : undefined));
+}
+function createOfficeHour() {
+  const weekDays = Object.keys(data.hours);
+  return weekDays.reduce((acc, weekDay) => {
+    const { open } = data.hours[weekDay];
+    const { close } = data.hours[weekDay];
+    if (weekDay === 'Monday') {
+      acc[weekDay] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    } else {
+      acc[weekDay] = {
+        officeHour: `Open from ${open}am until ${close}pm`,
+        // species.map((obj) => obj.name), - retorna nome de todos os animais
+        exhibition: exhibitionAnimals(weekDay).filter((animal) => animal !== undefined),
+      };
+    }
+    return acc;
+  }, {});
+}
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  if (!scheduleTarget) {
+    const schedule = createOfficeHour();
+    return schedule;
+  }
 }
-
+const a = getSchedule();
+console.log(a);
 module.exports = getSchedule;
