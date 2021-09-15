01fceaaf1a0695e71f85609b215ec80c1174a56a
fernandamichetti <f.michetti@hotmail.com>
2021-09-15 11:46:12 -0300

    lint requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -6,13 +6,13 @@ const animals = (animal) => animal.reduce((acc, crr) => {
 }, {});
 
 const countAnimals = (animal) => {
-if (!animal) return animals(data.species);
+  if (!animal) return animals(data.species);
 
-const { residents } = data.species.find((specie) => specie.name === animal.specie);
+  const { residents } = data.species.find((specie) => specie.name === animal.specie);
 
-return (!animal.gender) ? residents.length
- : residents.filter((name) => name.sex === animal.gender).length
-}
+  return (!animal.gender) ? residents.length
+    : residents.filter((name) => name.sex === animal.gender).length;
+};
 
-//ref usado para consulta: https://github.com/tryber/sd-015-b-project-zoo-functions/commit/98a9ac4e69814b509d6b3dd07ad0b61a515d5ae8
+// ref usado para consulta: https://github.com/tryber/sd-015-b-project-zoo-functions/commit/98a9ac4e69814b509d6b3dd07ad0b61a515d5ae8
 module.exports = countAnimals;
