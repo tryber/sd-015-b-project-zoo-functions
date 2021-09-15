35b04ae8048f65020a981c14a0d1331707fac826
Lucas Peres <lucasperesss@gmail.com>
2021-09-15 00:20:10 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = data.employees.find((emp) => emp.id === id);
+  const firstSpecieId = employee.responsibleFor[0];
+  const firstSpecie = data.species.find((specie) => specie.id === firstSpecieId);
+  const oldestResident = firstSpecie.residents.reduce((oldest, resident) => {
+    if (resident.age > oldest.age) {
+      return resident;
+    }
+    return oldest;
+  });
+  return [oldestResident.name, oldestResident.sex, oldestResident.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
