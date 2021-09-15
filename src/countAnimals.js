67be1a4fafaa48ebc59bb22bd4a9efee94a9d54b
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-09-15 01:07:28 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,14 +1,32 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const getAllAnimalsCount = () => {
+  const animalsCount = {};
+  species.forEach((specie) => {
+    animalsCount[specie.name] = specie.residents.length;
+  });
+  return animalsCount;
+};
+
 function countAnimals(animal) {
   // seu código aqui
-  const { gender, specie } = animal;
+  if (!animal) {
+    return getAllAnimalsCount();
+  }
+  if (animal.sex) {
+    const searchAnimal = species.find(
+      (especie) => especie.name === animal.specie,
+    );
+    const countAnimalBySex = searchAnimal.residents.filter(
+      (nomeDosAnimais) => nomeDosAnimais.sex === animal.sex,
+    ).length;
+    return countAnimalBySex;
+  }
+  const searchAnimal = species.find((especie) => especie.name === animal.specie)
+    .residents.length;
 
-  console.log(specie);
-  return !animal ? gender : species.find((especie) => especie.name === animal.specie).residents;
+  return searchAnimal;
 }
 
 module.exports = countAnimals;
-
-console.log(countAnimals({ specie: 'bears' }));
