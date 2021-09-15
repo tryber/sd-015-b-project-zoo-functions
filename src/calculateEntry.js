5f20042675f82b16200ee861a4de467650b297f8
Walace <walacewab@gmail.com>
2021-09-14 21:37:44 -0300

    Adiciona requisito 9

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -4,9 +4,9 @@ const { prices } = data;
 
 function countEntrants(entrants) {
   // seu código aqui
-  const child = entrants.filter((entrada) => entrada.age < 18);
-  const adult = entrants.filter((entrada) => entrada.age >= 18 && entrada.age < 50);
-  const senior = entrants.filter((entrada) => entrada.age >= 50);
+  const child = entrants.filter((entrant) => entrant.age < 18);
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50);
+  const senior = entrants.filter((entrant) => entrant.age >= 50);
   return { child: child.length, adult: adult.length, senior: senior.length };
 }
 
