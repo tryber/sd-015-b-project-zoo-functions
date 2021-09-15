234056489e9b7ee5a4dd2543155bcf6ce854091b
José Erivam <erivamdev@gmail.com>
2021-09-14 21:18:50 -0300

    adding requisite 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function returnList() {
+  const animals = {};
+  data.species.forEach((element) => { animals[element.name] = element.residents.length; });
+  return animals;
 }
 
+function animalsCount(animal) {
+  const animalCount = data.species.find((element) => element.name === animal.specie);
+  if (animal && !animal.sex) return animalCount.residents.length;
+  let count = 0;
+  animalCount.residents.forEach((element) => { if (element.sex === animal.sex) count += 1; });
+  if (animal) return count;
+}
+
+function countAnimals(animal) {
+  if (!animal) return returnList();
+  return animalsCount(animal);
+}
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 module.exports = countAnimals;
