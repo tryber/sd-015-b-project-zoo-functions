c516e5ba70315c2ef199301bcae2624186e29513
Amanda Fernandes <aferanda@gmail.com>
2021-09-15 10:18:16 -0300

    Refatora requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,17 +2,16 @@ const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(employeeId) {
-
   const getFirstSpecie = employees
     .find(({ id }) => employeeId === id).responsibleFor;
 
-  const getResidents = species.find(({ id }) => id === getFirstSpecie[0]).residents;
+  const getResidents = species
+    .find(({ id }) => id === getFirstSpecie[0]).residents;
 
-  const findOldest = getResidents.sort((a, b) => b.age - a.age)[0];
+  const findOldest = getResidents
+    .sort((a, b) => b.age - a.age)[0];
 
   return Object.values(findOldest);
 }
-
-console.log(getOldestFromFirstSpecies('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = getOldestFromFirstSpecies;
