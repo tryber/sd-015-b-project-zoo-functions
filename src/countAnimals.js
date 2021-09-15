3f0c7925f278d4254fe4d31b41598829cb2e8b4d
Murilo <murilocarrijoadm@gmail.com>
2021-09-14 23:01:05 -0300

    Adiciona o 5º requisito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function accountAnimals() {
+  const accont = species
+    .reduce((acc, { name, residents }) => {
+      acc[name] = residents.length; return acc;
+    }, {});
+
+  return accont;
+}
+
+function filterAccountAnimals(specie, gender) {
+  const thisSpecie = species.find(({ name }) => name === specie);
+  const accont = gender
+    ? thisSpecie.residents.filter(({ sex }) =>
+      sex === gender).length : thisSpecie.residents.length;
+
+  return accont;
+}
+
 function countAnimals(animal) {
-  // seu código aqui
+  const accont = animal ? filterAccountAnimals(animal.specie, animal.gender) : accountAnimals();
+
+  return accont;
 }
 
 module.exports = countAnimals;
