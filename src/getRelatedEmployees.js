346b1a42ac011ef2716420f4643e5c3de46201a8
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2021-09-15 09:39:10 -0300

    função getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,14 +1,21 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
-
-  // employees.some((pessoa) => pessoa.managers !== []);
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  const gerente = isManager(managerId);
+  if (!gerente) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const filtroDeColaborador = employees.filter((employee) => employee.managers.includes(managerId));
 
+  return filtroDeColaborador.map((employee) =>
+    `${employee.firstName} ${employee.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
