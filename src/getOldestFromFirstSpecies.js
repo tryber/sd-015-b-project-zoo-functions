503601a738d8bfe833d69aa337e284ca4271dea9
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-14 21:48:06 -0300

    Corrige lint requisito 09.

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -6,16 +6,16 @@ function getOldestFromFirstSpecies(id) {
   // seu código aqui
   const resultEmployee = employees.find((employee) => employee.id === id); // Retorna a pessoa com a ID
   const primeiroConjuntoAnimal = resultEmployee.responsibleFor[0]; // Captura ID do primeiro conjunto animal
-  const buscaEspecie = species.find( (specie) => specie.id === primeiroConjuntoAnimal);
+  const buscaEspecie = species.find((specie) => specie.id === primeiroConjuntoAnimal);
   const residentsDaEspecie = buscaEspecie.residents;
   // Array com animais, do mais velho para o mais novo
-  const animalMaisVelho = residentsDaEspecie.sort( (a, b) => b.age - a.age);
+  const animalMaisVelho = residentsDaEspecie.sort((a, b) => b.age - a.age);
   const arrayComAnimal = [animalMaisVelho[0].name, animalMaisVelho[0].sex, animalMaisVelho[0].age];
   // const animalMaisVelho = getOldestAnimal(primeiroConjuntoAnimal);
   // console.log(primeiroConjuntoAnimal); // ID do Primeiro animal da pessoa
   // console.log(resultEmployee);
   // console.log(residentsDaEspecie);
-  return arrayComAnimal
+  return arrayComAnimal;
 }
 
 // console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
