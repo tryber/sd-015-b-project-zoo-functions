6de0a4bb5632351fec0cea4e0849997faacfb24b
Alex Menezes <alexmenezes.business@outlook.com>
2021-09-14 23:43:05 -0300

    corrige exercicio 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -13,14 +13,14 @@ function countAnimals(animal) {
     return resultado;
   }
   // desestruturando  o parametro para usar dados individuais
-  const { specie, gender } = animal;
-  if (!gender) {
+  const { specie, sex } = animal;
+  if (!sex) {
     const animalsFounded = species.find((animals1) => (animals1.name === specie));
     return animalsFounded.residents.length;
   }
   const findAnimalByGender = species.find((animals2) => (animals2.name === specie));
-  return findAnimalByGender.residents.filter((animals3) => animals3.sex === gender).length;
+  return findAnimalByGender.residents.filter((animals3) => animals3.sex === sex).length;
 }
 
-console.log(countAnimals({ specie: 'giraffes', gender: 'female' }));
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 module.exports = countAnimals;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
+const findOldestResident = (animals) => ('cheguei');
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employeeById = employees.find((employee) => employee.id === id);
+  const firstSpecie = employeeById.resposibleFor[0];
+  const speciesGroup = species.find((animal) => animal.id === firstSpecie);
+  const { residents } = speciesGroup;
+  const oldestAnimal = residents.find((animal) => animal.age === findOldestResident(residents));
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
