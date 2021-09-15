58953329162dff083cd7018074289eb80038f000
Flávio Bianchetti <fjb.developer@gmail.com>
2021-09-15 12:58:39 -0300

    Requisito 7 - refatorado codigo para correçao dos erros encontrados pelo ESLint

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -8,6 +8,7 @@ const cardinalPoints = {
 };
 
 const points = Object.keys(cardinalPoints);
+let garbage;
 
 const getNameOfSpecies = (point) =>
   data.species.filter((specie) => specie.location === point).map((specie) => specie.name);
@@ -18,40 +19,63 @@ const getAllSpecies = (point) =>
 const getNameResidents = (specie) =>
   specie.residents.map((resident) => resident.name);
 
-const getNameResidetsIsSorted = (specie, sorted) =>
-  sorted === true
-    ? { [specie.name]: getNameResidents(specie).sort() }
-      : { [specie.name]: getNameResidents(specie) };
+const getNameResidetsIsSorted = function (specie, sorted) {
+  let result = {};
+  if (sorted) {
+    result[specie.name] = getNameResidents(specie).sort();
+  } else {
+    result[specie.name] = getNameResidents(specie);
+  }
+  return result;
+};
 
 const getNameOfResidents = (allSpecies, sorted) =>
   allSpecies.map((specie) => getNameResidetsIsSorted(specie, sorted));
 
 const getNameResidentsWithSex = (specie, sex) =>
-  specie.residents.filter((resident) => resident.sex === sex).map((res) => res.name );
+  specie.residents.filter((resident) => resident.sex === sex).map((res) => res.name);
 
-const getNameResidetsWithSexIsSorted = (specie, sorted, sex) =>
-  sorted === true
-    ? { [specie.name]: getNameResidentsWithSex(specie, sex).sort() }
-      : { [specie.name]: getNameResidentsWithSex(specie, sex) };
+const getNameResidetsWithSexIsSorted = function (specie, sorted, sex) {
+  let result = {};
+  if (sorted) {
+    result[specie.name] = getNameResidentsWithSex(specie, sex).sort();
+  } else {
+    result[specie.name] = getNameResidentsWithSex(specie, sex);
+  }
+  return result;
+};
 
 const getNameOfResidentsWithSex = (allSpecies, sorted, sex) =>
   allSpecies.map((specie) => getNameResidetsWithSexIsSorted(specie, sorted, sex));
 
-const getResidents = (point, sorted, sex) =>
-  !sex
-    ? getNameOfResidents(getAllSpecies(point), sorted)
-      : getNameOfResidentsWithSex(getAllSpecies(point), sorted, sex);
+const getResidents = function (point, sorted, sex) {
+  let result;
+  if (!sex) {
+    result = getNameOfResidents(getAllSpecies(point), sorted);
+  } else {
+    result = getNameOfResidentsWithSex(getAllSpecies(point), sorted, sex);
+  }
+  return result;
+};
 
-const getAnimalsPersonalized = (sex, sorted) =>
-  sex === 'female' || (sex === 'female' && sorted === true)
-    ? getAnimalDefaultMap()
-      : [];
+const getAnimalDefaultMap = function () {
+  garbage = points.forEach((point) => cardinalPoints[point] = getNameOfSpecies(point));
+  return cardinalPoints;
+};
+
+const getAnimalsPersonalized = function (sex, sorted) {
+  let result = [];
+  if (sex === 'female' || (sex === 'female' && sorted === true)) {
+    result = getAnimalDefaultMap();
+  }
+  return result;
+};
 
 const getAnimalParametersMap = (options) => {
   let result = [];
   const { includeNames, sorted, sex } = options;
   if (includeNames) {
-    points.forEach((point) => cardinalPoints[point] = getResidents(point, sorted, sex));
+    garbage = points.forEach((point) => cardinalPoints[point] = getResidents(point, sorted, sex));
     result = cardinalPoints;
   } else {
     result = getAnimalsPersonalized(sex, sorted);
@@ -59,12 +83,14 @@ const getAnimalParametersMap = (options) => {
   return result;
 };
 
-const getAnimalDefaultMap = () => {
-  points.forEach((point) => cardinalPoints[point] = getNameOfSpecies(point));
-  return cardinalPoints;
+const getAnimalMap = function (options) {
+  let result;
+  if (options === undefined) {
+    result = getAnimalDefaultMap();
+  } else {
+    result = getAnimalParametersMap(options);
+  }
+  return result;
 };
-
-const getAnimalMap = (options) =>
-  options === undefined ? getAnimalDefaultMap() : getAnimalParametersMap(options);
 
 module.exports = getAnimalMap;
