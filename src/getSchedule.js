383c5c5aa0bda413e537c9838be1e520d96db692
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-09-15 13:00:08 -0300

    Requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,12 +1,9 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const days = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+
 function exhibitionAnimals(weekDays) {
-  // return species.map((obj) => {
-  //   if (obj.availability.includes(weekDays)) {
-  //     return obj.name;
-  //   }
-  // });
   return species.map((obj) => (obj.availability.includes(weekDays) ? obj.name : undefined));
 }
 function createOfficeHour() {
@@ -30,13 +27,40 @@ function createOfficeHour() {
   }, {});
 }
 
+function animalDaysList(obj, scheduleTarget) {
+  return (obj.name === scheduleTarget ? obj.availability : undefined);
+}
+
+function verifyAnimalWeekDay(isAnimal, isWeekDay, scheduleTarget) {
+  if ((!isAnimal) && (!isWeekDay)) {
+    const schedule = createOfficeHour();
+    return schedule;
+  }
+
+  if (isAnimal) {
+    const schedule = species.map((obj) => animalDaysList(obj, scheduleTarget))
+      .filter((arr) => arr !== undefined);
+    return schedule[0];
+  }
+
+  if (isWeekDay) {
+    const schedule = createOfficeHour();
+    return { [scheduleTarget]: schedule[scheduleTarget] };
+  }
+  return 'Insira uma string válida!';
+}
 function getSchedule(scheduleTarget) {
+  const isAnimal = species.filter((obj) => obj.name === scheduleTarget).length !== 0;
+  const isWeekDay = days.includes(scheduleTarget);
+
   // seu código aqui
   if (!scheduleTarget) {
     const schedule = createOfficeHour();
     return schedule;
   }
+  const schedule = verifyAnimalWeekDay(isAnimal, isWeekDay, scheduleTarget);
+  return schedule;
 }
-const a = getSchedule();
+const a = getSchedule('penguins');
 console.log(a);
 module.exports = getSchedule;
