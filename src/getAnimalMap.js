5fbe4e2b7f779c244055de04499ed3b34194f332
Lucas Maieski Marques <lucmaieski@gmail.com>
2021-09-16 15:50:48 -0300

    Teste para requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,24 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
-  // Seu código aqui
+  if (options === undefined) {
+    return species.reduce((acc, atual) => {
+      acc[atual.location] = species.filter((objeto) => objeto.location === atual.location)
+        .map((nome) => nome.name);
+      return acc;
+    }, {});
+  }
+  return species.reduce((acc, curr) => {
+    acc[curr.location] = species.filter((objeto) => objeto.location === curr.location)
+      .reduce((acumulador, atual) => {
+        acumulador[atual.name] = atual.residents.map((residentsName) => residentsName.name)
+        return acumulador;
+      }, {})
+    return acc;
+  }, {});
 }
 
-module.exports = getAnimalMap;
+console.log(getAnimalMap({ includeNames: true }));
+
+// module.exports = getAnimalMap;
