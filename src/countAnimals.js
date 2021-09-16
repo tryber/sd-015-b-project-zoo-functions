ab9f30badaa6e4a4c3225f81f1d2a39c2d28ef68
israel jeronimo <silvaij16@gmail.com>
2021-09-16 02:46:25 -0300

    5. Implemente a função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,22 +1,26 @@
 const { species } = require('../data/zoo_data');
 
-function countAnimals() {
-  return species.reduce((acc, { name, residents }) => {
-    acc[name] = residents.length;
-    return acc;
-  }, {});
+const getAnimalsAmount = () => species.reduce((acc, { name, residents }) => {
+  acc[name] = residents.length;
+  return acc;
+}, {});
+
+const countFemale = (resid) => {
+  let result = 0;
+  resid.forEach(({ sex }) => {
+    if (sex === 'female') result += 1;
+  });
+  return result;
+};
+
+function countAnimals(parameter) {
+  const animalsAmount = getAnimalsAmount();
+  if (!parameter) return animalsAmount;
+  const animalValue = Object.values(parameter);
+  if (animalValue.length === 1) return animalsAmount[animalValue];//
+  const residentes = species.find(({ name }) => name === animalValue[0]).residents;
+  if (animalValue.length === 2) return countFemale(residentes);
 }
 
-console.log(countAnimals());
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 module.exports = countAnimals;
-// species: {
-//   id: string;
-//   name: string;
-//   popularity: number;
-//   location: string;
-//   availability: string[];
-//   residents: {
-//       name: string;
-//       sex: string;
-//       age: number;
-//   }
