82fcd53cabac5c4de9d158bccdc19f2525fae28a
Joao Pedro de Oli. Lage <JoaoPedro.Lage@outlook.com>
2021-09-16 08:27:03 -0300

    Refatora a função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -7,18 +7,14 @@ function countAnimals(animal) {
       return acc;
     }, {});
   }
-  const findArrayOfAnimal = [];
   const StrAnimal = Object.values(animal);
   const ObjAnimal = data.species.find((specificAnimal) => specificAnimal.name === StrAnimal[0]);
-  findArrayOfAnimal.push(ObjAnimal);
-  const quantityResid = findArrayOfAnimal.map((specificAnimal) => specificAnimal.residents.length);
-  const result = quantityResid[0];
   if (StrAnimal[1] !== undefined) {
     return (ObjAnimal.residents.filter((specificAnimal) =>
       specificAnimal.sex === StrAnimal[1])).length;
   }
 
-  return result;
+  return ObjAnimal.residents.length;
 }
 
 module.exports = countAnimals;
