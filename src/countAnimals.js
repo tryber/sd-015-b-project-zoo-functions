41a5224285e3812748f1e16d4239a2eb7204cd47
uroque <ulissesroque@protonmail.com>
2021-09-16 11:57:58 -0300

    Adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
-const data = require('../data/zoo_data');
+// Foi necessário corrigir um erro no teste.
+// Enquanto o teste espera por species.residents.gender,
+// no arquivo zoo_data.js há species.residents.sex
+
+const { species } = require('../data/zoo_data');
+
+function isEmpty() {
+  const allSpeciesObj = {};
+  species.forEach((_species) => { allSpeciesObj[_species.name] = _species.residents.length; });
+  return allSpeciesObj;
+}
+
+function includesSex(selectedSpecies, animal) {
+  return selectedSpecies.residents.filter((_animal) => _animal.sex === animal.sex).length;
+}
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) { return isEmpty(); }
+  const selectedSpecies = species.find((_species) => _species.name === animal.specie);
+  if (animal.sex) { return includesSex(selectedSpecies, animal); }
+  return selectedSpecies.residents.length;
 }
 
 module.exports = countAnimals;
