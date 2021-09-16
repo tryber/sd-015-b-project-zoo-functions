1967ea0b9ec8e8a8755f30f5e07148f808ce8526
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2021-09-16 14:12:46 -0300

    Welckson Valentim - Finaliza requisito 5, apos aprovaçao local

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const allAnimals = species.reduce((acumulador, item) =>
+      ((acumulador[item.name] = item.residents.length), acumulador), {});
+    return allAnimals;
+  }
+  const findAnimals = species.find((animals) => (animals.name === animal.specie));
+  if (animal.gender === '' || animal.gender === undefined) {
+    const countAnimal = findAnimals.residents.length;
+    return countAnimal;
+  }
+  const countAnimalsByGender = findAnimals.residents.filter((genders) =>
+    (genders.sex === animal.gender)).length;
+  return countAnimalsByGender;
 }
-
 module.exports = countAnimals;
