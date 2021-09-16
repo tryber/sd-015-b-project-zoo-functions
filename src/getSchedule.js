a76e20ef8ac72debe934f5868d55f9335f2c364d
vinicius rampazzo <vinicius_rampazzo10@hotmail.com>
2021-09-16 19:19:36 -0300

    tentando fazer a 8...

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,16 @@
+const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const horario = hours;
+  const especies = species.map((elemento) => elemento.name);
+  const diasSemana = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  // .find((elemento, index) => elemento[index]);
+
+  if (!scheduleTarget) {
+    return diasSemana;
+  }
 }
+console.log(getSchedule());
 
 module.exports = getSchedule;
