3d7242d45694487db509799f3cdf5ef7a0ae6dce
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-16 20:33:42 -0300

    Finaliza funçao argDay

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -6,28 +6,51 @@ const closedMessage = `'CLOSED', 'exhibition': 'The zoo will be closed!'`;
 const days = Object.keys(hours);
 const allSpecies = species.map( (specie) => specie.name);
 
-function argNull() {
-  const dataObj = {};
+function argNull(messageOffice, exibicaoDoDia) {
+  const argNullObj = {};
   days.forEach((day) => {
     const messageOffice = `Open from ${hours[day].open}am until ${hours[day].close}pm`;
     const exibicaoDoDia = species
     .filter((animal) => animal.availability.includes(day)).map((mDay) => mDay.name);
-
     if (day === 'Monday') {
-      dataObj[day] = {
+      argNullObj[day] = {
         officeHour: 'CLOSED',
         exhibition: 'The zoo will be closed!',
       };
     } else {
-      dataObj[day] = {
+      argNullObj[day] = {
         officeHour: messageOffice,
         exhibition: exibicaoDoDia,
       };
     }
   });
-  return dataObj;
+  return argNullObj;
 }
-// console.log(argNull()); [DEBUG]
+// console.log(argNull()); // [DEBUG]
+
+function argDay(scheduleTarget) {
+  const argDayObj = {};
+  days.find( (day) =>{
+    if(day === scheduleTarget) {
+      if (day === 'Monday') {
+        argDayObj[day] = {
+          officeHour: 'CLOSED',
+          exhibition: 'The zoo will be closed!',
+        };
+      } else {
+        argDayObj[day] = {
+          officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+          exhibition: species
+          .filter((animal) => animal.availability.includes(day)).map((mDay) => mDay.name),
+        };
+      }
+    }
+  });
+  return argDayObj;
+}
+// console.log(argDay('Monday'));
+// console.log(argDay('Sunday'));
+// console.log(argDay('Saturday'));
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
@@ -36,17 +59,16 @@ function getSchedule(scheduleTarget) {
   const animalFound = allSpecies.includes(scheduleTarget); // Verifica se argumento passado é um Animal
 
   if (!dayFound && !animalFound) return argNull(); // FUNÇÃO OK
-
   if (dayFound || animalFound) { // Se é passado um animal ou dia
     if (dayFound) { // Se for um dia
+      argDay(scheduleTarget);
     }
-    if (animalFound) { // Se for um dia
-
+    if (animalFound) { // Se for um Animal
+      argAnimal(scheduleTarget);
     }
-
   }
   return objetoRetorno;
 }
 module.exports = getSchedule;
 
-// console.log(getSchedule());
+console.log(getSchedule());
