83d01f28116e88956689cba38a6cc7d6e9e9e379
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-16 11:34:45 -0300

    Implementa funçao que verifica a exibiçao do dia.

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -5,6 +5,18 @@ const {species, hours} = data;
 const closedMessade = `'CLOSED', 'exhibition': 'The zoo will be closed!'`;
 const days = Object.keys(hours);
 const allSpecies = species.map( (specie) => specie.name);
+const messageOffice = `Open from ${hours[day].open}am until ${hours[day].close}pm`;
+
+function argNull() {
+  const dataObj = {};
+  days.forEach( (day) => {
+    const exibicaoDoDia = species
+      .filter( (animal) => animal.availability)
+      .includes( (day))
+      .map( (mDay) => mDay.name);
+
+  });
+}
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
