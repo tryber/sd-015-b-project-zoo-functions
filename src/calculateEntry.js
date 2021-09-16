3d218d6f5c4d91b80f4ba14581928752965340ef
Rodolpho Coller <rodolpho.coller12@gmail.com>
2021-09-16 11:05:27 -0300

    implementa funçao calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,37 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
+function addValues(entrants) {
+  const obj = { adult: 0, child: 0, senior: 0 };
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      obj.child += 1;
+    } else if (entrant.age >= 18 && entrant.age < 50) {
+      obj.adult += 1;
+    } else {
+      obj.senior += 1;
+    }
+  });
+  return obj;
+}
+
+function countEntrants(entrants = {}) {
+  if (Object.values(entrants).length > 0) {
+    return addValues(entrants);
+  }
+  return 0;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  const count = countEntrants(entrants);
+  if (count === 0) {
+    return 0;
+  }
+  const adt = count.adult * data.prices.adult;
+  const chd = count.child * data.prices.child;
+  const snr = count.senior * data.prices.senior;
+
+  return adt + chd + snr;
 }
+console.log(calculateEntry());
 
 module.exports = { calculateEntry, countEntrants };
