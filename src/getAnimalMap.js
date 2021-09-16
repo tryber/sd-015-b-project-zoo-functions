60b5ec6f6046f17767e83bb92e4edcb9884ee5d0
Flavio Junior <flaviopds.jr@gmail.com>
2021-09-16 10:58:55 -0300

    req 8

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,38 +1,7 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function wOutParam() {
-  const obj = {
-    NE: [],
-    NW: [],
-    SE: [],
-    SW: [],
-  };
-  species.forEach((e) => {
-    if (e.location === 'NE') {
-      obj.NE.push(e.name);
-    } else if (e.location === 'NW') {
-      obj.NW.push(e.name);
-    } else if (e.location === 'SE') {
-      obj.SE.push(e.name);
-    } else {
-      obj.SW.push(e.name);
-    }
-  });
-  return obj;
-}
-
-function inNames() {
-  
-  const obj = wOutParam();
-  console.log(obj.NE);
-}
-
-inNames();
 function getAnimalMap(options) {
-  if (!options) {
-    return wOutParam();
-  }
+  // Escreva aqui;
 }
 
 module.exports = getAnimalMap;
