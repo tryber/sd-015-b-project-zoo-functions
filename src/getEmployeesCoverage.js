0bdd5767c8af5e860073a057dfbc548d9c2ad888
Arthur Teixeira <tuzinf5@exemplo.br>
2021-09-16 18:50:40 -0300

    Requisito 10 - concluido

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,43 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+const { employees, species } = data;
+
+const getEmployee = (people) => employees
+  .find(({ id, firstName, lastName }) =>
+    Object.values(people).includes(id) || Object.values(people).includes(firstName)
+    || Object.values(people).includes(lastName));
+
+const getAnimalName = (id) => id.map((element) => species
+  .find((specie) => specie.id === element).name);
+
+const getAnimalLocation = (id) => id.map((element) => species
+  .find((specie) => specie.id === element).location);
+
+const buildTheObject = (people) => {
+  const object = {};
+  const result = getEmployee(people);
+  if (result) {
+    const { id, firstName, lastName, responsibleFor } = result;
+    Object.assign(object, {
+      id,
+      fullName: `${firstName} ${lastName}`,
+      species: getAnimalName(responsibleFor),
+      locations: getAnimalLocation(responsibleFor),
+    });
+    return object;
+  }
+  throw new Error('Informações inválidas');
+};
+
+const withoutParameters = () => {
+  const array = [];
+  employees.forEach((employee) => array.push(buildTheObject({ id: employee.id })));
+  return array;
+};
+
+function getEmployeesCoverage(people) {
   // seu código aqui
+  return (!people) ? withoutParameters() : buildTheObject(people);
 }
 
 module.exports = getEmployeesCoverage;
