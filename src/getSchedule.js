906c873b05f79a4974aaf6d7abb55928a78a0919
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-16 11:29:40 -0300

    Desenvolve inicio do codigo.

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,5 +1,11 @@
 const data = require('../data/zoo_data');
 
+const {species, hours} = data;
+
+const closedMessade = `'CLOSED', 'exhibition': 'The zoo will be closed!'`;
+const days = Object.keys(hours);
+const allSpecies = species.map( (specie) => specie.name);
+
 function getSchedule(scheduleTarget) {
   // seu código aqui
 }
