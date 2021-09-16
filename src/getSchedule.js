bc8af5f52aa8d6ba8ba2b861d54e92fba58c34d7
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-16 01:31:00 -0300

    Modifica funçoes.

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -3,9 +3,16 @@ const data = require('../data/zoo_data');
 const { species, hours } = data;
 
 const days = Object.keys(hours);
+const allspecies = [];
+const checkSpecies = species.map( (specie) => {
+  allspecies.push(specie.name);
+})
+console.log(allspecies);
 
-function argNull() { // Retornado quando nenhum argumento é passado, ou argumento é inválido
-  const objetoComDias = {};
+function argNull() {
+
+  const dataObj = {};
+  
   days.forEach((day) => {
     const messageOffice = `Open from ${hours[day].open}am until ${hours[day].close}pm`;
     const animaisExibicao = species
@@ -13,36 +20,36 @@ function argNull() { // Retornado quando nenhum argumento é passado, ou argumen
     .includes(day))
     .map((day) => day.name);
     if (day === 'Monday') {
-      objetoComDias[day] = `{ 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' }`;
+      dataObj[day] = `{ 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' }`;
     } else {
-      objetoComDias[day] = `{ 'officeHour': '${messageOffice}','exhibition': '[ ${animaisExibicao.join(`, `)} ]'}`;
+      dataObj[day] = `{ 'officeHour': '${messageOffice}','exhibition': '[ ${animaisExibicao.join(`, `)} ]'}`;
     }
   });
-  return objetoComDias;
+  return dataObj;
 }
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
   const objetoRetorno = {};
-  const found = days.includes(scheduleTarget);
+  const dayFound = days.includes(scheduleTarget);
 
-  if(found) { // Se encontrar o nome do dia passado no: diasDaSemana
+  // if(found) { // Se encontrar o nome do dia passado no: diasDaSemana
     // console.log("ENCONTRADO!!!");
-    days.forEach((day) => {
-      const messageOffice = `Open from ${hours[day].open}am until ${hours[day].close}pm`;
-      const animaisExibicao = species //Verifica as espécies
-      .filter( (animal) => animal.availability // Verifica se o animal está disponível naquele dia
-      .includes(day)) // Para isso, verifica se o dia passado no scheduleTarget está localizado no specie.avaliability 
-      .map((day) => day.name);
-      // console.log(animaisExibicao);
-      if (day === 'Monday') {
-        objetoRetorno[day] = `{ 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' }`;
-      } else {
-        objetoRetorno[day] = `{ 'officeHour': '${messageOffice}','exhibition': '[ ${animaisExibicao.join(`, `)} ]'}`;
-      }
-    });
-  }
-  if (!scheduleTarget || !found) return argNull();
+  //   days.forEach((day) => {
+  //     const messageOffice = `Open from ${hours[day].open}am until ${hours[day].close}pm`;
+  //     const animaisExibicao = species //Verifica as espécies
+  //     .filter( (animal) => animal.availability // Verifica se o animal está disponível naquele dia
+  //     .includes(day)) // Para isso, verifica se o dia passado no scheduleTarget está localizado no specie.avaliability 
+  //     .map((day) => day.name);
+  //     // console.log(animaisExibicao);
+  //     if (day === 'Monday') {
+  //       objetoRetorno[day] = `{ 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' }`;
+  //     } else {
+  //       objetoRetorno[day] = `{ 'officeHour': '${messageOffice}','exhibition': '[ ${animaisExibicao.join(`, `)} ]'}`;
+  //     }
+  //   });
+  // }
+  if (!scheduleTarget /*|| !found*/) return argNull();
   
   return objetoRetorno;
 }
