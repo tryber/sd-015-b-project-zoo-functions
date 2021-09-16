24b838ef79937aded3776669948c58c5ddc2bbd7
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-16 17:24:07 -0300

    Finaliza funçao argNull.

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,6 +1,6 @@
 const data = require('../data/zoo_data');
 
-const {species, hours} = data;
+const { species, hours } = data;
 
 const closedMessage = `'CLOSED', 'exhibition': 'The zoo will be closed!'`;
 const days = Object.keys(hours);
@@ -8,20 +8,24 @@ const allSpecies = species.map( (specie) => specie.name);
 
 function argNull() {
   const dataObj = {};
-  days.forEach( (day) => {
+  days.forEach((day) => {
     const messageOffice = `Open from ${hours[day].open}am until ${hours[day].close}pm`;
-    const exibicaoDoDia = species
-      .filter((animal) => animal.availability
-      .includes(day))
-      .map((mDay) => mDay.name);
+    const exibicaoDoDia = species.filter((animal) => animal.availability.includes(day)).map((mDay) => mDay.name);
       if (day === 'Monday') {
-        dataObj[day] = `{ 'officeHour': ${closedMessage} }`;
+        dataObj[day] = {
+          officeHour: 'CLOSED',
+          exhibition: 'The zoo will be closed!',
+        };
+      } else {
+        dataObj[day] = {
+          officeHour: '',
+          exhibition: exibicaoDoDia,
+        };
       }
-      return dataObj;
   });
   return dataObj;
 }
-console.log( argNull() );
+console.log(argNull());
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
