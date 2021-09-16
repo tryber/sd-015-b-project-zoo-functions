508c8a5a1a87eb5df3719f66f4fd23a2191ee5d4
jpcp0614 <jpcp0614@gmail.com>
2021-09-16 10:29:38 -0300

    Requisito 7 OK

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,36 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+// * Localization
+const animalLocation = () => {
+  const obj = {};
+  species.forEach(({ location, name }) => {
+    if (!obj[location]) { obj[location] = []; }
+    obj[location].push(name);
+  });
+  return obj;
+};
+
+// * Animal sex
+const animalSex = (sorted, sex) => {
+  const obj = {};
+  species.forEach(({ location, name, residents }) => {
+    if (!obj[location]) { obj[location] = []; }
+
+    let residentS = [...residents];
+    if (sex) { residentS = residentS.filter((gender) => gender.sex === sex); }
+    residentS = residentS.map((resident) => resident.name);
+
+    if (sorted) residentS.sort();
+    obj[location].push({ [name]: residentS });
+  });
+  return obj;
+};
+
+function getAnimalMap(options = {}) {
+  const { includeNames, sorted, sex } = options;
+  if (includeNames) return animalSex(sorted, sex);
+  return animalLocation();
 }
 
 module.exports = getAnimalMap;
