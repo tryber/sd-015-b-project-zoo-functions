b9b399bb3c84768be529b8d8982b81bace413bed
Flávio Bianchetti <fjb.developer@gmail.com>
2021-09-16 08:57:15 -0300

    Atendido requisito 9 - validado via npm

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
+const getIdFirstSpecie = (id) =>
+  data.employees.filter((employee) =>
+    employee.id === id).map((employee) =>
+      employee.responsibleFor.find((value) => value))[0];
+
+const getListResidents = (idSpecie) =>
+  data.species.filter((specie) =>
+    specie.id === idSpecie).map((specie) => specie.residents)[0];
+
+const getMostOldFirstSpecie = (idSpecie) =>
+  Object.values(getListResidents(idSpecie).filter((res) =>
+    res).sort((a, b) => b.age - a.age)[0]);
+
+const findMostOldSpecieFromEmployee = (id) => {
+  let result = getIdFirstSpecie(id);
+  console.log(result);
+  result = getMostOldFirstSpecie(result);
+  return result;
+};
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  let result =[];
+  if (id) {
+    result = findMostOldSpecieFromEmployee(id);
+  }
+  return result;
 }
 
 module.exports = getOldestFromFirstSpecies;
