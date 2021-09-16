ad34ceb1b9992c067eff783130434770c2285681
Flávia Labanca <flavialabanca@gmail.com>
2021-09-16 17:17:47 -0300

    requisito 6 - primeira parte

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  // recebe: array de visitantes
+  // retorna: objeto com a contagem de cada tipo de pessoa - { child: 3, adult: 2, senior: 1 }.
+  const child = entrants.filter((entrant) => entrant.age < 18);
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50);
+  const senior = entrants.filter((entrant) => entrant.age < 18);
+
+  return {
+    child: child.length,
+    adult: adult.length,
+    senior: senior.length,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  // recebe objeto gerado na countEntrants e calcula valor a ser pago
+  // prices: {
+  // adult: 49.99,
+  // senior: 24.99,
+  // child: 20.99,
 }
 
 module.exports = { calculateEntry, countEntrants };
+
+// Pessoas com idade menor que 18 anos são classificadas como crianças (child);
+// Pessoas com idade maior ou igual a 18 anos e menor que 50 são classicadas como adultas (adult);
+// Pessoas com idade maior ou igual 50 anos são classificadas como pessoas com mais idade (senior).
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+const { hours } = require('../data/zoo_data');
+
+// A função é responsável por disponibilizar as informações de horário dos animais em uma consulta para o usuário, que pode querer ter acesso ao cronograma da semana, de um dia ou de um animal em específico.
+
+// - analise os testes unitários para entender os retornos que são esperados para esta função;
+// - quebre o problema em funções menores para que fique mais simples de administrar a responsabilidade de cada uma delas.
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  // chama função pra parâmetro vazio - retorna dia/hora de todos animais
+  // chama função pra parâmetro preenchido - retorna dia/hora
+
+
 }
 
 module.exports = getSchedule;
+
+// se não passar nenhuma animal, retorna todos animais e horários.
+
+// se passar animal, pega dia e hora que ele ta disponível
