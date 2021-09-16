64e8ad509862b90ea9bb8d847855582876d19811
douglas <douglasmonteiro.m@gmail.com>
2021-09-16 19:29:41 -0300

    Adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const specieList = {};
+  if (animal === undefined) {
+    data.species.forEach((specie) => { specieList[specie.name] = specie.residents.length; });
+    return specieList;
+  }
+  if (Object.keys(animal).length === 1) {
+    return data.species.find((especie) => especie.name === animal.specie).residents.length;
+  }
+  if (Object.keys(animal).length > 1) {
+    const specificSpecie = data.species.find((specie) => specie.name === animal.specie);
+    const residents = specificSpecie.residents.filter((resident) => resident.sex === animal.sex);
+    return residents.length;
+  }
 }
 
 module.exports = countAnimals;
