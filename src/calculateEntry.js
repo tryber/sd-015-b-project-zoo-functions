ee12794dca39e2f51b0f78d3564ac9d4b26ab1cb
Lucas Maieski Marques <lucmaieski@gmail.com>
2021-09-15 21:08:08 -0300

    Feito o sexto requisito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,31 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  if (entrants === undefined) {
+    return 0;
+  }
+  const totaleEntrants = {
+    child: entrants.filter((child) => child.age < 18).length,
+    adult: entrants.filter((adult) => adult.age >= 18 && adult.age < 50).length,
+    senior: entrants.filter((senior) => senior.age >= 50).length,
+  };
+  return totaleEntrants;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const getPrices = data.prices;
+  const entradas = countEntrants(entrants);
+  const valoresTotais = {
+    adult: entradas.adult * getPrices.adult,
+    child: entradas.child * getPrices.child,
+    senior: entradas.senior * getPrices.senior,
+  };
+  const valoresParaSoma = Object.values(valoresTotais);
+  return valoresParaSoma.reduce((acc, atual) => acc + atual);
 }
 
+console.log(calculateEntry({}));
 module.exports = { calculateEntry, countEntrants };
