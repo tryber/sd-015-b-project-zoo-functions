0bd780a862a8621f2e1e03e8d72cacf0d72d6033
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-09-15 21:42:20 -0300

    2 tasks do requisito 8 - Incompleto

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const workDays = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+
+function paramIsAnAnimal(scheduleTarget) {
+  const parameterIsAnAnimal = data.species.some((specie) => specie.name === scheduleTarget);
+  const animalAvalable = data.species.find((specie) => specie.name === scheduleTarget).availability;
+  if (parameterIsAnAnimal) return animalAvalable;
 }
+function getSchedule(scheduleTarget) {
+
+  if (scheduleTarget === 'Monday') {
+    return { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
+  }
+  if (paramIsAnAnimal(scheduleTarget)) return paramIsAnAnimal(scheduleTarget);
+}
+getSchedule('lions');
 
 module.exports = getSchedule;
