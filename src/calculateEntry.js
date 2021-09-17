f2b38b3de039e9a81dad08dc3c3624df03c7eb96
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2021-09-17 13:04:44 -0300

    Welckson Valentim - Finaliza requisito 6, apos aprovaçao local

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,16 +2,23 @@ const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  return classifyByAge = {
+  const classifyByAge = {
     child: entrants.filter((child) => child.age < 18).length,
     adult: entrants.filter((adult) => adult.age >= 18 && adult.age < 50).length,
     senior: entrants.filter((senior) => senior.age >= 50).length,
-  }
+  };
+  return classifyByAge;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const returnCountEntrants = countEntrants(entrants);
+  const priceChild = returnCountEntrants.child * prices.child;
+  const priceAdult = returnCountEntrants.adult * prices.adult;
+  const priceCSenior = returnCountEntrants.senior * prices.senior;
+  return priceChild + priceAdult + priceCSenior;
 }
 
 module.exports = { calculateEntry, countEntrants };
-
