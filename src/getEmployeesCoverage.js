a6f772011705d93dc39fba1a16f5c4f3d022a057
fernandamichetti <f.michetti@hotmail.com>
2021-09-17 11:48:09 -0300

    requisito 10 e 80% do total

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
+const { employees, species} = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const people = () => employees.map(({ id, firstName, lastName, responsibleFor }) => {
+  const infos = {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: [],
+    locations: [],
+  };
+  for (let i = 0; i < responsibleFor.length; i += 1) {
+    infos.species.push(species.find((kind) => kind.id === responsibleFor[i]).name);
+    infos.locations.push(species.find((kind) => kind.id === responsibleFor[i]).location);
+  }
+  return infos;
+});
+
+function getEmployeesCoverage(person) {
+  if (!person) return people();
+
+  if (!people().some((individual) => (individual.id === person.id || individual.fullName.includes(person.name)))) { throw new Error('Informações inválidas');}
+
+  return people().find((individual) => (individual.id === person.id || individual.fullName.includes(person.name)))
 }
 
+console.log(getEmployeesCoverage({name: 'Nigel' }));
 module.exports = getEmployeesCoverage;
