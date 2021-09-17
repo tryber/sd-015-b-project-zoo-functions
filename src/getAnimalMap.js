2c20f0e261a472f80d3ba2831db2ac90f678bce1
Rodrigo Lima <rodrigootavio19@gmail.com>
2021-09-17 00:16:28 -0400

    finaliza getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,29 +1,85 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-const animalsByRegion = () => {
+const generateRegions = () => {
   const regions = {
     NE: [],
     NW: [],
     SE: [],
     SW: [],
   };
-  data.species.forEach(({ name, location }) => {
-    regions[location].push(name);
-  });
+
   return regions;
 };
 
-const verifyAnimalsByRegion = (options) => {
-  let verify = false;
-  if (!options) verify = true;
+const getAnimalsByRegion = () => {
+  const regions = generateRegions();
+  species.forEach(({ name, location }) => regions[location].push(name));
 
-  return verify;
+  return regions;
 };
+
+const handleSex = (sex, name, onlyNames) => {
+  if (!sex) {
+    return onlyNames;
+  }
+  const getAnimal = species.find((animal) => animal.name === name);
+  const filterBySex = getAnimal.residents.filter((resident) => resident.sex === sex);
+  const filterByName = filterBySex.map((animal) => animal.name);
+  const newOnlyNames = onlyNames.filter((validName) => filterByName.includes(validName));
+
+  return newOnlyNames;
+};
+
+const handleSorted = (sorted, onlyNames) => {
+  if (sorted) {
+    return onlyNames.sort();
+  }
+  return onlyNames;
+};
+
+const filterResidents = (names, sorted, sex) => {
+  const animalGroup = [];
+  names.forEach((name) => {
+    const findResidents = species.find((animal) => animal.name === name).residents;
+    const onlyNames = findResidents.map((resident) => resident.name);
+    const group = {};
+    const hasSex = handleSex(sex, name, onlyNames);
+    const isSorted = handleSorted(sorted, hasSex);
+
+    group[name] = isSorted;
+    animalGroup.push(group);
+  });
+
+  return animalGroup;
+};
+
+const handleTypeMapReturn = (callBack, sorted, sex) => {
+  const allRegions = generateRegions();
+  const animals = getAnimalsByRegion();
+  const entries = Object.entries(animals);
+
+  const namesAndResidentes = entries.reduce((acc, [region, names]) => {
+    const residents = callBack(names, sorted, sex);
+    acc[region] = residents;
+
+    return acc;
+  }, allRegions);
+
+  return namesAndResidentes;
+};
+
 function getAnimalMap(options) {
   // seu código aqui
-  const teste = verifyAnimalsByRegion(options);
+  if (!options) {
+    return getAnimalsByRegion();
+  }
 
-  if (teste) return animalsByRegion();
+  const { includeNames, sorted, sex } = options;
+  if (includeNames) {
+    return handleTypeMapReturn(filterResidents, sorted, sex);
+  }
+
+  return getAnimalsByRegion();
 }
 
 module.exports = getAnimalMap;
