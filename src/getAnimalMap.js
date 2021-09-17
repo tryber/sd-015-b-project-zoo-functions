fa9ce4ff40fcc235370b5b2e40bb655527daeff6
Marina Marques <marina.marqueso@outlook.com>
2021-09-17 15:57:15 -0300

    Adiciona parte 1 do requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,32 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function mapingAll() {
+  const obj = {
+    NE: [],
+    NW: [],
+    SE: [],
+    SW: []
+  };
+  const animals = species.map((elemento) => {
+    const animal = elemento.name;
+    const locAnimal = elemento.location;
+    const keyObj = Object.keys(obj);
+    for (let i = 0; i < keyObj.length; i++) {
+      if (locAnimal === keyObj[i]) {
+        obj[keyObj[i]].push(animal);
+      }
+    }
+  });
+  return obj;
+}
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (options === undefined) {
+    return mapingAll();
+  }
 }
 
 module.exports = getAnimalMap;
+
+console.log(getAnimalMap());
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,17 @@
+const { hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+function officeSchedule() {
+  const day = Object.keys(hours);
+  const sched = Object.values(hours);
+  const valueSched = sched.map((elemento) => `Open from ${sched.open}am until ${sched.close}pm`)
+  return valueSched
+}
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
 }
 
 module.exports = getSchedule;
+
+console.log(officeSchedule())
