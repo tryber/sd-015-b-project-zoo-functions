b527080ca1cda3fb64e49c3a6e689df9b507207a
allpha23 <allpha23@gmail.com>
2021-09-17 15:21:09 -0300

    correção de erros

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,7 +2,7 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const getSpeciesName = data.species.find((species) => species.name === animal);
+  const getSpeciesName = species.find((species) => species.name === animal);
   const getAnimalsByAge = getSpeciesName.residents.every((species) => species.age > age);
   return getAnimalsByAge;
 }
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,7 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return ids.map((id) => data.species.find((animalId) => animalId.id === id));
+  return ids.map((id) => species.find((animalId) => animalId.id === id));
 }
 
-module.exports = getSpeciesByIds; 
+module.exports = getSpeciesByIds;
