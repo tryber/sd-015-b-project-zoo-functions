9df104ffd953223b77e3ba3e230dd8fadea7686b
assisMeneghetti <assismfabri@hotmail.com>
2021-09-16 22:45:22 -0300

    requisito 10 corrigindo lint

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,9 +1,8 @@
-const { employees, species } = require('../data/zoo_data');
+// const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
   // sem parametro
-  
 }
 
 module.exports = getSchedule;
