f9137eaf7d60d318d197322a4c6bbd29added49a
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-09-17 16:44:33 -0300

    Cumprido o requisito 5 - countAnimals()

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,32 @@
 const data = require('../data/zoo_data');
 
+const findIndividualsOnTheSpecie = ({ specie: receivedSpecie, sex: receivedSex }) => {
+  const foundedSpecie = data.species.find((specie) => specie.name === receivedSpecie);
+  if (!receivedSex) {
+    const newObjectValue = foundedSpecie.residents.length;
+    return newObjectValue;
+  }
+  const thisResidents = foundedSpecie.residents;
+  const newObjectValue = thisResidents.filter((resident) => resident.sex === receivedSex).length;
+  return newObjectValue;
+};
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const numbersPerSpecie = {};
+    data.species.forEach((specie) => {
+      const newObjectKey = specie.name;
+      const newObjectValue = specie.residents.length;
+      const newObject = { [newObjectKey]: newObjectValue };
+      Object.assign(numbersPerSpecie, newObject);
+    });
+    console.log(numbersPerSpecie);
+    return numbersPerSpecie;
+  }
+  return findIndividualsOnTheSpecie(animal);
 }
+// countAnimals();
+console.log(countAnimals({ specie: 'penguins' }));
+console.log(countAnimals({ specie: 'penguins', sex: 'male' }));
+console.log(countAnimals({ specie: 'penguins', sex: 'female' }));
 
 module.exports = countAnimals;
