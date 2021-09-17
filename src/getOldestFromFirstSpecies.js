3c4860bc98205db6c4f676a7b21ecd0220ef2028
Vinicius Fernandes <viniciusfernandes126@gmail.com>
2021-09-17 13:57:29 -0300

    [Requisito 9] Implementa função getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
+function whichAnimalId(caregiver) {
+  const animalsId = data.employees.find((id) => (id.id === caregiver));
+  const id = animalsId.responsibleFor[0];
+  return id;
+}
+
+function residents(animalId) {
+  const dwellers = data.species.find((animals) => (animals.id === animalId));
+  const animalsInfo = dwellers.residents;
+  return animalsInfo;
+}
+
+function oldestAnimal(animalInfo) {
+  const age = animalInfo.reduce((oldest, infos) => {
+    const ageInfo = infos.age;
+    if (oldest > ageInfo) return oldest;
+    return ageInfo;
+  }, 0);
+  const animalData = animalInfo.find((specie) => (specie.age === age));
+  return animalData;
+}
+
+function formatData(animalData) {
+  const turnIntoArray = Object.values(animalData);
+  return turnIntoArray;
+}
+
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  return formatData(oldestAnimal(residents(whichAnimalId(id))));
 }
 
 module.exports = getOldestFromFirstSpecies;
