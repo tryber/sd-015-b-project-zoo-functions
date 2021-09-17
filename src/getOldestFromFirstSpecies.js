5b686ece486cbc0ee88dfd3c86995c6632493759
samuel <samuelsoutosilva14@gmail.com>
2021-09-16 23:11:16 -0300

    requisito 8

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = require('../data/zoo_data');
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employeeArr = employees.find((employee) => employee.id === id);
+  const specieId = employeeArr.responsibleFor[0];
+  const specieArr = species.find((specie) => specie.id === specieId);
+  const oldestResident = specieArr.residents.reduce((oldest, resident) => {
+    if (resident.age > oldest.age) return resident;
+    return oldest;
+  });
+  return [oldestResident.name, oldestResident.sex, oldestResident.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
