265ca91c697f95ec10856b2b5d91c2e9050d7136
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-17 01:47:08 -0300

    Reseta getEmployeesCoverage.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,24 +1,7 @@
 const data = require('../data/zoo_data');
 
-const { employees } = data;
-
-const noData = () => {
-  const funcMap = employees.map((employee) => {
-    // const idsAnimais = employee.responsibleFor;
-    return {
-      id: employee.id,
-      fullName: `${employee.firstName} ${employee.lastName}`,
-      species: 'getAnimal(idsAnimais)',
-      locations: '0',
-    };
-  });
-  const allData = (funcMap);
-  return allData;
-};
-
 function getEmployeesCoverage(peopleId) {
   // seu código aqui
-  if (!peopleId) return noData();
 }
 
 module.exports = getEmployeesCoverage;
