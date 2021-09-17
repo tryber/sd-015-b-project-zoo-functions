d9687dbcccdcb58de2df75be9c1000e2b332ea3e
samuel <samuelsoutosilva14@gmail.com>
2021-09-16 23:08:21 -0300

    Revert "refatora getOldestFromFirstSpecies"
    This reverts commit 9d8b9185672d25aa81fc14e0c91a35f5c777e4a8.

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,15 +1,7 @@
-const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  const employeeArr = employees.find((employee) => employee.id === id);
-  const specieId = employeeArr.responsibleFor[0];
-  const specieArr = species.find((specie) => specie.id === specieId);
-  const oldestResident = specieArr.residents.reduce((oldest, resident) => {
-    if (resident.age > oldest.age) return resident;
-    return oldest;
-  });
-  return [oldestResident.name, oldestResident.sex, oldestResident.age];
+  // seu código aqui
 }
 
 module.exports = getOldestFromFirstSpecies;
