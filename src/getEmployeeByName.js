75a61cd37f69e09ffdc840f7e37724064119c0a9
allpha23 <allpha23@gmail.com>
2021-09-17 15:33:28 -0300

    correção erros requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,11 +2,10 @@ const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  if(employeeName === undefined) return {};
+  if (employeeName === undefined) return {};
 
-  return data.employees.find((employees) => employees.firstName === employeeName
-  || employees.lastName === employeeName);
-  
+  return employees.find((employee) => employee.firstName === employeeName
+  || employee.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
