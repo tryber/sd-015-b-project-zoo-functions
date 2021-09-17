c3b407e03395d5e473b1424c82f761fee9390f44
vinicius rampazzo <vinicius_rampazzo10@hotmail.com>
2021-09-17 13:26:13 -0300

    corrigindo lint para passar o requisito 5

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,9 +1,9 @@
-const { hours, species } = require('../data/zoo_data');
+// const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  const horario = hours;
-  const especies = species.map((elemento) => elemento.name);
+  // const horario = hours;
+  // const especies = species.map((elemento) => elemento.name);
   const diasSemana = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
   // .find((elemento, index) => elemento[index]);
 
