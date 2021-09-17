3c6e95eb2312dedcb2d29d12248b0eb8ce0ee5b1
allpha23 <allpha23@gmail.com>
2021-09-17 18:01:16 -0300

    finalizado requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const resolve = isManager(managerId);
+  
+  if (resolve === true){
+    const getEmployees = data.employees.filter((employee) => employee.managers.includes(managerId));
+    return getEmployees.map((employeeName) => `${employeeName.firstName} ${employeeName.lastName}`);
+  }
+  else throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
