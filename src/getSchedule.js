ec6fbac35ce80b2de511b170359cbea233d9f1d7
Hugoacr <hugoacr@yahoo.com.br>
2021-09-17 02:03:54 -0300

    responde requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,45 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const daysWeek = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+
+function getOfficeHourByDay(dayWeek) {
+  if (dayWeek === 'Monday') {
+    return { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
+  }
+  return { [dayWeek]: {
+    officeHour: `Open from ${data.hours[dayWeek].open}am until ${data.hours[dayWeek].close}pm`,
+    exhibition: data.species.filter((specie) => specie.availability.includes(dayWeek))
+      .map((element) => element.name) },
+  };
 }
+
+function agroupOfficeHourByDay(dayWeek) {
+  return daysWeek.reduce((acc, obj) => {
+    if (obj === 'Monday') {
+      acc[obj] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+      return acc;
+    }
+    acc[obj] = {
+      officeHour: `Open from ${data.hours[obj].open}am until ${data.hours[obj].close}pm`,
+      exhibition: data.species.filter((specie) => specie.availability.includes(obj))
+        .map((specie) => specie.name) };
+    return acc;
+  }, {});
+}
+
+function getSchedule(scheduleTarget) {
+  if (daysWeek.includes(scheduleTarget)) {
+    return getOfficeHourByDay(scheduleTarget);
+  }
+
+  if (scheduleTarget === undefined || !data.species.map((specie) => specie.name)
+    .includes(scheduleTarget)) {
+    return agroupOfficeHourByDay();
+  }
+
+  return data.species.find((specie) => specie.name === scheduleTarget).availability;
+}
+
+console.log(getSchedule());
 
 module.exports = getSchedule;
