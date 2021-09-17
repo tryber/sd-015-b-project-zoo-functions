9ca5277b3177e92fc88bc7f37cfe3df120b9c798
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2021-09-17 11:14:37 -0300

    Testa commit apos atualizaçao de arquivo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,7 +1,12 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return classifyByAge = {
+    child: entrants.filter((child) => child.age < 18).length,
+    adult: entrants.filter((adult) => adult.age >= 18 && adult.age < 50).length,
+    senior: entrants.filter((senior) => senior.age >= 50).length,
+  }
 }
 
 function calculateEntry(entrants) {
@@ -9,3 +14,4 @@ function calculateEntry(entrants) {
 }
 
 module.exports = { calculateEntry, countEntrants };
+
