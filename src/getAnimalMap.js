fdce4a67997ff3a4a7ec09904346f23f64ded9fd
Robert Martins <robert.rm131@gmail.com>
2021-09-16 21:14:14 -0300

    requisito 7 teste avaliação

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -7,48 +7,95 @@ const getAnimalsFromNW = () => species.filter((elemento) => elemento.location ==
 const getAnimalsFromSE = () => species.filter((elemento) => elemento.location === 'SE');
 const getAnimalsFromSW = () => species.filter((elemento) => elemento.location === 'SW');
 
-const reduceToName = (arrayOfAnimalFrom) => arrayOfAnimalFrom.reduce((acc, elemento) => 
- acc = acc.concat(elemento.name), []);
+const reduceToName = (arrayOfAnimalFrom) => arrayOfAnimalFrom.reduce((acc, elemento) =>
+  acc.concat(elemento.name), []);
 
-const createObj = (obj) => obj.map((elemento) => {
-  const { residents } = elemento;
-  const key = elemento['name'];
+const defaultOrder = (residents, _) => residents.map((resident) => resident.name);
 
-  const residentNames = residents.map((elemento) => elemento.name);
+const sortOrder = (residents, _) => residents.sort((a, b) => {
+  const value = (a.name > b.name) ? 1 : -1;
+  return value;
+}).map((resident) => resident.name);
 
-  return {[key]: residentNames}
+const sexOrder = (residents, sex) => residents.filter((resident) => resident.sex === sex)
+  .map((elemento) => elemento.name);
+
+const sortAndSexOrder = (obj) => obj.map((elemento) => {
+  const key = Object.keys(elemento);
+  elemento[key].sort();
+  return elemento;
 });
 
-const verifyOptions = (sorted, sex) => {
+const createObj = (obj, ordem, sex = undefined) => obj.map((elemento) => {
+  const { residents } = elemento;
+  const key = elemento.name;
+
+  const residentNames = ordem(residents, sex);
+
+  return { [key]: residentNames };
+});
+
+const getOne = (options) => {
+  if (options.sorted) {
+    return {
+      NE: createObj(getAnimalsFromNE(), sortOrder),
+      NW: createObj(getAnimalsFromNW(), sortOrder),
+      SE: createObj(getAnimalsFromSE(), sortOrder),
+      SW: createObj(getAnimalsFromSW(), sortOrder),
+    };
+  }
+  return {
+    NE: createObj(getAnimalsFromNE(), sexOrder, options.sex),
+    NW: createObj(getAnimalsFromNW(), sexOrder, options.sex),
+    SE: createObj(getAnimalsFromSE(), sexOrder, options.sex),
+    SW: createObj(getAnimalsFromSW(), sexOrder, options.sex),
+  };
+};
+
+const getSomething = (options) => {
+  if (options.sorted && options.sex) {
+    return {
+      NE: sortAndSexOrder(createObj(getAnimalsFromNE(), sexOrder, options.sex)),
+      NW: sortAndSexOrder(createObj(getAnimalsFromNW(), sexOrder, options.sex)),
+      SE: sortAndSexOrder(createObj(getAnimalsFromSE(), sexOrder, options.sex)),
+      SW: sortAndSexOrder(createObj(getAnimalsFromSW(), sexOrder, options.sex)),
+    };
+  }
+  return getOne(options);
+};
+
+const verifyOptions = (options) => {
   let result;
 
-  const params = (sorted || sex);
-  if (params) {
-    result = 10
+  if (options.sex || options.sorted) {
+    result = getSomething(options);
   } else {
-    result =  { NE: createObj(getAnimalsFromNE()), NW: createObj(getAnimalsFromNW()),
-    SE: createObj(getAnimalsFromSE()), SW: createObj(getAnimalsFromSW())};
+    result = {
+      NE: createObj(getAnimalsFromNE(), defaultOrder),
+      NW: createObj(getAnimalsFromNW(), defaultOrder),
+      SE: createObj(getAnimalsFromSE(), defaultOrder),
+      SW: createObj(getAnimalsFromSW(), defaultOrder),
+    };
   }
+
   return result;
-}
+};
 
 function getAnimalMap(options) {
-
   let result;
 
-  result = { NE: reduceToName(getAnimalsFromNE()), NW: reduceToName(getAnimalsFromNW()),
-    SE: reduceToName(getAnimalsFromSE()), SW: reduceToName(getAnimalsFromSW()) };
+  result = {
+    NE: reduceToName(getAnimalsFromNE()),
+    NW: reduceToName(getAnimalsFromNW()),
+    SE: reduceToName(getAnimalsFromSE()),
+    SW: reduceToName(getAnimalsFromSW()),
+  };
 
   if (options && options.includeNames) {
-    const { sorted, sex } = options;
-    result = verifyOptions(sorted, sex);
+    result = verifyOptions(options);
   }
 
-
   return result;
-  //(a, b) => (a.name > b.name) ? 1 : -1); ordenar
 }
-
-console.log(getAnimalMap({includeNames: true}))
 
 module.exports = getAnimalMap;
