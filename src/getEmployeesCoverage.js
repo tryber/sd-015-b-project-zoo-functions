b0c3259469d4714de8cf04c2eabb9852c5881fae
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-09-16 21:39:06 -0300

    requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,40 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+// fonte: usei como base o exercício de https://github.com/tryber/sd-015-b-project-zoo-functions/blob/annie-haurani-zoo-functions/src/getEmployeesCoverage.js
+function getEmployee(person) {
+  return data.employees.find((employee) => (person.name === employee.firstName
+    || person.name === employee.lastName || person.id === employee.id));
+}
+
+function getSpecie(employee) {
+  return employee.responsibleFor.map((id) =>
+    data.species.find((specie) =>
+      (specie.id === id)));
+}
+
+function getAllEmployees() {
+  return data.employees.map((employee) => {
+    const species = getSpecie(employee);
+    return {
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: species.map((specie) => specie.name),
+      locations: species.map((specie) => specie.location),
+    };
+  });
+}
+
+function getEmployeesCoverage(input) {
+  if (!input) return getAllEmployees();
+  const employee = getEmployee(input);
+  if (employee === undefined) throw new Error('Informações inválidas');
+  const species = getSpecie(employee);
+  return {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: species.map((specie) => specie.name),
+    locations: species.map((specie) => specie.location),
+  };
 }
 
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,18 +1,39 @@
 const data = require('../data/zoo_data');
 
-// const workDays = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+// function paramIsAnAnimal(scheduleTarget) {
+//   const parameterIsAnAnimal = data.species.some((specie) => specie.name === scheduleTarget);
+//   const animalAvalable = data.species.find((specie) => specie.name === scheduleTarget).availability;
+//   if (parameterIsAnAnimal) return animalAvalable;
+// }
 
-function paramIsAnAnimal(scheduleTarget) {
-  const parameterIsAnAnimal = data.species.some((specie) => specie.name === scheduleTarget);
-  const animalAvalable = data.species.find((specie) => specie.name === scheduleTarget).availability;
-  if (parameterIsAnAnimal) return animalAvalable;
-}
+// const zooSchedule = () => {
+//   const daysHours = {};
+//   data.hours.forEach((day) => {
+//     Object.assign(daysHours, { [day[0]]: {
+//       officeHour: `Open from ${day[1].open} until ${day[2].open}`,
+//       exhibition: data.species.filter((specie) => specie.availability.includes(day[1]))
+//         .map(({ name }) => name),
+//     },
+//     });
+//     return daysHours;
+//   });
+
+//   daysHours.Monday = {
+//     officeHour: 'CLOSED',
+//     exhibition: 'The zoo will be closed!',
+//   };
+//   return daysHours;
+// };
+
+// const returnDays = (scheduleTarget) => ({ [scheduleTarget]: zooSchedule()[scheduleTarget] });
+
 function getSchedule(scheduleTarget) {
-  if (scheduleTarget === 'Monday') {
-    return { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
-  }
-  if (paramIsAnAnimal(scheduleTarget)) return paramIsAnAnimal(scheduleTarget);
+  // if (Object.keys(data.hours).includes(scheduleTarget)) return returnDays(scheduleTarget);
+
+  // if (paramIsAnAnimal(scheduleTarget)) return paramIsAnAnimal(scheduleTarget);
+
+  // return zooSchedule();
 }
-getSchedule('lions');
+getSchedule('Tuesday');
 
 module.exports = getSchedule;
