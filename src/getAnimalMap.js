59b2a27c1e3eab4d2f32af9b0ba740811ad6c5e4
DanielHott <daniel_hott@outlook.com>
2021-09-16 22:35:57 -0300

    Adicionando o requisito 8

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,5 +1,20 @@
 const data = require('../data/zoo_data');
 
+const especies = data.species;
+
+function pegaArea(area) {
+  const animais = [];
+  especies.forEach((element) => {
+    if (element.location === area) {
+      animais.push(element.name);
+    }
+  });
+  return {
+    [area]: animais,
+  };
+}
+console.log(pegaArea('NW'));
+
 function getAnimalMap(options) {
   // seu código aqui
 }
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,60 @@
 const data = require('../data/zoo_data');
+// const getSpeciesByIds = require('./getSpeciesByIds');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const days = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+
+const result = {};
+
+function getHours() {
+  days.forEach((day) => {
+    const { open } = data.hours[day];
+    const { close } = data.hours[day];
+    result[day] = {};
+    if (open === 0 || close === 0) {
+      result[day].officeHour = 'CLOSED';
+    } else {
+      result[day].officeHour = `Open from ${open}am until ${close}pm`;
+    }
+  });
+  return result;
 }
 
+function getExhibition() {
+  days.forEach((day) => {
+    let currentAnimals = data.species.filter((animal) => animal.availability.includes(day));
+    currentAnimals = currentAnimals.map((animal) => animal.name);
+    if (currentAnimals.length === 0) {
+      result[day].exhibition = 'The zoo will be closed!';
+    } else {
+      result[day].exhibition = currentAnimals;
+    }
+  });
+}
+
+function getScheduleFromAnimal(scheduleTarget) {
+  if (!scheduleTarget) {
+    return false;
+  }
+  const animals = data.species.map((animal) => animal.name);
+  const animalExhibition = data.species.find((animal) => animal.name === scheduleTarget);
+  if (animals.includes(scheduleTarget)) {
+    return animalExhibition.availability;
+  }
+}
+
+function getSchedule(scheduleTarget) {
+  getHours();
+  getExhibition();
+  if (scheduleTarget === 'Monday') {
+    return { Monday: result[scheduleTarget] };
+  }
+  if (days.includes(scheduleTarget)) {
+    return { [scheduleTarget]: result[scheduleTarget] };
+  }
+  if (getScheduleFromAnimal(scheduleTarget)) {
+    return getScheduleFromAnimal(scheduleTarget);
+  }
+  return result;
+}
+// Paulo Flóra me ajudou na compreensão
 module.exports = getSchedule;
