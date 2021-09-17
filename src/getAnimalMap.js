7488862363a679514e88652292f686c24387fd84
Marina Marques <marina.marqueso@outlook.com>
2021-09-17 16:16:09 -0300

    Adiciona lint

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -6,17 +6,18 @@ function mapingAll() {
     NE: [],
     NW: [],
     SE: [],
-    SW: []
+    SW: [],
   };
-  const animals = species.map((elemento) => {
+  species.map((elemento) => {
     const animal = elemento.name;
     const locAnimal = elemento.location;
     const keyObj = Object.keys(obj);
-    for (let i = 0; i < keyObj.length; i++) {
+    for (let i = 0; i < keyObj.length; i += 1) {
       if (locAnimal === keyObj[i]) {
         obj[keyObj[i]].push(animal);
       }
     }
+    return obj;
   });
   return obj;
 }
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,17 +1,14 @@
-const { hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function officeSchedule() {
-  const day = Object.keys(hours);
-  const sched = Object.values(hours);
-  const valueSched = sched.map((elemento) => `Open from ${sched.open}am until ${sched.close}pm`)
-  return valueSched
-}
+// function officeSchedule() {
+//   const day = Object.keys(hours);
+//   const sched = Object.values(hours);
+//   const valueSched = sched.map((elemento) => `Open from ${sched.open}am until ${sched.close}pm`);
+//   return valueSched;
+// }
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
 }
 
 module.exports = getSchedule;
-
-console.log(officeSchedule())
