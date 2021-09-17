a6fa0e636757fa6376190c25baf85a8206229a16
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2021-09-17 10:57:57 -0300

    funçao getEmployeeByName.js

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return employees.find(
+    (pessoa) => pessoa.firstName === employeeName || pessoa.lastName === employeeName
+  );
 }
 
 module.exports = getEmployeeByName;
