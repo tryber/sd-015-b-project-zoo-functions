352bc302988e549a831344b744481a3568c892ba
Marcelo dos Santos Costa <marcelodscosta@yahoo.com.br>
2021-09-17 19:30:54 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const actual = data.species.filter((item) =>
+    item.name === animal)[0].residents.map((item) => item).length;
+  const filtered = data.species.filter((item) =>
+    item.name === animal)[0].residents.filter((item) => item.age > age).length;
+  return (actual === filtered);
 }
 
 module.exports = getAnimalsOlderThan;
