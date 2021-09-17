361ef2f3c797f4f40c4f9d571e62b56f78bc48f7
Gabriel Borges <igblborges@gmail.com>
2021-09-17 18:53:57 -0300

    adiciono requisito-4

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,8 +1,7 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-//
 function countAnimals(animal) {
-  return species;
+  return species.find();
 }
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,8 +2,13 @@ const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  return employees.some((element) => element.managers.includes(id));
+  return employees.some((element) => element.id === id);
 }
+
 function getRelatedEmployees(managerId) {
+  if (isManager(managerId)) {
+    const managerObjects = employees.filter((element) => element.managers.includes(managerId));
+    return managerObjects.map((element) => `${element.firstName} ${element.lastName}`);
+  }
 }
 module.exports = { isManager, getRelatedEmployees };
