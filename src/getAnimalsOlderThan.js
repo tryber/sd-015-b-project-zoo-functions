155f1f3b68fbe098dce137dfc65e2f4d4f7078ea
DanMondaini <danielmondaini@outlook.com>
2021-09-17 15:41:04 -0300

    req 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const specieObj = data.species.find((specie) => specie.name === animal);
+  const isOlderThan = specieObj.residents.every((resident) => resident.age >= age);
+  return isOlderThan;
 }
 
 module.exports = getAnimalsOlderThan;
