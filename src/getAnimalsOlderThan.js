e275febdaa60f958b0560066a2fe1e54f0c436a0
raphael <raphabarzotto@gmail.com>
2021-09-18 19:29:18 -0300

    entrega requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const getSpecieByName = (nameToFind) => data.species.find((name) => name.name === nameToFind);
+
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const specie = getSpecieByName(animal);
+  console.log(specie);
+  return specie.residents.every((specieToCompareAge) => specieToCompareAge.age >= age);
 }
+getAnimalsOlderThan('lions', 13);
 
 module.exports = getAnimalsOlderThan;
