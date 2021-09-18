a879928867551a58cea957248aaba7c64c37ff5d
Valdênio <valdeniomartinho@gmail.com>
2021-09-18 14:05:03 -0300

    adiciona requisito 8

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,5 +1,5 @@
 // const { employees } = require('../data/zoo_data');
-// const { employees } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeNa) {
@@ -7,7 +7,7 @@ function getEmployeeByName(employeeNa) {
   if (employeeNa === undefined) {
     return {};
   }
-  return data.employees
+  return employees
     .find((employees) => employees.firstName === employeeNa || employees.lastName === employeeNa);
 }
 
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,47 @@
+/* eslint-disable keyword-spacing */
 const data = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+function allAnimals() {
+  const Obj = {};
+  const valuesObj = Object.values(hours); // array de objetos com horas
+  Object.keys(hours)
+    .forEach((element, index) => {
+      Obj[element] = {
+        officeHour: ((valuesObj[index].open !== 0)
+          ? `Open from ${valuesObj[index].open}am until ${valuesObj[index].close}pm` : 'CLOSED'),
+        exhibition: ((valuesObj[index].open !== 0) ? (species
+          .filter((specie) => specie.availability
+            .includes(element))
+          .map((animal) => animal.name)) : 'The zoo will be closed!'),
+      };
+    });
+
+  return Obj;
 }
 
+function filterDays(filtro) {
+  const output = {};
+  output[filtro] = allAnimals()[filtro];
+  return (Object.keys(hours)
+    .some((element) => element === filtro) ? output : false);
+}
+
+function filterAnimals(animal) {
+  return ((species
+    .some((specie) => specie.name === animal))
+    ? species
+      .find((specie) => specie.name === animal)
+      .availability : false);
+}
+function getSchedule(scheduleTarget) {
+  // seu código aqui
+  // if(scheduleTarget === undefined) return allAnimals();
+  if(filterAnimals(scheduleTarget)) return filterAnimals(scheduleTarget);
+  if(filterDays(scheduleTarget)) return filterDays(scheduleTarget);
+  return allAnimals();
+}
+getSchedule();
 module.exports = getSchedule;
+
+// Requisito resolvido com a ajuda de Gabriel Benedicto
