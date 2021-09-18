24d0391bca9afbd7aa9dfcd22e5d40e796d0eb2a
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2021-09-17 23:51:54 -0300

    comita 6 quesito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,35 @@
-const data = require('../data/zoo_data');
+const { name } = require('faker');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const childs = entrants.filter((pessoa) => (pessoa.age < 18)).length;
+  const adults = entrants.filter((pessoa) => (pessoa.age >= 18 && pessoa.age < 50)).length;
+  const seniors = entrants.filter((pessoa) => (pessoa.age >= 50)).length;
+  const result = { child: childs, adult: adults, senior: seniors };
+  return result;
+}
+
+function check(entrants) {
+  if (!entrants || entrants.constructor === Object || Object.keys(entrants).length === 0) {
+    return true;
+  } return false;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (check(entrants)) {
+    return 0;
+  }
+  const price = [20.99, 49.99, 24.99];
+  const totalEnt = countEntrants(entrants);
+  const total = totalEnt.child * price[0] + totalEnt.adult * price[1] + totalEnt.senior * price[2];
+  return total;
 }
-
+const entrants = [
+  { name: name.findName(), age: 5 },
+  { name: name.findName(), age: 5 },
+  { name: name.findName(), age: 5 },
+  { name: name.findName(), age: 18 },
+  { name: name.findName(), age: 18 },
+  { name: name.findName(), age: 50 },
+];
+console.log(calculateEntry(entrants));
 module.exports = { calculateEntry, countEntrants };
