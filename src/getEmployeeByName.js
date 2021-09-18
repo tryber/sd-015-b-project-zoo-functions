e7e0dbc4b65c2910430c4ccf6073c715637e9ca1
mf <marcelo.cunha1@outlook.com>
2021-09-18 01:05:44 -0300

    resolved issue three

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return employees.find((identifierEmployee) =>
+    employeeName === identifierEmployee.firstName || employeeName === identifierEmployee.lastName);
 }
 
 module.exports = getEmployeeByName;
