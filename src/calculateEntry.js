7561b2e4f2c40550b2881be054910979cccd65de
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-09-18 14:47:06 -0300

    Limpa comenmtarios do requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,14 +1,5 @@
 const data = require('../data/zoo_data');
 
-// const peoĺeEntrent = [
-//   { name: 'Lara Carvalho', age: 5 },
-//   { name: 'Frederico Moreira', age: 5 },
-//   { name: 'Pedro Henrique Carvalho', age: 5 },
-//   { name: 'Maria Costa', age: 18 },
-//   { name: 'Núbia Souza', age: 18 },
-//   { name: 'Carlos Nogueira', age: 50 },
-// ];
-
 const objeto = {};
 const getAgesEntrants = (nomes) => {
   const valueChild = nomes.filter((nome) => nome.age < 18).length;
