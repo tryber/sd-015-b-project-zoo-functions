038ce7404a76ee32cdb098f2e107ba4068be53ed
Marina Marques <marina.marqueso@outlook.com>
2021-09-18 12:19:06 -0300

    Adicionando parte req8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,14 +1,37 @@
+const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-// function officeSchedule() {
-//   const day = Object.keys(hours);
-//   const sched = Object.values(hours);
-//   const valueSched = sched.map((elemento) => `Open from ${sched.open}am until ${sched.close}pm`);
-//   return valueSched;
+function allSche() {
+  const hoursKeys = Object.keys(hours);
+  const obj = {};
+  const animal = species.filter((elemento) => elemento.availability);
+  hoursKeys.map((elemento) => {
+    obj[elemento] = {officeHour: `Open from ${hours[elemento].open}am until ${hours[elemento].close}pm`,
+    exhibition: animal.map((elemento) => {
+      for (let i = 0; i < hoursKeys.length; i += 1) {
+        console.log(elemento.availability);
+        if (elemento.availability === hoursKeys[i]) {
+          console.log(elemento.name)
+        }
+      }
+    })};
+  })
+
+  return obj;
+}
+
+// function officeSche() {
+//   const days = days();
+//   return days
 // }
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (scheduleTarget === undefined) {
+
+  }
+  
 }
 
 module.exports = getSchedule;
+
+console.log(allSche())
