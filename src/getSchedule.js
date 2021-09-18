445dd18c6ebdcaa8608af250fa6014cc1492f02c
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2021-09-18 15:48:12 -0300

    comita segunda tentativa 6

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if(scheduleTarget < 10){
+    return
+  }
 }
 
 module.exports = getSchedule;
