c3ccc7667add4e40b93e1447ea48ce9b35cc4498
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2021-09-18 13:34:26 -0300

    função getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require("../data/zoo_data");
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const primeiraEspecie = employees.find((pessoa) => pessoa.id === id)
+    .responsibleFor[0];
+  const animal = species.find((specie) =>
+    specie.id === primeiraEspecie)
+    .residents.sort((age1, age2) => age2.age - age1.age);
+  return Object.values(animal[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
