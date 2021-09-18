6daf3f883472bb486391141544cc48c8096e9554
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2021-09-18 11:23:51 -0300

    função countAnimals.js

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,35 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
+function todosAnimais() {
+  const listaDeTodosAnimais = {};
+  species.forEach((animais) => {
+    listaDeTodosAnimais[`${animais.name}`] = animais.residents.length;
+  });
+  return listaDeTodosAnimais;
+}
+
+function animalPorEspecie(animal) {
+  const animalEspecie = species.find((especie) => especie.name === animal.specie);
+  return animalEspecie.residents.length;
+}
+
+function animalPorEspecieSexo(animal) {
+  const animalEspecie = species.find((especie) => especie.name === animal.specie);
+  const animalEspecieSexo = animalEspecie.residents
+    .filter((especieSexo) => especieSexo.sex === animal.sex);
+  return animalEspecieSexo.length;
+}
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    return todosAnimais();
+  }
+  if (Object.entries(animal).length === 1) {
+    return animalPorEspecie(animal);
+  }
+  if (Object.entries(animal).length === 2) {
+    return animalPorEspecieSexo(animal);
+  }
 }
 
 module.exports = countAnimals;
