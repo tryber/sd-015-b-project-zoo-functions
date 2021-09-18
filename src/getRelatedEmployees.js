c84ad33715ca46bec693c26570716f5fef7b535e
mf <marcelo.cunha1@outlook.com>
2021-09-18 01:59:32 -0300

    resolved issue four

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employeeInList) => employeeInList.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return employees.filter((employee) => employee.managers
+      .includes(managerId))
+      .map((employeeFullName) => `${employeeFullName.firstName} ${employeeFullName.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
