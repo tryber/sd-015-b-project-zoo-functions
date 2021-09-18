d4d1c85e92643b0823ba690ec9048bd1581b9401
mf <marcelo.cunha1@outlook.com>
2021-09-18 02:38:55 -0300

    resolved issue five

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+const defaultAnimalsListCounter = (specie) => specie.reduce((acc, curr) => {
+  acc[curr.name] = curr.residents.length;
+  return acc;
+}, {});
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) return defaultAnimalsListCounter(species);
+
+  const residentsListCounter = species.find((specie) => specie.name === animal.specie);
+
+  return (!animal.sex) ? residentsListCounter.residents.length
+    : residentsListCounter.residents.filter((specie) => specie.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
