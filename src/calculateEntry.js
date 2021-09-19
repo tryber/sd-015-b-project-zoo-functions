d9796ee525e073372468faff3200d63f7a47f849
Marcelo dos Santos Costa <marcelodscosta@yahoo.com.br>
2021-09-19 19:03:55 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,42 @@
 const data = require('../data/zoo_data');
 
+// function countEntrants(entrants) {
+//   // seu código aqui
+// }
+
+// function calculateEntry(entrants) {
+//   // seu código aqui
+// }
 function countEntrants(entrants) {
-  // seu código aqui
+  let child = 0;
+  let adult = 0;
+  let senior = 0;
+  entrants.map((item) => {
+    if (item.age < 18) {
+      child += 1;
+    } else if (item.age >= 18 && item.age < 50) {
+      adult += 1;
+    } else if (item.age >= 50) {
+      senior += 1;
+    }
+    return true;
+  });
+  return {
+    child: `${child}`,
+    adult: `${adult}`,
+    senior: `${senior}` };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.values(entrants).length === 0) {
+    return 0;
+  }
+  const publi = countEntrants(entrants);
+  const child = publi.child * data.prices.child;
+  const adult = publi.adult * data.prices.adult;
+  const senior = publi.senior * data.prices.senior;
+  const result = child + adult + senior;
+  return result;
 }
 
 module.exports = { calculateEntry, countEntrants };
