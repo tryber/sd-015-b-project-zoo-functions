a1dafa83f71e09d35960f44f01d3717de3771465
raphael <raphabarzotto@gmail.com>
2021-09-18 22:49:35 -0300

    entrega requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,18 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+const returnEmployee = (employee) => {
+  if (employee) {
+    return employee;
+  }
+  return {};
+};
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const employeeObject = data.employees.find((employee) =>
+    employee.firstName === employeeName || employee.lastName === employeeName);
+  return returnEmployee(employeeObject);
 }
 
 module.exports = getEmployeeByName;
