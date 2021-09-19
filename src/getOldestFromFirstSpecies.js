3102a3793e8be078567dc9d526c2c69a7ca958cc
Thirki <cicero.santos62@etec.sp.gov.br>
2021-09-19 14:49:18 -0300

    feat: concluido o teste e checagem do lint do arquivo getOldestFromFirstSpecies.test.js

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animalID = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const animal = data.species.find((specie) => specie.id === animalID);
+  let olderAnimal;
+  animal.residents.forEach((currentResident) => {
+    if (!olderAnimal) {
+      olderAnimal = currentResident;
+    }
+    olderAnimal = olderAnimal.age > currentResident.age ? olderAnimal : currentResident;
+  });
+  return [olderAnimal.name, olderAnimal.sex, olderAnimal.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
