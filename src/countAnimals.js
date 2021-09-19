91049f10afb5af0e9fa622118a5482ac6eca5bf1
raphael <raphabarzotto@gmail.com>
2021-09-19 18:11:13 -0300

    entrega requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,34 @@
 const data = require('../data/zoo_data');
 
+const countAllAnimals = () => {
+  const result = {};
+  data.species.forEach((specie) => {
+    result[`${specie.name}`] = specie.residents.length;
+  });
+  return result;
+};
+
+const countSpecificAnimalWithSex = (specificAnimal, animal) => {
+  const specificAnimalWithSex = specificAnimal.filter((resident) => resident.sex === animal.gender);
+  console.log(specificAnimalWithSex);
+  return specificAnimalWithSex.length;
+};
+
+const countSpecificAnimal = (specificAnimal, animal) => {
+  if (animal.sex === undefined && animal.gender === undefined) {
+    return specificAnimal.length;
+  }
+  return countSpecificAnimalWithSex(specificAnimal, animal);
+  // o código não foi arrumado para
+};
+
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    return countAllAnimals();
+  }
+  const specificAnimal = data.species.find((specie) => specie.name === animal.specie).residents;
+  return countSpecificAnimal(specificAnimal, animal);
 }
 
 module.exports = countAnimals;
