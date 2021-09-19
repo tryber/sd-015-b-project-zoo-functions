4ff1b10fb8f4c19823e1dd42900d4d929ab45a4d
André E. Souto <andreesouto@hotmail.com>
2021-09-19 11:49:00 -0300

    adicionou requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+
+  const totalEntradas = {
+    child: entrants.filter((childs) => childs.age < 18).length,
+    adult: entrants.filter((adults) => adults.age >= 18 && adults.age < 50).length,
+    senior: entrants.filter((seniors) => seniors.age >= 50).length,
+  };
+  return totalEntradas;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  const totalEntrants = countEntrants(entrants);
+  const numeroEntrants = Object.values(totalEntrants);
+  if (totalEntrants === 0) return 0;
+  const pagamentoPorIdade = {
+    child: numeroEntrants[0] * data.prices.child,
+    adult: numeroEntrants[1] * data.prices.adult,
+    senior: numeroEntrants[2] * data.prices.senior,
+  };
+  const valoresPagos = Object.values(pagamentoPorIdade);
+  const totalPago = valoresPagos.reduce((acc, numero) => acc + numero);
+  return totalPago;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,34 @@
 const data = require('../data/zoo_data');
 
+function semParametro() {
+  const retornoAnimais = {};
+  data.species.forEach((registroAnimal) => {
+    retornoAnimais[`${registroAnimal.name}`] = registroAnimal.residents.length;
+  });
+  return retornoAnimais;
+}
+
+function animalParametro(animal) {
+  const especie = data.species.find((especies) => especies.name === animal.name);
+  return especie.length;
+}
+
+function animalGeneroParametro(animal) {
+  const especie = data.species.find((especieAnimal) => especieAnimal.name === animal.name);
+  const generos = especie.residents.filter((genero) => genero.sex === animal.sex);
+  return generos.length;
+}
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return semParametro();
+  }
+  if (Object.entries(animal).length === 1) {
+    return animalParametro(animal);
+  }
+  if (Object.entries(animal).length === 2) {
+    return animalGeneroParametro(animal);
+  }
 }
 
 module.exports = countAnimals;
