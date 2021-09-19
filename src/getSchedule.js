6ff462ff87d7021272b4021e79cf62d787325b95
Flávia Labanca <flavialabanca@gmail.com>
2021-09-19 01:20:12 -0300

    requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,17 +1,48 @@
+// Requisito feito com ajuda dos colegas: Danielen Cestari, Leandro Oliveira e Ulisses Roque.
+
 const data = require('../data/zoo_data');
 
-// A função é responsável por disponibilizar as informações de horário dos animais em uma consulta para o usuário, que pode querer ter acesso ao cronograma da semana, de um dia ou de um animal em específico.
+function exhibition(day) {
+  // filtra e organiza de acordo com o nome - referência do Leandro Oliveira
+  return data.species.filter((specie) => specie.availability.includes(day)).map(({ name }) => name);
+}
 
-// - analise os testes unitários para entender os retornos que são esperados para esta função;
-// - quebre o problema em funções menores para que fique mais simples de administrar a responsabilidade de cada uma delas.
+function scheduleTime() {
+  const schedule = {};
+  const days = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+  days.forEach((day) => {
+    schedule[day] = {};
+    schedule[day].officeHour = `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`;
+    schedule[day].exhibition = exhibition(day);
+    if (day === 'Monday') {
+      schedule[day].officeHour = 'CLOSED';
+      schedule[day].exhibition = 'The zoo will be closed!';
+    }
+  });
+  return schedule;
+}
 
-function getSchedule(scheduleTarget) {
-  // chama função pra parâmetro vazio - retorna dia/hora de todos animais
-  // chama função pra parâmetro preenchido - retorna dia/hora
+function dailySchedule(day) {
+  // feita com MUITA ajuda da Danielen e do Leandro
+  const weekSchedule = scheduleTime(); 
+  const todayDailySchedule = {}; 
+  todayDailySchedule[day] = weekSchedule[day]; 
+  // retorna agenda do dia
+  return todayDailySchedule;
+}
+
+function animalSchedule(animal) {
+  const specie = data.species.find((elementAnimal) => elementAnimal.name === animal);
+  const dayList = specie.availability;
+  return dayList;
+}
+
+function getSchedule(scheduleTarget) { // aqui filtra os resultados, por isso usa as funções de cima aqui.
+  const days = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+  const bichos = data.species.map(({ name }) => name);
+  if (days.includes(scheduleTarget)) return dailySchedule(scheduleTarget); // qdo pesquisa por dia
+  if (bichos.includes(scheduleTarget)) return animalSchedule(scheduleTarget); // qdo pesquisa por bicho
+  return scheduleTime(); // qdo a pesquisa é vazia e tem que retornar TUDÃO
 }
 
 module.exports = getSchedule;
-
-// se não passar nenhuma animal, retorna todos animais e horários.
-
-// se passar animal, pega dia e hora que ele ta disponível
