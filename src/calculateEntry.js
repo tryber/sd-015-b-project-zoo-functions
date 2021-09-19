32ed6f87d28511b121a420d7b1eb2ca81e14ca5d
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-09-19 16:08:51 -0300

    Finaliza requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,7 +1,8 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const objeto = {};
 const getAgesEntrants = (nomes) => {
+  const objeto = { child: 0, adult: 0, senior: 0 };
   const valueChild = nomes.filter((nome) => nome.age < 18).length;
   const valueAdult = nomes.filter((nome) => nome.age >= 18 && nome.age < 50).length;
   const valueSenior = nomes.filter((nome) => nome.age >= 50).length;
@@ -12,10 +13,9 @@ const getAgesEntrants = (nomes) => {
     if (nome.age >= 50) objeto.senior = valueSenior;
     return objeto;
   });
+  console.log(objeto);
   return objeto;
 };
-
-// console.log(getAgesEntrants(peoĺeEntrent));
 
 function countEntrants(entrants) {
   const ageGroup = getAgesEntrants(entrants);
@@ -23,8 +23,19 @@ function countEntrants(entrants) {
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined) {
+    return 0;
+  } if (Object.values(entrants).length === 0) {
+    return 0;
+  }
+  const ageGroupCount = countEntrants(entrants);
+  const priceForAdult = prices.adult * ageGroupCount.adult;
+  console.log(ageGroupCount.adult);
+  const priceForChild = prices.child * ageGroupCount.child;
+  const priceForSenior = prices.senior * ageGroupCount.senior;
+  const sumeAll = priceForAdult + priceForChild + priceForSenior;
+
+  return sumeAll;
 }
 
 module.exports = { calculateEntry, countEntrants };
-// console.log(countEntrants(peoĺeEntrent));
