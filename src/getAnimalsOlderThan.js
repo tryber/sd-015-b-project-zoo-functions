173360efcafd64b0d9dc1d42abf8e2e230de9219
alinemsiqueira <aline.mscruz1@gmail.com>
2021-09-19 16:26:38 -0300

    Add requirement 02

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  return species.find(animalName => animalName.name === animal).residents.every(animalSpecie => animalSpecie.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
