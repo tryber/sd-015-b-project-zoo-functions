6a5bbe15fcfdfe4123d278cd90d7962700c8a82b
Claudio Junior Haas <claudiohaas.cjh@gmail.com>
2021-09-19 19:25:22 -0300

    Requisito 5: Refatorado linch

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -5,19 +5,19 @@ function countAnimals(animal) {
   let count = 0;
   if (!animal) {
     const result = {};
-    species.map((specie) => {
+    species.forEach((specie) => {
       count = specie.residents.length;
       result[`${specie.name}`] = count;
     });
-    return result; 
+    return result;
   }
   const findAnimal = species.find((i) => i.name === animal.specie).residents;
   if (!animal.gender) {
-  count = findAnimal.length
-  return count;
+    count = findAnimal.length;
+    return count;
   }
   count = findAnimal.filter((j) => j.sex === animal.gender).length;
-  return count;  
+  return count;
 }
 
 module.exports = countAnimals;
