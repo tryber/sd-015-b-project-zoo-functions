91007ab1ba6c9d14e9c5241ff0bd292d359656a6
Gabriel Borges <igblborges@gmail.com>
2021-09-19 10:52:26 -0300

    concertando requisito-5-new-function-withoutparameters

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,12 +1,16 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function withOutParameters  {
+  return species.reduce((animals, currentValue) => {
+    animals[currentValue.name] = currentValue.residents.length;
+    return animals;
+  }, {})
+
+}
 function countAnimals(animal) {
   if (!animal) {
-    return species.reduce((animals, currentValue) => {
-      animals[currentValue.name] = currentValue.residents.length;
-      return animals;
-    }, {});
+    return withOutParameters();    
   } if (animal.specie && animal.gender) {
     const specieAnimal = species.find((element) => animal.specie === element.name);
     const animalGender = specieAnimal.residents
