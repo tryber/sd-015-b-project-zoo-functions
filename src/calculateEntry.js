799c5a72e20fb37d479d3bd7136d49b81de55d53
João Pedro de Oliveira Lage <87338925+JoaoPedroLage@users.noreply.github.com>
2021-09-19 13:49:26 -0300

    Implementa a função getSchedule

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,42 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const result = {};
+  const filterChild = entrants.filter((generationsCount) =>
+    generationsCount.age < 18);
+  result.child = filterChild.length;
+  const filterAdult = entrants.filter((generationsCount) =>
+    generationsCount.age >= 18 && generationsCount.age < 50);
+  result.adult = filterAdult.length;
+  const filterSenior = entrants.filter((generationsCount) =>
+    generationsCount.age >= 50);
+  result.senior = filterSenior.length;
+  return result;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined) return 0;
+  if (Object.entries(entrants).length === 0) return 0;
+  const pricesArray = Object.values(data.prices);
+  const quantityOfEntrantsArray = Object.values(countEntrants(entrants));
+  const sortPricesArray = [];
+  sortPricesArray.push(pricesArray[2], pricesArray[0], pricesArray[1]);
+  let result = 0;
+  for (let i = 0; i < pricesArray.length; i += 1) {
+    result = (sortPricesArray[i] * quantityOfEntrantsArray[i]) + result;
+  }
+  return result;
 }
+
+const entrants = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
+console.log(calculateEntry(entrants));
 
 module.exports = { calculateEntry, countEntrants };
