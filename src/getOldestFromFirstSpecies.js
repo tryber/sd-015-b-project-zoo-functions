24b28383abe76c22d2cac7b8750205abfe981d23
Murilo <murilocarrijoadm@gmail.com>
2021-09-19 11:22:01 -0300

    Refatorna o 9º requisito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,31 +2,33 @@
 const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getIdFirstSpecie = (id) => employees
-  .filter((employee) => employee.id === id)
+const getFirstAnimal = (id) => employees
+  .filter((employee) => id === employee.id)
   .map((employee) => employee.responsibleFor
     .find((value) => value))[0];
 
-const getListResidents = (idSpecie) =>
-  species.filter((specie) =>
-    specie.id === idSpecie).map((specie) => specie.residents)[0];
+const getResidents = (idAnimal) => species
+  .filter((specie) => idAnimal === specie.id)
+  .map((specie) => specie.residents)[0];
 
-const getMostOldFirstSpecie = (idSpecie) =>
-  Object.values(getListResidents(idSpecie).filter((res) =>
-    res).sort((a, b) => b.age - a.age)[0]);
+const getOldestAnimal = (idAnimal) => Object.values(getResidents(idAnimal)
+  .filter((residents) => residents)
+  .sort((a, b) => b.age - a.age)[0]);
 
-const findMostOldSpecieFromEmployee = (id) => {
-  let result = getIdFirstSpecie(id);
-  result = getMostOldFirstSpecie(result);
+const findOldestAnimal = (id) => {
+  let result = getFirstAnimal(id);
+  result = getOldestAnimal(result);
   return result;
 };
 
 function getOldestFromFirstSpecies(id) {
   let result = [];
   if (id) {
-    result = findMostOldSpecieFromEmployee(id);
+    result = findOldestAnimal(id);
   }
   return result;
 }
+
+console.log(getOldestFromFirstSpecies('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = getOldestFromFirstSpecies;
