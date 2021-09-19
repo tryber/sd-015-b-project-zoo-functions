7c100e50629739b31afa3cc9c09466ae8362e178
Gabriel Borges <igblborges@gmail.com>
2021-09-19 10:22:51 -0300

    salvando requisito 5-para-atualizar-o-gender

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,21 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  return species.find();
+  if (!animal) {
+    return species.reduce((animals, currentValue) => {
+      // eslint-disable-next-line no-param-reassign
+      animals[currentValue.name] = currentValue.residents.length;
+      return animals;
+    }, {});
+  } if (animal.specie && animal.sex) {
+    const specieAnimal = species.find((element) => animal.specie === element.name);
+    const animalGender = specieAnimal.residents.filter((animalSex) => animalSex.sex === animal.sex);
+    return animalGender.length;
+  }
+  const specieAnimal = species.find((element) => animal.specie === element.name);
+  return specieAnimal.residents.length;
 }
+
+console.log(countAnimals({ specie: 'penguins' }));
+
 module.exports = countAnimals;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,13 @@
-const data = require('../data/zoo_data');
-
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
-
-module.exports = getSchedule;
+//  const { species, hours } = require('../data/zoo_data');
+//  const data = require('../data/zoo_data');
+//  const days = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+//  function getSchedule(scheduleTarget) {
+//     const obj = {};
+//    return cronogram.forEach((element,index) => element[index]);
+//  }
+//  const getHour = (day) => {
+//    const { open, close } = hours[day];
+//    return `Open from ${open}am until ${close}pm`;
+//  }
+//  console.log(getHour('Monday'));
+//  module.exports = getSchedule;
