c161e01f16198d1b4b785c347c452e5ff2a9a8a9
Claudio Junior Haas <claudiohaas.cjh@gmail.com>
2021-09-19 11:36:15 -0300

    Refatoraçao linch requisitos 1 e 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,10 +1,10 @@
 const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getEmployeeByName = (nameEmployee) => {
+const getEmployeeByName = (employee) => {
   if (!nameEmployee) return {};
-    const employee =  employees.find((i) => i.firstName === nameEmployee || i.lastName === nameEmployee);
-    return employee;
+  const employee = employees.find((i) => i.firstName === nameEmployee || i.lastName === nameEmployee);
+  return employee;
 };
 
 module.exports = getEmployeeByName;
