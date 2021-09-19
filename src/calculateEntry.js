1129f0f0320ef8605a3b5761ce5decb8ffbf1847
vinicius rampazzo <vinicius_rampazzo10@hotmail.com>
2021-09-19 17:06:07 -0300

    realizando requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,17 +2,27 @@ const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  return prices;
+  const senior = entrants.filter((element) => element.age >= 50);
+  const adult = entrants.filter((element) => element.age >= 18 && element.age < 50);
+  const child = entrants.filter((element) => element.age < 18);
+  const ages = { child: child.length, adult: adult.length, senior: senior.length };
+
+  return ages;
 }
-// console.log(countEntrants(prices));
 
 function calculateEntry(entrants) {
   if (!entrants || Object.keys(entrants).length === 0) {
     return 0;
-  } if (entrants) {
-    return 0;
+  }
+  if (entrants) {
+    const chamaFunc = countEntrants(entrants);
+    const child = chamaFunc.child * prices.child;
+    const adult = chamaFunc.adult * prices.adult;
+    const senior = chamaFunc.senior * prices.senior;
+    const result = child + adult + senior;
+    return result;
   }
 }
-console.log(calculateEntry({}));
+calculateEntry();
 
 module.exports = { calculateEntry, countEntrants };
