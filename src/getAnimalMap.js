e5b93266eb7af599d0b1638388a0fef10f9d07ea
iuryroque <iury.roque@hotmail.com>
2021-09-20 18:06:55 -0300

    Requisito 7 finalizado

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,34 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const { species } = data;
+
+function mapeamentoDeNome(location) {
+  return species.filter((specie) => specie.location === location).map(({ name }) => name);
 }
 
+function getAnimalMap(options) {
+  // const animalsLocation = {};
+  // // animalsLocation.map((location) => locations) = species.filter((specie) => specie.location === locationsSpecies[1]);
+
+  // return locations;
+}
+
+console.log(mapeamentoDeNome());
+
 module.exports = getAnimalMap;
+
+
+
+
+function noOptions(local) {
+  return data.species.filter(({ location }) => local === location)
+    .map(({ name }) => name);
+}
+
+function getAnimalMap(options) {
+  const locations = { NE: [], NW: [], SE: [], SW: [] };
+  data.species.forEach((specie) => {
+    locations[specie.location] = checkOptions(options, specie.location);
+  });
+  return locations;
+}
