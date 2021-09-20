26d27a641a7b038375159fa45b94829c2a323eb6
Carlos Lima <carlosedu151@gmail.com>
2021-09-20 11:34:39 -0300

    Requisito 6, agora vai?

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,7 +1,5 @@
 const data = require('../data/zoo_data');
 
-const { prices } = data;
-
 function countEntrants(entrants) {
   const kids = entrants.filter((entrada) => entrada.age < 18);
   const adultos = entrants.filter((entrada) => (entrada.age >= 18 && entrada.age <= 50));
@@ -14,9 +12,9 @@ function calculateEntry(entrants) {
     return 0;
   }
   const pagantes = countEntrants(entrants);
-  const kids = pagantes.child * prices.child;
-  const adultos = pagantes.adult * prices.adult;
-  const idosos = pagantes.senior * prices.senior;
+  const kids = pagantes.child * 20.99;
+  const adultos = pagantes.adult * 49.99;
+  const idosos = pagantes.senior * 24.99;
   return kids + adultos + idosos;
 }
 
