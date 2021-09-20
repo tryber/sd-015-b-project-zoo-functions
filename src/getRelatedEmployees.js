904f44e0e61a1bf60fbdf987a8e8c7b7976dec28
Arthur Bernardo Messias <turmessias@gmail.com>
2021-09-20 10:21:04 -0300

    Cumpre Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((worker) =>
+    worker.managers.find((manager) => manager === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return employees
+      .filter((worker) => worker.managers.includes(managerId))
+      .map((workers) => `${workers.firstName} ${workers.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
