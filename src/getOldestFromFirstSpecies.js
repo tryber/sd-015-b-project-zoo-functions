34192cf93de918eef958962943eb12c534d43492
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2021-09-20 16:00:38 -0300

    Welckson Valentim - Finaliza requisito 9, apos aprovaçao local

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const nameAnimals = employees.filter((employee) => (employee.id === id));
+  const firstSpecie = nameAnimals.find((animal) => animal.responsibleFor).responsibleFor[0];
+  const listOfSpecies = species.find((old) => old.id === firstSpecie).residents;
+  const oldestAnimal = listOfSpecies.reduce((acumulator, current) =>
+    ((acumulator.age > current.age) ? acumulator : current));
+  const valeOfInformation = [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
+  return valeOfInformation;
 }
 
 module.exports = getOldestFromFirstSpecies;
