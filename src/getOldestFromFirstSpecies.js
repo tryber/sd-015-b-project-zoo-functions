f062b2886a0bb60f969c06388bca16b7655d0ff4
BrenoRCBrito <brenorcbrito@gmail.com>
2021-09-20 19:35:21 -0300

    adiciona requisito 9 pronto

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const { employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstManaged = employees.filter((employee) => employee.id === id)[0].responsibleFor[0];
+  const oldest = species.filter((animal) => animal.id === firstManaged)[0].residents
+    .reduce((acc, namedAnimal) => {
+      if (acc.age > namedAnimal.age) return acc;
+      return namedAnimal;
+    });
+  return [oldest.name, oldest.sex, oldest.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
