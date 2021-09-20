1080b1b04641d9513cef3d3f080bab5d3230e10c
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-09-20 18:58:36 -0300

    Implementa a funçao countAnimals para o desafio 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  const animalObject = {};
+  if (!animal) {
+    species.forEach((specie) => {
+      animalObject[`${specie.name}`] = specie.residents.length;
+    });
+    return animalObject;
+  }
+  const selectSpecie = (specie) => specie.name === animal.specie;
+  const selectedSpecie = species.find(selectSpecie);
+  if (animal.gender) {
+    const selectGender = (resident) => resident.sex === animal.gender;
+    const selectedGender = selectedSpecie.residents.filter(selectGender);
+    return selectedGender.length;
+  }
+  return selectedSpecie.residents.length;
 }
 
 module.exports = countAnimals;
