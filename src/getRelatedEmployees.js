1614af2fb81eac4b4594bced70551a74d80180b8
BrenoRCBrito <brenorcbrito@gmail.com>
2021-09-19 22:36:38 -0300

    adiciona requisito 4 pronto

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.some((managerId) => id === managerId));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const related = employees.filter((employee) => employee.managers
+    .some((managers) => managers === managerId));
+  return related.map((names) => `${names.firstName} ${names.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
