5eaa16ca0523e8ee251dfab7ac85a6b998b608db
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-09-20 15:58:26 -0300

    Limpa consoles.log, requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -3,23 +3,20 @@ const data = require('../data/zoo_data');
 
 const obtAllSchedule = {};
 const arrHours = Object.entries(hours);
-// console.log(arrHours);
+
 const daysWeek = arrHours.map((day) => day[0]);
-// console.log(daysWeek);
+
 const keyDaysWeek = arrHours.map((day) => day[0])
   .map((dayWeek) => arrHours.find((hour) => hour.includes(dayWeek))
     .map((dayOpenClose) => dayOpenClose)[0]);
-// console.log(keyDaysWeek);
 
 const valueOpenClose = arrHours.map((day) => day[0])
   .map((dayWeek) => arrHours.find((hour) => hour.includes(dayWeek))
     .map((openClose) => `Open from ${openClose.open}am until ${openClose.close}pm`)[1]);
-// console.log(valueOpenClose);
 
 const valueExhibition = arrHours.map((day) => day[0])
   .map((dayWeek) => species.filter((specie) => specie.availability.includes(dayWeek))
     .map((specieDay) => specieDay.name));
-// console.log(valueExhibition);
 
 const getObjtAllSchedule = () => {
   for (let i = 0; i < daysWeek.length; i += 1) {
@@ -35,15 +32,14 @@ const getObjtAllSchedule = () => {
 };
 
 const ifAnimals = (nameAnimal) => species.some((specie) => specie.name === nameAnimal);
-// console.log(ifAnimals('lions'));
+
 const getDayAnimal = (nameAnimal) => species
   .find((specie) => specie.name === nameAnimal).availability;
-// console.log(getDayAnimal('lions'));
 
 const dayMonday = { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
 
 const ifOneDay = (oneDay) => arrHours.some((dayWeek) => dayWeek[0].includes(oneDay));
-// console.log(ifOneDay('Tuday'));
+
 const getForOneDay = (oneDay) => {
   if (oneDay === 'Monday') {
     return dayMonday;
