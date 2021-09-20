2ec95b7ebc26965f5adb7f737831cb5debe67c49
alinemsiqueira <aline.mscruz1@gmail.com>
2021-09-20 16:17:12 -0300

    Add requirement 06

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
-function countEntrants(entrants) {
+const countEntrants = (entrants) => {
   // seu código aqui
-}
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const countedEntrants = {
+    child: entrants.filter((child) => child.age < 18).length,
+    adult: entrants.filter((adult) => adult.age >= 18 && adult.age < 50).length,
+    senior: entrants.filter((senior) => senior.age >= 50).length,
+  };
+  return countedEntrants;
+};
 
 function calculateEntry(entrants) {
   // seu código aqui
+  const entries = countEntrants(entrants);
+  if (entries === 0) {
+    return 0;
+  }
+  const { child, adult, senior } = prices;
+  const sumChild = entries.child * child;
+  const sumAdult = entries.adult * adult;
+  const sumSenior = entries.senior * senior;
+  return sumChild + sumAdult + sumSenior;
 }
 
 module.exports = { calculateEntry, countEntrants };
