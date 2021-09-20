b44ba1a03a0fe1ceca88fb41fffe983875159a64
Jesse Benevides <jessebenevides@gmail.com>
2021-09-20 17:46:11 -0300

    Adiciona requisito 4 - getRelatedEmployees.js

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  let result = false;
+  employees.forEach((employee) => {
+    if (employee.managers.includes(id)) {
+      result = true;
+    }
+  });
+
+  return result;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  const relateds = employees.filter((employee) => employee.managers.includes(managerId));
+  const relatedNames = relateds.map((related) => `${related.firstName} ${related.lastName}`);
+
+  return relatedNames;
 }
 
 module.exports = { isManager, getRelatedEmployees };
