934276b6ca777ea1f9d5c4c692a69c881b54b5a3
Claudio Junior Haas <claudiohaas.cjh@gmail.com>
2021-09-20 12:11:35 -0300

    Atualizaçao linch requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,11 +2,10 @@ const data = require('../data/zoo_data');
 const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-
   const child = entrants.filter((person) => person.age < 18).length;
   const adult = entrants.filter((person) => person.age >= 18 && person.age < 50).length;
   const senior = entrants.filter((person) => person.age >= 50).length;
-  const entrantsByAge = { child: child, adult: adult, senior: senior };
+  const entrantsByAge = { child, adult, senior };
   return entrantsByAge;
 }
 
