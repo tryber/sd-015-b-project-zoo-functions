c485dd65bb35d29b99f8ecc74f0ec3654746dd8f
raphael <raphabarzotto@gmail.com>
2021-09-20 02:17:31 -0300

    entrega requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+const getOldest = (animals) => {
+  animals.sort((animalA, animalB) => animalB.age - animalA.age);
+  return animals[0];
+};
+
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employee = data.employees.find((idToFind) => idToFind.id === id);
+  const animals = data.species.find((idToFind) => idToFind.id === employee.responsibleFor[0]);
+  const oldest = getOldest(animals.residents);
+  const result = [oldest.name, oldest.sex, oldest.age];
+  return result;
 }
 
 module.exports = getOldestFromFirstSpecies;
