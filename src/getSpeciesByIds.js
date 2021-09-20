d9242104b50ac8a844dad528b10ceac6f47b8662
alinemsiqueira <aline.mscruz1@gmail.com>
2021-09-19 23:28:04 -0300

    Add requirement 01 - Fix lint issues

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,7 @@ const { species } = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
-  return ids.map(id => species.find(specie => specie.id === id));
+  return ids.map((id) => species.find((specie) => specie.id === id));
 }
 
 module.exports = getSpeciesByIds;
