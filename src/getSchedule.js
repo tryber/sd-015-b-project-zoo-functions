5210ddf83617a5d569510348935e6622bbb10c63
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-09-20 08:00:27 -0300

    Refatora requisito 8, retornos

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,48 +1,23 @@
-const { species } = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const openingHours = {
-  Tuesday: {
-    officeHour: 'Open from 8am until 6pm',
-    exhibition: [
-      'lions',
-      'tigers',
-      'bears',
-      'penguins',
-      'elephants',
-      'giraffes',
-    ],
-  },
-  Wednesday: {
-    officeHour: 'Open from 8am until 6pm',
-    exhibition: ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
-  },
-  Thursday: {
-    officeHour: 'Open from 10am until 8pm',
-    exhibition: ['lions', 'otters', 'frogs', 'snakes', 'giraffes'],
-  },
-  Friday: {
-    officeHour: 'Open from 10am until 8pm',
-    exhibition: ['tigers', 'otters', 'frogs', 'elephants', 'giraffes'],
-  },
-  Saturday: {
-    officeHour: 'Open from 8am until 10pm',
-    exhibition: [
-      'lions',
-      'tigers',
-      'bears',
-      'penguins',
-      'otters',
-      'frogs',
-      'snakes',
-      'elephants',
-    ],
-  },
-  Sunday: {
-    officeHour: 'Open from 8am until 8pm',
-    exhibition: ['lions', 'bears', 'penguins', 'snakes', 'elephants'],
-  },
-  Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+const openingHours = {};
+const arrHours = Object.entries(hours);
+// console.log(arrHoursForDays);
+const valueAndKeyDays = (days) => arrHours.find((hour) => hour.includes(days))
+  .map((openClose) => openClose)[0];
+console.log(valueAndKeyDays('Tuesday'));
+const valueOpenAndClose = (days) => arrHours.find((hour) => hour.includes(days))
+  .map((openClose) => `Open from ${openClose.open}am until ${openClose.close}pm`)[1];
+console.log(valueOpenAndClose('Tuesday'));
+const valueExhibition = (days) => species.filter((specie) => specie.availability.includes(days))
+  .map((specieDay) => specieDay.name);
+console.log(valueExhibition('Tuesday'));
+
+const horariosFuncionamento = () => {
+  arrHours.map((day) => {
+
+  });
 };
 
 const dayMonday = { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
