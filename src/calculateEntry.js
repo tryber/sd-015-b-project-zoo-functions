83eaf53762b3ecaeeb091edd55b4a5d40d754d92
fariasao <enzofarias2010@gmail.com>
2021-09-19 23:39:48 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age < 18).length;
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
+  return {
+    child,
+    adult,
+    senior,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || !entrants.length) {
+    return 0;
+  }
+  const visitors = countEntrants(entrants);
+  const totalChildren = visitors.child * data.prices.child;
+  const totalAdults = visitors.adult * data.prices.adult;
+  const totalSeniors = visitors.senior * data.prices.senior;
+  return totalChildren + totalAdults + totalSeniors;
 }
 
 module.exports = { calculateEntry, countEntrants };
