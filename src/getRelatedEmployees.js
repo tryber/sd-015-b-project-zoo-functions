a06e4b499c6f726a2643ba2d1da47f01c277893a
allpha23 <allpha23@gmail.com>
2021-09-19 23:25:01 -0300

    finalizado requisito 6

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -7,13 +7,12 @@ function isManager(id) {
 
 function getRelatedEmployees(managerId) {
   const resolve = isManager(managerId);
-  
   if (resolve === true) {
     const getEmployees = data.employees.filter((employee) => employee.managers.includes(managerId));
     return getEmployees.map((employeeName) => `${employeeName.firstName} ${employeeName.lastName}`);
   }
-  
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  
 }
 
 module.exports = { isManager, getRelatedEmployees };
