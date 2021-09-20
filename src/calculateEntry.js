ffd2baab4a1b5dc0a4ec585fb388c72ef020193b
alinemsiqueira <aline.mscruz1@gmail.com>
2021-09-19 23:40:22 -0300

    Add changed files - lint issues fixed

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,16 +1,6 @@
 const { prices } = require( '../data/zoo_data' );
 const data = require('../data/zoo_data');
 
-const entrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-
-
 function countEntrants(entrants) {
   // seu código aqui
   return prices.filter(({ price }) => price.child < 18);
--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,7 +3,7 @@ const { species } = require('../data/zoo_data');
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
   return species.find(({ name }) => name === animal).residents
-  .every(({ age }) => age >= age);
+    .every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
