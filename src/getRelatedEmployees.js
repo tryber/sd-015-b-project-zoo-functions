424ee985977c9a3c27ea8491f4ab0c9548f1d084
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-09-20 16:46:08 -0300

    Remove os console.log que esqueci

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -27,10 +27,4 @@ function getRelatedEmployees(managerId) {
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
-console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
-console.log(getRelatedEmployees('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
-console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
-// console.log(getRelatedEmployees('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
-// console.log(getRelatedEmployees('c1f50212-35a6-4ecd-8223-f835538526c2'));
-
 module.exports = { isManager, getRelatedEmployees };
