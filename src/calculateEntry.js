33c733d78656c312808405e27f3adc96b9861e8d
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2021-09-20 20:45:10 -0300

    comita o 9 quesito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,9 +1,9 @@
 const { name } = require('faker');
 
 function countEntrants(entrants) {
-  const childs = entrants.filter((pessoa) => (pessoa.age < 18)).length;
-  const adults = entrants.filter((pessoa) => (pessoa.age >= 18 && pessoa.age < 50)).length;
-  const seniors = entrants.filter((pessoa) => (pessoa.age >= 50)).length;
+  const childs = entrants.filter((person) => (person.age < 18)).length;
+  const adults = entrants.filter((person) => (person.age >= 18 && person.age < 50)).length;
+  const seniors = entrants.filter((person) => (person.age >= 50)).length;
   const result = { child: childs, adult: adults, senior: seniors };
   return result;
 }
@@ -18,9 +18,9 @@ function calculateEntry(entrants) {
   if (check(entrants)) {
     return 0;
   }
-  const price = [20.99, 49.99, 24.99];
+  const preço = [20.99, 49.99, 24.99];
   const totalEnt = countEntrants(entrants);
-  const total = totalEnt.child * price[0] + totalEnt.adult * price[1] + totalEnt.senior * price[2];
+  const total = totalEnt.child * preço[0] + totalEnt.adult * preço[1] + totalEnt.senior * preço[2];
   return total;
 }
 const entrants = [
@@ -31,5 +31,7 @@ const entrants = [
   { name: name.findName(), age: 18 },
   { name: name.findName(), age: 50 },
 ];
+
 console.log(calculateEntry(entrants));
+
 module.exports = { calculateEntry, countEntrants };
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,9 +1,60 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  if(scheduleTarget < 10){
-    return
+const dias = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+
+const resultado = {};
+
+function hoursAvaible() {
+  dias.forEach((dia) => {
+    const { open } = data.hours[dia];
+    const { close } = data.hours[dia];
+    resultado[dia] = {};
+    if (open === 0 || close === 0) {
+      resultado[dia].officeHour = 'CLOSED';
+    } else {
+      resultado[dia].officeHour = `Open from ${open}am until ${close}pm`;
+    } 
+  });
+  return resultado;
+}
+function getExhibition() {
+  dias.forEach((dia) => {
+    let animaisAtuais = data.species.filter((animal) => animal.availability.includes(dia));
+    animaisAtuais = animaisAtuais.map((animal) => animal.name);
+    if (animaisAtuais.length === 0) {
+      resultado[dia].exhibition = 'the zoo will be closed!';
+    } else {
+      resultado[dia].exhibition = animaisAtuais;
+    }
+  });
+}
+
+function programação(conograma) {
+  if (!conograma) {
+    return false;
+  }
+  const animals = data.species.map((animal) => animal.name);
+  const animaisExibiçao = data.species.find((animal) => animal.name === conograma)
+  if (animals.includes(programação)) {
+    return animaisExibiçao.availability;
   }
 }
+
+function getSchedule(scheduleTarget) {
+  hoursAvaible();
+  getExhibition();
+  if (scheduleTarget === 'Monday') {
+    return { Monday : resultado[scheduleTarget] };
+  }
+  if (dias.includes(scheduleTarget)) {
+    return { [scheduleTarget]: resultado[scheduleTarget] };
+  }
+  if (programação(scheduleTarget)) {
+    return programação(scheduleTarget);
+  }
+  return resultado;
+}
+
+
 
 module.exports = getSchedule;
