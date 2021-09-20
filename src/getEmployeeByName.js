ebe7d3f15936d873cbb8094e8ad8d0c8635c201c
alinemsiqueira <aline.mscruz1@gmail.com>
2021-09-20 14:19:48 -0300

    Add requirement 03

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  return employees.find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName) || {};
 }
 
+console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
