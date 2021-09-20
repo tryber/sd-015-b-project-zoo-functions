ae7e07b36e475f92af1672e528db6cd2bbb6c092
fariasao <enzofarias2010@gmail.com>
2021-09-19 22:44:05 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const filterManager = data.employees.filter((id) => id.managers.includes(managerId));
+  const listManager = filterManager.map((employee) => `${employee.firstName} ${employee.lastName}`);
+
+  return listManager;
 }
 
 module.exports = { isManager, getRelatedEmployees };
