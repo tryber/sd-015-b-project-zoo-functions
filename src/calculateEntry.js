788838f13c87d4a9e7b53da9c4ee23e5d67261d3
Claudio Junior Haas <claudiohaas.cjh@gmail.com>
2021-09-19 23:22:50 -0300

    Atualizaçao pull erro requisito 5

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const ticket = data.prices;
+  const child = 0;
+  const adult = 0;
+  const senior = 0;
+  if (entrants.age < 18) {
+    child += 1;
+  } else if (entrants.age >= 50) {
+    senior += 1;
+  } else {
+    adult +=1;
+  }
+
 }
 
 function calculateEntry(entrants) {
