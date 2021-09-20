ae8f7d85b1e6b7bb5c5bdfcd780791b8ab743a4a
alinemsiqueira <aline.mscruz1@gmail.com>
2021-09-19 23:08:31 -0300

    Add requirement 01

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,10 +1,8 @@
-const  data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
-  return ids.map(id => data.species.find(specie => specie.id === id));
+  return ids.map(id => species.find(specie => specie.id === id));
 }
 
-
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
