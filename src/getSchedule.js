fc46ec9f9bc13679ace18b55251d4fd894511017
Murilo <murilocarrijoadm@gmail.com>
2021-09-20 19:16:40 -0300

    Refatora o 9º requisito

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,30 +1,45 @@
-const { species } = require('../data/zoo_data');
+const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getOfficeHour(day) {
-  if (day !== 'Monday') {
-    return `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`;
+const getAllSchedule = () => {
+  const allSchedule = {};
+  Object.keys(hours).forEach((days) => {
+    const allTimes = hours[days];
+    if (days === 'Monday') {
+      allSchedule[days] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    } else {
+      allSchedule[days] = {
+        officeHour: `Open from ${allTimes.open}am until ${allTimes.close}pm`,
+        exhibition: species
+          .filter((animal) => animal.availability.includes(days))
+          .map((element) => element.name),
+      };
+    }
+  });
+  return allSchedule;
+};
+
+const animalAndDay = (scheduleTarget, day, animals) => {
+  if (animals) {
+    return species.find((animal) => animal.name === scheduleTarget).availability;
   }
-  return 'The zoo will be closed!';
-}
-
-const getAnialList = () => species.map(({ name }) => name);
-
-const getAnimal = (AnimalName) => species
-  .find(({ name }) => AnimalName === name).availability;
+  if (day) {
+    return { [scheduleTarget]: getAllSchedule()[scheduleTarget] };
+  }
+};
 
 function getSchedule(scheduleTarget) {
-  const listDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  const animals = getAnialList();
-
-  if (listDays.includes(scheduleTarget)) {
-    return { [scheduleTarget]: getOfficeHour(scheduleTarget) };
+  const day = Object.keys(hours).includes(scheduleTarget);
+  const animal = species.some((element) => element.name === scheduleTarget);
+  if (animal || day) {
+    return animalAndDay(scheduleTarget, day, animal);
   }
-
-  if (animals.includes(scheduleTarget)) {
-    return getAnimal(scheduleTarget);
-  }
-  return listDays.reduce((acc, day) => ({ ...acc, [day]: getOfficeHour(day) }), {});
+  return getAllSchedule();
 }
+
+getSchedule('Monday');
 
 module.exports = getSchedule;
