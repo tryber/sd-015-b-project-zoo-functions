9d65ee52f62063c168be8c5f7cc1593d4f6fa07c
Claudio Junior Haas <claudiohaas.cjh@gmail.com>
2021-09-20 12:03:37 -0300

    Verificaçao linch requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -6,19 +6,19 @@ function countEntrants(entrants) {
   const child = entrants.filter((person) => person.age < 18).length;
   const adult = entrants.filter((person) => person.age >= 18 && person.age < 50).length;
   const senior = entrants.filter((person) => person.age >= 50).length;
-  const entrantsByAge = { child: child, adult: adult, senior: senior};
+  const entrantsByAge = { child: child, adult: adult, senior: senior };
   return entrantsByAge;
 }
 
 function calculateEntry(entrants) {
-if (!entrants || Object.keys(entrants).length === 0) return 0;
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
 
-const totalEntrants = countEntrants(entrants);
-const priceChild = totalEntrants.child * prices.child;
-const priceAdult = totalEntrants.adult * prices.adult;
-const priceSenior = totalEntrants.senior * prices.senior;
-const priceSum = priceChild + priceAdult + priceSenior;
-return priceSum;
-};
+  const totalEntrants = countEntrants(entrants);
+  const priceChild = totalEntrants.child * prices.child;
+  const priceAdult = totalEntrants.adult * prices.adult;
+  const priceSenior = totalEntrants.senior * prices.senior;
+  const priceSum = priceChild + priceAdult + priceSenior;
+  return priceSum;
+}
 
 module.exports = { calculateEntry, countEntrants };
