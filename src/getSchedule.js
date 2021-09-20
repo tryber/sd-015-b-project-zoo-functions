e4662409aa576d4501cf949c4f9969667c1d34cf
Miguel_Reis <guelsouzar@gmail.com>
2021-09-19 22:13:46 -0300

    requisito 10

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
+const monday = {
+  Monday:
+   {
+     exhibition: 'The zoo will be closed!',
+     officeHour: 'CLOSED',
+   },
+};
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const dias = scheduleTarget;
+
+  if (scheduleTarget === 'Monday') {
+    return monday;
+  }
+  return { [scheduleTarget]: {
+    officeHour: `Open from ${data.hours[dias].open}am until ${data.hours[dias].close}pm`,
+    exhibition: data.species.filter((element) => element.availability.includes(scheduleTarget))
+      .map((elemento) => elemento.name),
+  } };
 }
 
 module.exports = getSchedule;
