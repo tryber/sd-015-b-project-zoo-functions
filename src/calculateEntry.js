86cbb115ad30103da26db91ba9baa38d39f8710c
Jesse Benevides <jessebenevides@gmail.com>
2021-09-20 12:13:53 -0300

    Requisito 6 - implementa a função countEntrants()

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,26 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age < 18).length;
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
+
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
 }
+
+const entrants = [
+  { name: 'joao', age: 5 },
+  { name: 'maria', age: 5 },
+  { name: 'pedro', age: 5 },
+  { name: 'julia', age: 18 },
+  { name: 'isabela', age: 18 },
+  { name: 'neide', age: 50 },
+];
+
+console.log(countEntrants(entrants));
 
 module.exports = { calculateEntry, countEntrants };
