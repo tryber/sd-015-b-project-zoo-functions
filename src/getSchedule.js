180be4440876f81f7dce4c7c57ddb417cb01ef40
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-09-21 13:06:32 -0300

    Implementa a funçao getSchedule para o desafio 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,44 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+function avaliabilityByWeekday(day) {
+  let officeHour;
+  let exhibition;
+  const closed = 'Monday';
+  if (day === closed) {
+    officeHour = 'CLOSED';
+    exhibition = 'The zoo will be closed!';
+  } else {
+    const findAvaliability = (specie) => specie.availability.includes(day);
+    const speciesAvaliable = species.filter(findAvaliability);
+    officeHour = `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`;
+    exhibition = speciesAvaliable.map((specie) => specie.name);
+  }
+  const infoFound = {
+    officeHour,
+    exhibition,
+  };
+  return infoFound;
+}
+
+function getSchedule(animalOrDay) {
+  const weekdays = Object.keys(data.hours);
+  const animals = species.map((specie) => specie.name);
+  const animalsAndDays = weekdays.concat(animals);
+  const infoList = {};
+  if (!animalOrDay || !animalsAndDays.includes(animalOrDay)) {
+    weekdays.forEach((weekday) => { infoList[weekday] = avaliabilityByWeekday(weekday); });
+    return infoList;
+  }
+  if (weekdays.includes(animalOrDay)) {
+    infoList[animalOrDay] = avaliabilityByWeekday(animalOrDay);
+    return infoList;
+  }
+  if (animals.includes(animalOrDay)) {
+    const findAnimal = (specie) => specie.name === animalOrDay;
+    const foundAnimal = species.find(findAnimal).availability;
+    return foundAnimal;
+  }
 }
 
 module.exports = getSchedule;
