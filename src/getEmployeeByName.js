24753214230ae3ce6a7329d9fc348497a7007970
Henrique Prim <henrique_salmprim@hotmail.com>
2021-09-21 10:14:48 -0300

    getEmployeeByName implemented

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
+const {employees} = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+    // Se for não for passado nenhum parâmetro para a função é retornado um obj vazio.
+  }
+  return employees.find((employee) =>
+    employee.firstName === employeeName || employee.lastName === employeeName);
+  // Procura dentro de 'employees' se o 'firstName' ou 'lastName' correspondem ao parâmetro passado. Se correspondem é retornado o obj do funcionário.
 }
 
 module.exports = getEmployeeByName;
