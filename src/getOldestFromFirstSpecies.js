5659ba58f0d2fa43f1f525aa46cfe33845cbded1
Henrique Prim <henrique_salmprim@hotmail.com>
2021-09-21 11:59:02 -0300

    getOldestFromFirstSpecies implemented

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  // Primeiro é verificado se se o id passado como parâmetro se iguala ao de algum id do data.js.
+  const searchedEmployee = employees.find((person) => person.id === id);
+  // Verificado se o id do funcionário se iguala, é então buscado dentro do 'responsible for' do respectivo id a espécie que estiver na primeira posição (posição 0 no caso).
+  const firstspecie = species.find((animal) => animal.id === searchedEmployee.responsibleFor[0]);
+  // Dentro de residents da primeira espécie é feito um sort das idades invertendo a ordem nas quais são organizadas, começando pela maior idade (maior número) ao invés da menor.
+  const sortedAnimal = firstspecie.residents.sort((a, b) => b.age - a.age);
+  // É então retornado a primeira posição de sortedAnimal, que corresponde ao animal mais velho encontrado.
+  return Object.values(sortedAnimal[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
+const { hours } = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+function getSchedule(dayName) {
+  // Acessa os keys e os values do objeto 'hours' e armazena em constantes.
+  const days = Object.keys(hours);
+  const dayHours = Object.values(hours);
+  const schedule = days.reduce((acc, crr, index) => {
+    // Se o valor de dayHours no objeto for estritamente igual a 0 irá retornar a mensagem 'CLOSED'.
+    if (dayHours[index].open === 0) {
+      acc[crr] = 'CLOSED';
+      return acc;
+    }
+    // Ao passar um dayName que não tenha o valor de 0 irá retornar o resultado de reduce do dia escolhido printando os horarios de abertura e fechamento do Zoo.
+    (acc[crr] = (`Open from ${dayHours[index].open}am until ${dayHours[index].close}pm`));
+    return acc;
+  }, {});
+  if (!dayName) {
+    return schedule;
+  } // Se o parâmetro passado não corresponder a nenhum dos keys dentro do objeto hours, ele retorna os horários de abertura e fechamento de TODOS os dias da semana usando o template de 'schedule'.
+  return { [dayName]: schedule[dayName] };
 }
 
 module.exports = getSchedule;
