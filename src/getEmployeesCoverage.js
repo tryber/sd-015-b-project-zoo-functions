e47221ae5a07a0ff5fcd2ed7f98ebc8f998dadc7
Henrique Prim <henrique_salmprim@hotmail.com>
2021-09-21 13:56:19 -0300

    getEmployeesCoverage implemented

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,42 @@
 const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function findEmployee(employee) {
+  const getEmployee = employee.find((inputedId) => inputedId.id === employee.id
+  || inputedId.firstName === employee.name
+  || inputedId.lastName === employee.name);
+  if (typeof getEmployee === 'undefined') {
+    throw new Error('Informações inválidas');
+  }
+  return getEmployee;
+}
+function getSpeciesFromEmployee(employee) {
+  const speciesFilter = species.filter((specie) => employee.includes(specie.id));
+  return speciesFilter;
+}
+function objectCreator(employee, animal) {
+  const createdObject = {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: animal.map((specie) => specie.name),
+    locations: animal.map((locations) => locations.location),
+  };
+  return createdObject;
+}
+
+function getEmployeesCoverage(id) {
+  if (typeof id === 'undefined') {
+    const allEmployees = [];
+    employees.forEach((employee) => {
+      allEmployees.push(objectCreator(employee, getSpeciesFromEmployee(employee.responsibleFor)));
+    });
+    return allEmployees;
+  }
+  const getEmployees = findEmployee(employees);
+  const employeeResponsibleFor = getEmployees.responsibleFor;
+  const getSpecieEmployeeAndLocal = getSpeciesFromEmployee(employeeResponsibleFor);
+
+  return objectCreator(getEmployees, getSpecieEmployeeAndLocal);
 }
 
 module.exports = getEmployeesCoverage;
