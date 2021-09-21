5c1197c93175715674a4504e6dc95f4f8d888729
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2021-09-20 21:07:26 -0300

    comita 8 quesito

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,60 +1,59 @@
 const data = require('../data/zoo_data');
 
-const dias = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+const dia = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
 
 const resultado = {};
 
-function hoursAvaible() {
-  dias.forEach((dia) => {
-    const { open } = data.hours[dia];
-    const { close } = data.hours[dia];
-    resultado[dia] = {};
+function getHours() {
+  dia.forEach((day) => {
+    const { open } = data.hours[day];
+    const { close } = data.hours[day];
+    resultado[day] = {};
     if (open === 0 || close === 0) {
-      resultado[dia].officeHour = 'CLOSED';
+      resultado[day].officeHour = 'CLOSED';
     } else {
-      resultado[dia].officeHour = `Open from ${open}am until ${close}pm`;
-    } 
+      resultado[day].officeHour = `Open from ${open}am until ${close}pm`;
+    }
   });
   return resultado;
 }
+
 function getExhibition() {
-  dias.forEach((dia) => {
-    let animaisAtuais = data.species.filter((animal) => animal.availability.includes(dia));
-    animaisAtuais = animaisAtuais.map((animal) => animal.name);
-    if (animaisAtuais.length === 0) {
-      resultado[dia].exhibition = 'the zoo will be closed!';
+  dia.forEach((day) => {
+    let currentAnimals = data.species.filter((animal) => animal.availability.includes(day));
+    currentAnimals = currentAnimals.map((animal) => animal.name);
+    if (currentAnimals.length === 0) {
+      resultado[day].exhibition = 'The zoo will be closed!';
     } else {
-      resultado[dia].exhibition = animaisAtuais;
+      resultado[day].exhibition = currentAnimals;
     }
   });
 }
 
-function programação(conograma) {
-  if (!conograma) {
+function getScheduleFromAnimal(scheduleTarget) {
+  if (!scheduleTarget) {
     return false;
   }
   const animals = data.species.map((animal) => animal.name);
-  const animaisExibiçao = data.species.find((animal) => animal.name === conograma)
-  if (animals.includes(programação)) {
-    return animaisExibiçao.availability;
+  const animalExhibition = data.species.find((animal) => animal.name === scheduleTarget);
+  if (animals.includes(scheduleTarget)) {
+    return animalExhibition.availability;
   }
 }
 
 function getSchedule(scheduleTarget) {
-  hoursAvaible();
+  getHours();
   getExhibition();
   if (scheduleTarget === 'Monday') {
-    return { Monday : resultado[scheduleTarget] };
+    return { Monday: resultado[scheduleTarget] };
   }
-  if (dias.includes(scheduleTarget)) {
+  if (dia.includes(scheduleTarget)) {
     return { [scheduleTarget]: resultado[scheduleTarget] };
   }
-  if (programação(scheduleTarget)) {
-    return programação(scheduleTarget);
+  if (getScheduleFromAnimal(scheduleTarget)) {
+    return getScheduleFromAnimal(scheduleTarget);
   }
   return resultado;
 }
-
-
 
 module.exports = getSchedule;
