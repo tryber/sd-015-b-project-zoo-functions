c054431a7f075df65448c4e2288756638f9ffd80
Adson Gomes Oliveira <adsongoliveira2021@gmail.com>
2021-09-21 06:19:43 -0300

    Primeiro Requisito concluido.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const findSpecie = ids.map((id) => species.find((specie) => specie.id === id));
+  return findSpecie;
 }
 
 module.exports = getSpeciesByIds;
