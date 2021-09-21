cd0f7ef97fbcb1f48399df27d93e28181abcdb5d
Carlos Lima <carlosedu151@gmail.com>
2021-09-21 01:36:53 -0300

    Corrigindo a trolagem do tabnine

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -7,7 +7,7 @@ function getAnimalsAndTime(day) {
   obj.officeHour = `Open from ${open} am until ${close}pm`;
   const specieFilter = data.species.filter((specie) => specie.availability.includes(day));
   const namesMap = specieFilter.map((animal) => animal.name);
-  obj.exhibition = namesMap.join(', ');
+  obj.exhibition = namesMap;
   if (day === 'Monday') {
     obj.officeHour = 'CLOSED';
     obj.exhibition = 'The zoo will be closed!';
