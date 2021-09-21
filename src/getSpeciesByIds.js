f33b3a41bf98c25e58f01cff7c0a171e7bc59d1a
Henrique Prim <henrique_salmprim@hotmail.com>
2021-09-21 09:56:55 -0300

    getSpeciesById implemented

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+// Os parâmetros 'ids' são mapeados e como callback do mapeamento é usado o find para encontrar todas as species que se igualarem ao id analisado.
+function getSpeciesByIds(...ids) {
+  return ids.map((id) => species.find((specie) => specie.id === id));
 }
 
 module.exports = getSpeciesByIds;
