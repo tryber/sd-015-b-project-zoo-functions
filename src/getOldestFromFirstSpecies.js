0de8ea619784b125664d5057a7357431add0cf76
Claudio Junior Haas <claudiohaas.cjh@gmail.com>
2021-09-20 22:42:53 -0300

    Requisito 9: Concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employeeFirstAnimal = employees.filter((element) => element.id.includes(id));
+  const filterFirstAnimal = employeeFirstAnimal[0].responsibleFor.shift();
+  const animalValues = species.filter((element) => element.id.includes(filterFirstAnimal));
+  const oldestAnimal = animalValues[0].residents.reduce((acumulator, element) => {
+    if (element.age > acumulator.age) return element;
+    else return acumulator;
+  });
+  return Object.values(oldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
