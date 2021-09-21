6ccc024068bcc4fd16e6b07d1b664c98a467e3ba
allpha23 <allpha23@gmail.com>
2021-09-21 11:50:25 -0300

    correção lint requisito 9 (2)

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -5,7 +5,7 @@ function getOldestFromFirstSpecies(id) {
   const employeeIdManeger = employees.find((employee) => id === employee.id).responsibleFor[0];
   const animal = species.find((specie) => specie.id === employeeIdManeger);
   const residents = animal.residents
-  .sort((fistAnimal, secondAnimal) => fistAnimal.age - secondAnimal.age);
+    .sort((fistAnimal, secondAnimal) => fistAnimal.age - secondAnimal.age);
   const oldest = residents[residents.length - 1];
   return Object.values(oldest);
 }
