433b1d588b5b22d2180298de8b4d6943c797fc43
Adson Gomes Oliveira <adsongoliveira2021@gmail.com>
2021-09-21 06:49:35 -0300

    Segundo Requisito concluido.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const findAnimal = species.find((crrAnimal) => crrAnimal.name === animal).residents;
+  const verifyAge = findAnimal.every((personAnimal) => personAnimal.age >= age);
+
+  return verifyAge;
 }
 
 module.exports = getAnimalsOlderThan;
