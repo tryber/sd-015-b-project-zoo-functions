247c98ed58bcac47f199ad2290e5cb6fbc66fec8
Vitor <vitoremerich.contato@gmail.com>
2021-09-22 15:46:46 -0300

    Requisito 3 lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,7 +2,8 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   const { employees } = data;
-  return employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName) || {};
+  return employees.find((employee) => (
+    employee.firstName === employeeName || employee.lastName === employeeName)) || {};
 }
 console.log(getEmployeeByName('Wilburn'));
 
