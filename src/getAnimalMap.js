8c09984e700cd2a921678499a9e7b82beaaafb3e
Yamamoto Andre <andre.yamamoto07@gmail.com>
2021-09-23 16:52:49 -0300

    parametro AnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+// fonte https://github.com/tryber/sd-015-b-project-zoo-functions/blob/annie-haurani-zoo-functions/src/getAnimalMap.js
+
+function createAnimalList(options, specie) {
+  return specie.residents
+    .filter((animal) => (options.sex && animal.sex === options.sex) || (!options.sex))
+    .map((animal) => animal.name);
+}
+
+function getAnimalMap(options = { includeNames: false, sorted: false, sex: false }) {
+  const result = data.species.reduce((obj, specie) => {
+    if (options.includeNames) {
+      const animalList = createAnimalList(options, specie);
+      if (options.sorted) animalList.sort();
+      obj[specie.location].push({ [specie.name]: animalList });
+    } else {
+      obj[specie.location].push(specie.name);
+    }
+    return obj;
+  }, { NE: [], NW: [], SE: [], SW: [] });
+  return result;
 }
 
 module.exports = getAnimalMap;
