f6c12e3b96c3f194b1eea7210c187ee59920d490
alinemsiqueira <aline.mscruz1@gmail.com>
2021-09-23 12:02:58 -0300

    Add requirement 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,27 +1,25 @@
-const { employees, species} = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
 const getSpecies = (employee) => employee.responsibleFor.map((animalId) => species
   .find(({ id }) => id === animalId).name);
 
 const getLocation = (employee) => employee.responsibleFor.map((animalId) => species
   .find(({ id }) => id === animalId).location);
-  
-const employeeData = (identity) => {
+
+function employeeData(identity) {
   const { id, name } = identity;
-  return id? employees.find((employee) => employee.id === id) : employees
+  return id ? employees.find((employee) => employee.id === id) : employees
     .find(({ firstName, lastName }) => firstName === name || lastName === name);
 }
 
-const employeeReturnInfo = (identity) => { 
-  // if (employees.map(({ id, firstName, lastName }) => )) {
-    // throw new Error('Informações inválidas')};
+function employeeReturnInfo(identity) {
   const identifier = employeeData(identity);
-  if (identifier === undefined) { throw new Error('Informações inválidas') };
-  const id = identifier.id;
+  if (!identifier) throw new Error('Informações inválidas');
+  const { id } = identifier;
   const fullName = `${identifier.firstName} ${identifier.lastName}`;
-  const species = getSpecies(identifier);
+  const speciesName = getSpecies(identifier);
   const locations = getLocation(identifier);
-  const returnEmployee = { id, fullName, species, locations }
+  const returnEmployee = { id, fullName, species: speciesName, locations };
   return returnEmployee;
 }
 
@@ -35,11 +33,11 @@ const allEmployees = () => {
 
 function getEmployeesCoverage(identity) {
   // seu código aqui
-  return (identity) ? employeeReturnInfo(identity) : allEmployees();
+  return identity ? employeeReturnInfo(identity) : allEmployees();
 }
 
 // console.log(getEmployeesCoverage());
-console.log(getEmployeesCoverage({ name: 'Susan' }));
+// console.log(getEmployeesCoverage({ name: 'Susan' }));
 // console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 // console.log(getEmployeesCoverage({ name: 'Spry' }));
 // console.log(getEmployeesCoverage({ id: '4b40a139-d4dc-4f09-822d-ec25e819a5ad' }));
