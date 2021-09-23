e24bad18e5e4171f7322176f7deb6bde2feb68e6
Jamez Mayke <jamezmayke@gmail.com>
2021-09-23 11:58:50 -0300

    Requisito 01 completo

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,13 +2,12 @@ const { species } = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
-  const specieId = ids.map((searchSpecieById) => {
-    return species.find((getSpecieById) => {
-      return getSpecieById.id === searchSpecieById
-    })
-  })
+  const specieId = ids.map((searchSpecieById) =>
+    species.find((getSpecieById) =>
+    getSpecieById.id === searchSpecieById
+  ));
 
-  return specieId
-}
+  return specieId;
+};
 
 module.exports = getSpeciesByIds;
