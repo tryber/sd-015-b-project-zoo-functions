55758080cb493d6cca6c3e5aafca903602304256
Yamamoto Andre <andre.yamamoto07@gmail.com>
2021-09-23 17:38:04 -0300

    erro parametro 9 retentativa 1

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,29 +1,13 @@
 const data = require('../data/zoo_data');
-const { employees, species } = require('../data/zoo_data');
 // fonte https://github.com/tryber/sd-015-b-project-zoo-functions/blob/annie-haurani-zoo-functions/src/getEmployeesCoverage.js
-function getOldestAnimal(animalId) {
-  let oldestAnimal;
-  species.forEach(({ id, residents }) => {
-    if (id === animalId) {
-      oldestAnimal = residents.reduce((acc, resident) => {
-        if (acc.age < residents.age) return acc;
-        return resident;
-      });
-    }
-  });
-  oldestAnimal = [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
-  return oldestAnimal;
-}
 
-function getOldestFromFirstSpecies(identifier) {
-  let animalIdentifier = '';
-  employees.forEach(({ id, responsibleFor }) => {
-    if (id === identifier) {
-      const [animalId] = responsibleFor;
-      animalIdentifier = animalId;
-    }
-  });
-  return getOldestAnimal(animalIdentifier);
+function getOldestFromFirstSpecies(id) {
+  const specieId = data.employees.find((employee) => employee.id === id)
+    .responsibleFor[0];
+  const result = data.species.find((specie) => specie.id === specieId)
+    .residents.sort((ani1, ani2) => ani2.age - ani1.age).map((animal) =>
+      [animal.name, animal.sex, animal.age])[0];
+  return result;
 }
 
 module.exports = getOldestFromFirstSpecies;
