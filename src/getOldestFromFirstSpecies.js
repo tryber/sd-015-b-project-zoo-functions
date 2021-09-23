d356a2bc7a06e8c7d8b9c2777ebe93c8badecce9
Yamamoto Andre <andre.yamamoto07@gmail.com>
2021-09-23 17:03:46 -0300

    parametro 9 OldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function getOldestAnimal(animalId) {
+  let oldestAnimal;
+  species.forEach(({ id, residents }) => {
+    if (id === animalId) {
+      oldestAnimal = residents.reduce((acc, resident) => {
+        if (acc.age < residents.age) return acc;
+        return resident;
+      });
+    }
+  });
+  oldestAnimal = [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
+  return oldestAnimal;
+}
+
+function getOldestFromFirstSpecies(identifier) {
+  let animalIdentifier = '';
+  employees.forEach(({ id, responsibleFor }) => {
+    if (id === identifier) {
+      const [animalId] = responsibleFor;
+      animalIdentifier = animalId;
+    }
+  });
+  return getOldestAnimal(animalIdentifier);
 }
 
 module.exports = getOldestFromFirstSpecies;
