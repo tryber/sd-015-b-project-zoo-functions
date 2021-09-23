fe5ed8f8752694c81562e0fd2371a427e41289b4
Yamamoto Andre <andre.yamamoto07@gmail.com>
2021-09-23 15:36:37 -0300

    parametro 2 GetAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species.filter((objAnimal) => objAnimal.name === animal).map((idadeAnimal) => idadeAnimal.residents.every((resident) => resident.age > age)).shift();
 }
 
 module.exports = getAnimalsOlderThan;
