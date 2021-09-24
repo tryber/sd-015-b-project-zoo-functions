b787e5d1b49faf1e7d6f0cf7e4ac819cea721ded
Adson Gomes Oliveira <adsongoliveira2021@gmail.com>
2021-09-24 10:35:40 -0300

    Save

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,25 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
   // seu código aqui
+  const getNamesByGeoNE = species.filter((specie) => specie.location === 'NE');
+  const getNamesByGeoNW = species.filter((specie) => specie.location === 'NW');
+  const getNamesByGeoSE = species.filter((specie) => specie.location === 'SE');
+  const getNamesByGeoSW = species.filter((specie) => specie.location === 'SW');
+
+  const getName = getNamesByGeoSE.map((animalName) => animalName.name);
+  let obj = {
+    NE: [{}],
+    NW: [{}],
+    SE: [{}],
+    SW: [{}],
+  };
+
+  return getName;
 }
+
+const option = { includeNames: true }
+console.log(getAnimalMap(option));
 
 module.exports = getAnimalMap;
