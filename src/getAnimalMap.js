0d43e3368dd847ddf34c35654c7d0660755236cb
Arthur Bernardo Messias <turmessias@gmail.com>
2021-09-24 17:25:37 -0300

    Cumpre primeiro subrequisito do Requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,28 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const getAnimalsByLocation = () => {
+  const animalsLocation = {
+    NE: species
+      .filter((specie) => specie.location === 'NE')
+      .map((animal) => animal.name),
+    NW: species
+      .filter((specie) => specie.location === 'NW')
+      .map((animal) => animal.name),
+    SE: species
+      .filter((specie) => specie.location === 'SE')
+      .map((animal) => animal.name),
+    SW: species
+      .filter((specie) => specie.location === 'SW')
+      .map((animal) => animal.name),
+  };
+  return animalsLocation;
+};
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (options === undefined) {
+    return getAnimalsByLocation();
+  }
 }
 
 module.exports = getAnimalMap;
