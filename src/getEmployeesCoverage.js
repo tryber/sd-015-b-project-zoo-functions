7a8b7a65071d9cec02ff00e31231ed6d5110c119
raphael <raphabarzotto@gmail.com>
2021-09-25 14:30:46 -0300

    entrega requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,72 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+const firstNames = data.employees.map((element) => element.firstName);
+const lastNames = data.employees.map((element) => element.lastName);
+const fullNames = firstNames.map((element, index) => `${element} ${lastNames[index]}`);
+const ids = data.employees.map((element) => element.id);
+// const speciesIDS = data.species.map((element) => element.id);
+const speciesNames = data.species.map((element) => element.name);
+const speciesLocations = data.species.map((element) => element.location);
+
+const findEmployee = (nameToFind) => {
+  return data.employees.find((element) =>
+    element.firstName === nameToFind || element.lastName === nameToFind);
+};
+
+const findSpecie = (idSpecieToFind) =>
+  data.species.find((element) => element.id === idSpecieToFind).name;
+
+const findSpecieLocation = (idSpecieToFind) =>
+  data.species.find((element) => element.id === idSpecieToFind).location;
+
+const getSpecies = (nameEmployee) => {
+  const speciesIDS = findEmployee(nameEmployee).responsibleFor;
+  return speciesIDS.map((element) => findSpecie(element));
+};
+
+const getSpeciesLocations = (nameEmployee) => {
+  const speciesIDS = findEmployee(nameEmployee).responsibleFor;
+  return speciesIDS.map((element) => findSpecieLocation(element));
+};
+
+const employeesCoverage = ids.map((element, index) => {
+  const result = {
+    id: element,
+    fullName: fullNames[index],
+    species: getSpecies(firstNames[index]),
+    locations: getSpeciesLocations(firstNames[index]),
+  };
+  return result;
+});
+
+const testeString = (string, teste) => string.includes(teste);
+
+const findEmployeeByName = (employeeName) =>
+  employeesCoverage.find((element) => testeString(element.fullName, employeeName));
+
+const findEmployeeByID = (employeeID) => {
+  const employee = employeesCoverage.find((element) => testeString(element.id, employeeID));
+  if (employee) {
+    return employee;
+  }
+  throw new Error('Informações inválidas');
+};
+
+const getOneEmployee = (key, value) => {
+  if (key === 'name') {
+    return findEmployeeByName(value);
+  }
+  return findEmployeeByID(value);
+};
+
+console.log(getOneEmployee({ name: 'jorge' }));
+
+function getEmployeesCoverage(object) {
   // seu código aqui
+  if (!object) {
+    return employeesCoverage;
+  }
+  return getOneEmployee(Object.keys(object)[0], Object.values(object)[0]);
 }
 
 module.exports = getEmployeesCoverage;
