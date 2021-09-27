39ed6b1b55902eaa300374a0f1ee9ffedfe4eb05
Anderson Barbosa <andbmarques@gmail.com>
2021-09-27 14:57:09 -0300

    Requisitos 1 e 2 adicionados

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const speciesArray = ids.map(id => data.species.find(specie => specie.id === id));
+  return speciesArray;
 }
 
 module.exports = getSpeciesByIds;
