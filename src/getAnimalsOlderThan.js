3efaaee8b032d028bfebe7601cde43851c82550e
Anderson Barbosa <andbmarques@gmail.com>
2021-09-27 15:00:19 -0300

    Erros de lint corrigidos nos req. 1 e 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const animalsArray = data.species.find(specie => specie.name === animal);
-  return animalsArray.residents.every(ages => ages.age > age);
+  const animalsArray = data.species.find((specie) => specie.name === animal);
+  return animalsArray.residents.every((ages) => ages.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  const speciesArray = ids.map(id => data.species.find(specie => specie.id === id));
+  const speciesArray = ids.map((id) => data.species.find((specie) => specie.id === id));
   return speciesArray;
 }
 
