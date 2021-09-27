3eb3ee38e8e3bcb116f0082ef5f41f7e00213db5
Anderson Barbosa <andbmarques@gmail.com>
2021-09-27 15:08:36 -0300

    Requisito 3 adicionado

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  const employee = data.employees.find((element) => (
+    element.firstName === employeeName || element.lastName === employeeName
+  ));
+  return employee;
 }
 
 module.exports = getEmployeeByName;
