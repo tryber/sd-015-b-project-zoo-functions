c91829cc18cd4cf599cf909763b04e81dea643bb
joaocla <jc.claudio.bento@gmail.com>
2021-09-28 15:52:03 -0300

    Adiciona requisito 5 - countAnimals.js

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    const catchEmAll = species.reduce((acc, { name, residents }) => {
+      acc[name] = residents.length;
+      return acc;
+    }, {});
+    return catchEmAll;
+  }
+  const { specie, sex: gender = undefined } = animal;
+  if (gender === undefined) {
+    const findAnimalByName = species.find(({ name }) => name === specie).residents.length;
+    return findAnimalByName;
+  }
+  const findAnimalByGender = species.find(({ name }) => name === specie)
+    .residents.filter(({ sex }) => sex === gender).length;
+  return findAnimalByGender;
 }
 
 module.exports = countAnimals;
