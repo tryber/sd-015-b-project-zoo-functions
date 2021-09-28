b6a8f38726a17ac3653b464d3d1e693a33ecc57d
Jamez Mayke <jamezmayke@gmail.com>
2021-09-28 16:06:58 -0300

    Requisito 2 atualizado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,12 +1,8 @@
 const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animals, age) {
-  
-  return species.find(
-    animal => animal.name === animals
-  ).residents.every(
-    resident => resident.age >= age
-  )
+  return species.find((animal) => animal.name === animals)
+    .residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
