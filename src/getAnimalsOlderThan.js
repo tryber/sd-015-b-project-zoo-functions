c62ea69e5ca959a17f41310f89e1d4b09d46dab0
ZaQ5 <caique_moreira_sz@hotmail.com>
2021-09-29 10:05:55 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  let finde = ''
+  const specie = species.filter(elem => elem.name === animal)
+  specie.find(elem => {
+    finde = elem.residents.map(elem2 => elem2.age >= age)
+    retorno = finde.every(elem => elem === true)
+})
+  return retorno
 }
-
 module.exports = getAnimalsOlderThan;
