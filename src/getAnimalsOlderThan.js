f15c9bdfab625b50556243516cb3313b833c8480
lucasmassias <lucasmassiasfreitas@gmail.com>
2021-09-29 16:01:16 -0300

    requisito 2 refatorado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  let animalsOlderThan = [];
+  const specieByName = species.find((speciesName) => speciesName.name === animal);
+  animalsOlderThan = specieByName.residents.every((speciesAge) => speciesAge.age > age);
+  return animalsOlderThan;
 }
 
 module.exports = getAnimalsOlderThan;
