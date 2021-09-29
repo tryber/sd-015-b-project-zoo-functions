bcc3a975e4972266151d4f2ed2435365b0fd4478
Jamez Mayke <jamezmayke@gmail.com>
2021-09-29 11:07:02 -0300

    Requisito 5 feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+// obs: criação de funções fora pois, lint deu erro pela qtd de linhas, muito grande
+const selectAnimalByname = (animal) => species.find((specie) => specie.name === animal.specie);
+
+const selectAnimalByGender = (getAnimal, animal) => {
+  const getAnimalByGender = getAnimal.residents.filter((resident) => resident.sex === animal.sex);
+  return getAnimalByGender.length;
+};
 
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    const animals = {};
+    species.forEach(({ name, residents }) => { animals[name] = residents.length; });
+    return animals;
+  }
+
+  const selectAnimal = selectAnimalByname(animal);
+  if (animal.sex) {
+    return selectAnimalByGender(selectAnimal, animal);
+  }
+
+  return selectAnimal.residents.length;
 }
 
 module.exports = countAnimals;
