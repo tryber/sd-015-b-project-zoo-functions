b4c595dd01702ae30e9b7b6e26e3914785ac8112
dfernan6 <danilo.afernandes@outlook.com>
2021-09-29 16:46:48 -0300

    Enviando correçoes no lint questionario 5 e 6

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -5,25 +5,24 @@ const argumentUndefined = () => {
   const totalAnimals = {};
 
   species.forEach((animalName) => {
-    totalAnimals[`${ animalName.name }`] = animalName.residents.length;
+    totalAnimals[`${animalName.name}`] = animalName.residents.length;
   });
 
   return totalAnimals;
-}
+};
 
 const quantAnimals = (animal) => {
   const totalAnimals = species.find((animalName) => animalName.name === animal.specie);
   return totalAnimals.residents.length;
-}
+};
 
 const quantAnimalsBySex = (animal) => {
   const totalAnimals = species.find((animalName) => animalName.name === animal.specie);
   const animalsBySex = totalAnimals.residents.filter((specie) => specie.sex === animal.gender);
   return animalsBySex.length;
-}
+};
 
 function countAnimals(animal) {
-
   if (animal === undefined) return argumentUndefined();
   if (animal.gender === undefined) return quantAnimals(animal);
   return quantAnimalsBySex(animal);
