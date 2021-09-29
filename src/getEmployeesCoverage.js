b1a2dc22bd7694e85e86c9b9d9952539a2ecf677
Flávia Labanca <flavialabanca@gmail.com>
2021-09-29 19:33:10 -0300

    requisito 10 - com ajuda do Leandro Oliveira

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,58 @@
-const data = require('../data/zoo_data');
+// feito com consulta ao repositório e MUITA ajuda do colega Leandro Oliveira
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+
+function selectedEmployee(object) {
+  const firstName = employees.find((employee) => (object.name === employee.firstName));
+  if (firstName) {
+    return firstName;
+  }
+
+  const lastName = employees.find((employee) => (object.name === employee.lastName));
+  if (lastName) {
+    return lastName;
+  }
+
+  const id = employees.find((employee) => (object.id === employee.id));
+  if (id) {
+    return id;
+  }
+}
+
+function getEmployee(object) {
+  const employee = selectedEmployee(object);
+  if (!employee) {
+    throw new Error('Informações inválidas');
+  }
+  const animals = data.species.filter((specie) => employee.responsibleFor.includes(specie.id));
+  const species = [];
+  const locations = [];
+  animals.forEach((animal) => {
+    species.push(animal.name);
+    locations.push(animal.location);
+  });
+  return ({
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species,
+    locations,
+  });
+}
+
+function coverages() {
+  const allEmployee = [];
+  employees.forEach((employee) => {
+    allEmployee.push(getEmployee({ name: employee.firstName }));
+  });
+  return allEmployee;
+}
+
+function getEmployeesCoverage(object) {
+  if (!object) {
+    return coverages();
+  }
+  return getEmployee(object);
 }
 
 module.exports = getEmployeesCoverage;
