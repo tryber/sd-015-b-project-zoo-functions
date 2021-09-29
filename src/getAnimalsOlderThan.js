7e16b288ace8d5d0e9bfe5462ce547c4364d1487
lucasmassias <lucasmassiasfreitas@gmail.com>
2021-09-29 16:02:39 -0300

    refatorado de novo, detalhe minimo

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,9 +3,8 @@ const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  let animalsOlderThan = [];
   const specieByName = species.find((speciesName) => speciesName.name === animal);
-  animalsOlderThan = specieByName.residents.every((speciesAge) => speciesAge.age > age);
+  const animalsOlderThan = specieByName.residents.every((speciesAge) => speciesAge.age > age);
   return animalsOlderThan;
 }
 
