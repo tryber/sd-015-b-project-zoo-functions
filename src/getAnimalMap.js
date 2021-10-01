704243cf49585113f6fcb43655214b997d210091
Adson-Gomes-Oliveira <adsongoliveira2021@gmail.com>
2021-09-30 21:18:52 -0300

    Sétimo Requisito incompleto

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,25 +1,119 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-  const getNamesByGeoNE = species.filter((specie) => specie.location === 'NE');
-  const getNamesByGeoNW = species.filter((specie) => specie.location === 'NW');
-  const getNamesByGeoSE = species.filter((specie) => specie.location === 'SE');
-  const getNamesByGeoSW = species.filter((specie) => specie.location === 'SW');
+const defaultBehaviour = () => {
+  const animalsByLocation = { NE: [], NW: [], SE: [], SW: [] };
 
-  const getName = getNamesByGeoSE.map((animalName) => animalName.name);
-  let obj = {
-    NE: [{}],
-    NW: [{}],
-    SE: [{}],
-    SW: [{}],
-  };
+  const arrOfLocations = Object.keys(animalsByLocation);
+  arrOfLocations.forEach((loc) => {
+    const filtredAnimals = (species.filter((animal) => animal.location === loc));
+    animalsByLocation[loc] = filtredAnimals.map((name) => name.name);
+  });
 
-  return getName;
+  return animalsByLocation;
+};
+
+const includeNamesTrue = () => {
+  const animalsByLocation = { NE: [], NW: [], SE: [], SW: [] };
+
+  const arrOfLocations = Object.keys(animalsByLocation);
+  arrOfLocations.forEach((loc) => {
+    const filtredAnimals = (species.filter((animal) => animal.location === loc));
+    animalsByLocation[loc] = filtredAnimals.map((animalNames) => {
+      const resultObj = {};
+      resultObj[animalNames.name] = animalNames.residents.map((names) => names.name);
+      return resultObj;
+    });
+  });
+
+  console.log(animalsByLocation);
+  return animalsByLocation;
+};
+
+const includeNamesTrueSorted = () => {
+  const animalsByLocation = { NE: [], NW: [], SE: [], SW: [] };
+
+  const arrOfLocations = Object.keys(animalsByLocation);
+  arrOfLocations.forEach((loc) => {
+    const filtredAnimals = (species.filter((animal) => animal.location === loc));
+    animalsByLocation[loc] = filtredAnimals.map((animalNames) => {
+      const resultObj = {};
+      resultObj[animalNames.name] = animalNames.residents.map((names) => names.name).sort();
+      return resultObj;
+    });
+  });
+
+  return animalsByLocation;
+};
+
+const nameBySex = (sex) => {
+  const animalsByLocation = { NE: [], NW: [], SE: [], SW: [] };
+
+  const arrOfLocations = Object.keys(animalsByLocation);
+  arrOfLocations.forEach((loc) => {
+    const filtredAnimals = (species.filter((animal) => animal.location === loc));
+    animalsByLocation[loc] = filtredAnimals.map((animalNames) => {
+      const resultObj = {};
+      const filteredBySex = animalNames.residents.filter((names) => names.sex === sex);
+      const filteredBySexOfName = filteredBySex.map((name) => name.name);
+      resultObj[animalNames.name] = filteredBySexOfName;
+
+      return resultObj;
+    });
+  });
+
+  return animalsByLocation;
+};
+
+const nameBySexSorted = (sex) => {
+  const animalsByLocation = { NE: [], NW: [], SE: [], SW: [] };
+
+  const arrOfLocations = Object.keys(animalsByLocation);
+  arrOfLocations.forEach((loc) => {
+    const filtredAnimals = (species.filter((animal) => animal.location === loc));
+    animalsByLocation[loc] = filtredAnimals.map((animalNames) => {
+      const resultObj = {};
+      const filteredBySex = animalNames.residents.filter((names) => names.sex === sex);
+      const filteredBySexAndSorted = filteredBySex.map((name) => name.name).sort();
+      resultObj[animalNames.name] = filteredBySexAndSorted;
+
+      return resultObj;
+    });
+  });
+
+  return animalsByLocation;
+};
+
+const callOfFunctions = (optionSet, sex) => {
+  const probalitys = {
+    trueundefinedundefined: includeNamesTrue(),
+    truetrueundefined: includeNamesTrueSorted(),
+    trueundefinedmale: nameBySex(sex),
+    trueundefinedfemale: nameBySex(sex),
+    truetruemale: nameBySexSorted(sex),
+    truetruefemale: nameBySexSorted(sex),
+    falsefalsefemale: includeNamesTrue(),
+    falsetruefemale: includeNamesTrue(),
+  }
+  
+  const returnObject = probalitys[optionSet];
+
+  return returnObject;
 }
 
-const option = { includeNames: true }
+function getAnimalMap(options) {
+  // seu código aqui
+  if (options === undefined) {
+    return defaultBehaviour();
+  }
+
+  const {includeNames, sorted, sex} = options;
+  const stringfyOptions = `${includeNames}${sorted}${sex}`;
+  
+  callOfFunctions(stringfyOptions, options.sex);
+}
+
+const option = { includeNames: true };
 console.log(getAnimalMap(option));
 
 module.exports = getAnimalMap;
