9f48008905b3b578d4189bb364cd92c21c2010d1
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-10-01 18:09:09 -0300

    5-requisito terminado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,30 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+function findAnimal({ specie, sex }) {
+  return (
+    species.find(({ name }) => (
+      name === specie
+    ))
+  );
+}
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const newAnimal = {};
+    species.forEach(({ name, residents }) => {
+      newAnimal[name] = residents.length;
+    });
+    return newAnimal;
+  }
+
+  if (animal.sex) {
+    return (
+      findAnimal(animal).residents.filter(({ sex }) => (
+        sex === animal.sex
+      )).length
+    );
+  }
+  return findAnimal(animal).residents.length;
 }
 
 module.exports = countAnimals;
