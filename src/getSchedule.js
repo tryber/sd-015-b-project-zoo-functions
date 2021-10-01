826596e90c848be08d2e61874aa79a6d5eafd793
Jamez Mayke <jamezmayke@gmail.com>
2021-10-01 19:33:03 -0300

    Íniciando requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,41 @@
-const data = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
+// disponibilizar o horario dos animais para os users, que pode querer o cronograma da semana de um dia ou de um animal em especifico
+
+const getAnimalsByWeekDay = (weekDay) => {
+  const allAnimals = [];
+  species.filter(({ name, availability }) => availability.forEach((day) => {
+    if (day === weekDay) {
+      return allAnimals.push(name);
+    }
+  }));
+  return allAnimals;
+  // console.log(allAnimals);
+};
+
+const getScheduleWeek = () => {
+  const scheduleComplete = {};
+  // retorna um array de arrays, onde, cada array tem duas posições, a primeira, o dia da semana em string, a segunda, um objeto com duas propriedades.
+  Object.entries(hours).forEach((day) => { // day são os arrays separados, com duas posições
+    const weekDay = day[0];
+    const hourOpenAndClose = day[1];
+    const { open, close } = hourOpenAndClose;
+    if (!scheduleComplete[weekDay]) {
+      scheduleComplete[weekDay] = {
+        officeHour: `Open from ${open}am until ${close}pm`,
+        exhibition: getAnimalsByWeekDay(weekDay),
+      };
+      if (hourOpenAndClose.open === 0 && hourOpenAndClose.close === 0) {
+        scheduleComplete[weekDay] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+      }
+    }
+  });
+  // console.log(scheduleComplete);
+  return scheduleComplete;
+};
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  if (!scheduleTarget) return getScheduleWeek();
 }
-
+// getSchedule();
 module.exports = getSchedule;
