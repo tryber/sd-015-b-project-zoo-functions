d5e498dae06fe9862ca97f0c2b99fc87eb4ffab9
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-10-02 17:46:06 -0300

    6-requisito terminado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,32 @@
-const data = require('../data/zoo_data');
+/* eslint-disable max-lines-per-function */
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const visitor = {
+    adult: 0,
+    child: 0,
+    senior: 0,
+  };
+  entrants.forEach(({ age }) => {
+    if (age < 18) {
+      visitor.child += 1;
+    } else if (age >= 18 && age < 50) {
+      visitor.adult += 1;
+    } else {
+      visitor.senior += 1;
+    }
+  });
+  return visitor;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || !Object.keys(entrants).length) {
+    return 0;
+  }
+  const { adult, child, senior } = countEntrants(entrants);
+  return (
+    (adult * prices.adult) + (child * prices.child) + (senior * prices.senior)
+  );
 }
 
 module.exports = { calculateEntry, countEntrants };
