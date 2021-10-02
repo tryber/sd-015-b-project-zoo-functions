1ecd0df9a6f12d29c92d46037c782e94b6aa504c
joaocla <jc.claudio.bento@gmail.com>
2021-10-02 16:18:36 -0300

    requisito 6 - refeito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,44 +1,27 @@
 const data = require('../data/zoo_data');
 
-// Funcao para separar as entradas
-function separatingEntrants(entrant) {
-  if (entrant.age >= 18 && entrant.age < 50) {
-    return 'adult';
-  }
-  if (entrant.age >= 50) {
-    return 'senior';
-  }
-  if (entrant < 18) {
-    return 'child';
-  }
-}
-
 function countEntrants(entrants) {
   // seu código aqui
-  const total = {
+  const allEntrants = {
     adult: 0,
     senior: 0,
     child: 0,
   };
-  entrants.forEach((entrant) => {
-    total[separatingEntrants(entrant)] += 1;
+  entrants.forEach(({ age } = entrants) => {
+    if (age >= 18 && age < 50) {
+      allEntrants.adult += 1;
+    }
+    if (age >= 50) {
+      allEntrants.senior += 1;
+    }
+    if (age < 18) {
+      allEntrants.child += 1;
+    }
   });
-  return total;
+  return allEntrants;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
-  const condition = countEntrants(entrants);
-  if (condition === 0) { return 0; }
-
-  const {
-    adult,
-    senior,
-    child,
-  } = countEntrants(entrants);
-
-  const { prices } = data;
-  const totalPrice = (adult * prices.adult) + (senior * prices.senior) + (child * prices.child);
-  return totalPrice;
 }
 module.exports = { calculateEntry, countEntrants };
