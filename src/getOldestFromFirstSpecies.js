b4c5f6611f6562635fc933119ddb52a90f669c98
joaocla <jc.claudio.bento@gmail.com>
2021-10-02 17:14:56 -0300

    Adiciona requisito 9-getOldestFromFirstSpecies.js

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const findFirstId = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const findAnimalSpecie = species.find((specie) => specie.id === findFirstId).residents;
+  // const oldestAnimal = findAnimalSpecie.sort((ageX, ageY) => ageX.age - ageY.age)[0];
+  const oldestAnimal = findAnimalSpecie.sort((ageX, ageY) => ageY.age - ageX.age)[0];
+
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
