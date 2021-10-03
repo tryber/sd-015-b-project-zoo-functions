a94743e66126c4223c1c6f057002abf156d98372
Vitor <vitoremerich.contato@gmail.com>
2021-10-03 17:58:18 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const counter = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  entrants.forEach(({ age }) => {
+    if (age < 18) counter.child += 1;
+    if (age >= 18 && age < 50) counter.adult += 1;
+    if (age >= 50) counter.senior += 1;
+  });
+  return counter;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  let totalCost = 0;
+  if (!entrants || !entrants[0]) {
+    return totalCost;
+  }
+  const entries = countEntrants(entrants);
+  totalCost += entries.child * 20.99 + entries.adult * 49.99 + entries.senior * 24.99;
+  return totalCost;
 }
 
 module.exports = { calculateEntry, countEntrants };
