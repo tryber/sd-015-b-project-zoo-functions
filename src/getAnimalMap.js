b57026564e48cce38f1a7cbd030ba42add6c8fde
alvesgf16 <alvesgabriel1994@gmail.com>
2021-12-17 10:03:17 -0300

    Melhora nomes de funções do getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -4,20 +4,20 @@ const generateResidentsListPerSpecies = ({ sex }, { residents }) => (
   sex ? residents.filter(({ sex: gender }) => gender === sex) : residents
 );
 
-function generateNamesListPerSpecies(options, oneSpecies) {
+function includeAnimalsNames(options, oneSpecies) {
   const namesList = generateResidentsListPerSpecies(options, oneSpecies)
     .map(({ name }) => name);
 
   return { [oneSpecies.name]: options.sorted ? namesList.sort() : namesList };
 }
 
-const getOptions = (options, oneSpecies) => ((options && options.includeNames)
-  ? generateNamesListPerSpecies(options, oneSpecies) : oneSpecies.name);
+const getSpecies = (options, oneSpecies) => ((options && options.includeNames)
+  ? includeAnimalsNames(options, oneSpecies) : oneSpecies.name);
 
 const getAnimalMap = (options) => species
   .reduce((locations, oneSpecies) => (
     { ...locations,
-      [oneSpecies.location]: [...locations[oneSpecies.location], getOptions(options, oneSpecies)],
+      [oneSpecies.location]: [...locations[oneSpecies.location], getSpecies(options, oneSpecies)],
     }), { NE: [], NW: [], SE: [], SW: [] });
 
 module.exports = getAnimalMap;
